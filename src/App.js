a057afd006c4b6878befebec94e34bdf6e795323
art1shok <kvitkaart554@gmail.com>
2019-12-23 11:19:59 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,77 @@
 import React from 'react';
 import './App.css';
+import GoodList from './GoodsList';
+import getGoods from './Api';
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+    isStart: false,
+  };
+
+  loadGoods = () => {
+    getGoods()
+      .then((goods) => {
+        this.setState({
+          goods,
+          isStart: true,
+        });
+      });
+  };
+
+  fiveFirstGoods = () => {
+    getGoods()
+      .then((goods) => {
+        this.setState({
+          goods: goods.sort((a, b) => (a.name).localeCompare(b.name))
+            .slice(0, 5),
+        });
+      });
+  };
+
+  loadRedGoods = () => {
+    getGoods()
+      .then((goods) => {
+        this.setState({
+          goods: goods.filter(item => item.color === 'red'),
+        });
+      });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {this.state.isStart ? (
+          <section>
+            <button
+              onClick={this.fiveFirstGoods}
+              type="button"
+            >
+              5 first goods
+            </button>
+            <button
+              type="button"
+              onClick={this.loadRedGoods}
+            >
+              Load red goods
+            </button>
+          </section>
+        )
+          : (
+            <button
+              type="button"
+              onClick={this.loadGoods}
+            >
+              Load
+            </button>
+          )}
+        <GoodList goods={goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/GoodsList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GoodList = (props) => {
+  const { goods } = props;
+
+  return (
+    <ul>
+      {goods.map(item => (
+        <li style={{ color: item.color }}>
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+GoodList.propTypes = { goods: PropTypes.arrayOf(PropTypes.object).isRequired };
+
+export default GoodList;
