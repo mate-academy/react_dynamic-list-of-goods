f061bbedf0561d3fdb4bdf15cff08dd2c109d3ef
SahaPylypyuk <sashapulupyk@gmail.com>
2020-03-02 17:57:23 +0200

    task implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { GoodsList } from './GoodsList/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const API_URL: string = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
 
-export default App;
+const getGoods = () => {
+  return fetch(API_URL).then(res => res.json())
+}
+
+interface State {
+  listOfGoods: {id: number, name: string, color: string}[]
+}
+
+export class App extends Component<{},State> {
+  state = {
+    listOfGoods: [],
+  }
+
+  loadAllGoods = () => {
+    getGoods().then(goods => {
+      this.setState({
+        listOfGoods: goods,
+      })
+    })
+  }
+
+  loadFirstFiveGood = () => {
+    getGoods().then(goods => {
+      this.setState({
+        listOfGoods: goods.filter((good: {id: number}) => good.id <= 5),
+      })
+    })
+  }
+
+  loadRedGood = () => {
+    getGoods().then(goods => {
+      this.setState({
+        listOfGoods: goods.filter((good: {color: string}) => good.color === 'red'),
+      })
+    })
+  }
+
+  render() {
+    const { listOfGoods } = this.state;
+    return (
+      <div>
+          <GoodsList goods={listOfGoods}/>
+
+          <button
+            type="button"
+            onClick={this.loadAllGoods}
+          >
+            Load all goods
+          </button>
+
+          <button
+            type="button"
+            onClick={this.loadFirstFiveGood}
+          >
+            Load 5 first goods
+          </button>
+
+          <button
+            type="button"
+            onClick={this.loadRedGood}
+          >
+            Load red goods
+          </button>
+      </div>
+    );
+  }
+};
---
+++ src/GoodsList/GoodsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Goods {
+  goods: { id: number,name: string, color: string }[]
+}
+
+export const GoodsList: React.FC<Goods> = ( {goods} ) => (
+  <ul>
+    {goods.map(good => <li key={good.id} style={{color: good.color}}>{good.name}</li>)}
+  </ul>
+);
