dd0f07843b0fe208ff34e03708f837cbf0c03828
Natalia Lypovyk <cardinnata@gmail.com>
2020-03-02 23:00:09 +0200

    Dynamic List of Goods

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,12 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+interface Goods {
+  goods: Good[];
+}
+
+interface Good {
+  id: number;
+  name: string;
+  color: string;
+}
