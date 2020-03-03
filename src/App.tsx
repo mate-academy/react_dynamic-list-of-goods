824965cdf7f2eee3e0edae8e1cdaff75a4ae566f
Dan Denysovets <alen250299@gmail.com>
2020-03-03 10:32:55 +0200

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
 
-export default App;
+const getGoods = () => {
+  return fetch(URL).then(response => response.json());
+};
+
+interface AppState {
+  goods: Good[];
+}
+
+export class App extends Component<{}, AppState> {
+  state = {
+    goods: [],
+  };
+
+  handleLoadAll = () => {
+    getGoods()
+      .then((goods: AppState['goods']) => {
+        this.setState({ goods });
+      });
+  };
+
+  handleLoadFive = () => {
+    getGoods()
+      .then((goods: AppState['goods']) => {
+        this.setState(({
+          goods: goods
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .filter(good => good.id <= 5),
+        }));
+      });
+  };
+
+  handleLoadRed = () => {
+    getGoods()
+      .then((goods: AppState['goods']) => {
+        this.setState(({
+          goods: goods.filter(good => good.color === 'red'),
+        }));
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={this.handleLoadAll}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={this.handleLoadFive}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={this.handleLoadRed}
+        >
+          Load red goods
+        </button>
+        <GoodsList goods={this.state.goods} />
+      </div>
+    );
+  }
+}
