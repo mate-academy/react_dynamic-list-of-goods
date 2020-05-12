273901c7abc79bbf1698e3c653345b0bdefd93cf
Tata2222 <tataolcha@gmail.com>
2020-05-12 23:18:04 +0300

    solution

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,6 +1,6 @@
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
 
 export const getGoods = async (): Promise<Good[]> => {
-  const response = await fetch(`${API_URL}/goods.json`);
+  const response = await fetch(API_URL);
   return response.json();
 };
