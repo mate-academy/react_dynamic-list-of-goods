4e5fdd2bc7eb0b410e48fd23010a345d63b731b6
Andreas_Just <andreas.wq@gmail.com>
2020-05-14 01:19:39 +0300

    App.js was implemented in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,112 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { getGoods } from './api/getGoods';
+import GoodsList from './components/GoodsList';
+import Button from './components/Button';
+import './App.scss';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+interface FilterConfigs {
+  all: string;
+  firstFive: string;
+  color: string;
+}
+
+interface FilterButtons {
+  id: number;
+  name: string;
+}
+
+const filterConfigs: FilterConfigs = {
+  all: 'Load goods',
+  firstFive: 'Load first five goods',
+  color: 'Load red goods',
+};
+
+const FILTER_BUTTONS: FilterButtons[] = [
+  {
+    id: 0,
+    name: filterConfigs.all,
+  },
+  {
+    id: 1,
+    name: filterConfigs.firstFive,
+  },
+  {
+    id: 2,
+    name: filterConfigs.color,
+  },
+];
+
+type State = {
+  goods: Good[];
+  error: string;
+  active: boolean[];
+};
+
+class App extends Component<{}, State> {
+  state = {
+    goods: [],
+    error: '',
+    active: [false, false, false],
+  };
+
+  /* eslint-disable no-return-assign, no-param-reassign */
+  loadGoods = async (id: number, name: string) => {
+    getGoods()
+      .then(goods => this.filterGoods(goods, name))
+      .then(goods => this.setState(state => ({
+        goods,
+        error: '',
+        active: state.active.map((_bool: boolean, idx: number): boolean => (
+          id === idx ? _bool = true : _bool = false
+        )),
+      })))
+      .catch((error) => {
+        this.setState({
+          error: `Something went wrong! ${error}`,
+          goods: [],
+        });
+      });
+  };
+
+  filterGoods = async (goods: Good[], name: string) => {
+    if (name === filterConfigs.color) {
+      return goods.filter((good: Good) => good.color === name) || [];
+    }
+
+    if (name === filterConfigs.firstFive) {
+      return goods
+        .sort((a: Good, b: Good) => a.name.localeCompare(b.name))
+        .slice(0, 5) || [];
+    }
+
+    return goods;
+  };
+
+  render() {
+    const { goods, error, active } = this.state;
+
+    return (
+      <section className="goods">
+        <h1 className="main-title">Dynamic List Of Goods</h1>
+
+        <div className="goods__buttons-container">
+          {FILTER_BUTTONS.map(({ id, name }) => (
+            <Button
+              key={id}
+              handleClick={() => this.loadGoods(id, name)}
+              active={active[id]}
+            >
+              {name}
+            </Button>
+          ))}
+        </div>
+
+        <div className="goods__error">{error}</div>
+
+        <GoodsList goodsList={goods} />
+      </section>
+    );
+  }
+}
 
 export default App;
