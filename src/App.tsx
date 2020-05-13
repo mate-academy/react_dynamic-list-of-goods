d27b2fd8a66823d020227888853b6e298b2e4111
WincherZ <wincher2016@gmail.com>
2020-05-13 13:26:34 +0300

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { getGoodsList } from './helpers/api';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Goods[];
+};
+
+type Props = {};
+
+class App extends Component<Props, State> {
+  state = {
+    goods: [],
+  };
+
+  loadAllGoods = () => {
+    getGoodsList()
+      .then(data => {
+        this.setState({
+          goods: data,
+        });
+      });
+  };
+
+  loadFiveGoods = () => {
+    getGoodsList()
+      .then(data => {
+        this.setState({
+          goods: data.slice(0, 5),
+        });
+      });
+  };
+
+  loadRedGoods = () => {
+    getGoodsList()
+      .then(data => {
+        this.setState({
+          goods: data.filter((item: { color: string }) => item.color === 'red'),
+        });
+      });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        {(
+          !!goods.length
+          && <GoodsList goods={goods} />
+        )}
+        <button type="button" onClick={this.loadAllGoods}>Load All goods</button>
+        <button type="button" onClick={this.loadFiveGoods}>Load 5 first goods</button>
+        <button type="button" onClick={this.loadRedGoods}>Load red goods</button>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,13 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+interface Goods {
+  id: number;
+  name: string;
+  color: string;
+}
+
+type Props = {
+  name?: string;
+  color?: string;
+};
