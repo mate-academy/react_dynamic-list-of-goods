d572f0631356be4db3843a548b805d3816c2386b
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-05-13 15:41:29 +0300

    result

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,78 @@
 import React from 'react';
 import './App.css';
+import { getGoods } from './api/api';
+import { GoodsList } from './GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: GoodData[];
+  isLoading: boolean;
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+    isLoading: false,
+  };
+
+  loadAllGoods= () => {
+    this.setState({ isLoading: true });
+    getGoods()
+      .then((goodsFromServer) => {
+        this.setState({ goods: goodsFromServer, isLoading: false });
+      });
+  };
+
+
+  showFiveGoods = () => {
+    this.setState({ isLoading: true });
+    getGoods()
+      .then((goodsFromServer) => {
+        this.setState({ goods: goodsFromServer.slice(0, 5), isLoading: false });
+      });
+  };
+
+  filterRedGoods=() => {
+    this.setState({ isLoading: true });
+    getGoods()
+      .then((goodsFromServer) => {
+        this.setState({
+          goods: goodsFromServer
+            .filter((good: GoodData) => good.color === 'red'),
+          isLoading: false,
+        });
+      });
+  };
+
+  render() {
+    const { goods, isLoading } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={this.loadAllGoods}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={this.showFiveGoods}
+        >
+          Load 5 first
+        </button>
+        <button
+          type="button"
+          onClick={this.filterRedGoods}
+        >
+          Load red goods
+        </button>
+        {isLoading
+          ? <p>Loading...</p>
+          : (<GoodsList goods={goods} />)}
+      </>
+    );
+  }
+}
 
 export default App;
