6789b5af22228841db0bc2ca17e29bfd2ce894ba
Nina <pinkangelssugar@gmail.com>
2020-05-13 14:03:38 +0300

    Nina

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,6 +1,6 @@
 
 const getGoods = () => {
-  const API_URL = 'http://localhost:3000';
+  const API_URL = 'https://github.com/nchuhrina/react_dynamic-list-of-goods/blob/c1ba1d1595d51757b1526d6cb8b3a970bfe8196e';
   const goods = fetch(`${API_URL}/goods.json`).then(response => response.json());
 
   return goods;
