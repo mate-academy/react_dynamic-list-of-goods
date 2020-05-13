cc775be78f4fd8bd10ac5e013fc64204cf9f13b8
Vitalii Korol <vitaliikorol@192-168-0-100.rdsnet.ro>
2020-05-13 14:59:32 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
+import GoodList from './GoodList';
 
 const App = () => (
-  <h1>Dynamic list of Goods</h1>
+  <>
+    <h1>Dynamic list of Goods</h1>
+    <GoodList />
+  </>
 );
 
 export default App;
