14de9a78ab177bdeab716de3b2b5078716b4c422
Yarik1993 <makarov1603@icloud.com>
2020-05-13 18:15:49 +0300

    dinamic_list_of_goods

---
+++ src/API.ts
@@ -0,0 +1,4 @@
+const API_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
+
+export const API = fetch(API_URL)
+  .then((response): Promise<Goods[]> => response.json());
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,80 @@
 import React from 'react';
 import './App.css';
+import { API } from './API';
+import GoodsItem from './GoodsItem';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type AppState = {
+  listOfGood: Goods[];
+  isLoadAll: boolean;
+};
+
+type AppProps = {};
+
+class App extends React.Component<AppProps, AppState> {
+  state = {
+    listOfGood: [],
+    isLoadAll: false,
+  };
+
+  showAll = () => (
+    API.then(data => (
+      this.setState({
+        listOfGood: data,
+      })
+    ))
+  );
+
+  firstFive = () => (
+    API.then(data => (
+      this.setState({
+        listOfGood: data.sort((a: Goods, b: Goods) => (a.name.localeCompare(b.name))).slice(0, 5),
+      })
+    ))
+  );
+
+  onlyRed = () => (
+    API.then(data => (
+      this.setState({
+        listOfGood: data.filter((good: Goods) => (good.color === 'red')),
+      })
+    ))
+  );
+
+  render() {
+    const { listOfGood } = this.state;
+
+    return (
+      <>
+        <ul>
+          {listOfGood.map(({ id, name, color }: Goods) => (
+            <GoodsItem
+              id={id}
+              name={name}
+              color={color}
+            />
+          ))}
+        </ul>
+        <button
+          type="button"
+          onClick={this.showAll}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={this.firstFive}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={this.onlyRed}
+        >
+          Load red goods
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;
