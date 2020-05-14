7172c13d62b57b6ab92d8676e9b3acdfe6e8cd51
Vitalii Shytikov <v.shytikov@gmail.com>
2020-05-14 09:28:47 +0300

    Loader component added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { IGoodsItem } from './helpers/interfaces';
 import { FILTERS } from './helpers/filters';
 import { GoodsList } from './components/GoodsList';
 import './App.css';
+import { Loader } from './components/Loader';
 
 const App: React.FC = () => {
   const [goods, setGoods] = useState<IGoodsItem[]>([]);
@@ -56,17 +57,7 @@ const App: React.FC = () => {
       </div>
       {isLoading ? (
         <div className="row center-align">
-          <div className="preloader-wrapper small active">
-            <div className="spinner-layer spinner-green-only">
-              <div className="circle-clipper left">
-                <div className="circle"></div>
-              </div><div className="gap-patch">
-                <div className="circle"></div>
-              </div><div className="circle-clipper right">
-                <div className="circle"></div>
-              </div>
-            </div>
-          </div>
+          <Loader />
         </div>
         ) : (
           <div className="row">
