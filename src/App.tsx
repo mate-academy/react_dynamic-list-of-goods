2b4a9fb245e5ab5cc4624fd09dc3838480e7226b
Yarik1993 <makarov1603@icloud.com>
2020-05-14 11:16:57 +0300

    dynamic_list-of-goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,75 @@
 import React from 'react';
 import './App.css';
+import { API } from './API';
+import GoodsItem from './GoodsItem';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type AppState = {
+  listOfGood: Goods[];
+};
+type AppProps = {};
+class App extends React.Component<AppProps, AppState> {
+  state = {
+    listOfGood: [],
+  };
 
+  showAll = () => (
+    API.then(data => (
+      this.setState({
+        listOfGood: data,
+      })
+    ))
+  );
+
+  firstFive = () => (
+    API.then(data => (
+      this.setState({
+        listOfGood: data.sort((a: Goods, b: Goods) => (a.name.localeCompare(b.name))).slice(0, 5),
+      })
+    ))
+  );
+
+  onlyRed = () => (
+    API.then(data => (
+      this.setState({
+        listOfGood: data.filter((good: Goods) => (good.color === 'red')),
+      })
+    ))
+  );
+
+  render() {
+    const { listOfGood } = this.state;
+
+    return (
+      <>
+        <ul>
+          {listOfGood.map(({ id, name, color }) => (
+            <GoodsItem
+              id={id}
+              name={name}
+              color={color}
+            />
+          ))}
+        </ul>
+        <button
+          type="button"
+          onClick={this.showAll}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={this.firstFive}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={this.onlyRed}
+        >
+          Load red goods
+        </button>
+      </>
+    );
+  }
+}
 export default App;
