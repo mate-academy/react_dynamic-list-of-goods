dc0a09b7f620731688b438d3b0099dbe110d95dc
AnnieMarkina <anniemurkina@gmail.com>
2020-05-14 12:42:12 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,86 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+import { GoodsList } from './Components/GoodsList/GoodsList';
+import { getGoods } from './Helpers/api';
+
+interface Good {
+  id: number;
+  name: string;
+  color: string;
+}
+
+type AppState = {
+  goods: Good[];
+};
+
+type AppProps = {};
+
+class App extends React.Component<AppProps, AppState> {
+  state={
+    goods: [],
+  };
+
+  handleLoadAll = () => {
+    getGoods()
+      .then((goodsFromServer) => {
+        this.setState({
+          goods: [...goodsFromServer],
+        });
+      });
+  };
+
+  handleLoadFive = () => {
+    getGoods()
+      .then((goodsFromServer) => {
+        this.setState({
+          goods: [...goodsFromServer].slice(0, 5),
+        });
+      });
+  };
+
+  handleLoadRed = () => {
+    getGoods()
+      .then((goodsFromServer) => {
+        this.setState({
+          goods: [...goodsFromServer].filter(good => good.color === 'red'),
+        });
+      });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <div className="buttons__list">
+          <button
+            type="button"
+            onClick={this.handleLoadAll}
+          >
+            Load All
+          </button>
+
+          <button
+            type="button"
+            onClick={this.handleLoadFive}
+          >
+            Load 5
+          </button>
+
+          <button
+            type="button"
+            onClick={this.handleLoadRed}
+          >
+            Load red
+          </button>
+        </div>
+
+        <GoodsList goods={goods} />
+
+      </>
+    );
+  }
+}
 
 export default App;
