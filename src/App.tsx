9235514be837026b8442ef9de616c786110b43d7
AnnieMarkina <anniemurkina@gmail.com>
2020-05-16 15:25:37 +0300

    added async/await

--- src/App.tsx
+++ src/App.tsx
@@ -2,84 +2,82 @@ import React from 'react';
 import './App.css';
 
 import { GoodsList } from './Components/GoodsList/GoodsList';
-import { getGoods } from './Helpers/api';
-
-interface Good {
-  id: number;
-  name: string;
-  color: string;
-}
+import { getGoods, Good } from './Helpers/api';
 
 type AppState = {
   goods: Good[];
+  loadingError: string;
 };
 
 type AppProps = {};
 
 class App extends React.Component<AppProps, AppState> {
-  state={
+  state = {
     goods: [],
+    loadingError: '',
   };
 
-  handleLoadAll = () => {
-    getGoods()
-      .then((goodsFromServer) => {
-        this.setState({
-          goods: [...goodsFromServer],
-        });
-      });
+  async handleLoadAll() {
+    try {
+      const goodsFromServer = await getGoods();
+
+      this.setState({ goods: goodsFromServer });
+    } catch (error) {
+      this.setState({ loadingError: error.message }); 
+    }
   };
 
-  handleLoadFive = () => {
-    getGoods()
-      .then((goodsFromServer) => {
-        this.setState({
-          goods: [...goodsFromServer]
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .slice(0, 5),
-        });
-      });
+  async handleLoadFive() {
+    const goodsFromServer = await getGoods();
+
+    this.setState({
+      goods: [...goodsFromServer]
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .slice(0, 5),
+    });
   };
 
-  handleLoadRed = () => {
-    getGoods()
-      .then((goodsFromServer) => {
-        this.setState({
-          goods: [...goodsFromServer].filter(good => good.color === 'red'),
-        });
-      });
+  async handleLoadRed() {
+    const goodsFromServer = await getGoods();
+
+    this.setState({
+      goods: [...goodsFromServer]
+        .filter(good => good.color === 'red'),
+    });
   };
 
   render() {
-    const { goods } = this.state;
+    const { goods, loadingError } = this.state;
 
     return (
       <>
         <div className="buttons__list">
           <button
             type="button"
-            onClick={this.handleLoadAll}
+            onClick={() => this.handleLoadAll()}
           >
             Load All
           </button>
 
           <button
             type="button"
-            onClick={this.handleLoadFive}
+            onClick={() => this.handleLoadFive()}
           >
             Load 5
           </button>
 
           <button
             type="button"
-            onClick={this.handleLoadRed}
+            onClick={() => this.handleLoadRed()}
           >
             Load red
           </button>
         </div>
-
-        <GoodsList goods={goods} />
-
+        {loadingError ? (
+          <p>{loadingError}</p>
+        ) : (
+            <GoodsList goods={goods} />
+          )}
       </>
     );
   }
--- src/Helpers/api.ts
+++ src/Helpers/api.ts
@@ -1,4 +1,14 @@
-export const getGoods = () => {
-  return fetch('https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json')
-    .then(response => response.json());
+export interface Good {
+  id: number;
+  name: string;
+  color: string;
+};
+
+const API_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
+
+export const getGoods = async (): Promise<Good[]> => {
+  const response = await fetch(API_URL);
+
+  return response.json();
+  // always return PROMISE
 };
