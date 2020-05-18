01b3e5e2f65d5044654e7bc573eac1533b8b9120
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-05-18 23:15:17 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+import { getGoods, Good } from './api';
+import GoodsList from './GoodsList';
+
+const App = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const loadAllGoods = () => {
+    getGoods()
+      .then(setGoods);
+  };
+
+  const loadRedGoods = () => {
+    getGoods()
+      .then(goodsFromServer => goodsFromServer
+        .filter(good => good.color === 'red'))
+      .then(setGoods);
+  };
+
+  const loadFirst5Goods = () => {
+    const SortGoodsByName = (goodsFromServer: Good) => {
+      return [...goodsFromServer]
+        .sort((a, b) => a.name.localeCompare(b.name));
+    };
+
+    getGoods()
+      .then(SortGoodsByName)
+      .then((goodsFromServer: Good[]) => goodsFromServer.slice(0, 5))
+      .then(setGoods);
+  };
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" onClick={loadAllGoods}>Load all</button>
+      <button type="button" onClick={loadRedGoods}>Load red</button>
+      <button type="button" onClick={loadFirst5Goods}>Load first 5</button>
+
+      <GoodsList goods={goods} />
+    </>
+  );
+};
 
 export default App;
---
+++ src/api.ts
@@ -0,0 +1,13 @@
+const API_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
+
+export interface Good {
+  id: number;
+  name: string;
+  color: string;
+}
+
+export const getGoods = async (): Promise<Good[]> => {
+  const response = await fetch(API_URL);
+
+  return response.json();
+};
