14572bb2d0f3953b6bb8253080709e8b607d5bc8
Andrii Mishtal <mishtal.andriy@gmail.com>
2020-07-14 11:17:43 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,72 @@
 import React from 'react';
 import './App.css';
+import GoodsList from './GoodsList';
+import { loadGoods } from './api';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+interface State {
+  goods: Good[];
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+  };
+
+  loadAllGoods = () => {
+    loadGoods()
+      .then((loadedGoods) => {
+        this.setState({ goods: loadedGoods });
+      });
+  };
+
+  loadOnlyRedGoods = () => {
+    loadGoods()
+      .then((loadedGoods) => {
+        this.setState({ goods: loadedGoods.filter((good: Good) => good.color === 'red') });
+      });
+  };
+
+  loadSortGoods = () => {
+    loadGoods()
+      .then(loadedGoods => {
+        this.setState({
+          goods: [...loadedGoods]
+            .sort((a: Good, b: Good) => (a.name).localeCompare(b.name))
+            .slice(0, 5),
+        });
+      });
+  };
+
+  render() {
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={this.loadAllGoods}
+        >
+          Load All goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.loadSortGoods}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.loadOnlyRedGoods}
+        >
+          Load red goods
+        </button>
+        <GoodsList goods={this.state.goods} />
+
+      </>
+    );
+  }
+}
 
 export default App;
