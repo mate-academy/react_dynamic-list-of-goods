582d71e236cc4a8c9d84892d59332c187d62bc8e
Glib Rulev <velzepooz@gmail.com>
2020-07-15 09:53:09 +0300

    SOlution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,99 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList/GoodsList';
+import { fetchGoods } from './api/api';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+interface State {
+  goods: Good[];
+  error: boolean;
+}
+
+class App extends React.Component {
+  state: State = {
+    goods: [],
+    error: false,
+  };
+
+  getAllGoods = () => {
+    fetchGoods<Good>()
+      .then(response => {
+        this.setState({
+          goods: response.data,
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error,
+        });
+      });
+  };
+
+  getFiveGoods = () => {
+    fetchGoods<Good>()
+      .then(response => {
+        this.setState({
+          goods: [...response.data].sort((a, b) => (
+            a.name.localeCompare(b.name))).slice(0, 5),
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error,
+        });
+      });
+  };
+
+  getRedGoods = () => {
+    fetchGoods<Good>()
+      .then(response => {
+        this.setState({
+          goods: response.data.filter(item => item.color === 'red'),
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error,
+        });
+      });
+  };
+
+  render() {
+    const { goods, error } = this.state;
+
+    return (
+      <div className="container">
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={this.getAllGoods}
+        >
+          Get all goods
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={this.getFiveGoods}
+        >
+          Get 5 goods
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={this.getRedGoods}
+        >
+          Get only red
+        </button>
+        {error
+          ? (
+            <div className="alert alert-primary" role="alert">
+              Ooops! Something went wrong, maybe try later
+            </div>
+          )
+          : <GoodsList goods={goods} />}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,7 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+interface Good {
+  id: number;
+  name: string;
+  color: string;
+}
