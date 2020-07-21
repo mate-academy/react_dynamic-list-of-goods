095f90da75d2554108b83c5ceebb52caf3662234
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-21 20:32:59 +0300

    add static list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
+import { Good } from './types';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const goodsFromServer = [
+  { id: 1, name: 'Potato', color: 'red' },
+  { id: 1, name: 'Pear', color: 'green' },
+  { id: 1, name: 'Mellon', color: 'blue' },
+];
+
+interface State {
+  goods: Good[];
+}
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="container">
+        <h1>Dynamic list of Goods</h1>
+        <GoodsList goods={goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
