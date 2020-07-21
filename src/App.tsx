4b9eab533bcb8e662d562d00371458f9b94a0cce
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-21 22:17:52 +0300

    add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,39 @@ import './App.css';
 import { GoodsList } from './components/GoodsList';
 import { Good } from './types';
 
+const API_URL = 'https://mate.academy/students-api/goods';
 const goodsFromServer = [
   { id: 1, name: 'Potato', color: 'red' },
-  { id: 1, name: 'Pear', color: 'green' },
-  { id: 1, name: 'Mellon', color: 'blue' },
+  { id: 2, name: 'Pear', color: 'green' },
+  { id: 3, name: 'Mellon', color: 'blue' },
 ];
 
 interface State {
   goods: Good[];
 }
 
+interface ResponseData<D> {
+  data: D;
+  error?: string;
+  meta?: any;
+}
+
+type GoodsData = ResponseData<Good[]>;
+
 export class App extends React.Component<{}, State> {
   state: State = {
     goods: goodsFromServer,
   };
+
+  componentDidMount() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(({ data }: GoodsData) => {
+        this.setState({
+          goods: data,
+        });
+      });
+  }
 
   render() {
     const { goods } = this.state;
