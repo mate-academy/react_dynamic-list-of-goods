125ed51f25accd18b7c287a55daca093c779f5f5
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-22 10:27:45 +0300

    switch back to classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,73 @@
-import React, { useState, FC, useEffect } from 'react';
+// import React, { useState, FC, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { GoodsList } from './components/GoodsList';
 import { Good } from './types';
 import { loadGoods } from './api';
 
-// interface State {
-//   goods: Good[];
-//   error: string;
-// }
+interface State {
+  goods: Good[];
+  error: string;
+}
 
-// export class App extends React.Component<{}, State> {
-//   state: State = {
-//     goods: [],
-//     error: '',
-//   };
+export class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+    error: '',
+  };
 
-//   componentDidMount() {
-//     loadGoods()
-//       .then((goods) => {
-//         this.setState({
-//           goods,
-//         });
-//       })
-//       .catch(error => this.setState({ error: error.message }));
-//   }
-
-//   render() {
-//     const { goods, error } = this.state;
-
-//     return (
-//       <div className="container">
-//         <h1>Dynamic list of Goods</h1>
-//         {error
-//           ? (<p>{error}</p>)
-//           : (<GoodsList goods={goods} />)}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-const useGoods = (): [Good[], string] => {
-  const [goods, setGoods] = useState<Good[]>([]); // touple
-  const [error, setError] = useState(''); // touple
-
-  useEffect(() => {
+  componentDidMount() {
     loadGoods()
-      .then(goodsFromServer => setGoods(goodsFromServer))
-      .catch(err => setError(err.message));
-  }, []);
+      .then((goods) => {
+        this.setState({
+          goods,
+        });
+      })
+      .catch(error => this.setState({ error: error.message }));
+  }
 
-  return [goods, error];
-};
+  render() {
+    const { goods, error } = this.state;
 
-const AppHooks: FC = () => {
-  const [goods, error] = useGoods();
+    return (
+      <div className="container">
+        <h1>Dynamic list of Goods</h1>
+        {error
+          ? (<p>{error}</p>)
+          : (<GoodsList goods={goods} />)}
+      </div>
+    );
+  }
+}
 
-  return (
-    <div className="container">
-      <h1>Dynamic list of Goods</h1>
-      {error
-        ? (<p>{error}</p>)
-        : (<GoodsList goods={goods} />)}
-    </div>
-  );
-};
+export default App;
 
-export {
-  AppHooks as App,
-};
+// const useGoods = (): [Good[], string] => {
+//   const [goods, setGoods] = useState<Good[]>([]); // touple
+//   const [error, setError] = useState(''); // touple
+
+//   useEffect(() => {
+//     loadGoods()
+//       .then(goodsFromServer => setGoods(goodsFromServer))
+//       .catch(err => setError(err.message));
+//   }, []);
+
+//   return [goods, error];
+// };
+
+// const AppHooks: FC = () => {
+//   const [goods, error] = useGoods();
+
+//   return (
+//     <div className="container">
+//       <h1>Dynamic list of Goods</h1>
+//       {error
+//         ? (<p>{error}</p>)
+//         : (<GoodsList goods={goods} />)}
+//     </div>
+//   );
+// };
+
+// export {
+//   AppHooks as App,
+// };
