e4b2d5dd54eb9625ab044fb88a5b44dfeece8c22
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-22 10:44:27 +0300

    add buttons and loading functions

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,60 @@ export class App extends React.Component<{}, State> {
       .catch(error => this.setState({ error: error.message }));
   }
 
+  onLoadAll = () => {
+    loadGoods()
+      .then((goods) => {
+        this.setState({
+          goods,
+        });
+      })
+      .catch(error => this.setState({ error: error.message }));
+  };
+
+  onLoad5 = () => {
+    loadGoods()
+      .then((goods) => {
+        this.setState({
+          goods: goods.slice(0, 5),
+        });
+      })
+      .catch(error => this.setState({ error: error.message }));
+  };
+
+  onLoadRed = () => {
+    loadGoods()
+      .then((goods) => {
+        this.setState({
+          goods: goods.filter(good => good.color === 'red'),
+        });
+      })
+      .catch(error => this.setState({ error: error.message }));
+  };
+
   render() {
     const { goods, error } = this.state;
 
     return (
       <div className="container">
         <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={this.onLoadAll}
+        >
+          All goods
+        </button>
+        <button
+          type="button"
+          onClick={this.onLoad5}
+        >
+          5 goods
+        </button>
+        <button
+          type="button"
+          onClick={this.onLoadRed}
+        >
+          red goods
+        </button>
         {error
           ? (<p>{error}</p>)
           : (<GoodsList goods={goods} />)}
