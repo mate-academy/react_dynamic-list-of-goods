74207d82289ea805fc5aca6d44e80aeda1f5e8fa
Vovasef <vovasef@gmail.com>
2020-08-12 21:03:23 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import GoodsList from './components/GoodsList';
+import { Goods, Good } from './components/interfaces';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+// interface State extends Goods {
+//   hello?: any;
+// }
 
-export default App;
+const GOODSURL = 'https://mate.academy/students-api/goods';
+
+export default class App extends Component {
+  state: Goods = {
+    goods: [],
+  };
+
+  getGoods = () => {
+    fetch(GOODSURL)
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({
+          goods: result.data,
+        });
+      });
+  };
+
+  get5Goods = () => {
+    fetch(GOODSURL)
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({
+          goods: result.data
+            .sort((a: Good, b: Good) => (a.name).localeCompare(b.name))
+            .slice(0, 5),
+        });
+      });
+  };
+
+  getRedGoods = () => {
+    fetch(GOODSURL)
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({
+          goods: result.data
+            .filter((good: Good) => good.color === 'red'),
+        });
+      });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <GoodsList goods={this.state.goods} />
+        <button type="button" onClick={this.getGoods}>
+          Load All good
+        </button>
+        <button type="button" onClick={this.get5Goods}>
+          Load 5 first goods
+        </button>
+        <button type="button" onClick={this.getRedGoods}>
+          Load red goods
+        </button>
+      </>
+    );
+  }
+}
