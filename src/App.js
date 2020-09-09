f9a5cbd985362b65e2827318c2001f8b6a33d1d3
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-09-09 19:55:33 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,66 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { GoodsList } from './components/GoodsList';
+
+import { getGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  };
+
+  loadAllGoods = async() => {
+    this.setState({
+      goods: await getGoods(),
+    });
+  }
+
+  load5first = async() => {
+    this.setState({
+      goods: (await getGoods())
+        .sort((goodA, goodB) => goodA.name.localeCompare(goodB.name))
+        .filter((_, index) => index < 5),
+    });
+  }
+
+  loadRedGoods = async() => {
+    this.setState({
+      goods: (await getGoods())
+        .filter(good => good.color === 'red'),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          onClick={this.loadAllGoods}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={this.load5first}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={this.loadRedGoods}
+        >
+          Load red only
+        </button>
+
+        <GoodsList goods={this.state.goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -1,11 +1,12 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll() {
+// this is my personal compendium for future
+/*
+export function getGoodsFromServer() {
   return fetch(API_URL)
     .then(response => response.json());
 }
+*/
 
-export const get5First = () => {};
-
-export const getRedGoods = () => {};
+export const getGoods = async() => (await fetch(API_URL)).json();
