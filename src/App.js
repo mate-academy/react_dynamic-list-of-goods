0fcd535535702ff232639326900124cd1f8abb70
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-09-09 17:32:42 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.PureComponent {
+  state = {
+    goods: [],
+  }
+
+  enlistGoods = async(promise) => {
+    const goods = await promise();
+
+    this.setState({
+      goods,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button type="button" onClick={() => this.enlistGoods(getAll)}>
+          Load All goods
+        </button>
+        <button type="button" onClick={() => this.enlistGoods(get5First)}>
+          Load 5 first goods
+        </button>
+        <button type="button" onClick={() => this.enlistGoods(getRedGoods)}>
+          Load red goods
+        </button>
+        <GoodsList goods={this.state.goods} />
+      </>
+
+    );
+  }
+}
 
 export default App;
