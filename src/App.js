428d2b77e3d759d9ca16213e8b0d594ff1b1fd22
ChrisZinch <chrispakulyak@gmail.com>
2020-09-10 15:56:26 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import React from 'react';
-
+import { GoodsList } from './components/GoodsList/GoodsList';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  };
+
+  loadAll = () => {
+    getAll()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  loadFirst = () => {
+    get5First()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  loadRed = () => {
+    getRedGoods()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="buttons">
+        <button
+          type="submit"
+          onClick={() => {
+            this.loadAll();
+          }}
+        >
+          Load All goods
+        </button>
+        <button
+          type="submit"
+          onClick={() => {
+            this.loadFirst();
+          }}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="submit"
+          onClick={() => {
+            this.loadRed();
+          }}
+        >
+          Load red goods
+        </button>
+        <GoodsList goods={goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
