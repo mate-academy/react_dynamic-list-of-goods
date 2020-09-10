d76fea7c6c9f95790e79f03492df58a30e3819a1
Yurii Rysyn <zerocool_cs@ukr.net>
2020-09-10 09:56:31 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { GoodsList } from './components/GoodsList';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends PureComponent {
+ state = {
+   data: [],
+ }
+
+  getAllGoods = () => {
+    getAll()
+      .then((result) => {
+        this.setState({
+          data: result,
+        });
+      });
+  }
+
+  getFirstFiveGoods = () => {
+    get5First().then((result) => {
+      this.setState({
+        data: result,
+      });
+    });
+  }
+
+  getRedGoods = () => {
+    getRedGoods().then((result) => {
+      this.setState({
+        data: result,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <div className="open-list-buttons">
+          <button
+            type="button"
+            onClick={this.getAllGoods}
+          >
+            Show all goods
+          </button>
+          <button
+            type="button"
+            onClick={this.getFirstFiveGoods}
+          >
+            Show first 5
+          </button>
+          <button
+            type="button"
+            onClick={this.getRedGoods}
+          >
+            Show red goods
+          </button>
+        </div>
+        <GoodsList
+          list={this.state.data}
+        />
+      </>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -3,9 +3,23 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll() {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => response.json()).then(data => data);
 }
 
-export const get5First = () => {};
+export const get5First = () => getAll()
+  .then(result => result.slice(0, 5).sort(
+    (a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
 
-export const getRedGoods = () => {};
+      if (a.name < b.name) {
+        return -1;
+      }
+
+      return 0;
+    },
+  ));
+
+export const getRedGoods = () => getAll()
+  .then(result => result.filter(item => item.color === 'red'));
