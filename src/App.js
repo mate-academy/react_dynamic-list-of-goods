210668f7b461bd5bc86b5ebc78c19a0c64b2f841
oivannikov <alex0502ivannikov@gmail.com>
2020-09-10 18:41:58 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,62 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+import GoodsList from './components/GoodsList/GoodsList';
 
-export default App;
+export class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  getAllFromServer = () => {
+    getAll()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  };
+
+  get5FirstElement = () => {
+    get5First()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  };
+
+  getColorElement = () => {
+    getRedGoods()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={this.getAllFromServer}
+        >
+          Get All
+        </button>
+        <button
+          type="button"
+          onClick={this.get5FirstElement}
+        >
+          Get five first elements
+        </button>
+        <button
+          type="button"
+          onClick={this.getColorElement}
+        >
+          Get red elements
+        </button>
+        <GoodsList goods={goods} />
+      </div>
+    );
+  }
+}
