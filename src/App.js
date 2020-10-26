8e1564219ebb3266d43d602e0efcc561441950f7
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-10-26 16:32:52 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import React from 'react';
 
 import './App.scss';
+import { GoodsList } from './Components/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+state = {
+  loadAllGoods: getAll,
+  load5FirstGoods: get5First,
+  loadRedGoods: getRedGoods,
+  receivedGoods: [],
+}
+
+render() {
+  const {
+    loadAllGoods,
+    load5FirstGoods,
+    loadRedGoods,
+    receivedGoods,
+  } = this.state;
+
+  return (
+    <div>
+      <h1>Dynamic list of Goods</h1>
+      <button
+        type="button"
+        onClick={() => {
+          loadAllGoods()
+            .then((arrReceivedGoods) => {
+              this.setState({ receivedGoods: arrReceivedGoods });
+            });
+        }}
+      >
+        All goods
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          load5FirstGoods()
+            .then((arrReceivedGoods) => {
+              this.setState({ receivedGoods: arrReceivedGoods });
+            });
+        }}
+      >
+        Firts five goods
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          loadRedGoods()
+            .then((arrReceivedGoods) => {
+              this.setState({ receivedGoods: arrReceivedGoods });
+            });
+        }}
+      >
+        All red goods
+      </button>
+
+      <GoodsList goodsList={receivedGoods} />
+    </div>
+
+  );
+}
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -6,6 +6,12 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => getAll()
+  .then((arrAllGoods) => {
+    arrAllGoods.sort((a, b) => a.name.localeCompare(b.name));
 
-export const getRedGoods = () => {};
+    return arrAllGoods.slice(0, 5);
+  });
+
+export const getRedGoods = () => getAll()
+  .then(arrAllGoods => arrAllGoods.filter(good => good.color === 'red'));
