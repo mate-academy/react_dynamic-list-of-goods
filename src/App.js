8ea57be76956233df438063fed20b3ed4beb17ba
andlap24 <andrew.laptev24@gmail.com>
2020-11-02 21:04:57 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,80 @@
 import React from 'react';
+import 'bulma';
+
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodList } from './components/GoodList';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+class App extends React.Component {
+  state = {
+    goods: [],
+  };
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  getAllHandler = () => {
+    getAll()
+      .then((goods) => {
+        this.setState({
+          goods,
+        });
+      });
+  }
+
+  get5FirstHandler = () => {
+    get5First()
+      .then((goods) => {
+        this.setState({
+          goods,
+        });
+      });
+  }
+
+  getRedHandler = () => {
+    getRedGoods()
+      .then((goods) => {
+        this.setState({
+          goods,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App__title">Dynamic list of Goods</h1>
+
+        <div className="App__buttons">
+          <button
+            type="button"
+            className="button is-link"
+            onClick={this.getAllHandler}
+          >
+            get all
+          </button>
+
+          <button
+            type="button"
+            className="button is-link"
+            onClick={this.get5FirstHandler}
+          >
+            get 5
+          </button>
+
+          <button
+            type="button"
+            className="button is-link"
+            onClick={this.getRedHandler}
+          >
+            get red
+          </button>
+        </div>
+
+        <div className="App__list list">
+          <GoodList goods={this.state.goods} />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
