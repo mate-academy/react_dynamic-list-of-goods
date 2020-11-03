cdce3b28075790e94265a27fd749dcfc04d0b9d9
Anastasia Chervona <av.chervona@gmail.com>
2020-11-03 23:20:42 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React from 'react';
-
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.PureComponent {
+  state = {
+    goods: [],
+  }
+
+  setGoods = (callback) => {
+    callback().then((goods) => {
+      this.setState({
+        goods,
+      });
+    });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="title">Dynamic list of Goods</h1>
+
+        <div className="app__buttons">
+          <button
+            className="button is-primary"
+            type="button"
+            onClick={() => this.setGoods(getAll)}
+          >
+            Load All goods
+          </button>
+
+          <button
+            className="button is-primary"
+            type="button"
+            onClick={() => this.setGoods(get5First)}
+          >
+            Load 5 first goods
+          </button>
+
+          <button
+            className="button is-danger"
+            type="button"
+            onClick={() => this.setGoods(getRedGoods)}
+          >
+            Load red goods
+          </button>
+        </div>
+
+        <div className="content">
+          <GoodsList goods={goods} />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
