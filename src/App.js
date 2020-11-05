848f1fad7227fcca62ed2399b77979123a4ab347
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-11-05 17:15:34 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import GoodsList from './components/GoodsList';
+import 'bulma';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+const App = () => {
+  const [goods, setList] = useState([]);
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  async function fetchData(fetchMethod) {
+    setList(await fetchMethod());
+  }
+
+  return (
+    <div className="content">
+      <div className="content__inner">
+        <h1>Dynamic list of Goods</h1>
+        <button
+          className="button is-primary content__button"
+          type="button"
+          onClick={() => fetchData(getAll)}
+        >
+          Load All goods
+        </button>
+        <button
+          className="button is-warning content__button"
+          type="button"
+          onClick={() => fetchData(get5First)}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          className="button is-danger content__button"
+          type="button"
+          onClick={() => fetchData(getRedGoods)}
+        >
+          Load red goods
+        </button>
+        <GoodsList goods={goods} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
