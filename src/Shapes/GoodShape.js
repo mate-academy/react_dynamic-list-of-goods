ebeb4a89ccb849f69c07f4e030d0749eb29ad494
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-11-05 12:47:26 +0300

    changes

--- src/Shapes/GoodShape.js
+++ src/Shapes/GoodShape.js
@@ -3,4 +3,5 @@ import PropTypes from 'prop-types';
 export const GoodShape = PropTypes.shape({
   name: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 }).isRequired;
