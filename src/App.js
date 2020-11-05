6df8b1dbed415732a259723ee44aed60d634b3fb
Lancaelot <Lancaelot@gmail.com>
2020-11-05 18:48:44 +0200

    create button component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import GoodsList from './components/GoodsList';
 import { getAll, get5First, getRedGoods } from './api/goods';
 import './App.scss';
+import Button from './components/Button';
 // eslint-disable-next-line max-len
 
 class App extends PureComponent {
@@ -26,27 +27,18 @@ class App extends PureComponent {
       <div className="App">
         <h1>Dynamic list of Goods</h1>
         <div className="ui buttons">
-          <button
-            className="ui button"
-            onClick={this.renderGoods}
-            type="button"
-          >
-            Load All goods
-          </button>
-          <button
-            className="ui button"
-            onClick={this.getSortedGoods}
-            type="button"
-          >
-            Load 5 first goods
-          </button>
-          <button
-            className="ui button"
-            onClick={this.getRedsGoods}
-            type="button"
-          >
-            Load red goods
-          </button>
+          <Button
+            handler={this.renderGoods}
+            buttonName="Load All goods"
+          />
+          <Button
+            handler={this.getSortedGoods}
+            buttonName="Load 5 first goods"
+          />
+          <Button
+            handler={this.getRedsGoods}
+            buttonName="Load red goods"
+          />
         </div>
         {this.state.goodsToRender
         && (
--- src/components/GoodsList.jsx
+++ src/components/GoodsList.jsx
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types';
 const GoodsList = ({ goods }) => (
   <ul className="ui middle aligned animated list">
     {goods.map(good => (
-      <div className="item">
-        <div className="content">
+      <div className="item" key={`${good.name}item`}>
+        <div className="content" key={`${good.name}content`}>
           <li
             className="header"
+            key={good.name}
             style={{ color: `${good.color}` }}
           >
             {good.name}
