cd29968c556a9bfdd2628752f0ad5f5d1f0fcbf8
goncharukop <goncharukop@gmail.com>
2020-11-05 22:14:02 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import React from 'react';
 
 import './App.scss';
+import 'semantic-ui-css/semantic.min.css';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.PureComponent {
+  state = {
+    goods: [],
+  }
+
+  preparedGoods = (func) => {
+    func().then((goods) => {
+      this.setState({
+        goods,
+      });
+    });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="app">
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          className="ui inverted green large button"
+          type="button"
+          onClick={() => (
+            this.preparedGoods(getAll)
+          )}
+        >
+          Get All
+        </button>
+
+        <button
+          className="ui inverted orange large button"
+          type="button"
+          onClick={() => (
+            this.preparedGoods(get5First)
+          )}
+        >
+          Get five
+        </button>
+
+        <button
+          className="ui inverted red large button"
+          type="button"
+          onClick={() => (
+            this.preparedGoods(getRedGoods)
+          )}
+        >
+          Get red
+        </button>
+
+        <div className="app_list">
+          <GoodsList goods={goods} />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
