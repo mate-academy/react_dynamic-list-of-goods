c3f338fb93f47aec5b5b2459464a7c8efcfa3d0e
goncharukop <goncharukop@gmail.com>
2020-11-06 12:24:43 +0200

    add component Button

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import 'semantic-ui-css/semantic.min.css';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
 import { GoodsList } from './components/GoodsList';
+import { Button } from './components/Button';
 
 class App extends React.PureComponent {
   state = {
     goods: [],
   }
 
-  preparedGoods = (func) => {
+  setGoods = (func) => {
     func().then((goods) => {
       this.setState({
         goods,
@@ -26,35 +27,29 @@ class App extends React.PureComponent {
       <div className="app">
         <h1>Dynamic list of Goods</h1>
 
-        <button
-          className="ui inverted green large button"
-          type="button"
-          onClick={() => (
-            this.preparedGoods(getAll)
+        <Button
+          color="green"
+          title="Get All"
+          handleClick={() => (
+            this.setGoods(getAll)
           )}
-        >
-          Get All
-        </button>
+        />
 
-        <button
-          className="ui inverted orange large button"
-          type="button"
-          onClick={() => (
-            this.preparedGoods(get5First)
+        <Button
+          color="orange"
+          title="Get five"
+          handleClick={() => (
+            this.setGoods(get5First)
           )}
-        >
-          Get five
-        </button>
+        />
 
-        <button
-          className="ui inverted red large button"
-          type="button"
-          onClick={() => (
-            this.preparedGoods(getRedGoods)
+        <Button
+          color="red"
+          title="Get red"
+          handleClick={() => (
+            this.setGoods(getRedGoods)
           )}
-        >
-          Get red
-        </button>
+        />
 
         <div className="app_list">
           <GoodsList goods={goods} />
