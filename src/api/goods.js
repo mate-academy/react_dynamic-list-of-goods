2a24d454713fd420622599b45923f9202704ae56
OnlyRaydek <bayramsaid00@gmail.com>
2020-11-06 08:26:37 +0200

    solution

--- src/api/goods.js
+++ src/api/goods.js
@@ -6,6 +6,12 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const getFiveFirst = () => getAll()
+  .then(goods => goods
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .slice(0, 5));
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => getAll()
+  .then(goods => (
+    goods.filter(item => (item.color === 'red'))
+  ));
