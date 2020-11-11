98864bb89cca29410809270ede5bb91b0bedf5bb
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-11 18:48:23 +0200

    done

--- src/api/goods.js
+++ src/api/goods.js
@@ -1,11 +1,20 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable arrow-parens */
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll() {
-  return fetch(API_URL)
-    .then(response => response.json());
+  return fetch(API_URL).then((response) => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => {
+  return fetch(API_URL)
+    .then((response) => response.json())
+    .then((res) => res.slice(0, 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return fetch(API_URL)
+    .then((response) => response.json())
+    .then((res) => res.filter((product) => product.color === 'red'));
+};
