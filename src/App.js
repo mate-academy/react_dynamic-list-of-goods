7424c3f1da60510fd78f23f13f8afba256c53987
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-11-17 14:10:04 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+export class App extends Component {
+  state = {
+    goods: [],
+  }
 
+  loadAllGoods = async() => {
+    const goods = await getAll();
+    this.setState({ goods });
+  }
+
+  loadFirst5Goods = async() => {
+    const goods = await get5First();
+    this.setState({ goods: goods.sort((a, b) => a.name.localeCompare(b.name))
+      .slice(0, 5) });
+  }
+
+  loadRedGoods = async() => {
+    const goods = await getRedGoods();
+    this.setState({ goods: goods.filter(good => good.color === 'red') });
+  }
+
+  render() {
+    const {
+      goods
+    } = this.state;
+
+    return (
+      <>
+        <h1 align="center">
+          Dynamic list of Goods
+        </h1>
+        <br></br>
+        <button
+        type="button"
+        onClick={this.loadAllGoods}
+        >
+          Load All goods
+        </button>
+        <button
+        type="button"
+        onClick={this.loadFirst5Goods}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={this.loadRedGoods}
+        >
+                Load red goods
+        </button>
+
+        <div className="list">
+          {goods.length === 0 ? (
+            <>
+              <p>You haven't loaded items from the server yet</p>
+            </>
+          ) : (
+            <div className="goodsList">
+              <GoodsList goods={goods} />
+            </div>
+          )}
+        </div>
+      </>
+    );
+  }
+}
 export default App;
