dd53b8a8e67657ef6209f0dc6dcb58804618a2cc
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-11-18 12:54:21 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -12,40 +12,43 @@ export class App extends Component {
 
   loadAllGoods = async() => {
     const goods = await getAll();
+
     this.setState({ goods });
   }
 
   loadFirst5Goods = async() => {
     const goods = await get5First();
+
     this.setState({ goods: goods.sort((a, b) => a.name.localeCompare(b.name))
       .slice(0, 5) });
   }
 
   loadRedGoods = async() => {
     const goods = await getRedGoods();
+
     this.setState({ goods: goods.filter(good => good.color === 'red') });
   }
 
   render() {
     const {
-      goods
+      goods,
     } = this.state;
 
     return (
-      <>
+      <div>
         <h1 align="center">
           Dynamic list of Goods
         </h1>
-        <br></br>
+
         <button
-        type="button"
-        onClick={this.loadAllGoods}
+          type="button"
+          onClick={this.loadAllGoods}
         >
           Load All goods
         </button>
         <button
-        type="button"
-        onClick={this.loadFirst5Goods}
+          type="button"
+          onClick={this.loadFirst5Goods}
         >
           Load 5 first goods
         </button>
@@ -53,21 +56,19 @@ export class App extends Component {
           type="button"
           onClick={this.loadRedGoods}
         >
-                Load red goods
+          Load red goods
         </button>
 
         <div className="list">
           {goods.length === 0 ? (
-            <>
-              <p>You haven't loaded items from the server yet</p>
-            </>
+            <p>You have not loaded items from the server yet</p>
           ) : (
             <div className="goodsList">
               <GoodsList goods={goods} />
             </div>
           )}
         </div>
-      </>
+      </div>
     );
   }
 }
