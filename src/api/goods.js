ebba552a214035f589a18ff391c0a69f73228c65
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-11-23 17:19:23 +0200

    Fixed Solution

--- src/api/goods.js
+++ src/api/goods.js
@@ -6,12 +6,10 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => fetch(API_URL)
-  .then(response => response.json())
+export const get5First = () => getAll()
   .then(todos => todos.sort(
     (todo1, todo2) => todo1.name.localeCompare(todo2.name),
   ).filter((_, index) => index < 5));
 
-export const getRedGoods = () => fetch(API_URL)
-  .then(response => response.json())
+export const getRedGoods = () => getAll()
   .then(todos => todos.filter(todo => todo.color === 'red'));
