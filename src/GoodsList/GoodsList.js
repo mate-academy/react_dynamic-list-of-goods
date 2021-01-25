4985b6a72df583f2625e28707388c96c7c03b61e
Andrii Haidai <haidai.andriy@gmail.com>
2021-01-25 15:22:38 +0200

    colors was added

--- src/GoodsList/GoodsList.js
+++ src/GoodsList/GoodsList.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export const GoodsList = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={good.id}>
+      <li key={good.id} style={{ color: good.color.toString() }}>
         {good.name}
       </li>
     ))}
