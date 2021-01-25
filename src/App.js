dff499c6f55edd13f32cfa1e491776c7bd195a0d
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-25 23:59:53 +0200

    Reordered folders

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.scss';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
-import { GoodsList } from './api/components/GoodsList/GoodsList';
+import { GoodsList } from './components/GoodsList/GoodsList';
 // or
 // import * as goodsAPI from './api/goods';
 
