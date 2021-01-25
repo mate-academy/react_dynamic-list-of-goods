45412251b47bba53e44e93a5f6df399eac6f1e28
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-25 23:47:32 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,66 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './api/components/GoodsList/GoodsList';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+    isListVisible: false,
+  }
+
+  showAll = () => {
+    getAll()
+      .then((goods) => {
+        this.setState(prevState => ({
+          goods,
+          isListVisible: !prevState.isListVisible,
+        }));
+      });
+  }
+
+  show5First = () => {
+    get5First()
+      .then((goods) => {
+        this.setState(prevState => ({
+          goods,
+          isListVisible: !prevState.isListVisible,
+        }));
+      });
+  }
+
+  showRedGoods = () => {
+    getRedGoods()
+      .then((goods) => {
+        this.setState(prevState => ({
+          goods,
+          isListVisible: !prevState.isListVisible,
+        }));
+      });
+  }
+
+  render() {
+    const { isListVisible } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button type="button" onClick={this.showAll}>
+          Load All goods
+        </button>
+        <button type="button" onClick={this.show5First}>
+          Load 5 first goods
+        </button>
+        <button type="button" onClick={this.showRedGoods}>
+          Load red goods
+        </button>
+        {isListVisible && <GoodsList goods={this.state.goods} />}
+      </>
+    );
+  }
+}
 
 export default App;
