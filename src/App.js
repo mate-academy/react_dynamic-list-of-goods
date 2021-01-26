6da51f31d4d6cee0ee916135c8c6675b5e5ab552
Layrovell <marina95prodan@gmail.com>
2021-01-26 16:27:36 +0200

    develop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-
 import './App.scss';
+import { GoodList } from './components/GoodList/GoodList';
+import { getAll } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  getAllHandler = () => {
+    getAll()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  };
+
+  get5FirstHandler = () => {
+    getAll()
+      .then((goods) => {
+        this.setState({
+          goods: goods
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .slice(0, 5),
+        });
+      });
+  };
+
+  getRedHandler = () => {
+    getAll()
+      .then((goods) => {
+        this.setState({
+          goods: goods.filter(good => good.color === 'red'),
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Dynamic list of Goods</h1>
+        <GoodList goods={this.state.goods} />
+        <button onClick={this.getAllHandler} type="button">getAll</button>
+        <button onClick={this.get5FirstHandler} type="button">get5First</button>
+        <button onClick={this.getRedHandler} type="button">getRed</button>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -1,10 +1,9 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll() {
-  return fetch(API_URL)
-    .then(response => response.json());
-}
+export const getAll = () => fetch(API_URL)
+  .then(response => response.json())
+  .then(goods => goods);
 
 export const get5First = () => {};
 
