ff9309dac0fc9cee6d96422b71c3d51adcbd2c30
PaulT <ptimofejev@gmail.com>
2021-01-26 14:17:49 +0200

    Dynamic list of goods solution

--- src/api/goods.js
+++ src/api/goods.js
@@ -1,11 +1,23 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll() {
+export function getAllGoods() {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      return response.json();
+    });
 }
 
-export const get5First = () => {};
+export const get5FirstGoods = () => (
+  getAllGoods()
+    .then(goods => goods.slice(0, 5))
+);
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => (
+  getAllGoods()
+    .then(goods => goods.filter(good => good.color === 'red'))
+);
