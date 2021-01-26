6ab27ea2c21a2a3bc3d6dbb6b8d8845d66faacfd
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2021-01-26 09:59:58 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
 import React from 'react';
+import GoodsList from './Component/GoodsList';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  };
+
+  getAllGoods = () => {
+    getAll()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  getFifeGoods = () => {
+    get5First()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  getAllRedGoods = () => {
+    getRedGoods()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="container">
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          onClick={this.getAllGoods}
+        >
+          Load All goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.getFifeGoods}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.getAllRedGoods}
+        >
+          Load red goods
+        </button>
+
+        <div className="columns">
+          {goods.length > 0 && (
+            <div className="goods">
+              <GoodsList goods={goods} />
+            </div>
+          )}
+        </div>
+
+      </div>
+    );
+  }
+}
 
 export default App;
