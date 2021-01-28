5668ee984f3b74594b75da22eb0ce13259e3f41f
Your Name <mogolgogol.90@gmail.com>
2021-01-28 21:20:48 +0200

    added forgotten sorting

--- src/api/goods.js
+++ src/api/goods.js
@@ -8,7 +8,7 @@ export function getAll() {
 
 export const get5First = () => getAll()
   .then(result => (
-    result.slice(0, 5)
+    result.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5)
   ));
 
 export const getRed = () => getAll()
