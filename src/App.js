ea71c1ce3c48fd79c8fd1f4b5f0d45e5bf59f642
Alexandr Petrenko <alexandr.petrenko.fe@gmail.com>
2021-02-08 19:24:41 +0200

    solution with Hook

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAll, get5First, getRedGoods } from './api/goods';
 import { GoodList } from './components/GoodList/GoodList';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+const App = () => {
+  const [stateList, setStateList] = useState([]);
 
-class App extends React.Component {
-  state = {
-    list: [],
-  }
+  const buttonHandler = (callback) => {
+    callback()
+      .then(result => setStateList(result));
+  };
 
-  buttonHandler = (callback) => {
-    callback().then(result => this.setState({
-      list: result,
-    }));
-  }
-
-  render() {
-    return (
-      <>
-        <button
-          type="button"
-          onClick={() => this.buttonHandler(getAll)}
-        >
-          Load All goods
-        </button>
-        <button
-          type="button"
-          onClick={() => this.buttonHandler(get5First)}
-        >
-          Load 5 first goods
-        </button>
-        <button type="button" onClick={() => this.buttonHandler(getRedGoods)}>
-          Load red goods
-        </button>
-        <GoodList list={this.state.list} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => buttonHandler(getAll)}
+      >
+        Load All goods
+      </button>
+      <button
+        type="button"
+        onClick={() => buttonHandler(get5First)}
+      >
+        Load 5 first goods
+      </button>
+      <button type="button" onClick={() => buttonHandler(getRedGoods)}>
+        Load red goods
+      </button>
+      <GoodList list={stateList} />
+    </>
+  );
+};
 
 export default App;
