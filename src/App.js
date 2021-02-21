2cb68138a47fdf2b8b3ae35fbb127ced1d7ae127
Inna Golovina <igigig978@gmail.com>
2021-02-21 23:03:15 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import React from 'react';
+import { GoodsList } from './components/GoodsList';
 
+import 'bulma/css/bulma.css';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  loadGoods = (goodsFunction) => {
+    goodsFunction()
+      .then((goods) => {
+        this.setState({
+          goods: [...goods],
+        });
+      });
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="box">
+        <h1 className="title is-1">Dynamic list of Goods</h1>
+        <div className="button_wrap">
+          <button
+            className="button is-link is-rounded"
+            onClick={() => (this.loadGoods(getAll))}
+            type="button"
+          >
+            Load All goods
+          </button>
+
+          <button
+            className="button is-primary is-rounded"
+            onClick={() => (this.loadGoods(get5First))}
+            type="button"
+          >
+            Load 5 first goods
+          </button>
+
+          <button
+            className="button is-danger is-rounded"
+            onClick={() => (this.loadGoods(getRedGoods))}
+            type="button"
+          >
+            Load 5 first goods
+          </button>
+        </div>
+        <GoodsList goods={goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
