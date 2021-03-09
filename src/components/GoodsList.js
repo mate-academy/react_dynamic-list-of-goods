10737b1e9046402bdfe293f32f1e7ac4e7b66fd6
gorzer <gorzer.rostyslav@gmail.com>
2021-03-09 14:57:08 +0200

    commited GoodsList

---
+++ src/components/GoodsList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const GoodsList = ({ goods }) => (
+  <>
+    {goods.length !== 0 && (
+      <div className="box">
+        <ul>
+          {goods.map(good => (
+            <li
+              key={good.id}
+              style={{ color: good.color }}
+            >
+              {good.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )}
+  </>
+);
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.object),
+};
+
+GoodsList.defaultProps = {
+  goods: [],
+};
