9975534941e013074cb63c49bc16b80af52da21c
gorzer <gorzer.rostyslav@gmail.com>
2021-03-09 14:48:12 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React from 'react';
-
+import 'bulma';
 import './App.scss';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  loadGoods = (goodsFromServer) => {
+    goodsFromServer()
+      .then((response) => {
+        this.setState({
+          goods: [...response],
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App box">
+        <div className="content">
+          <h1>Dynamic list of Goods</h1>
+        </div>
+
+        <button
+          type="button"
+          className="button is-dark"
+          onClick={() => this.loadGoods(getAll)}
+        >
+          Load All goods
+        </button>
+
+        <button
+          type="button"
+          className="button is-dark"
+          onClick={() => this.loadGoods(get5First)}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          className="button is-dark"
+          onClick={() => this.loadGoods(getRedGoods)}
+        >
+          Load red goods
+        </button>
+
+        <GoodsList goods={this.state.goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
