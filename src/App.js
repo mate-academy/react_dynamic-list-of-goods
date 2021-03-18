f3cfcaa906c7980cdc4717c1371810111f8ea3db
Misha Lysak <myhaillysak@gmail.com>
2021-03-18 19:31:55 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React from 'react';
-
+import { GoodList } from './components/GoodList';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  loadAllGoods() {
+    getAll()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  load5First() {
+    get5First()
+      .then((goods) => {
+        goods.sort((currGood, nextGood) => (
+          currGood.name.localeCompare(nextGood.name)
+        ));
+        this.setState({ goods });
+      });
+  }
+
+  loadRedGoods() {
+    getRedGoods()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          onClick={() => this.loadAllGoods()}
+          type="button"
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={() => this.load5First()}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={() => this.loadRedGoods()}
+        >
+          Load red goods
+        </button>
+        <GoodList goods={this.state.goods} />
+      </>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -6,6 +6,14 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => (
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(response => response.slice(0, 5))
+);
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => (
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(response => response.filter(good => good.color === 'red'))
+);
