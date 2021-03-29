0f1756df521a4e39239d8b0fb2ebb9eff3c752aa
Andrii Badekha <badekha@gmail.com>
2021-03-29 20:20:54 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,47 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = { goods: [] }
+
+  fetchGoods = (getData) => {
+    getData()
+      .then(goods => this.setState({ goods }));
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={() => this.fetchGoods(getAll)}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={() => this.fetchGoods(get5First)}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={() => this.fetchGoods(getRedGoods)}
+        >
+          Load red goods
+        </button>
+        <GoodsList goods={goods} />
+      </>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/GoodsList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const GoodsList = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li
+        key={good.id}
+        style={{ color: good.color }}
+      >
+        {good.name}
+      </li>
+    ))}
+  </ul>
+);
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(
+    PropTypes.object.isRequired,
+  ).isRequired,
+};
