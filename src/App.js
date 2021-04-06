be3bde6b3151c2aa1205751eb4c81d4cfcffcd58
Oleg Bezriadin <muhachov86@gmail.com>
2021-04-06 14:19:17 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { GoodsList } from './components/GoodsList';
+import 'bulma';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends Component {
+  state = {
+    goods: '',
+  }
+
+  handleGoods = (hadledGoods) => {
+    hadledGoods()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <h1 className="title">Dynamic list of Goods</h1>
+
+        <div>
+          <button
+            className="goods-button button"
+            type="button"
+            onClick={() => this.handleGoods(getAll)}
+          >
+            Load All goods
+          </button>
+          <button
+            className="goods-button button"
+            type="button"
+            onClick={() => this.handleGoods(get5First)}
+          >
+            Load 5 first goods
+          </button>
+          <button
+            className="goods-button button"
+            type="button"
+            onClick={() => this.handleGoods(getRedGoods)}
+          >
+            Load red goods
+          </button>
+        </div>
+
+        {this.state.goods.length > 0
+          && <GoodsList goods={this.state.goods} />}
+      </>
+    );
+  }
+}
 
 export default App;
