53b669602ac6aa6f3c30952d1da1396045edf553
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-04-06 11:03:05 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React from 'react';
 
+import { GoodsList } from './components/GoodsList';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  showAllGoods = async() => {
+    const goods = await getAll();
+
+    this.setState({ goods });
+  }
+
+  showFirst5Goods = async() => {
+    const goods = await get5First();
+
+    this.setState({ goods });
+  }
+
+  showRedGoods = async() => {
+    const goods = await getRedGoods();
+
+    this.setState({ goods });
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="goods">
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={this.showAllGoods}
+        >
+          All goods
+        </button>
+        {' '}
+        <button
+          type="button"
+          onClick={this.showFirst5Goods}
+        >
+          First five goods
+        </button>
+        {' '}
+        <button
+          type="button"
+          onClick={this.showRedGoods}
+        >
+          Red goods
+        </button>
+
+        <GoodsList goods={goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -1,11 +1,27 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll() {
-  return fetch(API_URL)
-    .then(response => response.json());
+export function request(url, options) {
+  return fetch(url, options)
+    .then((result) => {
+      if (!result.ok) {
+        throw new Error(`${result.status} - ${result.statusText}`);
+      }
+
+      return result.json();
+    });
 }
 
-export const get5First = () => {};
+export function getAll() {
+  return request(API_URL);
+}
 
-export const getRedGoods = () => {};
+export function get5First() {
+  return getAll()
+    .then(result => result.slice(0, 5));
+}
+
+export function getRedGoods() {
+  return getAll()
+    .then(result => result.filter(good => good.color === 'red'));
+}
