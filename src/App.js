ff8e6d911ad7631fd508d921fba6a67f8eabae9d
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-04-10 14:32:50 +0300

    smooth changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { GoodsList } from './api/components/GoodsList';
 import { getAll, get5First, getRedGoods } from './api/goods';
-import { GoodsList } from './api/components/GoodsList/GoodsList';
 
 import './App.scss';
 
@@ -9,13 +9,18 @@ export class App extends React.Component {
     goods: [],
   }
 
-  getList = (method) => {
-    method()
-      .then((goods) => {
-        this.setState({
-          goods,
-        });
-      });
+  getList = async(method) => {
+    const response = await method();
+
+    this.setState({
+      goods: await response,
+    });
+  }
+
+  hideList = () => {
+    this.setState({
+      goods: [],
+    });
   }
 
   render() {
@@ -23,37 +28,61 @@ export class App extends React.Component {
 
     return (
       <>
-        <h1>Dynamic list of Goods</h1>
-        <button
-          type="button"
-          onClick={() => {
-            this.getList(getAll);
-          }}
-        >
-          Get all
-        </button>
-        {' '}
-        <button
-          type="button"
-          onClick={() => {
-            this.getList(get5First);
-          }}
-        >
-          Get 5 first
-        </button>
-        {' '}
-        <button
-          type="button"
-          onClick={() => {
-            this.getList(getRedGoods);
-          }}
-        >
-          Get red
-        </button>
-        <GoodsList goods={goods} />
+        <h1 className="headerName">Dynamic list of Goods</h1>
+        <div className="container">
+          {goods.length === 0 ? (
+            <button
+              className="button is-link"
+              type="button"
+              onClick={() => {
+                this.getList(getAll);
+              }}
+            >
+              Show list of goods
+            </button>
+          ) : (
+            <>
+              <button
+                className="button is-link"
+                type="button"
+                onClick={this.hideList}
+              >
+                Hide
+              </button>
+              <button
+                type="button"
+                className="button is-primary"
+                onClick={() => {
+                  this.getList(getAll);
+                }}
+              >
+                Load All goods
+              </button>
+              {' '}
+              <button
+                type="button"
+                className="button is-primary"
+                onClick={() => {
+                  this.getList(get5First);
+                }}
+              >
+                Load 5 first goods
+              </button>
+              {' '}
+              <button
+                type="button"
+                className="button is-primary"
+                onClick={() => {
+                  this.getList(getRedGoods);
+                }}
+              >
+                Load red goods
+              </button>
+              <GoodsList goods={goods} />
+            </>
+          )}
+        </div>
       </>
     );
   }
 }
-
-export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll() {
-  return fetch(API_URL)
-    .then(response => response.json());
-}
+export const getAll = () => (
+  fetch(API_URL)
+    .then(response => response.json())
+);
 
 export const get5First = () => (
   getAll()
