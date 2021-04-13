bc341d3bd4331cd9cee1ee44ae8bd707418c96bf
Onishchuk <onishchuk.danil@gmail.com>
2021-04-13 11:43:47 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Buttons } from './components/Buttons';
+import { GoodsList } from './components/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+export class App extends PureComponent {
+  state = {
+    visible: [],
+  }
 
-export default App;
+  handleGetAll = () => {
+    getAll()
+      .then((goods) => {
+        this.setState({
+          visible: goods,
+        });
+      });
+  }
+
+  handleGetFive = () => {
+    get5First()
+      .then((goods) => {
+        this.setState({
+          visible: goods,
+        });
+      });
+  }
+
+  handleGetRed = () => {
+    getRedGoods()
+      .then((goods) => {
+        this.setState({
+          visible: goods,
+        });
+      });
+  }
+
+  render() {
+    const { visible } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <Buttons
+          handleGetAll={this.handleGetAll}
+          handleGetFive={this.handleGetFive}
+          handleGetRed={this.handleGetRed}
+        />
+        <GoodsList visible={visible} />
+      </>
+    );
+  }
+}
