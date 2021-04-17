3de5f1ce9d73d125727dbf45623a08ca8bc38ccd
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-04-18 00:14:40 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,61 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+export class App extends React.PureComponent {
+  state = {
+    visibleGoods: [],
+  }
 
-export default App;
+  handleGetAll = async() => {
+    const goods = await getAll();
+
+    this.setState({ visibleGoods: goods });
+  }
+
+  handleGetFive = async() => {
+    const goods = await get5First();
+
+    this.setState({ visibleGoods: goods });
+  }
+
+  handleGetRed = async() => {
+    const goods = await getRedGoods();
+
+    this.setState({ visibleGoods: goods });
+  }
+
+  render() {
+    const { visibleGoods } = this.state;
+
+    return (
+      <div className="wrapper">
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={this.handleGetAll}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={this.handleGetFive}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={this.handleGetRed}
+        >
+          Load red goods
+        </button>
+        <GoodsList goods={visibleGoods} />
+      </div>
+    );
+  }
+}
---
+++ src/types.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+export const propTypes = {
+  goods: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
