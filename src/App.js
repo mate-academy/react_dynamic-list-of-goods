5fad9c686e8d91d74d80c4d979bc9cb6d9e2af44
Iryna Haiduk <$ git config --global user.name Iryna>
2021-04-17 19:14:54 +0300

    Created React dynamic list of goods

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 import React from 'react';
+import { GoodsList } from './components/GoodsList';
 
 import './App.scss';
+import { get5First, getAll, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  showAllGoods = () => {
+    getAll()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  showFiveGoods = () => {
+    get5First()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  showRedGoods= () => {
+    getRedGoods()
+      .then((goods) => {
+        this.setState({ goods });
+      });
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <section className="goods">
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={this.showAllGoods}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={this.showFiveGoods}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={this.showRedGoods}
+        >
+          Load red goods
+        </button>
+        {goods.length > 0 && (
+          <GoodsList goods={goods} />
+        )}
+      </section>
+    );
+  }
+}
 
 export default App;
