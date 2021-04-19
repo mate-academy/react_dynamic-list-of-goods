3219fcb04f47701997634e71ad04180de63d9293
Oleg Hluhk <glug.oleg@gmail.com>
2021-04-19 09:51:45 +0300

    solutin

---
+++ src/component/GoodList/GoodList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const GoodList = ({ goods }) => (
+  <ul className="">
+    {
+      goods.map(good => (
+        <li
+          key={good.id}
+          style={{ color: good.color }}
+        >
+          {good.name}
+        </li>
+      ))
+    }
+  </ul>
+);
+
+GoodList.propTypes = {
+  goods: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
