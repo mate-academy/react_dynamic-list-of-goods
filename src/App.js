ba512b87ac95c156f93027c381d9f97ebd8a9cae
Mariia Kornieva <mariiakornieva@gmail.com>
2021-05-15 12:09:25 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React from 'react';
 
 import './App.scss';
+import 'bulma';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  };
+
+  loadData = async(callback) => {
+    const goods = await callback();
+
+    this.setState({ goods });
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Dynamic list of Goods</h1>
+
+          <div className="content">
+            <GoodsList goods={this.state.goods} />
+          </div>
+
+          <div className="buttons">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => this.loadData(getAll)}
+            >
+              Load All Goods
+            </button>
+
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => this.loadData(get5First)}
+            >
+              Load 5 First Goods
+            </button>
+
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => this.loadData(getRedGoods)}
+            >
+              Load Red Goods
+            </button>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default App;
