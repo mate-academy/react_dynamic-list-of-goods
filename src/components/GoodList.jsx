4b2c2e79f3c9cae38da43802095a87ac97091cca
Igor Lisogor <illisogori@gmail.com>
2021-06-01 21:43:06 +0300

    changes after review

--- src/components/GoodList.jsx
+++ src/components/GoodList.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const GoodList = ({ goods }) => (
-  goods.map(good => (
-    <li key={good.id} style={{ color: good.color }}>{good.name}</li>
-  ))
+  <ul>
+    {goods.map(good => (
+      <li key={good.id} style={{ color: good.color }}>{good.name}</li>
+    ))}
+  </ul>
 );
 
 GoodList.propTypes = {
   goods: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
+    color: PropTypes.string,
   })).isRequired,
 };
