15b3647d8812f1c8664eee5f93690f329a775ebb
Yurii Lisovenko <yriyliso@gmail.com>
2021-06-01 11:52:59 +0300

    created dynamic-goods

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
 import React from 'react';
-
 import './App.scss';
+import { GoodList } from './GoodList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+    visibleGoods: false,
+  }
+
+  render() {
+    const { goods, visibleGoods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          onClick={() => getAll()
+            .then((goodsFromServer) => {
+              this.setState({
+                goods: goodsFromServer,
+                visibleGoods: true,
+              });
+            })}
+        >
+          Load All goods
+        </button>
+
+        <button
+          type="button"
+          onClick={() => getAll()
+            .then((goodsFromServer) => {
+              this.setState({
+                goods: goodsFromServer
+                  .sort((a, b) => a.name.localeCompare(b.name))
+                  .slice(0, 5),
+                visibleGoods: true,
+              });
+            })
+          }
+        >
+          Load 5 goods
+        </button>
+
+        <button
+          type="button"
+          onClick={() => getAll()
+            .then((goodsFromServer) => {
+              this.setState({
+                goods: goodsFromServer
+                  .filter(good => good.color === 'red'),
+                visibleGoods: true,
+              });
+            })
+          }
+        >
+          Load red goods
+        </button>
+
+        {visibleGoods && (
+          <GoodList
+            goods={goods}
+          />
+        )
+        }
+      </>
+    );
+  }
+}
 
 export default App;
