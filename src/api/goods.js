e48b6956fd15f34625cb41f6344e23622662cd58
Igor Lisogor <illisogori@gmail.com>
2021-06-01 20:26:06 +0300

    sort added

--- src/api/goods.js
+++ src/api/goods.js
@@ -6,7 +6,12 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => getAll().then(response => response.splice(0, 5));
+export const get5First = () => getAll().then(
+  response => response.splice(0, 5)
+    .sort((
+      firstGood, secondGood,
+    ) => firstGood.name.localeCompare(secondGood.name)),
+);
 
 export const getRedGoods = () => getAll()
   .then(response => response.filter(good => good.color === 'red'));
--- src/components/GoodList.jsx
+++ src/components/GoodList.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const GoodList = ({ goods }) => (
   goods.map(good => (
     <li key={good.id} style={{ color: good.color }}>{good.name}</li>
   ))
 );
+
+GoodList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
+};
