c193b41fe79330d62f0b5ec57e2f8f6740ad79d7
Vasyl Habalevych <vasylhab@gmail.com>
2021-06-02 22:36:35 +0200

    react_dynamic_list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import React from 'react';
 
 import './App.scss';
+import { GoodsList } from './components';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  handleGetGoods = () => (
+    getAll()
+      .then((goods) => {
+        this.setState({ goods });
+      })
+  );
+
+  handleFirst5 = () => (
+    get5First()
+      .then((goods) => {
+        this.setState({ goods });
+      })
+  )
+
+  handleRedGoods = () => (
+    getRedGoods()
+      .then((goods) => {
+        this.setState({ goods });
+      })
+  )
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={this.handleGetGoods}
+        >
+          Load All goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.handleFirst5}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.handleRedGoods}
+        >
+          Load red goods
+        </button>
+        <GoodsList goods={goods} />
+      </>
+    );
+  }
+}
 
 export default App;
