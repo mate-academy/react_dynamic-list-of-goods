85ad462932aa4423723101edf0460002d0e1eb8f
Dmitry Pukhliakov <d.puhliakov@gmail.com>
2021-06-02 12:15:35 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,61 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
+import GoodsList from './components/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  getAllGoods = async() => {
+    const prepearedGoods = await getAll();
+
+    this.setState({ goods: prepearedGoods });
+  }
+
+  get5FirstGoods = async() => {
+    const prepearedGoods = await get5First();
+
+    this.setState({ goods: prepearedGoods });
+  }
+
+  getRedGoods = async() => {
+    const prepearedGoods = await getRed();
+
+    this.setState({ goods: prepearedGoods });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Dynamic list of Goods</h1>
+        <GoodsList goods={this.state.goods} />
+
+        <button
+          type="button"
+          onClick={this.getAllGoods}
+        >
+          Display all goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.get5FirstGoods}
+        >
+          Display 5 firs goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.getRedGoods}
+        >
+          Display all red goods
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
