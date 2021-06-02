1a328605f219ddb23b7a42d52776979bbd62dc1d
Minchuk Olga <olga@MacBook-Pro-Olga.local>
2021-06-02 05:22:24 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
-
+import { GoodsList } from './GoodsList';
 import './App.scss';
+import { getAll, getRedGoods, get5First } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+class App extends React.Component {
+  state = {
+    goods: [],
+  };
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  showAllHandler = (callback) => {
+    callback()
+      .then(goods => this.setState({ goods }));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          onClick={() => this.showAllHandler(getAll)}
+        >
+          show all
+        </button>
+        <button
+          type="button"
+          onClick={() => this.showAllHandler(get5First)}
+        >
+          show 5 first
+        </button>
+        <button
+          type="button"
+          onClick={() => this.showAllHandler(getRedGoods)}
+        >
+          show only red
+        </button>
+        {this.state.goods.length > 0 && (
+          <div className="wrapper">
+            <h2>Table</h2>
+            <GoodsList goods={this.state.goods} />
+          </div>
+        )}
+      </>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -1,11 +1,21 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll() {
-  return fetch(API_URL)
-    .then(response => response.json());
-}
+export const getAll = () => (
+  fetch(API_URL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
 
-export const get5First = () => {};
+      return response.json();
+    })
+);
 
-export const getRedGoods = () => {};
+export const get5First = () => getAll()
+  .then(goods => goods
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .splice(0, 5));
+
+export const getRedGoods = () => getAll()
+  .then(goods => goods.filter(good => good.color === 'red'));
