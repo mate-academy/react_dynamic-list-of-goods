b079ea8193f0f256ade74e6613e73d81a1b5c3a9
Oleh Senkiv <o.senkiv.v@gmail.com>
2021-06-02 18:42:06 +0300

    Solution-001

--- src/App.js
+++ src/App.js
@@ -2,12 +2,57 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoogsList/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state ={
+    goods: [],
+  }
+
+  listOfGoods = (functionName) => {
+    functionName.then(goods => (
+      this.setState({ goods })
+    ));
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          className="button"
+          type="button"
+          onClick={() => {
+            this.listOfGoods(getAll());
+          }}
+        >
+          Load All goods
+        </button>
+        <button
+          className="button"
+          type="button"
+          onClick={() => {
+            this.listOfGoods(get5First());
+          }}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          className="button"
+          type="button"
+          onClick={() => {
+            this.listOfGoods(getRedGoods());
+          }}
+        >
+          Load red goods
+        </button>
+        <GoodsList goods={goods} />
+      </>
+    );
+  }
+}
 
 export default App;
