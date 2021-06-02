910329561325180ca8b42c7fcfb137d687e0410f
Taras Konovalov <taras.konovalov2014@gmail.com>
2021-06-02 15:03:37 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React from 'react';
-
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { GoodsList } from './Components/GoodsList/GoodsList';
+import { getAll, get5First, getRed } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  loadData = (request) => {
+    request()
+      .then(data => this.setState({ goods: [...data] }));
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <GoodsList goods={goods} />
+        <button
+          type="button"
+          onClick={() => this.loadData(getAll)}
+        >
+          Load All goods
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.loadData(get5First)}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.loadData(getRed)}
+        >
+          Load red goods
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -6,6 +6,16 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => (
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(result => result
+      .sort((prev, next) => prev.name.localeCompare(next.name))
+      .slice(0, 5))
+);
 
-export const getRedGoods = () => {};
+export const getRed = () => (
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(result => result.filter(good => good.color === 'red'))
+);
