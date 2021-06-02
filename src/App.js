8081f00d1269806e16cdd9a961394435709c465e
Georgy Dzumenko <erterj05@gmail.com>
2021-06-03 00:10:11 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,60 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+import { GoodsList } from './components/GoodsList';
+
+class App extends React.Component {
+  state = {
+    allGoods: [],
+    fiveFirstGoods: [],
+    redGoods: [],
+    chosenGoods: [],
+  }
+
+  async componentDidMount() {
+    const allGoods = await getAll();
+    const fiveFirstGoods = await get5First();
+    const redGoods = await getRedGoods();
+
+    this.setState({
+      allGoods, fiveFirstGoods, redGoods,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={() => this.setState(state => (
+            { chosenGoods: state.allGoods }
+          ))}
+        >
+          all goods
+        </button>
+        <button
+          type="button"
+          onClick={() => this.setState(state => (
+            { chosenGoods: state.fiveFirstGoods }
+          ))}
+        >
+          five first goods
+        </button>
+        <button
+          type="button"
+          onClick={() => this.setState(state => (
+            { chosenGoods: state.redGoods }
+          ))}
+        >
+          all red goods
+        </button>
+        <GoodsList goods={this.state.chosenGoods} />
+      </>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -6,6 +6,14 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => fetch(API_URL)
+  .then(response => response.json())
+  .then(response => response.sort(
+    (a, b) => a.name.localeCompare(b.name),
+  ).slice(0, 5));
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => fetch(API_URL)
+  .then(response => response.json())
+  .then(response => response.filter(
+    product => product.color === 'red',
+  ));
