474dd3c71091498e40f2ea8d627f6ebb60a3cd47
Grygoriy Shytikov <gregorysgm@gmail.com>
2021-06-02 15:50:35 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,93 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import PropTypes from 'prop-types';
+import { getAll, getFiveFirst, getRed, getBlue, getGreen } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  showGoods = (callback) => {
+    callback()
+      .then(goods => this.setState({ goods }));
+  }
+
+  render() {
+    return (
+      <div>
+
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          onClick={() => this.showGoods(getAll)}
+        >
+          Load All goods
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.showGoods(getFiveFirst)}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          style={{ color: 'red' }}
+          type="button"
+          onClick={() => this.showGoods(getRed)}
+        >
+          Load red goods
+        </button>
+
+        <button
+          style={{ color: 'blue' }}
+          type="button"
+          onClick={() => this.showGoods(getBlue)}
+        >
+          Load blue goods
+        </button>
+
+        <button
+          style={{ color: 'green' }}
+          type="button"
+          onClick={() => this.showGoods(getGreen)}
+        >
+          Load green goods
+        </button>
+        {this.state.goods.length > 0 && <GoodsList goods={this.state.goods} />}
+      </div>
+    );
+  }
+}
+
+const GoodsList = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li
+        key={good.id}
+        style={{ color: `${good.color}` }}
+      >
+        {good.name}
+      </li>
+    ))}
+  </ul>
 );
+
+const goodsType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+});
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(goodsType),
+};
+
+GoodsList.defaultProps = {
+  goods: [],
+};
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -3,9 +3,25 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll() {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    });
 }
 
-export const get5First = () => {};
+export const getFiveFirst = () => getAll()
+  .then(goods => goods
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .slice(0, 5));
 
-export const getRedGoods = () => {};
+export const getRed = () => getAll()
+  .then(goods => goods.filter(good => good.color === 'red'));
+
+export const getBlue = () => getAll()
+  .then(goods => goods.filter(good => good.color === 'blue'));
+
+export const getGreen = () => getAll()
+  .then(goods => goods.filter(good => good.color === 'green'));
