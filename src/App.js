0b23a2d0d97599f89b3f40fc900c4ca7e05989ac
Serhii Kolesnyk <kolesniks203@gmail.com>
2021-06-02 10:14:32 +0300

    dynamic list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React from 'react';
 
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './Components/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  responseHandler = (request) => {
+    request()
+      .then(data => this.setState({ goods: [...data] }));
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+
+        <div>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => {
+              this.responseHandler(getAll);
+            }}
+          >
+            Load all goods
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => {
+              this.responseHandler(get5First);
+            }}
+          >
+            Load five first goods
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => {
+              this.responseHandler(getRedGoods);
+            }}
+          >
+            Load all red goods
+          </button>
+        </div>
+
+        <GoodsList goods={goods} />
+      </>
+    );
+  }
+}
 
 export default App;
