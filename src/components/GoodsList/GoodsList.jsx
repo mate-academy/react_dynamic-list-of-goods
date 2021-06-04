6d87c31fddabd471ae83af52e7e6656eed23890c
herkoss <dmytroherko@icloud.com>
2021-06-04 14:01:45 +0300

    Buttons fixed

--- src/components/GoodsList/GoodsList.jsx
+++ src/components/GoodsList/GoodsList.jsx
@@ -1,73 +1,68 @@
 import React from 'react';
 import './GoodsList.scss';
 
+import { getAll, get5First, getRedGoods } from '../../api/goods';
+
 class GoodsList extends React.Component {
   state = {
-    products: this.props.allGoods,
-    isVisible: false,
+    goods: null,
   };
 
-  showAllGoods = () => {
-    this.setState({
-      products: this.props.allGoods,
-      isVisible: true,
-    });
+  getAllGoods = () => {
+    getAll()
+      .then(goods => this.setState({ goods }));
   }
 
-  show5FirstOnly = () => {
-    this.setState({
-      products: this.props.fiveFirstGoods,
-      isVisible: true,
-    });
+  get5FirstOnly = () => {
+    get5First()
+      .then(goods => this.setState({ goods }));
   }
 
-  showRedOnly = () => {
-    this.setState({
-      products: this.props.onlyRedGoods,
-      isVisible: true,
-    });
+  getRedOnly = () => {
+    getRedGoods()
+      .then(goods => this.setState({ goods }));
   }
 
   render() {
     return (
-      <div>
+        <div>
         <button
           type="button"
-          onClick={this.showAllGoods}
+          onClick={this.getAllGoods}
         >
           Load All goods
         </button>
         <button
           type="button"
-          onClick={this.show5FirstOnly}
+          onClick={this.get5FirstOnly}
         >
           Load 5 first goods
         </button>
         <button
           type="button"
-          onClick={this.showRedOnly}
+          onClick={this.getRedOnly}
         >
           Load red goods
         </button>
-        {this.state.isVisible && (
-        <ul>
-          {this.state.products.map(
-            product => (
-              <li
-                key={product.id}
-                className={product.color}
-              >
-                {product.name}
-                {' '}
-                {product.color}
-              </li>
-            )
-          )}
-        </ul>
+        {this.state.goods && (
+          <ul>
+            {this.state.goods.map(
+              good => (
+                <li
+                  key={good.id}
+                  className={good.color}
+                >
+                  {good.name}
+                  {' '}
+                  {good.color}
+                </li>
+              )
+            )}
+          </ul>
         )}
       </div>
     );
-  };
+  }
 }
 
 export default GoodsList;
