301df75ddf637916fe7dc58653767e213092ed12
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-07-12 14:59:00 +0300

    Solution

--- src/api/goods.js
+++ src/api/goods.js
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-body-style */
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
@@ -6,6 +7,18 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => {
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(goods => goods
+      .sort((a, b) => {
+        return a.name > b.name ? 1 : -1;
+      })
+      .slice(0, 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(goods => goods.filter(good => good.color === 'red'));
+};
