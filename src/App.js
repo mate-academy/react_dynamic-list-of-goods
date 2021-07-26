d372628929a292e6ac0c0d8df360c87fabd7116b
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-26 22:18:27 +0300

    feat: completed task dynemic-list-of-goods

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { GoodsList } from './components/GoodsList';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends Component {
+  state = {
+    goods: [],
+  }
+
+  goodsFromServer = (value) => {
+    value().then((goods) => {
+      this.setState({ goods });
+    });
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={() => {
+            this.goodsFromServer(getAll);
+          }}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.goodsFromServer(get5First);
+          }}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.goodsFromServer(getRedGoods);
+          }}
+        >
+          Load red goods
+        </button>
+        {goods.length > 0 && (
+          <GoodsList goods={goods} />
+        )}
+      </>
+    );
+  }
+}
 
 export default App;
