5dee54541e376b2eb2ea5ff4f86bf853cfff55aa
Viktor Dzhyranov <v.dzhyranov@gmail.com>
2021-07-26 15:22:58 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import GoodsList from './component/GoodsList/GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+class App extends React.Component {
+  state = {
+    goods: [],
+    isGoodsLoaded: false,
+  }
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  fetchData = (event, getFunc) => {
+    event.preventDefault();
 
+    getFunc()
+      .then(goods => this.setState({
+        goods,
+        isGoodsLoaded: true,
+      }));
+  }
+
+  render() {
+    const { goods, isGoodsLoaded } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          onClick={(event => this.fetchData(event, getAll))}
+        >
+          Get all goods
+        </button>
+        <button
+          type="button"
+          onClick={(event => this.fetchData(event, get5First))}
+        >
+          Get 5 goods
+        </button>
+        <button
+          type="button"
+          onClick={(event => this.fetchData(event, getRedGoods))}
+        >
+          Get red goods
+        </button>
+
+        {isGoodsLoaded
+          ? <GoodsList goods={goods} />
+          : 'Please press button'}
+      </>
+    );
+  }
+}
 export default App;
