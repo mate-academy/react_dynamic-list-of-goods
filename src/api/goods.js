5af439b6576bff1fb2270ca9e6a6f9e5984f5b4c
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-27 12:59:18 +0300

    fix: add catch

--- src/api/goods.js
+++ src/api/goods.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.jsn`;
+const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll() {
   return fetch(API_URL)
     .then(response => response.json())
-    .catch(new Error());
+    // eslint-disable-next-line
+    .catch(alert('Error: 404'));
 }
 
 export const get5First = () => (
