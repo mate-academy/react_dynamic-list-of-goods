f54fedbc3bdd9f24c7a0dc81dc6cae4cddd7305d
Tolik Bilokrylov <enkorn1715@gmail.com>
2021-08-01 17:24:25 +0300

    done

--- src/api/goods.js
+++ src/api/goods.js
@@ -6,6 +6,15 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = async() => {
+  const goods = await getAll();
+  const firsFiveGoods = [...goods]
+    .sort((firsGood, secondGood) => firsGood.name
+      .localeCompare(secondGood.name))
+    .slice(0, 5);
 
-export const getRedGoods = () => {};
+  return firsFiveGoods;
+};
+
+export const getRedGoods = async() => getAll()
+  .then(goods => goods.filter(good => good.color === 'red'));
