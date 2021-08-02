9b3d91d7bae54a7ab815ff81828607a3d2f94f15
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-08-02 23:58:41 +0300

    2.5 hours of theory for this task

--- src/App.js
+++ src/App.js
@@ -2,12 +2,53 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.PureComponent {
+  state = {
+    goods: [],
+  }
+
+  setGoods = (receiveGoods) => {
+    receiveGoods()
+      .then(goods => this.setState({ goods }));
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <GoodsList goods={goods} />
+        <button
+          type="submit"
+          onClick={() => {
+            this.setGoods(getAll);
+          }}
+        >
+          Load All goods
+        </button>
+        <button
+          type="submit"
+          onClick={() => {
+            this.setGoods(get5First);
+          }}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="submit"
+          onClick={() => {
+            this.setGoods(getRedGoods);
+          }}
+        >
+          Load red goods
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;
