8937cf2b5461e1ffa765c65f6ff9a0ac628c5738
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-08-02 09:27:34 +0300

    dynamic list react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
 
 import './App.scss';
+import 'bulma';
+import { GoodList } from './components/GoodList';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+class App extends React.Component {
+  state = {
+    goods: [],
+  }
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  goodsList = (func) => {
+    func().then((goods) => {
+      this.setState({ goods });
+    });
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Dynamic list of Goods</h1>
+        <div className="App__button">
+          <button
+            type="button"
+            className="button is-primary"
+            onClick={() => this.goodsList(getAll)}
+          >
+            Load ALL goods
+          </button>
+          <button
+            type="button"
+            className="button is-link"
+            onClick={() => this.goodsList(get5First)}
+          >
+            Load 5 first goods
+          </button>
+          <button
+            type="button"
+            className="button is-danger"
+            onClick={() => this.goodsList(getRedGoods)}
+          >
+            Load red goods
+          </button>
+        </div>
+        <GoodList goods={goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
