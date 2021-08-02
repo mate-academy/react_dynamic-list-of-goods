f9039ea93b8f0625297a87a040bf1585269d2419
Artem <temaqwerty15@gmail.com>
2021-08-02 19:43:11 +0300

    fin

--- src/App.js
+++ src/App.js
@@ -2,12 +2,53 @@ import React from 'react';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Button } from './Button';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  };
+
+  getGoods = () => {
+    getAll()
+      .then(goods => this.setState({ goods }));
+  }
+
+  onlyFiveGoods = () => {
+    get5First()
+      .then(goods => this.setState({ goods }));
+  }
+
+  onlyRedGoods = () => {
+    getRedGoods()
+      .then(goods => this.setState({ goods }));
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods </h1>
+        {!!goods.length && (
+          <ul>
+            {goods.map(good => (
+              <li key={good.id} style={{ color: good.color }}>
+                {good.name}
+              </li>
+            ))}
+          </ul>
+        )}
+        <Button name="Load All goods" onClick={this.getGoods} />
+        <Button name="Load 5 first goods" onClick={this.onlyFiveGoods} />
+        <Button name="Load red goods" onClick={this.onlyRedGoods} />
+
+      </>
+    );
+  }
+}
 
 export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Button = ({ name, onClick }) => (
+  <button type="button" onClick={onClick}>
+    {name}
+  </button>
+);
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
