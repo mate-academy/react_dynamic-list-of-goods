4f681e7caa421471aec7dd787df554bfb4c2d81e
Sergei <serheifil@gmail.com>
2021-08-02 18:51:50 +0300

    it works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { GoodsList } from './components/GoodsList';
 import './App.scss';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+class App extends Component {
+  state = {
+    goods: [],
+    goodsListIsDisplaying: false,
+  }
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  setGoodsFromServer = promise => promise.then(goods => this.setState({
+    goods,
+    goodsListIsDisplaying: true,
+  }));
+
+  render() {
+    const { goodsListIsDisplaying, goods } = this.state;
+    const btnClasses = 'btn btn-outline-info';
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="submit"
+          className={btnClasses}
+          onClick={() => this.setGoodsFromServer(getAll())}
+        >
+          Load all goods
+        </button>
+
+        <button
+          type="submit"
+          className={btnClasses}
+          onClick={() => this.setGoodsFromServer(get5First())}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="submit"
+          className={btnClasses}
+          onClick={() => this.setGoodsFromServer(getRedGoods())}
+        >
+          Load red goods
+        </button>
+
+        {goodsListIsDisplaying && <GoodsList goods={goods} />}
+      </>
+    );
+  }
+}
 
 export default App;
