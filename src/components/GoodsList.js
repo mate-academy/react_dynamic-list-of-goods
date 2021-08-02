ecd2af1d9395c72be3be6d56b175ba0d9d7a2947
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-08-02 17:19:44 +0300

    solution

---
+++ src/components/GoodsList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const GoodsList = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li
+        key={good.id}
+        style={{ color: good.color }}
+      >
+        {good.name}
+      </li>
+    ))}
+  </ul>
+);
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+  })).isRequired,
+};
