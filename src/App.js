f414f13679f04bdaf6ba49f81fabb16fa183bdac
Mykola shcherbak <hohotun21121987@gmail.com>
2021-08-02 15:35:13 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button } from './components/Button';
+import { GoodsList } from './components/GoodsList';
 
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends Component {
+  state = {
+    goods: [],
+  }
+
+  getAllAwait = async() => {
+    const goods = await getAll();
+
+    this.setState({ goods });
+  }
+
+  getRedAwait = async() => {
+    const goods = await getRedGoods();
+
+    this.setState({ goods });
+  }
+
+  get5FirstAwait = async() => {
+    const goods = await get5First();
+
+    this.setState({ goods });
+  }
+
+  render() {
+    const { goods } = this.state;
+    const { get5FirstAwait, getAllAwait, getRedAwait } = this;
+
+    return (
+      <div className="App">
+        <h1>Dynamic list of Goods</h1>
+        <div className="buttons">
+          <Button
+            text="All"
+            action={getAllAwait}
+          />
+          <Button
+            text="First 5"
+            action={get5FirstAwait}
+          />
+          <Button
+            text="Red"
+            action={getRedAwait}
+          />
+        </div>
+        <GoodsList goods={goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
