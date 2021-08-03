a05989295053bbd70fec0aa110e568e1de3681d2
Leonid <manu31051996@gmail.com>
2021-08-03 15:08:47 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-
+import { GoodsList } from './components/GoodsList/GoodsList';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+  };
+
+  setGoods = (getGoods) => {
+    getGoods().then((goods) => {
+      this.setState({
+        goods,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <div>
+          <button
+            type="button"
+            onClick={() => this.setGoods(getAll)}
+          >
+            Load All goods
+          </button>
+          <button
+            type="button"
+            onClick={() => this.setGoods(get5First)}
+          >
+            Load 5 first goods
+          </button>
+          <button
+            type="button"
+            onClick={() => this.setGoods(getRed)}
+          >
+            Load red goods
+          </button>
+        </div>
+        <GoodsList goods={this.state.goods} />
+      </>
+
+    );
+  }
+}
 
 export default App;
--- src/api/goods.js
+++ src/api/goods.js
@@ -6,6 +6,10 @@ export function getAll() {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => getAll()
+  .then(goods => goods
+    .sort((prevGood, nextGood) => prevGood.name.localeCompare(nextGood.name))
+    .slice(0, 5));
 
-export const getRedGoods = () => {};
+export const getRed = () => getAll()
+  .then(goods => goods.filter(good => good.color === 'red'));
