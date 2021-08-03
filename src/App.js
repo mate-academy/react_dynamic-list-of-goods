5641de0476214e6bd649e3ea6bf5fe95e72f6a69
Sergiy Fartushniak <fartux97@gmail.com>
2021-08-03 12:10:27 +0300

    change the code in places where it is repeated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 import { Button } from './components/Button';
 
 import { getAll, get5First, getRed } from './api/goods';
-import { GoodsList } from './components/GoodsList/GoodsList';
+import { GoodsList } from './components/GoodsList';
 
 export class App extends React.Component {
   state = {
@@ -12,20 +12,8 @@ export class App extends React.Component {
 
   }
 
-  allGoods = async() => {
-    const goods = await getAll();
-
-    this.setState({ goods });
-  }
-
-  fiveFirstGoods = async() => {
-    const goods = await get5First();
-
-    this.setState({ goods });
-  }
-
-  getRedGoods = async() => {
-    const goods = await getRed();
+  getGoodsFromServer = async(goodsFromServer) => {
+    const goods = await goodsFromServer();
 
     this.setState({ goods });
   }
@@ -34,9 +22,18 @@ export class App extends React.Component {
     return (
       <>
         <h1>Dynamic list of Goods</h1>
-        <Button onClick={this.allGoods} text="Load All goods" />
-        <Button onClick={this.fiveFirstGoods} text="Load 5 first goods" />
-        <Button onClick={this.getRedGoods} text="Load red goods" />
+        <Button
+          onClick={() => this.getGoodsFromServer(getAll)}
+          text="Load All goods"
+        />
+        <Button
+          onClick={() => this.getGoodsFromServer(get5First)}
+          text="Load 5 first goods"
+        />
+        <Button
+          onClick={() => this.getGoodsFromServer(getRed)}
+          text="Load red goods"
+        />
         <GoodsList goods={this.state.goods} />
       </>
     );
