d485840fbed7e3b418febbdc4c0d158cbd662d07
Kandiak Oleg <kandio1604@gmail.com>
2021-08-03 14:53:49 +0300

    implement loading goods

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { GoodsList } from './GoodsList/GoodsList';
 import './App.scss';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+class App extends Component {
+  state = {
+    goods: [],
+  }
 
-const App = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  setGoods = async(getGoods) => {
+    const goods = await getGoods();
+
+    this.setState({ goods });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={() => this.setGoods(getAll)}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={() => this.setGoods(get5First)}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={() => this.setGoods(getRedGoods)}
+        >
+          Load All goods
+        </button>
+        <GoodsList goods={this.state.goods} />
+      </>
+    );
+  }
+}
 
 export default App;
