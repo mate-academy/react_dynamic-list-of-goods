4d2c1c0aaeab43c60da7993c5db5207b4f147a7a
lera-yaskevych <redrum.back@gmail.com>
2021-09-10 14:22:01 +0300

    solved but catch is still to be added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,82 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
+import { Button } from './components/Button';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+interface State {
+  goodsFromServer: Good[],
+}
+
+enum RequestType {
+  all = 'all',
+  fiveFirst = 'fiveFirst',
+  red = 'red',
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    goodsFromServer: [],
+  };
+
+  request = async (type: RequestType) => {
+    let goods = null;
+
+    switch (type) {
+      case 'all':
+        goods = await getAll()
+          .then(goodsFromServer => this.setState({ goodsFromServer }));
+
+        return goods;
+      case 'fiveFirst':
+        goods = await get5First()
+          .then(goodsFromServer => this.setState({ goodsFromServer }));
+
+        return goods;
+      case 'red':
+        goods = await getRedGoods()
+          .then(goodsFromServer => this.setState({ goodsFromServer }));
+
+        return goods;
+      default:
+        return null;
+    }
+  };
+
+  render() {
+    const { goodsFromServer } = this.state;
+
+    return (
+      <div className="container">
+        <div className="App">
+          {goodsFromServer.length > 0 && (
+            <GoodsList goods={goodsFromServer} />
+          )}
+
+          <div className="App__buttons">
+            <Button
+              text="Load all goods"
+              action={() => this.request('all' as RequestType.all)}
+              className="App__button"
+            />
+
+            <Button
+              text="Load 5 first goods"
+              action={() => this.request('fiveFirst' as RequestType.fiveFirst)}
+              className="App__button"
+            />
+
+            <Button
+              text="Load red goods"
+              action={() => this.request('red' as RequestType.red)}
+              className="App__button"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,24 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export const getAll = () => {
   return fetch(API_URL)
     .then(response => response.json());
-}
+};
 
-export const get5First = () => {};
+export const get5First = () => {
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(goods => (goods
+      .sort((good1: Good, good2: Good) => (
+        good1.name.localeCompare(good2.name)
+      ))
+      .slice(0, 5)));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(goods => (goods
+      .filter((good: Good) => good.color === 'red')));
+};
