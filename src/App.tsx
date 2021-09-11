d82f660375a3991e58a2d80b2eca8b83c2248567
lera-yaskevych <redrum.back@gmail.com>
2021-09-11 11:44:21 +0300

    simplified the code

--- src/App.tsx
+++ src/App.tsx
@@ -6,81 +6,41 @@ import { GoodsList } from './components/GoodsList';
 import { Button } from './components/Button';
 
 interface State {
-  goodsFromServer: Good[];
+  goods: Good[];
   hasRequestError: boolean;
-}
-
-enum RequestType {
-  all = 'all',
-  fiveFirst = 'fiveFirst',
-  red = 'red',
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
-    goodsFromServer: [],
+    goods: [],
     hasRequestError: false,
   };
 
-  request = async (type: RequestType) => {
-    let goods = null;
+  makeRequest = async (query: () => Promise<Good[]>) => {
+    let requestResult = null;
 
-    switch (type) {
-      case 'all':
-        try {
-          goods = await getAll()
-            .then(goodsFromServer => this.setState({ goodsFromServer }));
+    try {
+      requestResult = await query()
+        .then(goods => this.setState({ goods }));
 
-          return goods;
-        } catch (error) {
-          this.setState({
-            hasRequestError: true,
-          });
-
-          return null;
-        }
-
-      case 'fiveFirst':
-        try {
-          goods = await get5First()
-            .then(goodsFromServer => this.setState({ goodsFromServer }));
-
-          return goods;
-        } catch (error) {
-          this.setState({
-            hasRequestError: true,
-          });
-
-          return null;
-        }
-
-      case 'red':
-        try {
-          goods = await getRedGoods()
-            .then(goodsFromServer => this.setState({ goodsFromServer }));
-
-          return goods;
-        } catch (error) {
-          this.setState({
-            hasRequestError: true,
-          });
-
-          return null;
-        }
-
-      default:
-        return null;
+      return requestResult;
+    } catch (error) {
+      this.setState({
+        hasRequestError: true,
+      });
     }
+
+    return requestResult;
   };
 
   render() {
-    const { goodsFromServer, hasRequestError } = this.state;
+    const { goods, hasRequestError } = this.state;
 
     return (
       <div className="container">
         <div className="App">
-          {goodsFromServer.length > 0 && (
-            <GoodsList goods={goodsFromServer} />
+          {goods.length > 0 && (
+            <GoodsList goods={goods} />
           )}
 
           {hasRequestError && (
@@ -94,19 +54,19 @@ class App extends React.Component<{}, State> {
           <div className="App__buttons">
             <Button
               text="Load all goods"
-              action={() => this.request('all' as RequestType.all)}
+              action={() => this.makeRequest(getAll)}
               className="App__button"
             />
 
             <Button
               text="Load 5 first goods"
-              action={() => this.request('fiveFirst' as RequestType.fiveFirst)}
+              action={() => this.makeRequest(get5First)}
               className="App__button"
             />
 
             <Button
               text="Load red goods"
-              action={() => this.request('red' as RequestType.red)}
+              action={() => this.makeRequest(getRedGoods)}
               className="App__button"
             />
           </div>
