c8049c1c93e0fb15015b4c06c9086a871baa64f8
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-09-15 14:18:30 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,98 @@
 import React from 'react';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classNames from 'classnames';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+enum SortType {
+  default = '',
+  all = 'all',
+  five = 'five',
+  red = 'red',
+}
+
+interface State {
+  goods: Good[];
+  sortBy: SortType;
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+    sortBy: SortType.default,
+  };
+
+  showAll = () => {
+    getAll()
+      .then(goods => {
+        this.setState({
+          goods,
+          sortBy: SortType.all,
+        });
+      });
+  };
+
+  showFive = () => {
+    get5First()
+      .then(goods => {
+        this.setState({
+          goods,
+          sortBy: SortType.five,
+        });
+      });
+  };
+
+  showRed = () => {
+    getRedGoods()
+      .then(goods => {
+        this.setState({
+          goods,
+          sortBy: SortType.red,
+        });
+      });
+  };
+
+  render() {
+    const { goods, sortBy } = this.state;
+
+    return (
+      <>
+        <h1 className="text-center py-2">Dynamic list of Goods</h1>
+        { goods.length > 0 && (
+          <GoodsList
+            goods={goods}
+          />
+        )}
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            className={classNames('border bg-primary rounded text-light py-2 m-2', { 'bg-success': sortBy === SortType.all })}
+            type="submit"
+            onClick={this.showAll}
+          >
+            Load All goods
+          </button>
+
+          <button
+            className={classNames('border bg-primary rounded text-light py-2 m-2', { 'bg-success': sortBy === SortType.five })}
+            type="submit"
+            onClick={this.showFive}
+          >
+            Load 5 first goods
+          </button>
+
+          <button
+            className={classNames('border bg-primary rounded text-light py-2 m-2', { 'bg-success': sortBy === SortType.red })}
+            type="submit"
+            onClick={this.showRed}
+          >
+            Load red goods
+          </button>
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,6 +6,18 @@ export function getAll(): Promise<Good[]> {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => {
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(goods => {
+      return goods.sort((g1: Good, g2: Good) => g1.name.localeCompare(g2.name)).slice(0, 5);
+    });
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(goods => {
+      return goods.filter((good: Good) => good.color === 'red');
+    });
+};
