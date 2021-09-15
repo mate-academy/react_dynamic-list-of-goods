44a7fd6f3e727df8dcfafdcb8f696fec8d571152
Dmitriy Pleshakov <dmitriypleshakov1@gmail.com>
2021-09-15 17:16:43 +0300

    React_dynamic-list-of-goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,67 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+interface State {
+  goods: Good[];
+}
 
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+  };
+
+  showAllGoods = () => {
+    getAll()
+      .then(goods => {
+        this.setState({ goods });
+      });
+  };
+
+  show5FirstGoods = () => {
+    get5First()
+      .then(goods => {
+        this.setState({ goods });
+      });
+  };
+
+  showAllRedGoods = () => {
+    getRedGoods()
+      .then(goods => {
+        this.setState({ goods });
+      });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <GoodsList goods={goods} />
+        <button
+          type="submit"
+          onClick={this.showAllGoods}
+        >
+          Load All goods
+        </button>
+        <button
+          type="submit"
+          onClick={this.show5FirstGoods}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="submit"
+          onClick={this.showAllRedGoods}
+        >
+          Load red goods
+        </button>
+      </>
+    );
+  }
+}
 export default App;
