1825a82e427e7534f4effa0340feaead9473d9e9
Roman Fedusevych <romanfyks@gmail.com>
2021-09-15 18:08:41 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,71 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[];
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+  };
+
+  loadAllgoods = async () => {
+    const goods = await getAll();
+
+    this.setState({ goods });
+  };
+
+  load5First = async () => {
+    const goods = await get5First();
+
+    this.setState({ goods });
+  };
+
+  loadRedGoods = async () => {
+    const goods = await getRedGoods();
+
+    this.setState({ goods });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <div className="App">
+          <h1>Dynamic list of Goods</h1>
+          <div className="App__button">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.loadAllgoods}
+            >
+              Load All goods
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.load5First}
+            >
+              Load 5 first goods
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={this.loadRedGoods}
+            >
+              Load red goods
+            </button>
+          </div>
+          <GoodsList goods={goods} />
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
