5be0d9f30aeeabae198b3154f1e6701fcf23c320
Oleksandr Zhitenko <geetonio28@gmail.com>
2021-09-16 14:23:32 +0300

    task is done

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,20 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
-}
+export const getAll = async (): Promise<Good[]> => {
+  const response: Response = await fetch(API_URL);
 
-export const get5First = () => {};
+  return response.json();
+};
 
-export const getRedGoods = () => {};
+export const get5First = async (): Promise<Good[]> => {
+  const response = await getAll();
+
+  return response.sort().slice(0, 5);
+};
+
+export const getRed = async (): Promise<Good[]> => {
+  const response = await getAll();
+
+  return response.filter((item: Good) => item.color === 'red');
+};
