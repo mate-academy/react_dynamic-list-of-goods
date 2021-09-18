a4b3bb763ce761d5ced8cfd36104e214d4fd2686
zadorojniiarsen <zadorojniiarsen@gmail.com>
2021-09-18 21:15:37 +0300

    goodslist class component changed to functional one

--- src/components/GoodsList/GoodsList.tsx
+++ src/components/GoodsList/GoodsList.tsx
@@ -4,18 +4,12 @@ interface Props {
   goods: Good[];
 }
 
-export class GoodsList extends React.Component<Props> {
-  state = {};
-
-  render() {
-    return (
-      <ul>
-        {this.props.goods.map(item => (
-          <li key={item.id} style={{ color: item.color }}>
-            {item.name}
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+export const GoodsList: React.FC<Props> = ({ goods }) => (
+  <ul>
+    {goods.map(item => (
+      <li key={item.id} style={{ color: item.color }}>
+        {item.name}
+      </li>
+    ))}
+  </ul>
+);
