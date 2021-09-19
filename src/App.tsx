77901ff0df1f06fd6be0732854fa0871ec0f5b91
Clon <clonvhs@gmail.com>
2021-09-19 17:18:43 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,87 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goodsList: Good[] | null;
+};
+
+class App extends React.Component <{}, State> {
+  state = {
+    goodsList: null,
+  };
+
+  handleEvent = async (event:React.MouseEvent<HTMLButtonElement, MouseEvent>, eventType:string) => {
+    event.preventDefault();
+
+    switch (eventType) {
+      case ('showAll'):
+        this.setState({
+          goodsList: await getAll(),
+        });
+        break;
+      case ('showFiveFirst'):
+        this.setState({
+          goodsList: await get5First(),
+        });
+        break;
+      case ('showOnlyRed'):
+        this.setState({
+          goodsList: await getRedGoods(),
+        });
+        break;
+
+      default:
+        throw new Error('Wrong type of event');
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App__Tittle">Dynamic list of Goods</h1>
+        <div className="App__Button-wrapper">
+          <button
+            type="button"
+            onClick={
+              event => {
+                this.handleEvent(event, 'showAll');
+              }
+            }
+            className="button LoadAllButton"
+          >
+            Load all goods
+          </button>
+          <button
+            type="button"
+            onClick={
+              event => {
+                this.handleEvent(event, 'showFiveFirst');
+              }
+            }
+            className="button LoadFiveButton"
+          >
+            Load 5 first goods
+          </button>
+          <button
+            type="button"
+            onClick={
+              event => {
+                this.handleEvent(event, 'showOnlyRed');
+              }
+            }
+            className="button LoadFiveButton"
+          >
+            Load only red goods
+          </button>
+        </div>
+
+        {this.state.goodsList && <GoodsList goodsList={this.state.goodsList} />}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,22 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
-}
+export const getAll = async ():Promise<Good[]> => {
+  const response = await fetch(API_URL);
 
-export const get5First = () => {};
+  return response.json();
+};
 
-export const getRedGoods = () => {};
+export const get5First = async ():Promise<Good[]> => {
+  const response = await fetch(API_URL);
+  const result = await response.json();
+
+  return result.slice(0, 5);
+};
+
+export const getRedGoods = async ():Promise<Good[]> => {
+  const response = await fetch(API_URL);
+  const result = await response.json();
+
+  return result.filter((good:Good) => good.color === 'red');
+};
