d4c828e74f9a53e87539cb2ceff28ff8bdf0bfe7
Kate Kuzmenko <rina.punina@gmail.com>
2021-10-23 10:51:38 +0300

    solution without styles

---
+++ src/components/GoodsList.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type Props = {
+  goods: Good[];
+};
+
+export const GoodsList: React.FC<Props> = ({ goods }) => {
+  return (
+    <ul className="goods">
+      {goods.map(good => {
+        const { id, name, color } = good;
+
+        return (
+          <li
+            className="goods__item"
+            key={id}
+            style={{ color }}
+          >
+            {name}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
