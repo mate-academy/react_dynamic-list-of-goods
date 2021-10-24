eabc7c09cedbe3e1a07ecb23b9794fccd9795cc8
caelum29 <caelum29@gmail.com>
2021-10-24 17:20:50 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,69 @@
 import React from 'react';
 import './App.scss';
+import { GoodsList } from './components/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+interface State {
+  goods: Good[],
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+  };
+
+  showAll = async () => {
+    const goodsFromServer = await getAll();
+
+    this.setState({ goods: goodsFromServer });
+  };
+
+  show5First = async () => {
+    const goodsFromServer = await get5First();
+
+    this.setState({ goods: goodsFromServer });
+  };
+
+  showRed = async () => {
+    const goodsFromServer = await getRedGoods();
+
+    this.setState({ goods: goodsFromServer });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="max-w-lg mx-auto mt-8 text-gray-700 p-4">
+          <h1 className="text-center text-4xl font-bold mb-4">Dynamic list of Goods</h1>
+          <div className="flex justify-center gap-x-3 mb-4 gap-0">
+            <button
+              type="button"
+              onClick={this.showAll}
+              className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 shadow-md rounded-full"
+            >
+              Get All
+            </button>
+            <button
+              type="button"
+              className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 shadow-md rounded-full"
+              onClick={this.show5First}
+            >
+              Get 5 First
+            </button>
+            <button
+              type="button"
+              className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 shadow-md rounded-full"
+              onClick={this.showRed}
+            >
+              Get red
+            </button>
+          </div>
+          <GoodsList goods={this.state.goods} />
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,9 +3,39 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    });
 }
 
-export const get5First = () => {};
+export const get5First = (): Promise<Good[]> => {
+  return fetch(API_URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
 
-export const getRedGoods = () => {};
+      return response.json();
+    })
+    .then((response: Good[]) => {
+      return response.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
+    });
+};
+
+export const getRedGoods = (): Promise<Good[]> => {
+  return fetch(API_URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    })
+    .then((response: Good[]) => {
+      return response.filter(item => item.color === 'red');
+    });
+};
