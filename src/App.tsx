332bb5a7903f2c238f236e1b5b32b385e123f392
Artem Bolshak <fcdnipro20152705@gmail.com>
2021-10-25 00:54:19 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+interface State {
+  goods: Good[],
+}
 
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+  };
+
+  loadGoods = async (loader: () => Promise<Good[]>) => {
+    const good = await loader();
+
+    this.setState({ goods: good });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <div>
+          <button
+            type="button"
+            className="button"
+            onClick={() => this.loadGoods(getAll)}
+          >
+            Load all goods
+          </button>
+
+          <button
+            type="button"
+            className="button"
+            onClick={() => this.loadGoods(get5First)}
+          >
+            Load 5 first goods
+          </button>
+
+          <button
+            type="button"
+            className="button"
+            onClick={() => this.loadGoods(getRedGoods)}
+          >
+            Load red goods
+          </button>
+        </div>
+        <GoodsList goods={goods} />
+      </>
+    );
+  }
+}
 export default App;
