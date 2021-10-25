43442494ee2f0729aa24f796afb483f1efe64503
Liliia Kuleba <kuleba.lilia.1872003@gmail.com>
2021-10-25 14:42:29 +0300

    dynamic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import {ListGoods} from "./component/ListGoods/ListGoods";
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[];
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+  };
+
+  handleGoods = async (typeOfList: () => Promise<Good[]>) => {
+    const goods = await typeOfList();
+
+    this.setState({ goods });
+  };
+
+  render() {
+    const { handleGoods } = this;
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={() => handleGoods(getAll)}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={() => handleGoods(get5First)}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={() => handleGoods(getRedGoods)}
+        >
+          Load red goods
+        </button>
+        <ListGoods goods={goods} />
+      </>
+    );
+  }
+}
 
 export default App;
