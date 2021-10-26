14973538c557fa4920075ba9381d83016c8b0fdb
Anton Ishchenko <anton.ishchenko@nure.ua>
2021-10-26 07:30:09 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,67 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { get5First, getAll, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[];
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+  };
+
+  getNeededGoods = async (type: () => Promise<Good[]>) => {
+    const goods = await type();
+
+    this.setState({ goods });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={() => {
+            this.getNeededGoods(getAll);
+          }}
+        >
+          Show All
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.getNeededGoods(get5First);
+          }}
+        >
+          Show 5
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.getNeededGoods(getRedGoods);
+          }}
+        >
+          Show red
+        </button>
+        <ul>{goods
+          .map(good => 
+            <li 
+              key={good.id}
+              style={{ color: good.color }}
+            >
+              {good.name}
+            </li>)}
+          </ul>
+      </>
+    );
+  }
+}
 
 export default App;
