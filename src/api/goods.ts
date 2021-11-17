29e37d749545b01764f0a127ec2cbc274e87d763
MaxStets <weawer458@gmail.com>
2021-11-17 23:58:08 +0200

    Solution -- react_dynamic-list-of-goods

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,27 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+const request = () => {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} -- ${response.statusText}`);
+      }
+
+      return response.json();
+    });
+};
+
+export function getAll(): Promise<Good[]> {
+  return request();
 }
 
-export const get5First = () => {};
+export const get5First = () => {
+  return request()
+    .then(data => data.slice(0, 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return request()
+    .then(data => data.filter((el: Good) => el.color === 'red'));
+};
