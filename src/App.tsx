bdbb68761e75ab16b142bccb6bbc884ed871bfc3
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-17 16:16:54 +0200

    Task was solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,70 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[];
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [] as any[],
+  };
+
+  loadAllGoods = async () => {
+    const goods = await getAll();
+
+    this.setState({ goods });
+  };
+
+  load5FirstGoods = async () => {
+    const goods = await get5First();
+
+    this.setState({ goods });
+  };
+
+  loadRedGoods = async () => {
+    const goods = await getRedGoods();
+
+    this.setState({ goods });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="goods">
+        <h1 className="goods__title">Dynamic list of Goods</h1>
+        <form className="goods__form">
+          <button
+            type="button"
+            className="goods__button"
+            onClick={this.loadAllGoods}
+          >
+            Load all goods
+          </button>
+          <button
+            type="button"
+            className="goods__button"
+            onClick={this.load5FirstGoods}
+          >
+            Load 5 first goods
+          </button>
+          <button
+            type="button"
+            className="goods__button"
+            onClick={this.loadRedGoods}
+          >
+            Load red goods
+          </button>
+        </form>
+
+        {goods && <GoodsList preparedGoods={goods} />}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,9 +3,21 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    });
 }
 
-export const get5First = () => {};
+export const get5First = () => {
+  return getAll()
+    .then(goods => goods.slice(0, 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return getAll()
+    .then(goods => goods.filter(good => good.color === 'red'));
+};
