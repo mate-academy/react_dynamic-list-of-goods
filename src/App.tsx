b5336a24530c6409a37b167f9e33300ee72c6d11
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-11-18 21:18:44 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
 import React from 'react';
 import './App.scss';
+import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+export interface State {
+  goods: Good[],
+
+}
+
+export class App extends React.Component {
+  state = {
+    goods: [],
+  }
+
+  render() {
+    return (
+      <div className="container" >
+        Dynamic list of goods
+        <div>
+        <button
+        className="button"
+          type="button"
+          onClick={async () => {
+            const goods = await getAll();
+            this.setState({ goods: goods });
+          }}
+        >
+          show all
+        </button>
+
+        <button
+        className="button"
+          type="button"
+          onClick={async () => {
+            const goods = await get5First();
+            this.setState({ goods: goods });
+          }}
+        >
+          show first 5
+        </button>
+
+        <button
+        className="button"
+          type="button"
+          onClick={async () => {
+            const goods = await getRed();
+            this.setState({ goods: goods });
+          }}
+        >
+          show red ones
+        </button>
+        </div>
+        <GoodsList goods={this.state.goods}/>
+      </div>
+    )
+  }
+}
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
@@ -6,6 +5,16 @@ export function getAll(): Promise<Good[]> {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = async () => {
+  const goods = await getAll();
 
-export const getRedGoods = () => {};
+  return goods
+    .slice(0, 5)
+    .sort((a, b) => a.name.localeCompare(b.name));
+};
+
+export const getRed = async () => {
+  const goods = await getAll();
+
+  return goods.filter(item => item.color === 'red');
+};
