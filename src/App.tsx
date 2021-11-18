d4deddfe525abac19e522d931d5caa47c064f315
Darina Savytska <coldshine99@gmail.com>
2021-11-18 18:00:45 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,62 @@
 import React from 'react';
+import { GoodsList } from './components/GoodsList';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+interface Good {
+  id: number,
+  name: string,
+  color: string,
+}
+
+interface State {
+  goods: Good[],
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+  };
+
+  handleClick = async (callback: () => Promise<Good[]>) => {
+    const goods = await callback();
+
+    this.setState({ goods });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          onClick={() => this.handleClick(getAll)}
+        >
+          Load All goods
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.handleClick(get5First)}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.handleClick(getRedGoods)}
+        >
+          Load red goods
+        </button>
+
+        <GoodsList goods={this.state.goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/GoodsList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface Props {
+  goods: Good[],
+}
+
+export const GoodsList: React.FC<Props> = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li
+        style={{ color: good.color }}
+        key={good.id}
+      >
+        {good.name}
+      </li>
+    ))}
+  </ul>
+);
