23c11e355c4ab782cd8193c2ad5fa667d278882a
Alex <alex.krynytskyi@gmail.com>
2021-11-20 01:08:50 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+const App: React.FC = () => {
+  const [data, setData] = useState<Good[]>();
+  const getGoods = async (type: () => Promise<Good[]>) => {
+    const goods = await type();
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+    setData(goods);
+  };
+
+  return (
+    <div>
+      <h1>
+        Goods
+      </h1>
+      <button
+        type="button"
+        onClick={() => {
+          getGoods(getAll);
+        }}
+      >
+        Get all
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          getGoods(get5First);
+        }}
+      >
+        Get 5 first
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          getGoods(getRedGoods);
+        }}
+      >
+        Get red only
+      </button>
+      {data?.map(({ name, color, id }) => (
+        <h2 style={{ color }} key={id}>{name}</h2>
+      ))}
+    </div>
+  );
+};
 
 export default App;
