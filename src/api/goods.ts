953f68a01af4d36f06d314d79d8a2d9e791fb330
Lera Litfullina <litfullina@icloud.com>
2021-12-16 19:28:55 +0300

    dynamic list of goods

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,29 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+function wait(delay: number) {
+  return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+export async function getAll(): Promise<Good[]> {
+  await wait(2000);
+
   return fetch(API_URL)
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = async () => {
+  const [allGoods] = await Promise.all([
+    getAll(),
+  ]);
 
-export const getRedGoods = () => {};
+  return allGoods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
+};
+
+export const getRedGoods = async () => {
+  const [allGoods] = await Promise.all([
+    getAll(),
+  ]);
+
+  return allGoods.filter(good => good.color === 'red');
+};
---
+++ src/components/GoodsList/GoodsList.tsx
@@ -0,0 +1,16 @@
+// import classNames from 'classnames';
+import React from 'react';
+
+type Props = {
+  goods: Good[];
+};
+
+export const GoodsList:React.FC<Props> = ({ goods }) => {
+  return (
+    <ul>
+      {goods.map(good => (
+        <li key={good.id} style={{ color: good.color }}>{good.name}</li>
+      ))}
+    </ul>
+  );
+};
