3baab3e78df5419c8942048bca312a5d46a2007c
Daryna Dudar <keks9ra1286@gmail.com>
2021-12-20 21:14:19 +0200

    List of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,81 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+import { getAll, get5First, getRedGoods } from './api/goods';
+
+type State = {
+  goods: Good[] | [];
+  isLoading: boolean;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+    isLoading: false,
+  };
+
+  loadData = async (getGoods: () => Promise<Good[]>) => {
+    this.setState({
+      isLoading: true,
+    });
+
+    try {
+      const goods = await getGoods();
+
+      this.setState({
+        goods,
+        isLoading: false,
+      });
+    } catch {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  };
+
+  render() {
+    const { goods, isLoading } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App__content">
+          <h1 className="title">
+            Dynamic list of Goods
+          </h1>
+          <div className="App__buttons">
+            <button
+              type="button"
+              className="button"
+              onClick={() => this.loadData(getAll)}
+            >
+              Load All goods
+            </button>
+
+            <button
+              type="button"
+              className="button"
+              onClick={() => this.loadData(get5First)}
+            >
+              Load 5 first goods
+            </button>
+            <button
+              type="button"
+              className="button"
+              onClick={() => this.loadData(getRedGoods)}
+            >
+              Load red goods
+            </button>
+          </div>
+
+          <div className="App__goodsList">
+            {!isLoading && <GoodsList goods={goods} />}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
