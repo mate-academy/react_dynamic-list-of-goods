ad047536712a1c596530336ebc17e5d99047b518
Vladyslav Gonsales <www.magicbox@gmail.com>
2021-12-20 22:55:15 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { GoodsList } from './components/GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[] | [],
+  loadingGoods: boolean,
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+    loadingGoods: false,
+  };
+
+  loadingData = (method: { (): Promise<Good[]>; }) => {
+    method()
+      .then(goods => {
+        this.setState({ goods });
+      });
+  };
+
+  render() {
+    const { goods, loadingGoods } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App__container">
+          <h1 className="App__title">
+            Dynamic list of goods
+          </h1>
+          <div className="App__buttons">
+            <button
+              className="App__button"
+              type="button"
+              onClick={() => this.loadingData(getAll)}
+            >
+              Load all goods
+            </button>
+            <button
+              className="App__button"
+              type="button"
+              onClick={() => this.loadingData(get5First)}
+            >
+              Load 5 first goods
+            </button>
+            <button
+              className="App__button"
+              type="button"
+              onClick={() => this.loadingData(getRedGoods)}
+            >
+              Load red goods
+            </button>
+          </div>
+
+          <div className="App__goods">
+            {!loadingGoods && <GoodsList goods={goods} />}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
