70ca829da6e8dbcedfe7494412b80f511a0a5299
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-12-20 12:42:11 +0200

    solution was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.scss';
+import { GoodsList } from './components/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[],
+  listIsVisible: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+    listIsVisible: false,
+  };
+
+  loadGoodsFromServer = async (getServerData: () => Promise<Good[]>) => {
+    const goods = await getServerData();
+
+    this.setState({ goods, listIsVisible: true });
+  };
+
+  render() {
+    const { goods, listIsVisible } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button type="submit" onClick={() => this.loadGoodsFromServer(getAll)}>
+          Load All goods
+        </button>
+        <button type="submit" onClick={() => this.loadGoodsFromServer(get5First)}>
+          Load 5 first goods
+        </button>
+        <button type="submit" onClick={() => this.loadGoodsFromServer(getRedGoods)}>
+          Load red goods
+        </button>
+        {listIsVisible && <GoodsList goods={goods} />}
+      </>
+    );
+  }
+}
 
 export default App;
