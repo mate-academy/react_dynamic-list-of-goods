85b87cfd4cb6193533bb2d345c8fc67b3fb5547c
TatianaMorozova04 <morozovatatiana04@gmail.com>
2021-12-21 14:57:30 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,41 @@ import './App.scss';
 
 import { GoodsList } from './components/GoodsList';
 import { Good } from './types/Good';
-
 import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
 
 type State = {
   goods: Good [];
+  isLoading: boolean;
+  hasLoadingError: boolean;
 };
 
 class App extends React.Component<{}, State> {
   state = {
     goods: [],
+    isLoading: false,
+    hasLoadingError: false,
   };
 
-  setGoods = (data: Good[]) => this.setState({ goods: data });
+  loadGoods = async (promise: () => Promise<Good []>) => {
+    this.setState({ isLoading: true });
 
-  getAllGoods = () => getAll().then(this.setGoods);
+    try {
+      const goods = await promise();
 
-  get5FirstGoods = () => get5First().then(this.setGoods);
-
-  getRedGoods = () => getRed().then(this.setGoods);
+      this.setState({
+        isLoading: false,
+        goods,
+      });
+    } catch (error) {
+      this.setState({
+        isLoading: false,
+        hasLoadingError: true,
+      });
+    }
+  };
 
   render() {
-    const { goods } = this.state;
+    const { goods, isLoading, hasLoadingError } = this.state;
 
     return (
       <div className="App">
@@ -34,27 +45,29 @@ class App extends React.Component<{}, State> {
         <div className="App__buttons">
           <button
             type="button"
-            onClick={this.getAllGoods}
+            onClick={() => this.loadGoods(getAll)}
             className="App__button"
           >
             Load All goods
           </button>
           <button
             type="button"
-            onClick={this.get5FirstGoods}
+            onClick={() => this.loadGoods(get5First)}
             className="App__button"
           >
             Load 5 first goods
           </button>
           <button
             type="button"
-            onClick={this.getRedGoods}
+            onClick={() => this.loadGoods(getRed)}
             className="App__button"
           >
             Load red goods
           </button>
         </div>
-        {goods.length > 0 && <GoodsList goods={goods} />}
+        {isLoading && <span>Loading...</span>}
+        {hasLoadingError && (!isLoading && <span>Loading Error</span>)}
+        {goods.length > 0 && (!isLoading && <GoodsList goods={goods} />)}
       </div>
     );
   }
