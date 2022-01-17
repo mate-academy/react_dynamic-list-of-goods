192fffc7bed1411ff8c6f2d96248e586b5e3fdbc
Anton Kulchytskyi <antek1809@gmail.com>
2022-01-17 22:14:59 +0100

    react dynamic list of goods solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,74 @@
 import React from 'react';
+import { getAll, get5First, getRed } from './api/goods';
 import './App.scss';
+import { GoodsList } from './components/GoodsList/GoodsList';
+import { LoadingError } from './components/LoadingError/LoadingError';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+interface State {
+  goods: Good[],
+  goodsErrorLoading: boolean,
+}
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type Good = {
+  id: number,
+  name: string,
+  color: string,
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+    goodsErrorLoading: false,
+  };
+
+  loadFromServer = async (loadFun: () => Promise<Good[]>) => {
+    this.setState({ goodsErrorLoading: false });
+    try {
+      const goodsFromServer = await loadFun();
+
+      this.setState({ goods: goodsFromServer });
+    } catch (error) {
+      this.setState({ goodsErrorLoading: true });
+    }
+  };
+
+  allGoods = () => this.loadFromServer(getAll);
+
+  fiveAscAlphabeticSortGoods = () => this.loadFromServer(get5First);
+
+  redGoods = () => this.loadFromServer(getRed);
+
+  render() {
+    const { goods, goodsErrorLoading } = this.state;
+
+    return (
+      <div className="App">
+        <div className="buttons-container">
+          <button
+            className="button"
+            type="button"
+            onClick={this.allGoods}
+          >
+            Load all goods
+          </button>
+          <button
+            className="button"
+            type="button"
+            onClick={this.fiveAscAlphabeticSortGoods}
+          >
+            Load 5 goods
+          </button>
+          <button
+            className="button"
+            type="button"
+            onClick={this.redGoods}
+          >
+            Load red goods
+          </button>
+        </div>
+        {goodsErrorLoading && <LoadingError />}
+        {goods.length > 0 && <GoodsList goods={goods} />}
+      </div>
+    );
+  }
+}
---
+++ src/components/LoadingError/LoadingError.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './LoadingError.scss';
+
+export const LoadingError: React.FC = () => (
+  <p className="error">
+    Cannot load data from server
+  </p>
+);
