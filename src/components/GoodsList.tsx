256f9c0ee879e0ea33495a31ddfcbda4ef6dde73
Zahar Mychka <zikmychkas@gmail.com>
2022-01-21 01:11:13 +0200

    finished test task

--- src/components/GoodsList.tsx
+++ src/components/GoodsList.tsx
@@ -9,10 +9,11 @@ type Props = {
 export const GoodsList: React.FC<Props> = ({ goods }) => {
   return (
     <ul className="list mt-5">
-      {goods.map(({ name, color }) => (
+      {goods.map(({ name, color, id }) => (
         <li
           className="title is-5"
           style={{ color }}
+          key={id}
         >
           {name}
         </li>
