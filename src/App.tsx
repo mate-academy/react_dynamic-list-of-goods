0a3c15bbe0971f485e57d0b42fa571091709f47e
Olesya Parfylo <olesya.parfylo@gmail.com>
2022-01-20 18:08:14 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
-import './App.scss';
+import { GoodsList } from './components/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+export default class App extends React.Component {
+  state = {
+    goods: [],
+  };
 
-export default App;
+  loader = async (promise: Promise<Good[]>) => {
+    const goods = await promise
+      .then(result => result);
+
+    this.setState({
+      goods: goods.length !== 5 ? goods : (
+        goods.sort((a: Good, b: Good) => a.name.localeCompare(b.name))
+      ),
+    });
+  };
+
+  render() {
+    const { goods } = this.state;
+    const { loader } = this;
+
+    return (
+      <div>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={() => loader(getAll())}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          onClick={() => loader(get5First())}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          type="button"
+          onClick={() => loader(getRedGoods())}
+        >
+          Load red goods
+        </button>
+        {goods.length > 0 && (
+          <GoodsList
+            goods={goods}
+          />
+        )}
+      </div>
+    );
+  }
+}
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,6 +6,12 @@ export function getAll(): Promise<Good[]> {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = (): Promise<Good[]> => {
+  return getAll()
+    .then(details => details.slice(0, 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = (): Promise<Good[]> => {
+  return getAll()
+    .then(details => details.filter((good: Good) => good.color === 'red'));
+};
