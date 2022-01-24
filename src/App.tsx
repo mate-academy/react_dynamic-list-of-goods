5af59db2e1252f7560d12d2979866d76b10bf335
E-Sharpilo <evgeniy.sharpilo@gmail.com>
2022-01-24 14:54:42 +0200

    some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,74 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import GoodsList from './components/GoodList/GoodsList';
+import Error from './components/ErrorMessage/ErrorMessage';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[],
+  isLoading: boolean,
+  error: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+    isLoading: false,
+    error: false,
+  };
+
+  loadHandler = async (callback: () => Promise<Good[]>) => {
+    this.setState({
+      isLoading: true,
+    });
+
+    try {
+      this.setState({
+        goods: await callback(),
+      });
+    } catch {
+      this.setState({ error: true });
+    } finally {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  };
+
+  render() {
+    const { goods, isLoading, error } = this.state;
+
+    return (
+      <div className="App">
+        <div className="button-wrapper">
+          <button
+            type="button"
+            onClick={() => this.loadHandler(getAll)}
+          >
+            All goods
+          </button>
+          <button
+            type="button"
+            onClick={() => this.loadHandler(get5First)}
+          >
+            First 5
+          </button>
+          <button
+            type="button"
+            onClick={() => this.loadHandler(getRedGoods)}
+          >
+            get Red
+          </button>
+        </div>
+        {error && <Error />}
+        {!error && (isLoading
+          ? <div>Loading Data...</div>
+          : <GoodsList goods={goods} />
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <div>
+      Oops something went wrong
+    </div>
+  );
+};
+
+export default Error;
