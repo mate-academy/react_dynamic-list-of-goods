9c718f8818faa40547641842c441ca94aaa3ade3
Vova Alputov <alputov1998@gmail.com>
2022-01-28 14:55:39 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import './App.scss';
+import 'bulma';
+import { Table } from './components/Table';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[]
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+  };
+
+  componentDidMount() {
+    this.loadAll();
+  }
+
+  loadAll = () => {
+    getAll()
+      .then(goods => {
+        this.setState({ goods });
+      });
+  };
+
+  load5First = () => {
+    get5First()
+      .then(goods => {
+        this.setState({ goods });
+      });
+  };
+
+  loadRedGoods = () => {
+    getRedGoods()
+      .then(goods => {
+        this.setState({ goods });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <button type="button" className="button" onClick={this.loadAll}>Load All goods</button>
+        <button type="button" className="button" onClick={this.load5First}>Load 5 first goods</button>
+        <button type="button" className="button" onClick={this.loadRedGoods}>Load red goods</button>
+        <Table goods={this.state.goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
