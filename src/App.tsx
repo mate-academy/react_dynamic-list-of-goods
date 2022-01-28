913bf52a1f74eb76b73ead0087102b1f424895a4
Samson Sim <samson.sim@ukr.net>
2022-01-28 18:29:39 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import './App.scss';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+type Good = {
+  id: number,
+  name: string,
+  color: string,
+};
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[];
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+  };
+
+  displayAllGoods = async () => {
+    const goods = await getAll();
+
+    this.setState({ goods });
+  };
+
+  displayFiveFirstGoods = async () => {
+    const goods = await get5First();
+
+    this.setState({ goods });
+  };
+
+  displayRedGoods = async () => {
+    const goods = await getRedGoods();
+
+    this.setState({ goods });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <button type="button" className="button" onClick={this.displayAllGoods}>All Goods</button>
+        <button type="button" className="button" onClick={this.displayFiveFirstGoods}>Get 5 first</button>
+        <button type="button" className="button" onClick={this.displayRedGoods}>get Red goods</button>
+        <GoodsList goods={this.state.goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
