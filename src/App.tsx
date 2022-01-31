9ebd803df78de35d518cb04a574396f4459852d3
Oleksandr Leshchenko <bulochkin72@gmail.com>
2022-01-31 17:32:09 +0200

    job done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type Props = {};
+type State = {
+  goods: Good[],
+};
+
+class App extends React.Component<Props, State> {
+  state: State = {
+    goods: [],
+  };
+
+  setGoods = (callback: () => Promise<Good[]>) => {
+    callback()
+      .then(goods => this.setState({ goods }));
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+
+        <button type="button" onClick={() => this.setGoods(getAll)}>
+          Load All goods
+        </button>
+
+        <button type="button" onClick={() => this.setGoods(get5First)}>
+          Load 5 first goods
+        </button>
+
+        <button type="button" onClick={() => this.setGoods(getRedGoods)}>
+          Load red goods
+        </button>
+
+        <ul>
+          {goods.map(good => (
+            <li
+              key={good.id}
+              style={{ color: good.color }}
+            >
+              {good.name}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,6 +6,12 @@ export function getAll(): Promise<Good[]> {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => {
+  return getAll()
+    .then(goods => goods.filter(good => good.id < 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return getAll()
+    .then(goods => goods.filter(good => good.color === 'red'));
+};
