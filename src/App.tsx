e9549c0072874c731a319442263d4d11a47da1a4
Nikolas <tes.kolia@gmail.com>
2022-01-31 18:52:21 +0200

    v-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,66 @@
 import React from 'react';
 import './App.scss';
+import { get5First, getAll, getRedGoods } from './api/goods';
+import { GoodList } from './component/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
 // import * as goodsAPI from './api/goods';
+// or
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[];
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+  };
+
+  handleGetGoods = async () => {
+    const goodsAll = await getAll();
+
+    this.setState({ goods: goodsAll });
+  };
+
+  handleGet5FirstGoods = async () => {
+    const goods5First = await get5First();
+
+    this.setState({ goods: goods5First });
+  };
+
+  handleGetRedGoods = async () => {
+    const goodsRed = await getRedGoods();
+
+    this.setState({ goods: goodsRed });
+  };
+
+  render() {
+    return (
+      <div className="box p-10">
+        <button
+          type="button"
+          onClick={this.handleGetGoods}
+          className="button is-info mr-5"
+        >
+          Load All goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.handleGet5FirstGoods}
+          className="button is-info mr-5"
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          onClick={this.handleGetRedGoods}
+          className="button is-info"
+        >
+          Load red goods
+        </button>
+        <GoodList goods={this.state.goods} />
+      </div>
+    );
+  }
+}
