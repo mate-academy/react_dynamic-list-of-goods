d195d171fcb998148b2cb956ca51a5400c8169c0
Denys Sierhiievskyi <luckdenny@gmail.comecho>
2022-01-31 17:29:01 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
 import React from 'react';
 import './App.scss';
+import { GoodsList } from './components/GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+type State = {
+  goodsFromServer: Good[] | []
+};
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component<{}, State> {
+  state: State = {
+    goodsFromServer: [],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="funny-title section-title">Dynamic list of Goods</h1>
+        <div className="App__buttons-container">
+          <button
+            type="button"
+            onClick={async () => {
+              this.setState({
+                goodsFromServer: await getAll(),
+              });
+            }}
+            className="button"
+          >
+            Load All goods
+          </button>
+          <button
+            type="button"
+            onClick={async () => {
+              this.setState({
+                goodsFromServer: await get5First(),
+              });
+            }}
+            className="button"
+          >
+            Get 5 first
+          </button>
+          <button
+            type="button"
+            onClick={async () => {
+              this.setState({
+                goodsFromServer: await getRedGoods(),
+              });
+            }}
+            className="button"
+          >
+            Get red goods
+          </button>
+        </div>
+        {this.state.goodsFromServer !== [] && <GoodsList goods={this.state.goodsFromServer} />}
+      </div>
+    );
+  }
+}
 
 export default App;
