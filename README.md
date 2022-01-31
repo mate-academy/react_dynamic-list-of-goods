502db18661b60c7ddd58378263c61504c13d6a91
Marina Tilniak <marinatilnak@gmail.com>
2022-01-31 22:49:10 +0200

    Solution

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React dynamic list of goods
 - Replace `<your_account>` with your Github username in the
- [DEMO LINK](https://<your_account>.github.io/react_dynamic-list-of-goods/)
+ [DEMO LINK](https://marina-tilniak.github.io/react_dynamic-list-of-goods/)
 - Follow the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline)
 - Use [React TypeScript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript)
 
@@ -11,8 +11,8 @@
     - `Load All goods` shows all the received `goods`
     - `Load 5 first goods` shows 5 first `goods` after sorting them by name
     - `Load red goods` containing only `red` goods
-1. Server has only 1 endpoint returning all the goods, so you should prepare them after receiving.
-1. Create a component `GoodsList` accepting an array of goods and rendering them inside a `<ul>`
-1. Print a `name` of each `good` using `good.color`(for example `style={{ color: 'red' }}`)
-1. There should be a new request to the server on each button click.
-1. `GoodsList` should show the last loaded goods.
+2. Server has only 1 endpoint returning all the goods, so you should prepare them after receiving.
+3. Create a component `GoodsList` accepting an array of goods and rendering them inside a `<ul>`
+4. Print a `name` of each `good` using `good.color`(for example `style={{ color: 'red' }}`)
+5. There should be a new request to the server on each button click.
+6. `GoodsList` should show the last loaded goods.
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,81 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { get5First, getAll, getRedGoods } from './api/goods';
+import { GoodsList } from './GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[],
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+  };
+
+  showGoods = async (
+    getter: () => Promise<Good[]>,
+  ) => {
+    const goods = await getter();
+
+    this.setState({ goods });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="
+        is-flex
+        is-flex-direction-column is-align-items-center"
+      >
+        <h1 className="title">Dynamic list of Goods</h1>
+        <div>
+          <button
+            type="button"
+            className="
+              button
+              mr-4
+              is-link
+              is-light
+              is-outlined"
+            onClick={() => this.showGoods(getAll)}
+          >
+            Load All goods
+          </button>
+          <button
+            type="button"
+            className="
+              button
+              mr-4
+              is-link
+              is-light
+              is-outlined"
+            onClick={() => this.showGoods(get5First)}
+          >
+            Load 5 first goods
+          </button>
+          <button
+            type="button"
+            className="
+              button
+              mr-4
+              is-link
+              is-light
+              is-outlined"
+            onClick={() => this.showGoods(getRedGoods)}
+
+          >
+            Load red goods
+          </button>
+        </div>
+        {goods.length > 0 && (
+          <GoodsList goods={goods} />
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
