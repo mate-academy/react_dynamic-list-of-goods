8460b3a52e61caa228d7155ab29483a9fd3fc418
Nikolas <tes.kolia@gmail.com>
2022-02-01 12:29:48 +0200

    fixed get5... method

--- src/component/GoodsList.tsx
+++ src/component/GoodsList.tsx
@@ -2,14 +2,12 @@ type Props = {
   goods: Good[],
 };
 
-export const GoodList: React.FC<Props> = ({ goods }) => {
-  return (
-    <ul className="box is-flex-direction-column pt-10">
-      {goods.map(good => (
-        <li key={good.id}>
-          <span style={{ color: `${good.color}` }}>{good.name}</span>
-        </li>
-      ))}
-    </ul>
-  );
-};
+export const GoodList: React.FC<Props> = ({ goods }) => (
+  <ul className="box is-flex-direction-column pt-10">
+    {goods.map(good => (
+      <li key={good.id}>
+        <span style={{ color: `${good.color}` }}>{good.name}</span>
+      </li>
+    ))}
+  </ul>
+);
