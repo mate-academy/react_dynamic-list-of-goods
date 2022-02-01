d7f2ff9644bb42e15816b2cfaf32c93fca53a970
Oleg Shanin <79807285+OlegShanin@users.noreply.github.com>
2022-02-01 14:14:39 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
 import './App.scss';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[];
+};
+type Props = {};
 
-export default App;
+export class App extends React.Component<Props, State> {
+  state: State = {
+    goods: [],
+  };
+
+  showAllGoods = async () => {
+    const goods = await getAll();
+
+    this.setState({ goods });
+  };
+
+  showFiveFirstGoods = async () => {
+    const goods = await get5First();
+
+    this.setState({ goods });
+  };
+
+  showRedGoods = async () => {
+    const goods = await getRedGoods();
+
+    this.setState({ goods });
+  };
+
+  render(): React.ReactNode {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <div className="btn-group">
+          <button
+            type="button"
+            onClick={this.showAllGoods}
+            className="btn btn-outline-primary"
+          >
+            Load All goods
+          </button>
+          <button
+            type="button"
+            onClick={this.showFiveFirstGoods}
+            className="btn btn-outline-primary"
+          >
+            Load 5 first goods
+          </button>
+          <button
+            type="button"
+            onClick={this.showRedGoods}
+            className="btn btn-outline-primary"
+          >
+            Load red goods
+          </button>
+        </div>
+        <GoodsList goods={goods} />
+      </>
+    );
+  }
+}
