a28166b48434321fe3568d5cc7f02ad9da4eb283
Artur Ermolenko <ermolenkoartyr90@gmail.com>
2022-02-01 16:18:11 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,76 @@
 import React from 'react';
 import './App.scss';
+import { GoodsList } from './Component/GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+type Props = {};
+type State = {
+  goods: Good[]
+};
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends React.Component<Props, State> {
+  state: State = {
+    goods: [],
+  };
+
+  showAllGoods = async () => {
+    const goods = await getAll();
+
+    this.setState({
+      goods,
+    });
+  };
+
+  showFirstFive = async () => {
+    const goods = await get5First();
+
+    this.setState({
+      goods,
+    });
+  };
+
+  showRed = async () => {
+    const goods = await getRedGoods();
+
+    this.setState({
+      goods,
+    });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          className="ui brown basic button"
+          type="button"
+          onClick={this.showAllGoods}
+        >
+          Load All goods
+        </button>
+
+        <button
+          className="ui brown basic button"
+          type="button"
+          onClick={this.showFirstFive}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          className="ui brown basic button"
+          type="button"
+          onClick={this.showRed}
+        >
+          Load red goods
+        </button>
+
+        <GoodsList goods={goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
