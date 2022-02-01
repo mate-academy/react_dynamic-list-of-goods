441c11cff7f18f64ff5e3e56dad128452e910eba
Nick Datsiuk <perronogrande@gmail.com>
2022-02-01 14:06:20 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,76 @@
-import React from 'react';
+import { Component } from 'react';
+import { Oval } from 'react-loader-spinner';
+import { List } from './List';
 import './App.scss';
+import { get5First, getAll, getRedGoods } from './api/goods';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+interface State {
+  goodsList: Good[];
+  loading: boolean;
+  error: boolean;
+}
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+class App extends Component<{}, State> {
+  state: State = {
+    goodsList: [],
+    loading: false,
+    error: false,
+  };
+
+  loadGoods = async (goodsFromServer: () => Promise<Good[]>) => {
+    this.setState({ error: false, loading: true });
+    try {
+      const goods = await goodsFromServer();
+
+      this.setState({ goodsList: goods, error: false, loading: false });
+    } catch {
+      this.setState({ error: true, loading: false });
+    }
+  };
+
+  render() {
+    const {
+      goodsList, error, loading,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">Goods</h1>
+        <div className="App__buttons">
+          <button
+            type="button"
+            className="App__button"
+            onClick={() => this.loadGoods(getAll)}
+          >
+            Get all Goods
+          </button>
+          <button
+            type="button"
+            className="App__button"
+            onClick={() => this.loadGoods(get5First)}
+          >
+            Get First 5
+          </button>
+          <button
+            type="button"
+            className="App__button"
+            onClick={() => this.loadGoods(getRedGoods)}
+          >
+            Get only Red
+          </button>
+        </div>
+        <ul className="App__list">
+          {error && (
+            <div className="notification">
+              Something went wrong. Try again
+            </div>
+          )}
+          {!error && (loading ? <Oval color="#ff6600" height={80} width={80} /> : <List goods={goodsList} />)}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,25 @@
-// eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+const API_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
 
-export function getAll(): Promise<Good[]> {
+export const getAll = async (): Promise<Good[]> => {
   return fetch(API_URL)
-    .then(response => response.json());
-}
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} – ${response.statusText}`);
+      }
 
-export const get5First = () => {};
+      return response.json();
+    });
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return getAll().then(goods => goods.filter(g => g.color === 'red'));
+};
+
+export const get5First = () => {
+  return getAll()
+    .then(goods => {
+      goods.sort((a, b) => a.name.localeCompare(b.name));
+
+      return goods.slice(0, 5);
+    });
+};
