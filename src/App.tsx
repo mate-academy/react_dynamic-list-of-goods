5cb4797d83c8da70725d04976142543347fc5f9a
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2022-02-01 12:29:22 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
+import { GoodsList } from './GoodsList';
+
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[];
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+  };
+
+  loadData = async () => {
+    const goods = await getAll();
+
+    this.setState({ goods });
+  };
+
+  loadFiveItem = async () => {
+    const goods = await get5First();
+
+    goods.length = 5;
+
+    this.setState({ goods });
+  };
+
+  loadRedItem = async () => {
+    const goods = (await getRedGoods()).filter(good => good.color === 'red');
+
+    this.setState({ goods });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">Dynamic list of Goods</h1>
+        <button
+          className="App__button"
+          type="button"
+          onClick={this.loadData}
+        >
+          Load All goods
+        </button>
+        <button
+          className="App__button"
+          type="button"
+          onClick={this.loadFiveItem}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          className="App__button"
+          type="button"
+          onClick={this.loadRedItem}
+        >
+          Load red goods
+        </button>
+        <GoodsList goods={goods} />
+      </div>
+    );
+  }
+}
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,6 +6,10 @@ export function getAll(): Promise<Good[]> {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => {
+  return getAll();
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return getAll();
+};
