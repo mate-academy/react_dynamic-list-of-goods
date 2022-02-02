fcd845af9e454e4b29ef1364ce4e1f35a36b2c98
Serhii Haidukov <sergeyglazgo@gmail.com>
2022-02-02 13:55:41 +0200

    sorted

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -9,7 +9,7 @@ export function getAll(): Promise<Good[]> {
 export const get5First = async () => {
   const goods = await getAll();
 
-  return goods.slice(0, 5);
+  return goods.slice(0, 5).sort((g1, g2) => g1.name.localeCompare(g2.name));
 };
 
 export const getRedGoods = async () => {
