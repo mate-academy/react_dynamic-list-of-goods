e11b34a81a4bba783fadc43b67096a019ee92dd0
Mikedacheka <mikedacheka@gmail.com>
2022-02-02 02:19:19 +0200

    sm msg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,82 @@
 import React from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { GoodsList } from './components/GoodsList';
+import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type Props = {};
+type State = {
+  goods: Good[],
+};
 
-export default App;
+export class App extends React.PureComponent<Props, State> {
+  state: State = {
+    goods: [],
+  };
+
+  componentDidMount() {
+    this.getAllGoods();
+  }
+
+  getAllGoods = () => {
+    goodsAPI.getAll()
+      .then(goodsFromServer => {
+        this.setState({
+          goods: [...goodsFromServer],
+        });
+      });
+  };
+
+  getFirstFive = () => {
+    goodsAPI.getFirstFive()
+      .then(goodsFromServer => {
+        this.setState({
+          goods: [...goodsFromServer],
+        });
+      });
+  };
+
+  getRedGoods = () => {
+    goodsAPI.getRedGoods()
+      .then((goodsFromServer) => {
+        this.setState({
+          goods: goodsFromServer,
+        });
+      });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>Dynamic list of Goods</h1>
+        <button
+          type="button"
+          onClick={() => (
+            this.getAllGoods()
+          )}
+        >
+          Get all
+        </button>
+        <button
+          type="button"
+          onClick={() => (
+            this.getFirstFive()
+          )}
+        >
+          Get 5
+        </button>
+        <button
+          type="button"
+          onClick={() => (
+            this.getRedGoods()
+          )}
+        >
+          Get red goods
+        </button>
+        <GoodsList goods={goods} />
+      </>
+    );
+  }
+}
