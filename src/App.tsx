a44a97118ff426a7e39291ae45baeadaddd4eabb
DenysCheporniuk <denys.cheporniuk@gmail.com>
2022-02-03 14:30:53 +0200

    react dynamic list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import './App.scss';
+import { getAllGoods } from './api/goods';
+import { GoodsButtons } from './components/GoodsButtons';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+type Props = {};
+type State = {
+  goods: Good[],
+  error: boolean,
+};
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+export class App extends React.Component<Props, State> {
+  state: State = {
+    goods: [],
+    error: false,
+  };
 
-export default App;
+  async componentDidMount() {
+    try {
+      const goods = await getAllGoods();
+
+      this.setState({
+        goods: [...goods],
+      });
+    } catch (e) {
+      this.setState({
+        error: true,
+      });
+    }
+  }
+
+  loadGoods = async (getGoods: () => Promise<Good[]>) => {
+    const goods = await getGoods();
+
+    this.setState({
+      goods: [...goods],
+    });
+  };
+
+  render() {
+    const { goods, error } = this.state;
+
+    // eslint-disable-next-line no-console
+    console.log('goods', goods);
+
+    if (error) {
+      return (
+        <div>error</div>
+      );
+    }
+
+    return (
+      <div>
+        <h1>Goods</h1>
+        <GoodsButtons loadGoods={this.loadGoods} />
+      </div>
+    );
+  }
+}
