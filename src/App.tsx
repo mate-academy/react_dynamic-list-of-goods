fb5f35eb610d3be53a49d2c242066e873cbdc77c
Ernesto Fernandez <ernesto.fernandez.1980@gmail.com>
2022-02-06 23:48:36 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
+import 'bulma';
 import './App.scss';
+import { GoodsList } from './Components/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { get5First, getAll, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type State = {
+  goods: Good[],
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: [],
+  };
+
+  LoadHandler = async (getFunction: Promise<Good[]>) => {
+    const goods = await getFunction;
+
+    // eslint-disable-next-line no-console
+    console.log(goods);
+
+    this.setState({
+      goods: [...goods],
+    });
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <section
+        className="is-half "
+      >
+        <div className="container">
+          <button
+            type="button"
+            className="button is-link"
+            onClick={() => this.LoadHandler(getAll())}
+          >
+            Load All goods
+          </button>
+          <button
+            type="button"
+            className="button is-link"
+            onClick={() => this.LoadHandler(get5First())}
+          >
+            Load 5 first goods
+          </button>
+          <button
+            type="button"
+            className="button is-link"
+            onClick={() => this.LoadHandler(getRedGoods())}
+          >
+            Load red goods
+          </button>
+          <div className="columns">
+            <div className="column">
+              {goods.length > 0 && (<GoodsList goods={goods} />)}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default App;
