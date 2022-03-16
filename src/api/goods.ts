99e13ab951ba17557f9e633207677a0469f7b2e0
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-03-16 18:44:06 +0200

    changed Readme.md, fixed mistakes i was told about

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,19 +1,24 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export const getAll = (): Promise<Good[]> => {
-  return fetch(API_URL)
-    .then(response => response.json());
+export const getAll = async (): Promise<Good[]> => {
+  const result = await fetch(API_URL);
+
+  return result.json();
 };
 
-export const get5First = (): Promise<Good[]> => {
-  return getAll()
-    .then(response => response.splice(0, 5));
+export const get5First = async (): Promise<Good[]> => {
+  const result = await getAll();
+
+  return result.sort((a, b) => a.name.localeCompare(b.name))
+    .splice(0, 5);
 };
 
-export const getRedGoods = (): Promise<Good[]> => {
-  return getAll()
+export const getRedGoods = async (): Promise<Good[]> => {
+  const result = await getAll()
     .then(response => response.filter(good => good.color === 'red'));
+
+  return result.filter(good => good.color === 'red');
 };
 
 export const getGreenGoods = async (): Promise<Good[]> => {
