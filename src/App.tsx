1d37ff70cbccf0772ed99bb863ecb9298a11cc78
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-03-16 15:06:19 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useState } from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import {
+  getAll,
+  get5First,
+  getRedGoods,
+  getGreenGoods,
+} from './api/goods';
+import { GoodsList } from './components/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+type GoodsGetterFunction = () => Promise<Good[]>;
+
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const loadGoods = (goodsGetterFunction: GoodsGetterFunction) => {
+    goodsGetterFunction()
+      .then(goodsFromServer => (
+        setGoods(goodsFromServer)
+      ));
+  };
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+      <button
+        type="button"
+        onClick={() => loadGoods(getAll)}
+      >
+        Load All goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => loadGoods(get5First)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => loadGoods(getRedGoods)}
+      >
+        Load red goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => loadGoods(getGreenGoods)}
+      >
+        Load green goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </>
+  );
+};
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,23 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export const getAll = (): Promise<Good[]> => {
   return fetch(API_URL)
     .then(response => response.json());
-}
+};
 
-export const get5First = () => {};
+export const get5First = (): Promise<Good[]> => {
+  return getAll()
+    .then(response => response.splice(0, 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = (): Promise<Good[]> => {
+  return getAll()
+    .then(response => response.filter(good => good.color === 'red'));
+};
+
+export const getGreenGoods = async (): Promise<Good[]> => {
+  const result = await getAll();
+
+  return result.filter(good => good.color === 'green');
+};
