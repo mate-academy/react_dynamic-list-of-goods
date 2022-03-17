0929e9c074a1e5cb601d3a7712fb8fd413b7ca82
Artem Paskal <artem.yogi@gmail.com>
2022-03-17 21:13:07 +0200

    Solution

---
+++ src/Components/GoodsList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './GoodsList.scss';
+
+type Props = {
+  goods: Good[];
+};
+
+export const GoodsList: React.FC<Props> = ({ goods }) => {
+  return (
+    <ul>
+      {goods.map(good => (
+        <li
+          key={good.id}
+          style={{ color: good.color }}
+        >
+          {good.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
