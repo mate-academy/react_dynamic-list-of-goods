f0910d1587b0b54853967acd03434b96b7c4bc8f
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-03-17 10:18:27 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { Good } from './react-app-env';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+import { GoodsList } from './components/GoodList';
+
+import { getAll, get5First, getRedGoods } from './api/goods';
+
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  useEffect(() => {
+    getAll().then((result: Good[]) => {
+      return setGoods(result);
+    });
+  }, []);
+
+  const getAllGoods = () => {
+    getAll().then((result: Good[]) => {
+      return setGoods(result);
+    });
+  };
+
+  const getFive = () => {
+    get5First().then((result: Good[]) => {
+      return setGoods(result);
+    });
+  };
+
+  const getRed = () => {
+    getRedGoods().then((result: Good[]) => {
+      return setGoods(result);
+    });
+  };
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+
+      {goods.length > 0 && (
+        <GoodsList goods={goods} />
+      )}
+
+      <button type="button" onClick={getAllGoods}>Load All goods</button>
+      <button type="button" onClick={getFive}>Load 5 first goods</button>
+      <button type="button" onClick={getRed}>Load red goods</button>
+    </>
+  );
+};
 
 export default App;
+
+/*
+<button type="button" onClick={get5First(setGoods)}>Load 5 first goods</button> *
+/* <button type="button" onClick={getRedGoods(setGoods)}>Load red goods</button> */
