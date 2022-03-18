268073d7444c0e4f85783e53184faf4c9bbb64e5
maxim2310 <m23a09x96@gmail.com>
2022-03-18 22:22:32 +0200

    its alive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodList } from './GoodList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          getAll()
+            .then(goodsFromServer => {
+              setGoods(goodsFromServer);
+            });
+        }}
+      >
+        Load All goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => {
+          get5First()
+            .then(sortedGoods => setGoods(sortedGoods));
+        }}
+      >
+        Load first 5
+      </button>
+
+      <button
+        type="button"
+        onClick={() => {
+          getRedGoods()
+            .then(x => setGoods(x));
+        }}
+      >
+        Load red goods
+      </button>
+
+      <GoodList goods={goods} />
+    </>
+  );
+};
 
 export default App;
