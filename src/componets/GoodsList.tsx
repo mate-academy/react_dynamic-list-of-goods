0d95ad951802692489dd0739a2703b7ba57ce778
Kostya Kovbel <dovkerdota@gmail.com>
2022-03-18 11:09:04 +0200

    sol

---
+++ src/componets/GoodsList.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  goods: Good[]
+};
+
+export const GoodsList: React.FC<Props> = ({ goods }) => {
+  return (
+    <ul>
+      {goods.map(good => (
+        <li
+          style={{ color: good.color }}
+          key={good.id}
+        >
+          {good.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
