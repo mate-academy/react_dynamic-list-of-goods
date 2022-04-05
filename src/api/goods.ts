ef9c84a10e8964b5adcbe7ce7f6c1f3969bdab19
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-04-05 21:18:21 +0300

    sort was addded

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -7,7 +7,7 @@ export function getAll(): Promise<Good[]> {
 }
 
 export const get5First = () => {
-  return getAll().then(goods => goods.slice(0, 5));
+  return getAll().then(goods => goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5));
 };
 
 export const getRed = () => {
