f0501c29c64ed33044a525ccc1c52c31258a169a
Mary <anokhinamasha27@gmail.com>
2022-04-11 16:34:43 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState, memo } from 'react';
 import './App.scss';
+import { GoodsList } from './components';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsToLoad } from './enums/GoodsToLoad';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = memo(() => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const loadGoods = (toLoad: GoodsToLoad) => {
+    switch (toLoad) {
+      case GoodsToLoad.AllGoods:
+        getAll().then(items => setGoods(items));
+        break;
+      case GoodsToLoad.firstFiveGoods:
+        get5First().then(items => setGoods(items));
+        break;
+      case GoodsToLoad.RedGoods:
+        getRedGoods().then(item => setGoods(item));
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        onClick={() => loadGoods(GoodsToLoad.AllGoods)}
+      >
+        Load All goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => loadGoods(GoodsToLoad.firstFiveGoods)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => loadGoods(GoodsToLoad.RedGoods)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </>
+  );
+});
 
 export default App;
---
+++ src/enums/GoodsToLoad.ts
@@ -0,0 +1,5 @@
+export enum GoodsToLoad {
+  AllGoods,
+  firstFiveGoods,
+  RedGoods,
+}
