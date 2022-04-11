0c80334e217656074ad3c9ccb0f0edb3eaf7819d
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-04-11 15:37:30 +0300

    smth

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import './App.scss';
 import { getAll, get5First, getRed } from './api/goods';
 import { GoodsList } from './components/GoodsList';
 
+type DataToLoad = 'all' | 'five' | 'red';
+
 const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
-  const loadAll = () => {
-    getAll().then(loadedGoods => setGoods(loadedGoods));
-  };
-
-  const loadFive = () => {
-    get5First().then(loadedGoods => setGoods(loadedGoods));
-  };
-
-  const loadRed = () => {
-    getRed().then(loadedGoods => setGoods(loadedGoods));
+  const loadData = (dataToLoad: DataToLoad) => {
+    switch (dataToLoad) {
+      case 'five':
+        get5First().then(loadedGoods => setGoods(loadedGoods));
+        break;
+      case 'red':
+        getRed().then(loadedGoods => setGoods(loadedGoods));
+        break;
+      case 'all':
+        getAll().then(loadedGoods => setGoods(loadedGoods));
+        break;
+      default:
+        break;
+    }
   };
 
   return (
@@ -24,19 +30,19 @@ const App: React.FC = () => {
       <GoodsList goods={goods} />
       <button
         type="button"
-        onClick={loadAll}
+        onClick={() => loadData('all')}
       >
         Load All goods
       </button>
       <button
         type="button"
-        onClick={loadFive}
+        onClick={() => loadData('five')}
       >
         Load 5 first goods
       </button>
       <button
         type="button"
-        onClick={loadRed}
+        onClick={() => loadData('red')}
       >
         Load red goods
       </button>
