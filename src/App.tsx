18f5921cd6751ed4a4019f3b06b982979311aeef
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-04-11 19:28:56 +0300

    List of goods from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import 'bulma';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll } from './api/goods';
+import { GoodList } from './GoodList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [isInitialized, setInitialized] = useState(false);
+
+  const getAllGoods = () => {
+    getAll()
+      .then(goodsFromServer => {
+        setGoods(goodsFromServer);
+        setInitialized(true);
+      });
+  };
+
+  const get5First = () => {
+    getAll()
+      .then(goodsFromServer => {
+        goodsFromServer.sort((good1, good2) => {
+          return good1.name.localeCompare(good2.name);
+        });
+
+        setGoods(goodsFromServer.slice(0, 5));
+        setInitialized(true);
+      });
+  };
+
+  const getRedGoods = () => {
+    getAll()
+      .then(goodsFromServer => {
+        const filteredGoods = goodsFromServer.filter(
+          good => good.color === 'red',
+        );
+
+        setGoods(filteredGoods);
+        setInitialized(true);
+      });
+  };
+
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Dynamic list of Goods</h1>
+
+        {isInitialized && (
+          <div className="columns">
+            <div className="column">
+              <p className="subtitle">Goods table</p>
+
+              <GoodList goods={goods} />
+            </div>
+          </div>
+        )}
+
+        <button
+          className="button is-primary button--gap"
+          type="button"
+          onClick={getAllGoods}
+        >
+          Load All goods
+        </button>
+
+        <button
+          className="button is-primary button--gap"
+          type="button"
+          onClick={get5First}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          className="button is-primary"
+          type="button"
+          onClick={getRedGoods}
+        >
+          Load red goods
+        </button>
+
+      </div>
+    </section>
+  );
+};
 
 export default App;
---
+++ src/GoodList.tsx
@@ -0,0 +1,29 @@
+import { FC, memo } from 'react';
+
+interface Props {
+  goods: Good[],
+}
+
+export const GoodList: FC<Props> = memo(({ goods }) => (
+  <table className="table is-hoverable">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Color</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {goods.length > 0 && goods.map(good => (
+        <tr>
+          <td>{good.id}</td>
+          <td>{good.name}</td>
+          <td>
+            <span style={{ color: good.color }}>{good.color}</span>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+));
