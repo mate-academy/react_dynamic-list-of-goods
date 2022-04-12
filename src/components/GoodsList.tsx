4d8d85bcada1ae4383721e48c0bf9103c12564e2
Vitaliy Zhyvyliak <veltjj@gmail.com>
2022-04-12 19:43:28 +0300

    solution

---
+++ src/components/GoodsList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface Props {
+  goods: Good[]
+}
+
+export const GoodsList: React.FC<Props> = React.memo(
+  ({ goods }) => {
+    return (
+      <ul>
+        {goods.map(good => (
+          <li
+            key={good.id}
+            style={{ color: good.color }}
+          >
+            {good.name}
+          </li>
+        ))}
+      </ul>
+    );
+  },
+);
