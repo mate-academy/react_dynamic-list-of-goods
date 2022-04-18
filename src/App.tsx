009269317ed8b9d5270339ef7f64f2899147f5f2
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2022-04-18 13:05:34 +0300

    some message

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAll } from './api/goods';
+import { GoodsList } from './GoodsList';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+const App: React.FC = () => {
+  const [visualGoods, setVisualGoods] = useState<Good[]>([]);
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  const getGoods = (type = 'all') => {
+    getAll()
+      .then(goods => setVisualGoods(() => {
+        switch (type) {
+          case 'all': {
+            return goods;
+          }
+
+          case '5': {
+            return goods.slice(0, 5)
+              .sort((a, b) => a.name.localeCompare(b.name));
+          }
+
+          case 'red': {
+            return goods.filter(good => good.color === 'red');
+          }
+
+          default: {
+            return goods;
+          }
+        }
+      }));
+  };
+
+  useEffect(() => {
+    getGoods();
+
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <button type="button" onClick={() => getGoods()}>All</button>
+      <button type="button" onClick={() => getGoods('5')}>5 first</button>
+      <button type="button" onClick={() => getGoods('red')}>Only red</button>
+      <GoodsList goods={visualGoods} />
+    </>
+  );
+};
 
 export default App;
