a9cbdbbc971f958042df382074792787e439f09c
Olha Blokhina <olhablokhina@ip-10-10-0-199.eu-central-1.compute.internal>
2022-05-16 10:59:10 +0300

    NewTests

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -4,31 +4,65 @@ describe('Page', () => {
   before(() => {
     cy.visit('/');
   });
-  it('should has 13 goods', () => {
-    cy.get('button').contains('all').click()
+
+  const page = {
+    clickBtn(value) {
+      return cy.get('button')
+        .contains(`${value}`)
+        .click()
+    }
+  };
+
+  it('should have 13 goods', () => {
+   page.clickBtn('all');
     cy.get('li')
-    .should(($li) => {
-      expect($li).to.have.length(13)
-      return 'a'
+      .should(($li) => {
+      expect($li).to.have.length(13);
     });
   });
 
-  it('should has 5 goods', () => {
-    cy.get('button').contains(5).click()
+  it('should have 5 goods', () => {
+    page.clickBtn(5);
     cy.get('li')
-    .should(($li) => {
-      expect($li).to.have.length(5)
-      return 'a'
+      .should(($li) => {
+     expect($li).to.have.length(5);
     });
   });
 
-  it('should has red goods', () => {
-    cy.get('button').contains('red').click()
-    cy.get('.goods').should('contain', 'Potato')
-    cy.get('.goods').should('contain', 'Ice cream')
-    cy.get('.goods').should('contain', 'Fish')
-    cy.get('.goods').should('contain', 'Garlic')
-    cy.get('.goods').should('contain', 'Dumplings')
-    
+  it('should have red goods', () => {
+  page.clickBtn('red');
+    cy.get('.goods')
+      .should('contain', 'Potato')
+      .and('contain', 'Ice cream')
+      .and('contain', 'Fish')
+      .and('contain', 'Garlic')
+      .and('contain', 'Dumplings');
+  });
+
+  it ('should be a new request to the server on [all goods] button click', () => {
+    cy.intercept('*goods*', cy.spy().as('apiCall'));
+    cy.visit('/');
+    page.clickBtn('all');
+    cy.get('@apiCall')
+      .its('callCount')
+      .should('equal', 1);
   })
+
+  it ('should be a new request to the server on [first 5 goods] button click', () => {
+    cy.intercept('*goods*', cy.spy().as('apiCall'));
+    cy.visit('/');
+    page.clickBtn(5);
+    cy.get('@apiCall')
+      .its('callCount')
+      .should('equal', 1);
+    });
+
+    it ('should be a new request to the server on [red goods] button click', () => {
+      cy.intercept('*goods*', cy.spy().as('apiCall'));
+      cy.visit('/');
+      page.clickBtn('red');
+      cy.get('@apiCall')
+        .its('callCount')
+        .should('equal', 1);
+      });
 });
