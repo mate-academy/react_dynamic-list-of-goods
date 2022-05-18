9fd0b969d1dc12e9972c98392981f02c30120e1f
MariaBondar <marybondar777@gmail.com>
2022-05-18 14:01:49 +0200

    develop

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,22 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export const getAll = async (): Promise<Good[]> => {
+  const response = await fetch(API_URL);
+
+  return response.json();
+};
+
+export function get5First(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
-}
+    .then(response => response.json())
+    .then(response => response.slice(0, 5));
+};
 
-export const get5First = () => {};
-
-export const getRedGoods = () => {};
+export function getRedGoods(): Promise<Good[]> {
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(response => response.filter((good: Good) => {
+      return good.color === 'red'
+    }));
+};
