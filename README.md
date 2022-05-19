7651d8e0e4c2f15c184ec046b6c810e8e9ad5d9a
Misha Kravtsov <aniahucm@gmail.com>
2022-05-19 12:10:06 +0300

    solution

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
 # React dynamic list of goods
-- Replace `<your_account>` with your Github username in the
- [DEMO LINK](https://<your_account>.github.io/react_dynamic-list-of-goods/)
-- Follow the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline)
-- Use [React TypeScript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript)
+ [DEMO LINK](https://misha200119.github.io/react_dynamic-list-of-goods/)
 
-## Task
-> Load [the goods](https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json) and show them on the page
-
-1. Render 3 buttons loading the goods from server and saving them in the state:
-    - `Load All goods` shows all the received `goods`
-    - `Load 5 first goods` shows 5 first `goods` after sorting them by name
-    - `Load red goods` containing only `red` goods
-1. Server has only 1 endpoint returning all the goods, so you should prepare them after receiving.
-1. Create a component `GoodsList` accepting an array of goods and rendering them inside a `<ul>`
-1. Print a `name` of each `good` using `good.color`(for example `style={{ color: 'red' }}`)
-1. There should be a new request to the server on each button click.
-1. `GoodsList` should show the last loaded goods.
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { GoodsList } from './components';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Array<Good>>([]);
+
+  return (
+    <div className="app">
+      <h1 className="app__title">
+        Dynamic list of Goods
+      </h1>
+
+      <GoodsList
+        goods={goods}
+      />
+
+      <div className="app__btns_container">
+        <button
+          className="app__button"
+          type="button"
+          onClick={() => {
+            goodsAPI.getAll()
+              .then(newGoods => {
+                setGoods(newGoods);
+              });
+          }}
+        >
+          All goods
+        </button>
+
+        <button
+          className="app__button"
+          type="button"
+          onClick={() => {
+            goodsAPI.get5First()
+              .then(newGoods => {
+                setGoods(newGoods);
+              });
+          }}
+        >
+          Five first goods
+        </button>
+
+        <button
+          className="app__button"
+          type="button"
+          onClick={() => {
+            goodsAPI.getRedGoods()
+              .then(newGoods => {
+                setGoods(newGoods);
+              });
+          }}
+        >
+          Only red goods
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
