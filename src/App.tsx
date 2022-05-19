92a401feb8239a7df3d9891443db02021768a977
SeKaM0 <polozhentsew02@gmail.com>
2022-05-20 00:52:13 +0300

    Finish Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { GoodsList } from './component/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const showAllGoods = async () => {
+    const newGood = await getAll();
+
+    setGoods([...newGood]);
+
+    // eslint-disable-next-line no-console
+    console.log(newGood);
+  };
+
+  const showFirsFiveGoods = async () => {
+    const newGood = await get5First();
+
+    setGoods([...newGood]);
+
+    // eslint-disable-next-line no-console
+    console.log(newGood);
+  };
+
+  const showRedGoods = async () => {
+    const newGood = await getRedGoods();
+
+    setGoods([...newGood]);
+
+    // eslint-disable-next-line no-console
+    console.log(newGood);
+  };
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+      <button type="button" onClick={showAllGoods}>
+        All Goods
+      </button>
+      <button type="button" onClick={showFirsFiveGoods}>
+        First five goods
+      </button>
+      <button type="button" onClick={showRedGoods}>
+        Red Goods
+      </button>
+      <GoodsList goods={goods} />
+    </>
+  );
+};
 
 export default App;
