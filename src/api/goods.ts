92042dd72c7913b85ff5c0db97692acfbe3a6d21
strukk3 <ira3rey@gmail.com>
2022-05-20 12:17:07 +0300

    Some changes

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,24 +1,24 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
+export async function getAll(): Promise<Good[]> {
+  const response = await fetch(API_URL);
+
+  return response.json();
 }
 
-export const get5First = () => {
-  return fetch(API_URL)
-    .then(response => response.json())
-    .then((first5) => {
-      const good = first5.slice(0, 5);
+export const get5First = async (): Promise<Good[]> => {
+  const response = await fetch(API_URL);
+  const first5 = await response.json();
+  const good = first5.slice(0, 5);
 
-      return good.sort((goodA: Good, goodB: Good) => (
-        goodA.name.localeCompare(goodB.name)));
-    });
+  return good.sort((goodA: Good, goodB: Good) => (
+    goodA.name.localeCompare(goodB.name)));
 };
 
-export const getRedGoods = () => {
-  return fetch(API_URL)
-    .then(response => response.json())
-    .then(goods => goods.filter((good: Good) => good.color === 'red'));
+export const getRedGoods = async (): Promise<Good[]> => {
+  const response = await fetch(API_URL);
+  const goods = await response.json();
+
+  return goods.filter((good: Good) => good.color === 'red');
 };
