d9d43cf3aca0c97af35045ad70b7b8e4d70427cc
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2022-05-20 16:22:27 +0300

    dynamic list of todos was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { GoodsList } from './components/GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
+
+  const showGoods = (requirement: () => Promise<Good[]>) => {
+    requirement()
+      .then(goods => setVisibleGoods(goods));
+  };
+
+  return (
+    <div className="app">
+      <div className="buttons">
+        <button
+          className="buttons__button"
+          type="button"
+          onClick={() => showGoods(getAll)}
+        >
+          Load all goods
+        </button>
+
+        <button
+          className="buttons__button"
+          type="button"
+          onClick={() => showGoods(get5First)}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          className="buttons__button"
+          type="button"
+          onClick={() => showGoods(getRedGoods)}
+        >
+          Load red goods
+        </button>
+      </div>
+
+      {visibleGoods.length > 0 && (
+        <GoodsList goods={visibleGoods} />
+      )}
+    </div>
+  );
+};
 
 export default App;
