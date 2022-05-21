f68ad6b21c184ca0adeb7864c889d51f49f83b58
Alexandr Nikytenko <nika200382@gmail.com>
2022-05-21 22:24:36 +0300

    without css

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,6 +6,16 @@ export function getAll(): Promise<Good[]> {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = (): Promise<Good[]> => {
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(goods => goods.sort(
+      (a: Good, b: Good) => a.name.localeCompare(b.name),
+    ).slice(0, 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = (): Promise<Good[]> => {
+  return fetch(API_URL)
+    .then(response => response.json())
+    .then(goods => goods.filter((good: Good) => good.color === 'red'));
+};
