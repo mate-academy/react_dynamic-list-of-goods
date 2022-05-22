a023b171a9c5ef5d2cfde97a85099f0ce952d803
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-22 23:53:15 +0300

    Add new files

---
+++ src/components/GoodList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './GoodList.scss';
+
+type Props = {
+  goods: Good[],
+};
+
+export const GoodList: React.FC<Props> = ({ goods }) => (
+  <ul className="goods-list">
+    {goods.map((good) => (
+      <li
+        key={good.id}
+        className="goods-list__good"
+        style={{ color: `${good.color}` }}
+      >
+        {good.name}
+      </li>
+    ))}
+  </ul>
+);
