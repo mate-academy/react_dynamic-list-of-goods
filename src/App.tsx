2356e3060015405bf7fe3e1fa11161bf13cae685
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-05-22 14:08:09 +0300

    dynamic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const getGoods = (get: () => Promise<Good[]>) => {
+    get()
+    .then(goodsFromServer => {
+      setGoods(goodsFromServer);
+    });
+  };
+
+  return (
+  <>
+    <h1>Dynamic list of Goods</h1>
+    <GoodsList goods={goods} />
+    <button
+    type='button'
+    onClick={() => getGoods(getAll)}>
+      load all goods
+    </button>
+
+    <button
+    type='button'
+    onClick={() => getGoods(getRedGoods)}>
+      load red goods
+    </button>
+
+    <button
+    type='button'
+    onClick={() => getGoods(get5First)}>
+      load 5 firs goods
+    </button>
+  </>
+  );
+};
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,6 +6,15 @@ export function getAll(): Promise<Good[]> {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = () => {
+  return getAll()
+  .then(goods => goods
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .slice(0, 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return getAll()
+  .then(goods => goods
+    .filter(good => good.color === 'red'));
+};
