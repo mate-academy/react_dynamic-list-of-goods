9efb1c4146ee5b068bc32cd0d5a820c0b2d2390e
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-05-22 14:14:08 +0300

    fixed lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
@@ -11,33 +11,38 @@ const App: React.FC = () => {
 
   const getGoods = (get: () => Promise<Good[]>) => {
     get()
-    .then(goodsFromServer => {
-      setGoods(goodsFromServer);
-    });
+      .then(goodsFromServer => {
+        setGoods(goodsFromServer);
+      });
   };
 
   return (
-  <>
-    <h1>Dynamic list of Goods</h1>
-    <GoodsList goods={goods} />
-    <button
-    type='button'
-    onClick={() => getGoods(getAll)}>
-      load all goods
-    </button>
+    <>
+      <h1>Dynamic list of Goods</h1>
 
-    <button
-    type='button'
-    onClick={() => getGoods(getRedGoods)}>
-      load red goods
-    </button>
+      <GoodsList goods={goods} />
 
-    <button
-    type='button'
-    onClick={() => getGoods(get5First)}>
-      load 5 firs goods
-    </button>
-  </>
+      <button
+        type="button"
+        onClick={() => getGoods(getAll)}
+      >
+        load all goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => getGoods(getRedGoods)}
+      >
+        load red goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => getGoods(get5First)}
+      >
+        load 5 firs goods
+      </button>
+    </>
   );
 };
 
