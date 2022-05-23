4a82ae3916216757040a2b63aa7d06d1a52af039
Elizaveta <elizacalvador@gmail.com>
2022-05-23 16:41:28 +0300

    fixed3

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -8,18 +8,16 @@ export const getAll = async (): Promise<Good[]> => {
 };
 
 export const get5First = async (): Promise<Good[]> => {
-  const response = await fetch(API_URL);
-  const data = await response.json();
+  const data = await getAll();
   const firstFive = data.sort(
-    (a: Good, b: Good) => a.name.localeCompare(b.name)
+    (a: Good, b: Good) => a.name.localeCompare(b.name),
   );
 
   return firstFive.slice(0, 5);
 };
 
 export const getRedGoods = async (): Promise<Good[]> => {
-  const response = await fetch(API_URL);
-  const data = await response.json();
+  const data = await getAll();
 
   return data.filter((product: Good) => product.color === 'red');
 };
