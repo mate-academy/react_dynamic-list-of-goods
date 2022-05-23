121f7d60ac2d1782a1cceae538a9905c3cb51355
Artem <zuller123@gmail.com>
2022-05-23 22:35:48 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { GoodsList } from './components/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const pickGoods = (getGoods: () => Promise<Good[]>) => {
+    getGoods()
+      .then(recievedGoods => setGoods(recievedGoods));
+  };
+
+  return (
+    <div className="app">
+      <h1 className="app__title">
+        Dynamic list of Goods
+      </h1>
+
+      <div className="app__buttons">
+        <button
+          className="app__button glow-on-hover"
+          type="button"
+          onClick={() => pickGoods(getAll)}
+        >
+          Load All goods
+        </button>
+
+        <button
+          className="app__button glow-on-hover"
+          type="button"
+          onClick={() => pickGoods(get5First)}
+        >
+          Load first 5 goods
+        </button>
+
+        <button
+          className="app__button glow-on-hover"
+          type="button"
+          onClick={() => pickGoods(getRed)}
+        >
+          Load red goods
+        </button>
+      </div>
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
 
 export default App;
