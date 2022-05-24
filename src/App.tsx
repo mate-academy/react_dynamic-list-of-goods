1acf1f2ca1ff800f14e0672ea2d34dd0dcc98bcc
Ihor Mural <igor201278789887000@gmail.com>
2022-05-24 11:01:33 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import 'bulma';
+import { GoodsList } from './components';
+import * as goodsAPI from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  const getAll = async () => {
+    const responce = await goodsAPI.getAll();
+
+    return setGoods(responce);
+  };
+
+  const getFirstFive = async () => {
+    const responce = await goodsAPI.get5First();
+
+    return setGoods(responce);
+  };
+
+  const getRedGoods = async () => {
+    const responce = await goodsAPI.getRedGoods();
+
+    return setGoods(responce);
+  };
+
+  return (
+    <div className="app">
+      <h1 className="app__title">Dynamic list of goods</h1>
+
+      <GoodsList goods={goods} />
+
+      <div className="app__buttons">
+        <button
+          className="app__button"
+          type="button"
+          onClick={() => getAll()}
+        >
+          Load all goods
+        </button>
+        <button
+          className="app__button"
+          type="button"
+          onClick={() => getFirstFive()}
+        >
+          Load first 5 goods
+        </button>
+        <button
+          className="app__button"
+          type="button"
+          onClick={() => getRedGoods()}
+        >
+          Load all red goods
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/GoodsList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+type Props = {
+  goods: Good[],
+};
+
+export const GoodsList: React.FC<Props> = ({ goods }) => {
+  return (
+    <ul className="goods__list">
+      {goods.length > 0 && (
+        goods.map((good) => (
+          <li
+            key={good.id}
+            className="goods__item"
+            style={{ color: `${good.color}` }}
+          >
+            {good.name}
+          </li>
+        ))
+      )}
+    </ul>
+  );
+};
