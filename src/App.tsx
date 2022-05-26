5e0597c43e2898808fbd455e07b4f8c0c3ebd444
unknown <avicostov2@gmail.com>
2022-05-27 01:36:25 +0200

    dynamic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { GoodList } from './components/GoodList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+import * as goodsAPI from './api/goods';
+
+import Class from 'classnames';
+
+const App: React.FC = () => {
+
+  const [goods, changeGoods] = useState<Good[]>([]);
+  const [isWaitingForResult, setWaiting] = useState(false);
+
+  const handleButton = async (asyncronicFunc: () => Promise<Good[]>) => {
+
+    if (isWaitingForResult) {
+      return;
+    }
+
+    setWaiting(true);
+
+    const totalGoods = await asyncronicFunc();
+
+    changeGoods(totalGoods);
+    setWaiting(false);
+  }
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+      <button
+        className={
+          Class("button",
+            {"is-loading": isWaitingForResult},
+            {"is-primary": !isWaitingForResult}
+          )}
+        type="button"
+        onClick={() => handleButton(goodsAPI.getAll)}
+      >
+        ShowAll
+      </button>
+
+      <button
+        className={
+          Class("button",
+            {"is-loading": isWaitingForResult},
+            {"is-primary": !isWaitingForResult}
+          )}
+        type="button"
+        onClick={() => handleButton(goodsAPI.get5First)}
+      >
+        First5
+      </button>
+      <button
+        className={
+          Class("button",
+            {"is-loading": isWaitingForResult},
+            {"is-primary": !isWaitingForResult}
+          )}
+        type="button"
+        onClick={() => handleButton(goodsAPI.getRedGoods)}
+      >
+        Only Red
+      </button>
+      <GoodList goods={goods} />
+    </>
+  )
+}
 
 export default App;
