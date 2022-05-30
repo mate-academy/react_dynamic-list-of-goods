22b71281327b6d683e6630f479c7575f0e451164
Andriy Shvets <PiratesQ@i.ua>
2022-05-30 18:18:07 +0300

    removed await before response.json()

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,22 +3,19 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export async function getAll(): Promise<Good[]> {
   const response = await fetch(API_URL);
-  const json = await response.json();
 
-  return json;
+  return response.json();
 }
 
 export async function get5First(): Promise<Good[]> {
-  const response = await fetch(API_URL);
-  const data = await response.json();
+  const data = await getAll();
 
   return data
     .sort((g1: Good, g2: Good) => g1.name.localeCompare(g2.name)).slice(0, 5);
 }
 
 export async function getRedGoods(): Promise<Good[]> {
-  const response = await fetch(API_URL);
-  const data = await response.json();
+  const data = await getAll();
 
   return data.filter((el: Good) => el.color === 'red');
 }
