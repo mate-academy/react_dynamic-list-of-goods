67f12d03f534bf662361fdebc371cbab2d7d542a
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-06-01 22:16:04 +0300

    updated names

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { getAll, get5First, getRedGoods } from './api/goods';
 const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
-  const handleClick = async (f: () => Promise<Good[]>) => {
-    const good = await f();
+  const getGoods = async (goodsCallback: () => Promise<Good[]>) => {
+    const good = await goodsCallback();
 
     setGoods(good);
   };
@@ -16,15 +16,15 @@ const App: React.FC = () => {
   return (
     <>
       <h1>Dynamic list of Goods</h1>
-      <button type="button" onClick={() => handleClick(getAll)}>
+      <button type="button" onClick={() => getGoods(getAll)}>
         Load All goods
       </button>
 
-      <button type="button" onClick={() => handleClick(get5First)}>
+      <button type="button" onClick={() => getGoods(get5First)}>
         Load 5 first goods
       </button>
 
-      <button type="button" onClick={() => handleClick(getRedGoods)}>
+      <button type="button" onClick={() => getGoods(getRedGoods)}>
         Load red goods
       </button>
       <GoodsList goods={goods} />
