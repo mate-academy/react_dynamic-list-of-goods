7427be0d36f8c6dd95477eefdb26655a9908f82e
tarstarcas@gmail.com <tarstarcas@gmail.com>
2022-06-06 16:40:00 +0300

    V2

--- src/App.tsx
+++ src/App.tsx
@@ -2,75 +2,87 @@ import React from 'react';
 import { GoodsList } from './components/GoodsList';
 import './App.scss';
 
-import { getAll, get5First, getRedGoods } from './api/goods';
+import {
+  getAll,
+  get5First,
+  getRedGoods,
+} from './api/goods';
 
 type State = {
   goods: Good[],
-  showGoods: Good[],
 };
 
 class App extends React.Component<{}, State> {
   state: State = {
     goods: [],
-    showGoods: [],
   };
 
-  async componentDidMount() {
-    const goods = await getAll();
+  sort = async (name: string) => {
+    const goodsList = await getAll();
+
+    let goods: Good[] = [];
+
+    switch (name) {
+      case 'all':
+        goods = goodsList;
+        break;
+
+      case 'five':
+        goods = get5First(goodsList);
+        break;
+
+      case 'red':
+        goods = getRedGoods(goodsList);
+        break;
+
+      default:
+        break;
+    }
 
     this.setState({ goods });
-  }
-
-  allGoods = () => {
-    this.setState(state => ({
-      showGoods: state.goods,
-    }));
-  };
-
-  firstFive = () => {
-    this.setState(state => ({
-      showGoods: get5First(state.goods),
-    }));
-  };
-
-  redGoods = () => {
-    this.setState(state => ({
-      showGoods: getRedGoods(state.goods),
-    }));
   };
 
   render() {
-    const { showGoods } = this.state;
+    const { goods } = this.state;
 
     return (
       <div className="container">
         <h1 className="container__title">Dynamic list of Goods</h1>
         <div className="buttons">
           <button
+            name="all"
             type="button"
             className="button"
-            onClick={this.allGoods}
+            onClick={(e) => {
+              this.sort(e.currentTarget.name);
+            }}
           >
             All
           </button>
 
           <button
+            name="five"
             type="button"
             className="button"
-            onClick={this.firstFive}
+            onClick={(e) => {
+              this.sort(e.currentTarget.name);
+            }}
           >
             5
           </button>
           <button
+            name="red"
             type="button"
             className="button"
-            onClick={this.redGoods}
+            onClick={(e) => {
+              this.sort(e.currentTarget.name);
+            }}
           >
             Red
           </button>
         </div>
 
-        <GoodsList goods={showGoods} />
+        <GoodsList goods={goods} />
       </div>
     );
   }
