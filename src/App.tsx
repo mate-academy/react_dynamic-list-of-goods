f9865b4ebcb5c358a1424f4664c26f1beb80b829
unknown <avicostov2@gmail.com>
2022-06-13 18:36:46 +0200

    dynamic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { get5First, getAll, getRedGoods } from './api/goods';
 import './App.scss';
+import { GoodsList } from './components/GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+enum ShowType {
+  All = 'all',
+  FirstFive = 'firstfive',
+  RedColor = 'redcolor'
+}
+
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  async function getGoods(showBy: ShowType) {
+    switch(showBy) {
+      case ShowType.All:
+        setGoods(await getAll());
+        break;
+      case ShowType.FirstFive:
+        setGoods(await get5First());
+        break;
+      case ShowType.RedColor:
+        setGoods(await getRedGoods());
+        break;
+      default:
+        break;
+    }
+  }
+
+  useEffect(() => {
+    getGoods(ShowType.All);
+  }, []);
+
+  return (
+    <div>
+      <GoodsList goods={[...goods]}/>
+      <button
+        type='button'
+        onClick={() => getGoods(ShowType.All)}
+      >
+        All
+      </button>
+      <button
+        type='button'
+        onClick={() => getGoods(ShowType.FirstFive)}
+      >
+        First5
+      </button>
+      <button
+        type='button'
+        onClick={() => getGoods(ShowType.RedColor)}
+      >
+        Only Red
+      </button>
+    </div>
+  );
+}
 
 export default App;
