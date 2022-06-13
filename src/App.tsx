1fa25e78c04dd5593c79286d623c565381c28da6
Roman Pavlenko <proman9973@gmail.com>
2022-06-13 15:38:07 +0300

    solutionI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList/GoodsList';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  async function finderAll() {
+    const result = await getAll();
+
+    setGoods(result);
+  }
+
+  async function finderFive() {
+    const result = await get5First();
+
+    setGoods(
+      result.sort(
+        (a : Good, b: Good) => a.name.localeCompare(b.name),
+      ).splice(0, 5),
+    );
+  }
+
+  async function finderRed() {
+    const result = await getRedGoods();
+
+    setGoods(result.filter((el : Good) => el.color === 'red'));
+  }
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          finderAll();
+        }}
+      >
+        List Of Goods
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          finderFive();
+        }}
+      >
+        First 5 Goods
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          finderRed();
+        }}
+      >
+        Red Goods
+      </button>
+      <GoodsList goodsArray={goods} />
+    </>
+  );
+};
 
 export default App;
