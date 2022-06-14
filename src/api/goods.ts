bf83f7c648bf9d66da9e6dd24003b6e8ffda5f54
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-06-14 21:04:18 +0300

    throwing error

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -2,19 +2,23 @@ import { Good } from '../react-app-env';
 // eslint-disable-next-line
 const API_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
+export async function getAll(): Promise<Good[]> {
+  const response = await fetch(API_URL);
+
+  return response.ok
+  // is it okay?
+    ? response.json()
+    : Promise.reject(new Error(`${response.status}: ${response.statusText}`));
 }
 
-export const get5First = () => {
-  return getAll()
-    .then(goods => goods
-      .slice(0, 5)
-      .sort((a, b) => a.name.localeCompare(b.name)));
+export const get5First = async () => {
+  const goods = await getAll();
+
+  return goods.slice(0, 5).sort((a, b) => a.name.localeCompare(b.name));
 };
 
-export const getRed = () => {
-  return getAll()
-    .then(goods => goods.filter(good => good.color === 'red'));
+export const getRed = async () => {
+  const goods = await getAll();
+
+  return goods.filter(good => good.color === 'red');
 };
