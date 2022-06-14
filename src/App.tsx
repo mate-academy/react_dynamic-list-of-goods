fa23c88d81edeb5591717647bab8829e118b6a2d
Anatolii Vlasiuk <vlasyukalid@gmail.com>
2022-06-14 22:15:59 +0300

    GoodsFetch was done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { getAll } from './api/goods';
+import { GoodList } from './components/GoodList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  async function Allgoods() {
+    setGoods(await getAll());
+  }
 
-export default App;
+  async function Five() {
+    const result = await getAll();
+
+    setGoods(result.splice(0, 5));
+  }
+
+  async function Red() {
+    const result = await getAll();
+
+    setGoods(result.filter((item : Good) => item.color === 'red'));
+  }
+
+  return (
+    <>
+      <GoodList goodsArr={goods} />
+      <button
+        type="button"
+        onClick={() => {
+          Allgoods();
+        }}
+      >
+        All goods
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          Five();
+        }}
+      >
+        5 Goods
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          Red();
+        }}
+      >
+        Red Goods
+      </button>
+    </>
+  );
+};
