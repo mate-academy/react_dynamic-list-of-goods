da3c6eb43431f28be9de99f9be14a192d0f97275
Maryna <savna023@gmail.com>
2022-06-14 18:06:07 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { GoodsList } from './Components/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [isActiveAll, setIsActiveAll] = useState(false);
+  const [isActiveFive, setIsActiveFive] = useState(false);
+  const [isActiveRed, setIsActiveRed] = useState(false);
+
+  async function goodsAllHandler() {
+    setGoods(await getAll());
+    setIsActiveAll(true);
+    setIsActiveFive(false);
+    setIsActiveRed(false);
+  }
+
+  async function goodsFiveHandler() {
+    setGoods(await get5First());
+    setIsActiveAll(false);
+    setIsActiveFive(true);
+    setIsActiveRed(false);
+  }
+
+  async function goodsRedHandler() {
+    setGoods(await getRedGoods());
+    setIsActiveAll(false);
+    setIsActiveFive(false);
+    setIsActiveRed(true);
+  }
+
+  return (
+    <div className="content">
+      <div className="buttons has-addons">
+        <button
+          type="button"
+          onClick={goodsAllHandler}
+          className={classNames('button', {
+            'is-warning': isActiveAll,
+          })}
+        >
+          Show All Goods
+        </button>
+        <button
+          type="button"
+          onClick={goodsFiveHandler}
+          className={classNames('button', {
+            'is-warning': isActiveFive,
+          })}
+        >
+          Show 5 Goods
+        </button>
+        <button
+          type="button"
+          onClick={goodsRedHandler}
+          className={classNames('button', {
+            'is-warning': isActiveRed,
+          })}
+        >
+          Show Red Goods
+        </button>
+      </div>
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,20 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
+export async function getAll(): Promise<Good[]> {
+  const response = await fetch(API_URL);
+
+  return response.json();
 }
 
-export const get5First = () => {};
+export const get5First = async () => {
+  const allGoods = await getAll();
 
-export const getRedGoods = () => {};
+  return allGoods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
+};
+
+export const getRedGoods = async () => {
+  const allGoods = await getAll();
+
+  return allGoods.filter(good => good.color === 'red');
+};
