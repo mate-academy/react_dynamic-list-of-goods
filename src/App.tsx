309bc13a7c8c7c986fd54df106acad7d32b707f8
Andrii Rutkovskii <greateman94@gmail.com>
2022-06-15 16:59:53 +0300

    first commit v0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { get5First, getAll, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const showAll = async () => {
+    const list = await getAll();
+
+    setGoods(list);
+  };
+
+  const showFive = async () => {
+    const list = await get5First();
+
+    setGoods(list);
+  };
+
+  const showRed = async () => {
+    const list = await getRedGoods();
+
+    setGoods(list);
+  };
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        onClick={showAll}
+      >
+        Load All goods
+      </button>
+      <button
+        type="button"
+        onClick={showFive}
+      >
+        Load 5 first goods
+      </button>
+      <button
+        type="button"
+        onClick={showRed}
+      >
+        Load red goods
+      </button>
+
+      <ul>
+        {goods.map((el: Good) => (
+          <li>{el.name}</li>
+        ))}
+      </ul>
+    </>
+
+  );
+};
 
 export default App;
