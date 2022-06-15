56629db20fa3032ec55091e47cfa231db4f0c69c
Vicky Miziukanova <enrikadej@gmail.com>
2022-06-15 16:59:48 +0300

    task without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { GoodsList } from './components/GoodsList';
+import { Good } from './react-app-env';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  // const [allGoods, setAllGoods] = useState([]);
+
+  // const [first5Goods, setFirst5Goods] = useState([]);
+  // const [redGoods, setRedGoods] = useState([]);
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+      <button
+        type="button"
+        onClick={() => getAll()
+          .then(products => {
+            setGoods(products);
+          })}
+      >
+        all
+      </button>
+
+      <button
+        type="button"
+        onClick={() => get5First()
+          .then(products => {
+            setGoods(products);
+          })}
+      >
+        5
+      </button>
+
+      <button
+        type="button"
+        onClick={() => getRedGoods()
+          .then(products => {
+            setGoods(products);
+          })}
+      >
+        red
+      </button>
+
+      {goods.length > 0 && (
+        <GoodsList goods={goods} />
+      )}
+    </>
+  );
+};
 
 export default App;
