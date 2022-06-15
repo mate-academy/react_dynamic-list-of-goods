741b5df4b7db2e134b1fc9b383c4d97fe929f361
Valerii Derkach <valerij.derkach@gmail.com>
2022-06-15 17:07:30 +0300

    dynamic list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { GoodList } from './components/GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './react-app-env';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  const setAllGoods = () => {
+    getAll()
+      .then(goodsFromServer => setGoods(goodsFromServer));
+  };
 
-export default App;
+  const set5FirstGoods = () => {
+    get5First()
+      .then(goodsFromServer => setGoods(goodsFromServer));
+  };
+
+  const setRedGoods = () => {
+    getRedGoods()
+      .then(goodsFromServer => setGoods(goodsFromServer));
+  };
+
+  return (
+    <main className="
+      box main pb-6
+      is-flex
+      is-flex-direction-column
+      is-align-items-center"
+    >
+      <h1
+        className="title is-3"
+      >
+        Dynamic list of Goods
+      </h1>
+      <div className="buttons">
+        <button
+          className="button is-link is-outlined"
+          type="button"
+          onClick={setAllGoods}
+        >
+          Load All goods
+        </button>
+        <button
+          className="button is-success is-outlined"
+          type="button"
+          onClick={set5FirstGoods}
+        >
+          Load 5 first goods
+        </button>
+        <button
+          className="button is-danger is-outlined"
+          type="button"
+          onClick={setRedGoods}
+        >
+          Load red goods
+        </button>
+      </div>
+      <GoodList goods={goods} />
+    </main>
+  );
+};
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,3 +1,4 @@
+import { Good } from '../react-app-env';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
@@ -6,6 +7,14 @@ export function getAll(): Promise<Good[]> {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = (): Promise<Good[]> => {
+  return getAll()
+    .then(goods => goods.sort((good1, good2) => (
+      good1.name.localeCompare(good2.name))))
+    .then(goods => goods.slice(0, 5));
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = (): Promise<Good[]> => {
+  return getAll()
+    .then(goods => goods.filter(good => good.color === 'red'));
+};
