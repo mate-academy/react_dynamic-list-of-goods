1a567b40e0be64851eb582c1eac8bc8a655518e6
Zubov Illia <contactduodiamonds@gmail.com>
2022-06-15 21:56:50 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const getAllGoods = () => {
+    getAll().then(allGoods => setGoods(allGoods));
+  };
+
+  const getFiveGoods = () => {
+    get5First().then(fiveGoods => setGoods(fiveGoods));
+  };
+
+  const getRedGoods = () => {
+    getRed().then(redGoods => setGoods(redGoods));
+  };
+
+  // eslint-disable-next-line no-console
+  console.log(goods);
+
+  return (
+    <>
+      <h1
+        className="title"
+      >
+        Dynamic list of Goods
+      </h1>
+
+      <div
+        className="btn-container"
+      >
+        <button
+          type="button"
+          className="button is-info"
+          onClick={getAllGoods}
+        >
+          Load All goods
+        </button>
+        <button
+          type="button"
+          className="button is-info"
+          onClick={getFiveGoods}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          className="button is-info"
+          onClick={getRedGoods}
+        >
+          Load red
+        </button>
+      </div>
+
+      <ul>
+        <GoodsList goods={goods} />
+      </ul>
+    </>
+  );
+};
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,6 +6,13 @@ export function getAll(): Promise<Good[]> {
     .then(response => response.json());
 }
 
-export const get5First = () => {};
+export const get5First = (): Promise<Good[]> => {
+  return getAll()
+    .then(goods => goods.slice(0, 5)
+      .sort((a, b) => a.name.localeCompare(b.name)));
+};
 
-export const getRedGoods = () => {};
+export const getRed = (): Promise<Good[]> => {
+  return getAll()
+    .then(goods => goods.filter(good => good.color === 'red'));
+};
