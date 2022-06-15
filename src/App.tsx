434242501477c729f52a8d1efc3908e25da86a34
Anastasiia Sopotnytska <nastyasopotnytska14@gmail.com>
2022-06-15 11:49:05 +0300

    first solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { get5First, getAll, getRedGoods } from './api/goods';
+import { GoodList } from './components/GoodsList';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [hasAll, setHasAll] = useState(false);
+  const [hasFive, setHasFive] = useState(false);
+  const [hasRed, setHasRed] = useState(false);
+
+  useEffect(() => {
+    if (hasAll) {
+      getAll()
+        .then(shownGoods => {
+          setGoods(shownGoods);
+        });
+    }
+
+    if (hasRed) {
+      getRedGoods()
+        .then(shownGoods => {
+          const redGoods = shownGoods
+            .filter((good: Good) => good.color === 'red');
+
+          setGoods(redGoods);
+        });
+    }
+
+    if (hasFive) {
+      get5First()
+        .then(shownGoods => {
+          const shown5Goods = shownGoods
+            .sort((a: Good, b: Good | any) => a.name.localeCompare(b.name))
+            .splice(0, 5);
+
+          setGoods(shown5Goods);
+        });
+    }
+  });
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          setHasFive(false);
+          setHasRed(false);
+          setHasAll(true);
+        }}
+      >
+        All goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => {
+          setHasAll(false);
+          setHasRed(false);
+          setHasFive(true);
+        }}
+      >
+        5 goods
+      </button>
+
+      <button
+        type="button"
+        onClick={() => {
+          setHasAll(false);
+          setHasFive(false);
+          setHasRed(true);
+        }}
+      >
+        Red goods
+      </button>
+
+      <GoodList goods={goods} />
+    </>
+  );
+};
 
 export default App;
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,15 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export const getAll = () => {
   return fetch(API_URL)
     .then(response => response.json());
-}
+};
 
-export const get5First = () => {};
+export const get5First = () => {
+  return getAll();
+};
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => {
+  return getAll();
+};
