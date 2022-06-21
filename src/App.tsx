ad18c1cb91c37b6d6ac2bf6154a1d91915889263
Danylo-Dziuban <dz.danylo@gmail.com>
2022-06-21 20:07:22 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, ButtonGroup, Box } from '@mui/material/';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC<{}> = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
+
+  const setAllGoods = () => {
+    getAll().then(response => setVisibleGoods(response));
+  };
+
+  const set5Goods = () => {
+    get5First().then(response => setVisibleGoods(response));
+  };
+
+  const setRedGoods = () => {
+    getRedGoods().then(response => setVisibleGoods(response));
+  };
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+      <ButtonGroup>
+        <Button
+          onClick={setAllGoods}
+        >
+          Load All goods
+        </Button>
+
+        <Button
+          onClick={set5Goods}
+        >
+          Load 5 first goods
+        </Button>
+
+        <Button
+          onClick={setRedGoods}
+        >
+          Load red goods
+        </Button>
+      </ButtonGroup>
+
+      {visibleGoods.map((good: Good) => (
+        <Box>
+          <span
+            style={{ color: good.color }}
+          >
+            {good.name}
+          </span>
+        </Box>
+      ))}
+    </>
+  );
+};
 
 export default App;
