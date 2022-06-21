821826a8a4595a363266b8860cd5dcbb64b788d1
Danylo-Dziuban <dz.danylo@gmail.com>
2022-06-21 20:09:50 +0300

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const App: React.FC<{}> = () => {
       </ButtonGroup>
 
       {visibleGoods.map((good: Good) => (
-        <Box>
+        <Box key={good.id}>
           <span
             style={{ color: good.color }}
           >
