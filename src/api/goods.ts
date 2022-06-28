c9ee9a8bf9a53685fcd002f19b11b40137304b80
Amit Patil <amitp0156@gmail.com>
2022-06-28 20:02:14 +0530

    minor changes

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goodssss.json`;
+const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export const getAll = (): Promise<Good[]> => {
   return fetch(API_URL)
