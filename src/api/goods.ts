312e92155033a5292deb5c251a43e90dc662d3da
Amit Patil <amitp0156@gmail.com>
2022-06-28 11:34:59 +0530

    added error handling

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,10 +1,18 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export const getAll = (): Promise<Good[]> => {
   return fetch(API_URL)
-    .then(response => response.json());
-}
+    .then((response) => {
+      if (!response.ok) {
+        const error = new Error(`${response.status} - ${response.statusText}`);
+
+        throw error;
+      }
+
+      return response.json();
+    });
+};
 
 export const get5First = async () => {
   const goods = await getAll();
