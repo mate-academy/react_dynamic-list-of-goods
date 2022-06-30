158fe07d2a2064467581528a683db6250a923af1
Mfano <mabuzamfano@gmail.com>
2022-06-30 22:25:54 +0100

    task complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bulma';
 import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { get5First, getAll, getRedGoods } from './api/goods';
+import { GoodsList } from './components/GoodsList';
 // or
 // import * as goodsAPI from './api/goods';
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+const App: React.FC = () => {
+  const [goodsList, setGoodsList] = useState<Good[]>([]);
+
+  const getAllGoods = async () => {
+    setGoodsList(await getAll());
+  };
+
+  const getFirst5Goods = async () => {
+    setGoodsList(await get5First());
+  };
+
+  const getAllRedGoods = async () => {
+    setGoodsList(await getRedGoods());
+  };
+
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+      <div className="button button-group">
+        <button
+          className="is-link"
+          type="button"
+          onClick={getAllGoods}
+        >
+          Get All
+        </button>
+
+        <button
+          className="is-link"
+          type="button"
+          onClick={getFirst5Goods}
+        >
+          5
+        </button>
+
+        <button
+          className="is-link"
+          type="button"
+          onClick={getAllRedGoods}
+        >
+          Red
+        </button>
+      </div>
+
+      <div>
+        <GoodsList goodsList={goodsList} />
+      </div>
+    </>
+  );
+};
 
 export default App;
