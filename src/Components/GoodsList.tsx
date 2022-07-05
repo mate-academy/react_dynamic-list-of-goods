fbc29c72dcc4b4486ca9d34833dcf00487341cc9
Amit Patil <amitp0156@gmail.com>
2022-07-06 00:51:24 +0530

    used React.memo

--- src/Components/GoodsList.tsx
+++ src/Components/GoodsList.tsx
@@ -4,15 +4,17 @@ interface Props {
   goods: Good[];
 }
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
-    {goods.map(good => (
-      <li
-        key={good.id}
-        style={{ color: good.color }}
-      >
-        {good.name}
-      </li>
-    ))}
-  </ul>
+export const GoodsList: React.FC<Props> = React.memo(
+  ({ goods }) => (
+    <ul>
+      {goods.map(good => (
+        <li
+          key={good.id}
+          style={{ color: good.color }}
+        >
+          {good.name}
+        </li>
+      ))}
+    </ul>
+  ),
 );
