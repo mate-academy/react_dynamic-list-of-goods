b5629dfa1e6b948d13974634380819e96de7594b
Rashaad Ebrahim <rmebrahim@gmail.com>
2022-07-07 10:59:24 +0200

    Task complete

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,21 @@
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
+export async function getAllGoods(): Promise<Good[]> {
+  const response = await fetch(API_URL);
+
+  return response.json();
 }
 
-export const get5First = () => {};
+export const get5First = () => (
+  getAllGoods()
+    .then(good => good
+      .sort((a: Good, b: Good) => a.name.localeCompare(b.name))
+      .slice(0, 5))
+);
 
-export const getRedGoods = () => {};
+export const getRedGoods = () => (
+  getAllGoods()
+    .then(goods => goods
+      .filter((good: Good) => good.color === 'red'))
+);
