17af2588736f17fac349e6c108fdf1f453dcd05d
efdalyalcin <efdalyalcin@hotmail.com>
2022-07-07 10:54:17 +0300

    small code optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,46 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import 'bulma';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
 import { GoodsList } from './components/GoodsList';
-// or
-// import * as goodsAPI from './api/goods';
 
 const App: React.FC = () => {
   const [goodsToShow, setGoodsToShow] = useState<Good[]>([]);
 
-  const showAll = () => {
-    getAll()
-      .then(goods => {
-        setGoodsToShow(goods);
-      });
-  };
+  const showAll = useCallback(
+    () => {
+      getAll()
+        .then(goods => {
+          setGoodsToShow(goods);
+        });
+    },
+    [],
+  );
 
-  const show5First = () => {
-    get5First()
-      .then(goods => {
-        setGoodsToShow(goods);
-      });
-  };
+  const show5First = useCallback(
+    () => {
+      get5First()
+        .then(goods => {
+          setGoodsToShow(goods);
+        });
+    },
+    [],
+  );
 
-  const showReds = () => {
-    getRedGoods()
-      .then((goods: Good[]) => {
-        setGoodsToShow(goods);
-      });
-  };
+  const showReds = useCallback(
+    () => {
+      getRedGoods()
+        .then((goods: Good[]) => {
+          setGoodsToShow(goods);
+        });
+    },
+    [],
+  );
 
   return (
-    <div
-      className="has-text-centered"
-    >
-      <h1
-        className="title m-4"
-      >
+    <div className="has-text-centered">
+      <h1 className="title m-4">
         Dynamic list of Goods
       </h1>
 
