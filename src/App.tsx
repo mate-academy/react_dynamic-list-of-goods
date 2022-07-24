efdcfd1bac1492f2b108ec23e203aced9fd46699
Andrii Nychaiuk <anichajuk@gmail.com>
2022-07-25 00:03:04 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,85 @@
-import React from 'react';
 import './App.scss';
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
+
 import { GoodsList } from './GoodsList';
+import 'bulma';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [allGoods, getAllGoods] = useState<Good[]>([]);
+  const [isLoaded, setLoaded] = useState(false);
+  const [isSorted, setSorted] = useState(false);
+  const [isFiltred, setFiltred] = useState(false);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  useEffect(() => {
+    if (isLoaded) {
+      getAll()
+        .then(goods => getAllGoods(goods));
+    }
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+    if (isSorted) {
+      get5First().then(goods => getAllGoods(goods));
+    }
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    if (isFiltred) {
+      getRedGoods().then(goods => getAllGoods(goods));
+    }
+  }, [isLoaded, isSorted, isFiltred]);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className={classNames(
+      'App',
+      'is-flex',
+      'is-flex-direction-column',
+      'is-align-items-center',
+    )}
+    >
+      <h1 className="title">Dynamic list of Goods</h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <div className="buttons">
+        <button
+          type="button"
+          className="button is-info"
+          data-cy="all-button"
+          onClick={() => {
+            setFiltred(false);
+            setSorted(false);
+            setLoaded(true);
+          }}
+        >
+          Load all goods
+        </button>
+
+        <button
+          type="button"
+          className="button is-info"
+          data-cy="first-five-button"
+          onClick={() => {
+            setFiltred(false);
+            setLoaded(false);
+            setSorted(true);
+          }}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          className="button is-info"
+          data-cy="red-button"
+          onClick={() => {
+            setSorted(false);
+            setLoaded(false);
+            setFiltred(true);
+          }}
+        >
+          Load red goods
+        </button>
+      </div>
+
+      <GoodsList goods={allGoods} />
+    </div>
+  );
+};
