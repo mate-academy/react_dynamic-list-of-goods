f849c7dd565f8a84377c0b2755004f795339b07c
Marta Fedoryshyn <fedoryshyn.marta@gmail.com>
2022-07-25 10:17:49 +0300

    added list

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import 'bootstrap/scss/bootstrap.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
+  const [hasLoadingError, setLoadingError] = useState(false);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const loadGoods = async (loadType: () => Promise<Good[]>) => {
+    setLoadingError(false);
+    try {
+      const goods = await loadType();
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      setVisibleGoods(goods);
+    } catch {
+      setLoadingError(true);
+    }
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App container-sm mx-auto">
+      <h1 className="text-center">
+        Dynamic list of Goods
+      </h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <div className="d-flex p-2 bd-highlight justify-content-center">
+        <button
+          type="button"
+          data-cy="all-button"
+          onClick={() => loadGoods(getAll)}
+        >
+          Load all goods
+        </button>
+
+        <button
+          type="button"
+          data-cy="first-five-button"
+          onClick={() => loadGoods(get5First)}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          data-cy="red-button"
+          onClick={() => loadGoods(getRedGoods)}
+        >
+          Load red goods
+        </button>
+      </div>
+
+      {!hasLoadingError && <GoodsList goods={visibleGoods} />}
+    </div>
+  );
+};
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Good } from './types/Good';
+import 'bootstrap/scss/bootstrap.scss';
 
 type Props = {
   goods: Good[]
 };
 
 export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
+  <ul className="
+    list-group
+    list-group-flush justify-content-center align-center"
+  >
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li
+        className="
+          list-group-item
+          text-center
+          list-group-item-action
+          text-uppercase"
+        key={good.id}
+        data-cy="good"
+        style={{ color: good.color }}
+      >
         {good.name}
       </li>
     ))}
