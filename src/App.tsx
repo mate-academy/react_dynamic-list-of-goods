d5061a4db86ff217a2ffe98fea7985effdbf80c4
Illia Vodonis <illavodonis@gmail.com>
2022-07-25 14:14:25 +0300

    dynamic list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { get5First, getAll, getRedGoods } from './api/goods';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
 // import { getAll, get5First, getRed } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  return (
+    <div className="App">
+      <div className="columns">
+        <div className="column is-three-fifths is-offset-one-fifth">
+          <div className="panel is-info">
+            <div className="panel-heading">
+              <h1 className="card-header-title">Dynamic list of Goods</h1>
+            </div>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+            <div className="panel-tabs is-8">
+              <button
+                type="button"
+                data-cy="all-button"
+                className="button is-info is-inverted"
+                onClick={async () => {
+                  setGoods(await getAll());
+                }}
+              >
+                Load all goods
+              </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+              <button
+                type="button"
+                data-cy="first-five-button"
+                className="button is-info is-inverted"
+                onClick={async () => {
+                  setGoods(await get5First());
+                }}
+              >
+                Load 5 first goods
+              </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+              <button
+                type="button"
+                data-cy="red-button"
+                className="button is-info is-inverted"
+                onClick={async () => {
+                  setGoods(await getRedGoods());
+                }}
+              >
+                Load red goods
+              </button>
+            </div>
+            <GoodsList goods={goods} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
