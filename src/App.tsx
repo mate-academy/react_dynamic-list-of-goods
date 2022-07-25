f87ea06650f146d2627a0b6b861a0b80aac3e739
Ostap Salo <jlumon211@gmail.com>
2022-07-25 12:34:22 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bulma';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [itemsGoods, setItemsGoods]: any[] = useState([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleAllGoods = () => {
+    getAll().then(allArray => setItemsGoods(allArray));
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleFiveElGoods = () => {
+    get5First().then(firstFiveElArray => setItemsGoods(firstFiveElArray));
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const onlyRedElGood = () => {
+    getRedGoods().then(onlyRedArray => setItemsGoods(onlyRedArray));
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1 className="App__title has-text-link">
+        Dynamic list of Goods
+      </h1>
+
+      <div className="App__button-container">
+        <button
+          className="button is-link is-outlined"
+          type="button"
+          data-cy="all-button"
+          value={itemsGoods}
+          onClick={handleAllGoods}
+        >
+          Load all goods
+        </button>
+
+        <button
+          className="button is-link is-outlined"
+          type="button"
+          data-cy="first-five-button"
+          value={itemsGoods}
+          onClick={handleFiveElGoods}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          className="button is-link is-outlined"
+          type="button"
+          data-cy="red-button"
+          value={itemsGoods}
+          onClick={onlyRedElGood}
+        >
+          Load red goods
+        </button>
+      </div>
+
+      <GoodsList goods={itemsGoods} />
+    </div>
+  );
+};
