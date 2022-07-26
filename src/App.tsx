0972eee20b6e7fcd14fb3b1c64df69c3697eed81
Julia Makarenko <julys.makarenko@gmail.com>
2022-07-26 21:07:40 +0300

    Add dynamic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import 'bulma';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [Goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const goodsEnter = async (callback: () => Promise<Good[]>) => {
+    const info = await callback();
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    setGoods(info);
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+      <div className="App__buttons">
+        <button
+          className="button is-focused"
+          type="button"
+          data-cy="all-button"
+          onClick={() => goodsEnter(getAll)}
+        >
+          Load all goods
+        </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+        <button
+          className="button is-focused"
+          type="button"
+          data-cy="first-five-button"
+          onClick={() => goodsEnter(get5First)}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          className="button is-focused"
+          type="button"
+          data-cy="red-button"
+          onClick={() => goodsEnter(getRed)}
+        >
+          Load red goods
+        </button>
+      </div>
+      <GoodsList goods={Goods} />
+    </div>
+  );
+};
