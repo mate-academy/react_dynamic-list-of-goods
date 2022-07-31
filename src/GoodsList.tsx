d7dfd3f675d2efcd79ad2f32f832c6301bfe09a5
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-07-31 12:48:26 +0300

    first commit

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { Good } from './types/Good';
 
 type Props = {
-  goods: Good[]
+  goods: Good[] | null;
 };
 
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
-    {goods.map(good => (
-      <li key={good.id} data-cy="good">
-        {good.name}
-      </li>
-    ))}
+    {
+      goods
+      && goods.map(good => (
+        <li
+          key={good.id}
+          data-cy="good"
+          style={{ color: `${good.color}` }}
+        >
+          {good.name}
+        </li>
+      ))
+    }
   </ul>
 );
