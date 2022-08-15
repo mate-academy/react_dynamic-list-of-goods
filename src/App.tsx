c8170648c4e5c414286363526ed3f65ef0af068f
Oleksii Budnik <budnikoleksii@gmail.com>
2022-08-15 18:40:30 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,74 @@
-import React from 'react';
+import { FC, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+enum GoodsType {
+  None,
+  All,
+  FirstFive,
+  Red,
+}
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
+  const [selectedGoods, setSelectedGoods] = useState(GoodsType.None);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const loadGoods = (apiCallBack: () => Promise<Good[]>, type: GoodsType) => {
+    setErrorMessage('');
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    if (selectedGoods !== type) {
+      setIsLoading(true);
+      apiCallBack()
+        .then(goodsFromServer => {
+          setVisibleGoods(goodsFromServer);
+          setIsLoading(false);
+        })
+        .catch(error => setErrorMessage(error));
+      setSelectedGoods(type);
+    }
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const loadAll = () => loadGoods(getAll, GoodsType.All);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  const loadFirstFive = () => loadGoods(get5First, GoodsType.FirstFive);
+
+  const loadRedGoods = () => loadGoods(getRedGoods, GoodsType.Red);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={loadAll}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={loadFirstFive}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={loadRedGoods}
+      >
+        Load red goods
+      </button>
+
+      {!isLoading && !errorMessage && (<GoodsList goods={visibleGoods} />)}
+      {isLoading && (<div>Loading...</div>)}
+      {errorMessage && (<div>Something gone wrong... Please, try later</div>)}
+    </div>
+  );
+};
