73d0e0d04932ae7e523ded46368ead66c843773e
DimaShm <shmagindmitry@gmail.com>
2022-08-16 19:44:11 +0300

    Solution react_dynamic-list-of-goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,64 @@
 import React from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+type State = {
+  goods: Good[],
+};
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export class App extends React.Component<{}, State> {
+  state = {
+    goods: [],
+  };
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  loadAllGoods = async () => {
+    await getAll()
+      .then(goods => this.setState({ goods }));
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  load5FirstGoods = async () => {
+    await get5First()
+      .then(goods => this.setState({ goods }));
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  loadRedGoods = async () => {
+    await getRedGoods()
+      .then(goods => this.setState({ goods }));
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  render() {
+    return (
+      <div className="App">
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          data-cy="all-button"
+          onClick={this.loadAllGoods}
+        >
+          Load all goods
+        </button>
+
+        <button
+          type="button"
+          data-cy="first-five-button"
+          onClick={this.load5FirstGoods}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          data-cy="red-button"
+          onClick={this.loadRedGoods}
+        >
+          Load red goods
+        </button>
+
+        <GoodsList goods={this.state.goods} />
+      </div>
+    );
+  }
+}
