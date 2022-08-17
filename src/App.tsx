bf0136563fff36839662dc6982b9a854b2b78525
Sergii Maniurov <maniurov_sv@ukr.net>
2022-08-17 13:41:36 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import * as goodsAPI from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
+type SelectedType = 'getAll' | 'get5First' | 'getRedGoods' | null;
+
+// import { getAll, get5First, getRedGoods } from './api/goods';
 // or
-// import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [selectedGoods, setSelectedGoods] = useState<SelectedType>(null);
+  const [error, setError] = useState(null);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    if (selectedGoods) {
+      goodsAPI[selectedGoods]()
+        .then(_goods => setGoods(_goods))
+        .catch(err => setError(err));
+    }
+  }, [selectedGoods]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setSelectedGoods('getAll')}
+      >
+        Load all goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setSelectedGoods('get5First')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setSelectedGoods('getRedGoods')}
+      >
+        Load red goods
+      </button>
+
+      { goods.length > 0 && <GoodsList goods={goods} />}
+
+      { error !== null && (
+        <p>{`Здається я заблукав! ${error}`}</p>
+      )}
+    </div>
+  );
+};
