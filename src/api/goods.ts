14c24c631c9f4f545c175c2989beaa13dd383b7e
silchyn <ivan.silchin@gmail.com>
2022-08-19 18:12:48 +0300

    added dynamic list of goods

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,17 +3,18 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export const getAll = (): Promise<Good[]> => {
   return fetch(API_URL)
     .then(response => response.json());
-}
+};
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods
+      .sort((a: Good, b: Good) => a.name.localeCompare(b.name)).slice(0, 5));
 };
 
-export const getRedGoods = () => {
+export const getRed = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods.filter((good: Good) => good.color === 'red'));
 };
