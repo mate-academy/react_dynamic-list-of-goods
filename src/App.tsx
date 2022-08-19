80c9d3035fa2321c41f798177a78588709028ecc
Oleg Vetrov <oleg94038@gmail.com>
2022-08-19 17:43:40 +0300

    Added dinamic list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,80 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import cn from 'classnames';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import 'bulma';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [isLoading, setIsLoading] = useState('');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handlerGetGoods = useCallback(async (amount: string) => {
+    setIsLoading(amount);
+    switch (amount) {
+      case 'all':
+        getAll().then(allGoods => {
+          setGoods(allGoods);
+          setIsLoading('');
+        });
+        break;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      case 'five':
+        get5First().then(firstFiveGoods => {
+          setGoods(firstFiveGoods);
+          setIsLoading('');
+        });
+        break;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case 'red':
+        getRedGoods().then(redGoods => {
+          setGoods(redGoods);
+          setIsLoading('');
+        });
+        break;
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      default:
+        break;
+    }
+  }, []);
+
+  return (
+    <div className="App p-5">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        className={cn('button is-primary',
+          { 'is-loading': isLoading === 'all' })}
+        type="button"
+        data-cy="all-button"
+        onClick={() => handlerGetGoods('all')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        className={cn('button is-info',
+          { 'is-loading': isLoading === 'five' })}
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handlerGetGoods('five')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        className={cn('button is-danger',
+          { 'is-loading': isLoading === 'red' })}
+        type="button"
+        data-cy="red-button"
+        onClick={() => handlerGetGoods('red')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
