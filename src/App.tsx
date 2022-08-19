7c107b49be9a643be9bb00c0ca9895f8813dc980
Nikita Biriukov <flimsy00@gmail.com>
2022-08-19 16:40:23 +0300

    deleted comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,11 @@ import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
   function loadGoodsByCondition(request: () => Promise<Good[]>) {
-    // switch (condition) {
-    //   case getAll:
-    //     getAll().then(visibleGoods => setGoods(visibleGoods));
-    //     break;
-
-    //   case get5First:
-    //     get5First().then(visibleGoods => setGoods(visibleGoods));
-    //     break;
-
-    //   case getRedGoods:
-    //     getRedGoods().then((visibleGoods) => setGoods(visibleGoods));
-    //     break;
-
-    //   default:
-    //     break;
-    // }
-
     request().then(visibleGoods => setGoods(visibleGoods));
   }
 
@@ -47,9 +28,7 @@ export const App: React.FC = () => {
         type="button"
         data-cy="first-five-button"
         onClick={() => loadGoodsByCondition(get5First)}
-        // onClick={
-        //   () => get5First().then(visibleGoods => setGoods(visibleGoods))
-        // }
+
       >
         Load 5 first goods
       </button>
@@ -58,9 +37,6 @@ export const App: React.FC = () => {
         type="button"
         data-cy="red-button"
         onClick={() => loadGoodsByCondition(getRedGoods)}
-        // onClick={
-        //   () => getRedGoods().then((visibleGoods) => setGoods(visibleGoods))
-        // }
       >
         Load red goods
       </button>
