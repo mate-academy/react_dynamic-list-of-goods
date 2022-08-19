783ae9d4ac006685999a5d9884c921c31c69a6e6
Nikita Biriukov <flimsy00@gmail.com>
2022-08-19 16:29:27 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  function loadGoodsByCondition(request: () => Promise<Good[]>) {
+    // switch (condition) {
+    //   case getAll:
+    //     getAll().then(visibleGoods => setGoods(visibleGoods));
+    //     break;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    //   case get5First:
+    //     get5First().then(visibleGoods => setGoods(visibleGoods));
+    //     break;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    //   case getRedGoods:
+    //     getRedGoods().then((visibleGoods) => setGoods(visibleGoods));
+    //     break;
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    //   default:
+    //     break;
+    // }
+
+    request().then(visibleGoods => setGoods(visibleGoods));
+  }
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => loadGoodsByCondition(getAll)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => loadGoodsByCondition(get5First)}
+        // onClick={
+        //   () => get5First().then(visibleGoods => setGoods(visibleGoods))
+        // }
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => loadGoodsByCondition(getRedGoods)}
+        // onClick={
+        //   () => getRedGoods().then((visibleGoods) => setGoods(visibleGoods))
+        // }
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
