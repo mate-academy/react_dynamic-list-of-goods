907adf3e88076a1404fe8d4a6be7b6e4634f998e
Yura Lysenko <yuralysenko1@gmail.com>
2022-08-21 22:03:27 +0300

    reworked

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import { getAll, get5First, getRedGoods } from './api/goods';
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
+  const loadGoods = (promise: () => Promise<Good[]>) => {
+    promise()
+      .then(goodsFromServer => setGoods(goodsFromServer));
+  };
+
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
@@ -15,8 +20,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="all-button"
-        onClick={() => getAll()
-          .then(goodsFromServer => setGoods(goodsFromServer))}
+        onClick={() => loadGoods(getAll)}
       >
         Load all goods
       </button>
@@ -24,8 +28,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={() => get5First()
-          .then(goodsFromServer => setGoods(goodsFromServer))}
+        onClick={() => loadGoods(get5First)}
       >
         Load 5 first goods
       </button>
@@ -33,8 +36,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="red-button"
-        onClick={() => getRedGoods()
-          .then(goodsFromServer => setGoods(goodsFromServer))}
+        onClick={() => loadGoods(getRedGoods)}
       >
         Load red goods
       </button>
