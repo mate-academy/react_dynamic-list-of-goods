90120fcac55701fb35e03ab880007964ece9d8ab
Oleksìi <bardyshev@gmail.com>
2022-08-21 12:30:36 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[] | null>(null);
+  const [hasLoadingError, setLoadingError] = useState(false);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  return (
+    <div className="App container">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      <div className="buttons">
+        <button
+          type="button"
+          className="button btn btn-outline-primary"
+          data-cy="all-button"
+          onClick={() => {
+            getAll()
+              .then(goodsList => setGoods(goodsList))
+              .catch(() => setLoadingError(true));
+          }}
+        >
+          Load all goods
+        </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+        <button
+          type="button"
+          className="button btn btn-outline-primary"
+          data-cy="first-five-button"
+          onClick={() => {
+            get5First()
+              .then(goodsList => setGoods(goodsList))
+              .catch(() => setLoadingError(true));
+          }}
+        >
+          Load 5 first goods
+        </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+        <button
+          className="button btn btn-outline-primary"
+          type="button"
+          data-cy="red-button"
+          onClick={() => {
+            getRed()
+              .then(goodsList => setGoods(goodsList))
+              .catch(() => setLoadingError(true));
+          }}
+        >
+          Load red goods
+        </button>
+      </div>
+
+      {!hasLoadingError
+        ? (goods && <GoodsList goods={goods} />)
+        : (
+          <div>
+            Something went wrong, try later
+          </div>
+        )}
+    </div>
+  );
+};
