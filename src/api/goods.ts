9d46372b33b6618417580ad5b60d8683dda3dc53
Kate <krasnova2912@gmail.com>
2022-08-22 15:47:00 +0300

    comments were delited

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -18,5 +18,5 @@ export const get5First = () => {
 export const getRedGoods = () => {
   return getAll()
     .then(goods => goods
-      .filter(good => good.color === 'red')); // get only red
+      .filter(good => good.color === 'red'));
 };
