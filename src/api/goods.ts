a0a565713a9d35eea0e4398b80ecbc85dfd46372
Mariya Sukhanova <suhanovamasha21@gmail.com>
2022-08-22 12:53:00 +0300

    Added solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,10 +10,18 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => {
+      const newGoods = [...goods].sort(
+        (a, b) => a.name.localeCompare(b.name),
+      );
+
+      newGoods.length = 5;
+
+      return newGoods;
+    });
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods.filter(good => good.color === 'red'));
 };
