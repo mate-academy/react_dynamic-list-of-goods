2a3c85dd154fbd36f223fe8e0f5da12dd3588399
Oleh Butenko <6083341@gmail.com>
2022-08-22 17:36:02 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [goodsRequest, setGoodsRequest] = useState('');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    switch (goodsRequest) {
+      case 'all':
+        getAll()
+          .then(setGoods);
+        break;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      case 'first 5':
+        get5First()
+          .then(setGoods);
+        break;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case 'red':
+        getRedGoods()
+          .then(setGoods);
+        break;
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      default:
+        break;
+    }
+  }, [goodsRequest]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setGoodsRequest('all')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setGoodsRequest('first 5')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setGoodsRequest('red')}
+      >
+        Load red goods
+      </button>
+
+      {goods.length > 0 && <GoodsList goods={goods} />}
+    </div>
+  );
+};
