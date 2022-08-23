2c707fc40f9ecd702d8ad8822b618060470c788e
lentyaeev <daniilshaliuk@gmail.com>
2022-08-23 21:58:54 +0300

    Fixxed

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,7 +10,6 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods)
     .then(goods => goods.sort((a, b) => a.name.localeCompare(b.name)))
     .then(goods => goods.slice(0, 5));
 };
