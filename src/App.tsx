c6a2778db37662e8abfdbd5ca8e51a566eb3f1fb
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-08-29 14:04:05 +0300

    dinamic-list

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,88 @@ import React from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { get5First, getAll, getRedGoods } from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+// import { Good } from './types/Good';
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+// export const App: React.FC = () => (
+//   <div className="App">
+//     <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+//     <button type="button" data-cy="all-button">
+//       Load all goods
+//     </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+//     <button type="button" data-cy="first-five-button">
+//       Load 5 first goods
+//     </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+//     <button type="button" data-cy="red-button">
+//       Load red goods
+//     </button>
+
+//     <GoodsList goods={[]} />
+//   </div>
+// );
+
+// type State = {
+//   goods: Good[],
+// };
+
+export class App extends React.Component {
+  state = {
+    goods: [],
+  };
+
+  loadGoods = async () => {
+    const goods = await getAll();
+
+    this.setState({ goods });
+  };
+
+  load5First = async () => {
+    const goods = await get5First();
+
+    this.setState({ goods });
+  };
+
+  loadColorRed = async () => {
+    const goods = await getRedGoods();
+
+    this.setState({ goods });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          data-cy="all-button"
+          onClick={this.loadGoods}
+        >
+          Load all goods
+        </button>
+
+        <button
+          type="button"
+          data-cy="first-five-button"
+          onClick={this.load5First}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          data-cy="red-button"
+          onClick={this.loadColorRed}
+        >
+          Load red goods
+        </button>
+
+        <GoodsList goods={this.state.goods} />
+      </div>
+    );
+  }
+}
