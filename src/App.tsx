056564609f59bb2227e23591312479e71459456a
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-08-30 18:12:14 +0300

    list-of-goods-2

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,6 @@ import { GoodsList } from './GoodsList';
 
 import { get5First, getAll, getRedGoods } from './api/goods';
 
-// import { Good } from './types/Good';
-
-// export const App: React.FC = () => (
-//   <div className="App">
-//     <h1>Dynamic list of Goods</h1>
-
-//     <button type="button" data-cy="all-button">
-//       Load all goods
-//     </button>
-
-//     <button type="button" data-cy="first-five-button">
-//       Load 5 first goods
-//     </button>
-
-//     <button type="button" data-cy="red-button">
-//       Load red goods
-//     </button>
-
-//     <GoodsList goods={[]} />
-//   </div>
-// );
-
-// type State = {
-//   goods: Good[],
-// };
-
 export class App extends React.Component {
   state = {
     goods: [],
