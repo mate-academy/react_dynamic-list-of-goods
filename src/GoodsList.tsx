38b990c27ef7701d50d6b34e8f85cdd5f720bfbb
Nikita Havryliuk <chinter.games1@gmail.com>
2022-09-15 18:20:55 +0300

    used destructuring

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -7,15 +7,19 @@ type Props = {
 
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
-    {goods.map(good => (
+    {goods.map(({
+      id,
+      name,
+      color,
+    }) => (
       <li
-        key={good.id}
+        key={id}
         data-cy="good"
         style={{
-          color: good.color,
+          color,
         }}
       >
-        {good.name}
+        {name}
       </li>
     ))}
   </ul>
