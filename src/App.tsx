db0b715aeff417b3ecc6df78f9ea042077d09514
Vadls <vadim_tanasienko54541@ukr.net>
2022-09-23 15:23:16 +0300

    Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import './App.scss';
 import { GoodsList } from './GoodsList';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
 import { Good } from './types/Good';
 
 export const App: React.FC = () => {
@@ -16,26 +14,6 @@ export const App: React.FC = () => {
         setGoods(value);
       });
   };
-
-  // const handleGet5First = () => {
-  //   goodsAPI.get5First() => {
-
-  //   }
-  // }
-
-  // const handleGetAll = async () => {
-  //   try {
-  //     const response = await goodsAPI.getAll();
-
-  //     setGoods(response);
-  //   } catch (error) {
-  //     console.log(error);
-  //   } finally {
-  //     console.log('finally');
-  //   }
-  // };
-
-  // console.log(goods);
 
   return (
     <div className="App">
