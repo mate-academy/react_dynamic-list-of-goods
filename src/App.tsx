874c060b9576c0ef718e5dbba80cc11ee3a7e794
Kostya Konev <kosgoh.214@gmail.com>
2022-09-25 18:55:39 +0300

    solved 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+/* eslint-disable */
 import './App.scss';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { useState } from 'react';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-const App: React.FC = () => (
-  <h1>Dynamic list of Goods</h1>
-);
+  return (
+    <>
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        onClick={() => {
+          getAll()
+            .then(products => setGoods(products));
+        }}
+      >
+        Load all goods
+      </button>
+
+      <button
+        onClick={() => {
+          get5First()
+            .then(products => setGoods(products));
+        }}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        onClick={() => {
+          getRedGoods()
+            .then(products => setGoods(products));
+        }}
+      >
+        Load red goods
+      </button>
+
+      {goods.length !== 0 && (
+        <ul>
+          {goods.map(good => (
+            <li key={good.id}>
+              <span className={`good ${good.color}`}>
+                {good.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
 
 export default App;
