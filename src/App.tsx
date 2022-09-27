df6abe463f47123d7853e1719d83292ea1f4f3d2
Max Kravchenko <max.kravchenko@gmail.com>
2022-09-27 16:58:55 +0300

    Make React dynamic list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getRedGoods, get5First, getAll } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [readyGoods, addGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleRedGoodsLoad = async () => {
+    const goods = await getRedGoods();
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    addGoods(goods);
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleSortedGoodsLoad = async () => {
+    const goods = await get5First();
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    addGoods(goods);
+  };
+
+  const handleAllGoodsLoad = async () => {
+    const goods = await getAll();
+
+    addGoods(goods);
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={handleAllGoodsLoad}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleSortedGoodsLoad}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={handleRedGoodsLoad}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={readyGoods} />
+    </div>
+  );
+};
