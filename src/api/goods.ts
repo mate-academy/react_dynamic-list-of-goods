ecf44d00558b77f1b449276a839b0da6d1a41631
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-10-06 21:36:54 +0700

    Task done

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,17 +3,27 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export async function getAll(): Promise<Good[]> {
   return fetch(API_URL)
     .then(response => response.json());
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => {
+      goods.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+
+      return goods.slice(0, 5);
+    });
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => {
+      return goods.filter(good => {
+        return good.color === 'red';
+      });
+    });
 };
