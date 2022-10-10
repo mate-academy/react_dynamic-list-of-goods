483db2847cb9a9d85b313a3373d679210e569e63
Elina Bahirova <eli4ka.bagirova@gmail.com>
2022-10-10 11:20:44 +0300

    Generic type used, state initialized removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,11 @@ import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
 import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
 
 export const App: React.FC = () => {
-  const [visibleGoods, setVisibleGoods] = useState([{
-    id: 0,
-    name: '',
-    color: '',
-  }]);
-  const [inicialized, setInicialized] = useState(false);
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>();
 
   return (
     <div className="App">
@@ -23,7 +17,6 @@ export const App: React.FC = () => {
         data-cy="all-button"
         onClick={async () => {
           await goodsAPI.getAll().then(goods => setVisibleGoods(goods));
-          setInicialized(true);
         }}
       >
         Load all goods
@@ -34,7 +27,6 @@ export const App: React.FC = () => {
         data-cy="first-five-button"
         onClick={async () => {
           await goodsAPI.get5First().then(goods => setVisibleGoods(goods));
-          setInicialized(true);
         }}
       >
         Load 5 first goods
@@ -45,13 +37,12 @@ export const App: React.FC = () => {
         data-cy="red-button"
         onClick={async () => {
           await goodsAPI.getRedGoods().then(goods => setVisibleGoods(goods));
-          setInicialized(true);
         }}
       >
         Load red goods
       </button>
 
-      {inicialized && <GoodsList goods={visibleGoods} />}
+      {visibleGoods && <GoodsList goods={visibleGoods} />}
     </div>
   );
 };
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Good } from './types/Good';
 
 type Props = {
-  goods: Good[]
+  goods: Good[];
 };
 
 export const GoodsList: React.FC<Props> = ({ goods }) => (
