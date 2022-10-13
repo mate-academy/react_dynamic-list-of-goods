d98943ef7ab8dc1fad34f55b6c1a6c46d4e9201d
Max Bovtun <maxbovtun@gmail.com>
2022-10-13 17:13:59 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from 'react';
+import { Good } from './types/Good';
 import { GoodsList } from './GoodsList';
+import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const goodsInitial: Good[] = [];
+  const [goods, setGoods] = useState(goodsInitial);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => {
+          getAll()
+            .then((data) => {
+              setGoods(data);
+            }).catch(() => {
+              throw new Error('Error, sorry :(');
+            });
+        }}
+      >
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => {
+          get5First()
+            .then((data) => {
+              setGoods(data);
+            }).catch(() => {
+              throw new Error('Error, sorry :(');
+            });
+        }}
+      >
+        Load 5 first goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => {
+          getRedGoods()
+            .then((data) => {
+              setGoods(data);
+            }).catch(() => {
+              throw new Error('Error, sorry :(');
+            });
+        }}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
