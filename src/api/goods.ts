a5619204bd88a1be3b7d5edade4ba05d33baa589
Datsenko Mariia <datsenko.mate@gmail.com>
2022-10-29 00:31:05 +0300

    dynamic list of goods has been created

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,17 +3,24 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export function getAllGoods(): Promise<Good[]> {
   return fetch(API_URL)
     .then(response => response.json());
 }
 
-export const get5First = () => {
-  return getAll()
-    .then(goods => goods); // sort and get the first 5
+export const get5FirstGoods = () => {
+  return getAllGoods()
+    .then(goods => {
+      return goods
+        .sort((g1, g2) => g1.name.localeCompare(g2.name))
+        .slice(0, 5);
+    });
 };
 
 export const getRedGoods = () => {
-  return getAll()
-    .then(goods => goods); // get only red
+  return getAllGoods()
+    .then(goods => {
+      return goods
+        .filter(good => good.color === 'red');
+    });
 };
