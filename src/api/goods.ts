5971b039c73ebfe56732556af3880a78785539e5
Killdex <kdcompany2018@gmail.com>
2022-10-29 15:59:48 +0300

    dynamic list of goods completed

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,19 +1,25 @@
 import { Good } from '../types/Good';
-
-// eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+import { request } from '../urils';
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
+  return request();
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .filter((good, index) => {
+        if (index < 5) {
+          return good;
+        }
+
+        return null;
+      }));
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods
+      .filter(good => good.color === 'red'));
 };
