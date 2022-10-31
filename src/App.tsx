8817a50e78566e9d16aa3ca0faaec33b95fc8ae3
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-10-31 15:22:14 +0200

    Implemented the dynamic list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bulma/css/bulma.css';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const handleAllGoods = () => {
+    getAll()
+      .then(goodsFromServer => {
+        setGoods(goodsFromServer);
+      });
+  };
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleFirst5Goods = () => {
+    get5First()
+      .then(goodsFromServer => {
+        setGoods(goodsFromServer);
+      });
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleRedGoods = () => {
+    getRedGoods()
+      .then(goodsFromServer => {
+        setGoods(goodsFromServer);
+      });
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1 className="title is-3">Dynamic list of Goods</h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        className="button is-light"
+        type="button"
+        data-cy="all-button"
+        onClick={handleAllGoods}
+      >
+        Load all goods
+      </button>
+
+      <button
+        className="button is-light"
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleFirst5Goods}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        className="button is-light"
+        type="button"
+        data-cy="red-button"
+        onClick={handleRedGoods}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
