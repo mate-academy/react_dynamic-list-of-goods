1e6ccf3fc579574bcd45f2b19f3a15d58fae2e57
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-11-02 18:10:20 +0200

    added destructing and handles

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,33 @@ import { getAll, get5First, getRedGoods } from './api/goods';
 
 export const App: React.FC = () => {
   const [list, setList] = useState<Good[]>([]);
+  const handleLoadAll = () => {
+    getAll().then((goods) => setList(goods));
+  };
+
+  const handleLoad5First = async () => {
+    setList(await get5First().then((goods) => goods.slice(5, -1)));
+  };
+
+  const handleLoadRedGoods = async () => {
+    setList(
+      await getRedGoods()
+        .then((goods) => goods.filter((good) => good.color === 'red')),
+    );
+  };
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={() => {
-          getAll().then((goods) => setList(goods));
-        }}
-      >
+      <button type="button" data-cy="all-button" onClick={handleLoadAll}>
         Load all goods
       </button>
 
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={async () => {
-          setList(await get5First().then((goods) => goods.slice(5, -1)));
-        }}
+        onClick={handleLoad5First}
       >
         Load 5 first goods
       </button>
@@ -37,12 +43,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={async () => {
-          setList(
-            await getRedGoods()
-              .then((goods) => goods.filter((good) => good.color === 'red')),
-          );
-        }}
+        onClick={handleLoadRedGoods}
       >
         Load red goods
       </button>
