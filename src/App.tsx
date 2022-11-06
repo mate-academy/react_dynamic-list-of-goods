3bf83cb61881a01051cd06c438eb027e127b883e
Oleksandr Korobko <oleksandr.korobko@gmail.com>
2022-11-06 22:45:12 +0200

    implemented dynamic list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bulma';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+type GetGoodsCallback = () => Promise<Good[]>;
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const setGoodsFromServer = (getGoods: GetGoodsCallback) => {
+    getGoods()
+      .then(newGoods => setGoods(newGoods))
+      .catch(error => {
+        throw new Error(error);
+      });
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  return (
+    <div className="App hero is-danger is-fullheight">
+      <div className="hero-body is-align-items-flex-start">
+        <div>
+          <h1 className="title is-1">
+            Dynamic list of Goods
+          </h1>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+          <div className="buttons block">
+            <button
+              type="button"
+              className="button is-primary mr-5"
+              data-cy="all-button"
+              onClick={() => setGoodsFromServer(getAll)}
+            >
+              Load all goods
+            </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+            <button
+              type="button"
+              className="button is-primary mr-5"
+              data-cy="first-five-button"
+              onClick={() => setGoodsFromServer(get5First)}
+            >
+              Load 5 first goods
+            </button>
+
+            <button
+              type="button"
+              className="button is-primary"
+              data-cy="red-button"
+              onClick={() => setGoodsFromServer(getRedGoods)}
+            >
+              Load red goods
+            </button>
+          </div>
+
+          {goods.length > 0 && (
+            <GoodsList goods={goods} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -6,10 +6,10 @@ type Props = {
 };
 
 export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
-    {goods.map(good => (
-      <li key={good.id} data-cy="good">
-        {good.name}
+  <ul className="box">
+    {goods.map(({ id, name, color }) => (
+      <li key={id} data-cy="good" style={{ color }}>
+        {name}
       </li>
     ))}
   </ul>
