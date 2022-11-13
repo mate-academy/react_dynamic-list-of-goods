6bd74ee4618d50e2499afde73d78d040849f7f04
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-11-14 01:07:50 +0800

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 import './App.scss';
 import { GoodsList } from './GoodsList';
-
+import 'bulma';
+// import { getAll } from './api/goods';
 // import { getAll, get5First, getRed } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
+import { getInfoApi } from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [todosAll, setTodosAll] = useState([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const [loadingAll, setLoadingAll] = useState(false);
+  const [loadingFiveGoods, setLoadingFiveGoods] = useState(false);
+  const [loadingRedGoods, setLoadingRedGoods] = useState(false);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  // useEffect(() => {
+  //   getAll()
+  //     .then(todos => {
+  //       setTodos(todos);
+  //     });
+  // }, []);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleLoadAllGoods = async () => {
+    setLoadingAll(true);
+    const listOfTodos = await getInfoApi();
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    setTodosAll(listOfTodos);
+
+    setLoadingAll(false);
+
+    // const todoList = await getAll();
+
+    // setTodos(todoList);
+  };
+
+  const handleFiveGoods = async () => {
+    setLoadingFiveGoods(true);
+    setTodosAll([]);
+
+    // const todos = await getAll();
+
+    // setTodos({ todos });
+
+    // console.log('the button was clicked');
+    // eslint-disable-next-line no-alert
+    alert("I'm an alert");
+    // getAll()
+    //   .then(todos => {
+    //     setTodos(todos);
+    //   });
+  };
+
+  const handleRedGoods = () => {
+    setLoadingRedGoods(true);
+    setTodosAll([]);
+
+    // const todos = await getAll();
+
+    // setTodos({ todos });
+
+    // console.log('the button was clicked');
+    // eslint-disable-next-line no-alert
+    alert("I'm an alert");
+    // getAll()
+    //   .then(todos => {
+    //     setTodos(todos);
+    //   });
+  };
+
+  return (
+    <div className="App">
+      <h1 className="title is-2">Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        className={classnames(
+          'button is-primary is-medium', { 'is-loading': loadingAll },
+        )}
+        onClick={handleLoadAllGoods}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        className={classnames(
+          'button is-link is-medium', { 'is-loading': loadingFiveGoods },
+        )}
+        onClick={handleFiveGoods}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        className={classnames(
+          'button is-danger is-medium', { 'is-loading': loadingRedGoods },
+        )}
+        onClick={handleRedGoods}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={todosAll} />
+    </div>
+  );
+};
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,19 +1,104 @@
-import { Good } from '../types/Good';
+// import { resolve } from 'path';
+// import { Good } from '../types/Good';
 
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
-}
+// export function getAll(): Promise<Good[]> {
+//   return fetch(API_URL)
+//     .then(response => {
+//       if (!response.ok) {
+//         // return Promise.reject('Can\'t load todos');
+//         throw new Error(`${response.status} - ${response.statusText}`);
+//       }
 
-export const get5First = () => {
-  return getAll()
-    .then(goods => goods); // sort and get the first 5
+//       return response.json();
+//     })
+//     .then(result => result.data);
+// }
+
+// export const getTodos = () => {
+//   return getAll();
+// };
+// =================
+
+// export const getAll = async () => {
+//   await fetch(API_URL)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`${response.status} - ${response.statusText}`);
+//       }
+
+//       return response.json();
+//     });
+
+//   // .then(result => result.data);
+// };
+// =======================
+
+export const getInfoApi = async () => {
+  const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error(`${response.status} - ${response.statusText}`);
+  }
+
+  const body = await response.json();
+
+  return body;
+  // await fetch(API_URL)
+  //   .then(response => {
+  //     response.json();
+  //   });
 };
 
-export const getRedGoods = () => {
-  return getAll()
-    .then(goods => goods); // get only red
-};
+// export function getAll(): Promise<Good[]> {
+//   return fetch(API_URL)
+//     .then(response => response.json());
+// }
+
+// export const get5First = () => {
+//   return getAll()
+//     .then(goods => goods); // sort and get the first 5
+// };
+
+// export const getRedGoods = () => {
+//   return getAll()
+//     .then(goods => goods); // get only red
+// };
+
+// fetch(API_URL)
+//   .then(response => response.json())
+//   .then(goods => {
+//     console.log(goods);
+//   });
+
+// function wait(delay) {
+//   return new Promise(resolve => setTimeout(resolve, delay));
+// }
+
+// export const request = (url, options) => {
+//   return fetch(`${API_URL}${url}`, options)
+//     .then(res => {
+//       if (!res.ok) {
+//         throw new Error(`${res.status} - ${res.statusText}`)
+//       }
+
+//       return res.json();
+//     });
+// };
+
+// export function getAll(): Promise<Good[]> {
+//   return fetch(API_URL)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`${response.status} - ${response.statusText}`)
+//       }
+
+//       return response.json();
+//     }
+// }
+
+// export const getTodos = () = {
+//   return request ('/goods')
+// }
