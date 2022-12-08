20a5ab7c3333eca697df7fdef23dc79943e1a445
Svitlana Yudina <osbka31@gmail.com>
2022-12-08 10:54:35 +0200

    fixes v02

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,12 +10,9 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => {
-      goods
-        .sort((good1, good2) => good1.name.localeCompare(good2.name));
-
-      return goods.slice(0, 5);
-    });
+    .then(goods => goods
+      .sort((good1, good2) => good1.name.localeCompare(good2.name))
+      .slice(0, 5));
 };
 
 export const getRedGoods = () => {
