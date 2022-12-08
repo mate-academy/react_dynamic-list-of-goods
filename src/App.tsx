e174dd0170af9b68674310061f5363565b087df1
Masha <teenderrr@gmail.com>
2022-12-08 10:46:07 +0200

    fixed2

--- src/App.tsx
+++ src/App.tsx
@@ -50,8 +50,7 @@ export const App: React.FC = () => {
         Load red goods
       </button>
 
-      {goods.length > 0
-        && <GoodsList goods={goods} />}
+      {goods.length > 0 && <GoodsList goods={goods} />}
     </div>
   );
 };
