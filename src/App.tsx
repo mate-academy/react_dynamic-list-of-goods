00d644cb80c63c7d56f7c279c9af5e22c3c621bd
Demut <uddarendeli1@gmail.com>
2022-12-29 05:52:14 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { get5First, getAll, getRedGoods } from './api/goods';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+type SortType = 'All' | 'FirstFive' | 'OnlyRed' | '';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [sortType, setSortType] = useState<SortType>('');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const sortGoods = () => {
+    switch (sortType) {
+      case 'All':
+        getAll().then(goodsFromServer => setGoods(goodsFromServer));
+        break;
+      case 'FirstFive':
+        get5First().then(goodsFromServer => setGoods(goodsFromServer));
+        break;
+      case 'OnlyRed':
+        getRedGoods().then(goodsFromServer => setGoods(goodsFromServer));
+        break;
+      default:
+        setGoods([]);
+    }
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  useEffect(() => {
+    sortGoods();
+  }, [sortType]);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setSortType('All')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setSortType('FirstFive')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setSortType('OnlyRed')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -2,18 +2,33 @@ import { Good } from '../types/Good';
 
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+// let allGoods: Promise<Good[]>;
 
+// Tests required me to send a new request for each click. So commented these out.
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
     .then(response => response.json());
+//  if (!allGoods) {
+  // return fetch(API_URL)
+  //   .then(response => {
+  //     allGoods = response.json();
+  //   })
+  //   .then(() => {
+  //     return allGoods;
+  //   });
+//  }
+//  return allGoods;
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods
+      .sort((good1, good2) => good1.name.localeCompare(good2.name)))
+    .then(goods => goods.slice(0, 5));
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods
+      .filter(good => good.color === 'red')); // get only red
 };
