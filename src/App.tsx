475eb54d56e90ef5162c7a99b1f85219d90f6fe4
Kolya Movchan <kmovchan261@gmail.com>
2023-01-12 22:02:23 +0200

    react_dynamic-list-of-goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
 import './App.scss';
+
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const initial: Good[] = [];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const [goods, setGoods] = useState(initial);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const updateState = (modifiedArray: Good[]) => setGoods(modifiedArray);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleAllGoods = () => getAll().then(updateState);
+  const handle5Goods = () => get5First().then(updateState);
+  const handleRedGoods = () => getRedGoods().then(updateState);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={handleAllGoods}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handle5Goods}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={handleRedGoods}
+      >
+        Load red goods
+      </button>
+
+      {goods.length
+        ? <GoodsList goods={goods} />
+        : (
+          <div className="empty-data">
+            Data from Server is not loaded yet
+          </div>
+        )}
+
+    </div>
+  );
+};
