153cb66b70a85e6be11802e59ad107ca68ac6441
Dima Kolesnyk <kolesnyk.dmytro.victorovych@gmail.com>
2023-01-17 15:40:12 +0200

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import 'bulma/css/bulma.css';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const handleLoadAllGoodsClick = () => {
+    getAll()
+      .then(AllGoods => setGoods(AllGoods));
+  };
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleFirst5GoodsClick = async () => {
+    setGoods(await get5First());
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleRedGoodsClick = async () => {
+    setGoods(await getRedGoods());
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div
+      className="App is-flex
+        is-justify-content-center
+        is-align-items-start"
+    >
+      <div className="wrapper">
+        <h1
+          className="title is-1 has-text-white mt-5"
+        >
+          Dynamic list of Goods
+        </h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+        <button
+          className="button is-dark mr-3 mb-3"
+          type="button"
+          data-cy="all-button"
+          onClick={handleLoadAllGoodsClick}
+        >
+
+          Load all goods
+        </button>
+
+        <button
+          className="button is-dark mr-3"
+          type="button"
+          data-cy="first-five-button"
+          onClick={handleFirst5GoodsClick}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          className="button is-dark"
+          type="button"
+          data-cy="red-button"
+          onClick={handleRedGoodsClick}
+        >
+          Load red goods
+        </button>
+
+        <GoodsList goods={goods} />
+      </div>
+    </div>
+  );
+};
