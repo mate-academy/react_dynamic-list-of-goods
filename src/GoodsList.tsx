2011e90e20fb00576876c794aa73def46b8869eb
Ninel Levchenko <blackmaplearts@gmail.com>
2023-01-17 17:54:43 +0200

    solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Good } from './types/Good';
+import './styles/GoodsList.scss';
 
 type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
+export const GoodsList: React.FC<Props> = memo(({ goods }) => (
+  <ul className="list">
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li
+        key={good.id}
+        data-cy="good"
+        className="list__item"
+        style={{ color: `${good.color}` }}
+      >
         {good.name}
       </li>
     ))}
   </ul>
-);
+));
