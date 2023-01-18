3c6a69037700217cfb2836177ad67bfc52f8b355
Vladimir Arkanov <vladimirarkanov@icloud.com>
2023-01-18 13:41:40 +0200

    first commit

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { memo } from 'react';
+import './GoodsList.scss';
 import { Good } from './types/Good';
 
 type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
+export const GoodsList: React.FC<Props> = memo(({ goods }) => (
+  <ul className="list">
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li
+        key={good.id}
+        data-cy="good"
+        style={{
+          color: good.color,
+          listStyle: 'none',
+          marginBottom: '5px',
+        }}
+      >
         {good.name}
       </li>
     ))}
   </ul>
-);
+));
