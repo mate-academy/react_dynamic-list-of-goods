1a8c851173e2ef8ef8b6cf12822a59aeb0537bbf
Andrii Petrinko <apetrinko@gmail.com>
2023-01-18 16:52:33 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState, memo } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import 'bulma/css/bulma.css';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = memo(() => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const handleGetGoods = (loadedGoods: Promise<Good[]>) => (
+    loadedGoods
+      .then(setGoods)
+  );
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  return (
+    <div className="App">
+      <h1 className="title is-1 has-text-centered">Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      <div className="App__buttons box has-text-centered">
+        <button
+          type="button"
+          data-cy="all-button"
+          className="button is-primary is-rounded"
+          onClick={() => handleGetGoods(getAll())}
+        >
+          Load all goods
+        </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+        <button
+          type="button"
+          data-cy="first-five-button"
+          className="button is-link is-rounded"
+          onClick={() => handleGetGoods(get5First())}
+        >
+          Load 5 first goods
+        </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+        <button
+          type="button"
+          data-cy="red-button"
+          className="button is-danger is-rounded"
+          onClick={() => handleGetGoods(getRed())}
+        >
+          Load red goods
+        </button>
+
+      </div>
+
+      {!goods.length ? (
+        <p className="text">Press the button, please</p>
+      ) : (
+        <GoodsList goods={goods} />
+      )}
+    </div>
+  );
+});
