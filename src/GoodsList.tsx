403a0b4fc91297c1bd66626cdb8414829091bc1b
Veronika Yasinska <yasinskaa.veronika@gmail.com>
2023-01-18 16:15:50 +0200

    added memo

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Good } from './types/Good';
 
 type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
+export const GoodsList: React.FC<Props> = memo(({ goods }) => (
   <ul>
     {goods.map(good => (
       <li
@@ -17,4 +17,4 @@ export const GoodsList: React.FC<Props> = ({ goods }) => (
       </li>
     ))}
   </ul>
-);
+));
