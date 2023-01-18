0aac9096a8a4e54b8ed3a372b4a3049d870a5e1d
Anatolii <batstolya@gmail.com>
2023-01-18 12:08:30 +0200

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,6 @@ export const App: React.FC = () => {
     promise
       .then((json) => {
         setGoods(json);
-
-        return json;
       })
       .catch((error) => {
         return error;
