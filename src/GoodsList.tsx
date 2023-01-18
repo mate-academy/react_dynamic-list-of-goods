bf17b56abc0a4224be977399f632091f593a17d2
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-01-18 17:37:48 +0200

    solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -6,20 +6,22 @@ type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
-    {goods.map(good => (
-      <li
-        key={good.id}
-        data-cy="good"
-        className={cn({
-          'has-text-primary': good.color === 'green',
-          'has-text-link': good.color === 'blue',
-          'has-text-danger': good.color === 'red',
-        })}
-      >
-        {good.name}
-      </li>
-    ))}
-  </ul>
+export const GoodsList: React.FC<Props> = React.memo(
+  ({ goods }) => (
+    <ul>
+      {goods.map(good => (
+        <li
+          key={good.id}
+          data-cy="good"
+          className={cn({
+            'has-text-primary': good.color === 'green',
+            'has-text-link': good.color === 'blue',
+            'has-text-danger': good.color === 'red',
+          })}
+        >
+          {good.name}
+        </li>
+      ))}
+    </ul>
+  ),
 );
