2d62167160fe93e6bdf7a09d555c9b67689a5636
Mykhailyk-Vadym <ihappyspam@gmail.com>
2023-01-19 00:21:30 +0200

    added a bit of styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
+import 'bulma';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
 // or
@@ -16,31 +17,35 @@ export const App: React.FC = () => {
 
   return (
     <div className="App">
-      <h1>Dynamic list of Goods</h1>
+      <h1 className="title is-1">Dynamic list of Goods</h1>
+      <div className="buttons is-centered">
+        <button
+          type="button"
+          className="button is-info is-rounded is-light"
+          data-cy="all-button"
+          onClick={() => handleButtonClick(getAll())}
+        >
+          Load all goods
+        </button>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={() => handleButtonClick(getAll())}
-      >
-        Load all goods
-      </button>
+        <button
+          type="button"
+          className="button is-link is-rounded is-light"
+          data-cy="first-five-button"
+          onClick={() => handleButtonClick(get5First())}
+        >
+          Load 5 first goods
+        </button>
 
-      <button
-        type="button"
-        data-cy="first-five-button"
-        onClick={() => handleButtonClick(get5First())}
-      >
-        Load 5 first goods
-      </button>
-
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={() => handleButtonClick(getRedGoods())}
-      >
-        Load red goods
-      </button>
+        <button
+          type="button"
+          className="button is-danger is-rounded is-light"
+          data-cy="red-button"
+          onClick={() => handleButtonClick(getRedGoods())}
+        >
+          Load red goods
+        </button>
+      </div>
 
       <GoodsList goods={goods} />
     </div>
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,5 +1,6 @@
 import React, { memo } from 'react';
 import { Good } from './types/Good';
+import 'bulma';
 
 type Props = {
   goods: Good[]
@@ -8,7 +9,11 @@ type Props = {
 export const GoodsList: React.FC<Props> = memo(({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={good.id} data-cy="good" className={good.color}>
+      <li
+        key={good.id}
+        data-cy="good"
+        className={`goods ${good.color}`}
+      >
         {good.name}
       </li>
     ))}
