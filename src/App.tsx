b58aaf01e8c3e287516a76805d5cea59e6b2661f
Anton Martynes <martynes1997@gmail.com>
2023-01-18 19:20:37 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleButtonClick = (getGoods: Promise<Good[]>) => (
+    getGoods.then(setGoods)
+  );
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      <Button
+        type="button"
+        variant="outlined"
+        data-cy="all-button"
+        onClick={() => handleButtonClick(getAll())}
+      >
+        Load all goods
+      </Button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <Button
+        type="button"
+        variant="outlined"
+        data-cy="first-five-button"
+        onClick={() => handleButtonClick(get5First())}
+      >
+        Load 5 first goods
+      </Button>
+
+      <Button
+        type="button"
+        variant="outlined"
+        data-cy="red-button"
+        onClick={() => handleButtonClick(getRedGoods())}
+      >
+        Load red goods
+      </Button>
+
+      <Button
+        type="button"
+        variant="outlined"
+        startIcon={<DeleteIcon />}
+        onClick={() => setGoods([])}
+      >
+        Clear
+      </Button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
