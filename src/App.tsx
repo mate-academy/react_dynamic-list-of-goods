d757b763e1fbc8b8f95f9cf5d9022c0775d9045c
OLEXANDRA KOROL <sandrakorol3@gmail.com>
2023-01-19 16:53:37 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { get5First, getAll, getRedGoods } from './api/goods';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import 'bulma/css/bulma.css';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+// стейт - на кожен клік міняємо дані з сервера - передаємо їх як пропси
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleClickAll = () => {
+    getAll().then((data) => setGoods(data))
+  }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  const handleClickMax5 = () => {
+    get5First().then((data) => setGoods(data))
+  }
+  const handleClickAllRed = () => {
+    getRedGoods().then((data) => setGoods(data))
+  }
+
+  return (
+    < div className="App" >
+      <h1 className='title'>Dynamic list of Goods</h1>
+
+      <button
+        className='button margin is-info'
+        type="button"
+        data-cy="all-button"
+        onClick={handleClickAll}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleClickMax5}
+        className='button margin is-info'
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        className='button margin is-info'
+        type="button"
+        data-cy="red-button"
+        onClick={handleClickAllRed}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div >
+  )
+}
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,19 +1,20 @@
-import { Good } from '../types/Good';
+import { Good } from "../types/Good";
 
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => response.json())
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods.sort((a, b) => a.name.localeCompare(b.name)))
+    .then(good => good.slice(0, 5)); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods.filter((good) => good.color === 'red')); // get only red
 };
