2082ef79a47d2f1e80ff52a01dc05b007552a8da
Olha Mamai <olgga.ju@gmail.com>
2023-01-20 12:10:26 +0200

    fixed solution & styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,63 @@
+/* eslint-disable max-len */
 import React, { useState } from 'react';
 import './App.scss';
+
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
-
 import { getAll, get5First, getRedGoods } from './api/goods';
 
 export const App: React.FC = () => {
   const [goodsList, setGoodsList] = useState<Good[]>([]);
 
-  const loadAllGoods = async () => {
-    const allGoods = await getAll();
+  const handleClick = async (callback: () => Promise<Good[]>) => {
+    const visibleGoods = await callback();
 
-    setGoodsList(allGoods);
+    setGoodsList(visibleGoods);
   };
 
-  const load5Goods = async () => {
-    const first5Goods = await get5First();
-
-    setGoodsList(first5Goods);
-  };
-
-  const loadRedGoods = async () => {
-    const redGoods = await getRedGoods();
-
-    setGoodsList(redGoods);
-  };
+  const buttons = [
+    <Button
+      type="button"
+      data-cy="all-button"
+      onClick={() => handleClick(getAll)}
+    >
+      Load all goods
+    </Button>,
+    <Button
+      type="button"
+      data-cy="first-five-button"
+      onClick={() => handleClick(get5First)}
+    >
+      Load 5 first goods
+    </Button>,
+    <Button
+      type="button"
+      data-cy="red-button"
+      onClick={() => handleClick(getRedGoods)}
+    >
+      Load red goods
+    </Button>,
+  ];
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
-
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={() => loadAllGoods()}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
       >
-        Load all goods
-      </button>
+        <ButtonGroup size="small" aria-label="small button group">
+          {buttons}
+        </ButtonGroup>
 
-      <button
-        type="button"
-        data-cy="first-five-button"
-        onClick={() => load5Goods()}
-      >
-        Load 5 first goods
-      </button>
-
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={() => loadRedGoods()}
-      >
-        Load red goods
-      </button>
-
-      <GoodsList goods={goodsList} />
+        <GoodsList goods={goodsList} />
+      </Box>
     </div>
   );
 };
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import './GoodsList.scss';
 import { Good } from './types/Good';
 
@@ -6,8 +6,8 @@ type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
+export const GoodsList: React.FC<Props> = memo(({ goods }) => (
+  <ul className="list">
     {goods.map(good => (
       <li
         key={good.id}
@@ -18,4 +18,4 @@ export const GoodsList: React.FC<Props> = ({ goods }) => (
       </li>
     ))}
   </ul>
-);
+));
