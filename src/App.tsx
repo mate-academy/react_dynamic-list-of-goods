1df1c96fa1828e4c044e44ebc9bbbaa528c6758f
Dariia Skoryk <skorykdariia@gmail.com>
2023-01-21 22:50:57 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const handleButtonSubmit = async (callback: Promise<Good[]>) => {
+    setVisibleGoods(await callback);
+  };
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  return (
+    <div className="App">
+      <h1 className="App__title">Dynamic list of Goods</h1>
+      <div className="App__container">
+        <button
+          className="App__button"
+          type="button"
+          data-cy="all-button"
+          onClick={() => handleButtonSubmit(getAll())}
+        >
+          Load all goods
+        </button>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+        <button
+          className="App__button"
+          type="button"
+          data-cy="first-five-button"
+          onClick={() => handleButtonSubmit(get5First())}
+        >
+          Load 5 first goods
+        </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
-
-    <GoodsList goods={[]} />
-  </div>
-);
+        <button
+          className="App__button"
+          type="button"
+          data-cy="red-button"
+          onClick={() => handleButtonSubmit(getRedGoods())}
+        >
+          Load red goods
+        </button>
+      </div>
+      <GoodsList goods={visibleGoods} />
+    </div>
+  );
+};
