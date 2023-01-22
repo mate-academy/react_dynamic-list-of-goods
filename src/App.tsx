dfe6624e6bbe9a4c3bad9ad4ee0a8acf64672c42
Anastasiia (Nelly) <116572743+anastasiia-roiek@users.noreply.github.com>
2023-01-22 12:56:14 +0200

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import { GoodsList } from './GoodsList';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
 import { Good } from './types/Good';
+import 'bulma/css/bulma.min.css';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
-  const handleGetGoods = (getGoods: Promise<Good[]>) => {
-    getGoods.then(setGoods);
+  const handleGetGoods = (loadedGoods: Promise<Good[]>) => {
+    loadedGoods.then(setGoods);
   };
 
   return (
     <div className="App">
-      <h1>Dynamic list of Goods</h1>
+      <h1
+        className="title is-1"
+      >
+        Dynamic list of Goods
+      </h1>
 
       <button
+        className="button is-info is-light"
         type="button"
         data-cy="all-button"
         onClick={() => handleGetGoods(getAll())}
@@ -28,6 +34,7 @@ export const App: React.FC = () => {
         type="button"
         data-cy="first-five-button"
         onClick={() => handleGetGoods(get5First())}
+        className="button is-success is-light"
       >
         Load 5 first goods
       </button>
@@ -36,6 +43,7 @@ export const App: React.FC = () => {
         type="button"
         data-cy="red-button"
         onClick={() => handleGetGoods(getRedGoods())}
+        className="button is-danger is-light"
       >
         Load red goods
       </button>
