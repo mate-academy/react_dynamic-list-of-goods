5fdc51277288e731f3a6a00c62822237dd1e4fbb
TetKopot <tet.kopot@gmail.com>
2023-01-25 02:03:40 +0200

    fixed

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -11,7 +11,7 @@ export function getAll(): Promise<Good[]> {
 export const get5First = () => {
   return getAll()
     .then(goods => {
-      return goods.sort().slice(0, 5);
+      return goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
     });
 };
 
