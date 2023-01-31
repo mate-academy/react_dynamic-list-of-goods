cc0311291b2c02e154c7a6632b5b5ebbfab46902
Elias Montauti <egsm147@gmail.com>
2023-01-31 11:29:39 +0000

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -6,45 +6,23 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
-  const isEmpty = goods.length === 0;
-  const areRed = goods.every((good) => good.color === 'red');
 
   const handleLoadAllGoods = async () => {
     const loadedGoods = await getAll();
 
-    return (isEmpty || goods.length === 5)
-      ? (
-        setGoods(loadedGoods)
-      )
-      : (
-        setGoods([])
-      );
+    setGoods(loadedGoods);
   };
 
   const handlerFirstFive = async () => {
     const loadedGoods = await get5First();
 
-    if (isEmpty || goods.length > 5) {
-      setGoods(loadedGoods);
-    } else if (goods.length === 5) {
-      if (!areRed) {
-        setGoods([]);
-      }
-
-      setGoods(loadedGoods);
-    }
+    setGoods(loadedGoods);
   };
 
   const handlerRedGoods = async () => {
     const loadedGoods = await getRedGoods();
 
-    return (isEmpty || !areRed)
-      ? (
-        setGoods(loadedGoods)
-      )
-      : (
-        setGoods([])
-      );
+    setGoods(loadedGoods);
   };
 
   return (
