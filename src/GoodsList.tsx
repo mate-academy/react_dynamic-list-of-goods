a3d9bbb6de98b718d4379323636eb901ad81fc17
Драгота Инна <dragotaIV@gmail.com>
2023-02-07 22:42:42 +0200

    third

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -8,11 +8,13 @@ type Props = {
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={good.id}
+      <li
+        key={good.id}
         data-cy="good"
         style={{
           color: good.color,
-        }}>
+        }}
+      >
         {good.name}
       </li>
     ))}
