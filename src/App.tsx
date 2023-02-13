ad58532bc67f9af8413178770f9014bf93673023
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-02-13 21:48:17 +0200

    Task done. Have a good time!)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import 'bulma';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import { get5First, getAll, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const getData = async (method:string) => {
+    let data;
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+    switch (method) {
+      case 'all':
+        data = await getAll();
+        setGoods(data);
+        break;
+      case 'firstFive':
+        data = await get5First();
+        setGoods(data);
+        break;
+      case 'onlyRed':
+        data = await getRedGoods();
+        setGoods(data);
+        break;
+      default:
+        throw new Error('no such data');
+    }
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  return (
+    <div className="App">
+      <h1 className="title is-1">Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        className="button is-info is-light is-outlined"
+        onClick={() => getData('all')}
+      >
+        Load all goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="first-five-button"
+        className="button is-primary is-light is-outlined"
+        onClick={() => getData('firstFive')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        className="button is-danger is-light is-outlined"
+        onClick={() => getData('onlyRed')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
