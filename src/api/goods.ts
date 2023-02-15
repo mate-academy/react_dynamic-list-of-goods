083f753ac3d57bb48f2d9482d08f8034503af5d6
Anastasiia-Khudych <anastasiiakhudych@gmail.com>
2023-02-15 09:00:47 +0200

    refactor code

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -11,13 +11,12 @@ export function getAll(): Promise<Good[]> {
 export const get5First = () => {
   return getAll()
     .then(goods => goods)
-    .then(goods => [...goods]
+    .then(goods => goods
       .sort((good1, good2) => good1.name.localeCompare(good2.name))
       .slice(0, 5));
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods)
     .then(goods => goods.filter(good => good.color === 'red'));
 };
