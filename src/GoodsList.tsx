6e222fa549bdc25912bb657061c15c30b8c9eb7a
Yuliia Andrieieva <andrieieva.y@gmail.com>
2023-02-15 14:01:04 +0200

    Fixed

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -5,21 +5,23 @@ type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <div className="content">
-    <ul className="mt-3">
-      {goods.map(good => (
-        <li
-          className="ml-6"
-          key={good.id}
-          data-cy="good"
-          style={{
-            color: good.color,
-          }}
-        >
-          {good.name}
-        </li>
-      ))}
-    </ul>
-  </div>
+export const GoodsList: React.FC<Props> = React.memo(
+  ({ goods }) => (
+    <div className="content">
+      <ul className="mt-3">
+        {goods.map(good => (
+          <li
+            className="ml-6"
+            key={good.id}
+            data-cy="good"
+            style={{
+              color: good.color,
+            }}
+          >
+            {good.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  ),
 );
