a1f8a868210a1cbd07231bc0381237bf8a2fbfa8
Yurii Demkiv <demkiv123@gmail.com>
2023-02-16 11:06:31 +0200

    Added list component optimization

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -5,22 +5,24 @@ type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
-    {goods.map(good => {
-      const { id, color, name } = good;
+export const GoodsList: React.FC<Props> = React.memo(
+  ({ goods }) => (
+    <ul>
+      {goods.map(good => {
+        const { id, color, name } = good;
 
-      return (
-        (
-          <li
-            key={id}
-            data-cy="good"
-            style={{ color }}
-          >
-            {name}
-          </li>
-        )
-      );
-    })}
-  </ul>
+        return (
+          (
+            <li
+              key={id}
+              data-cy="good"
+              style={{ color }}
+            >
+              {name}
+            </li>
+          )
+        );
+      })}
+    </ul>
+  ),
 );
