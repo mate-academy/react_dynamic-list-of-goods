b80ed4b2efe347300aad9db91ad501128572af58
miamarichka <ilchyshyn.marichka@gmail.com>
2023-02-18 00:14:23 +0300

    dolution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Good } from './types/Good';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goodsToRender, setGoodsToRender] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  async function clickHandler(event:React.MouseEvent<HTMLButtonElement>) {
+    const target = event?.target as HTMLButtonElement;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    if (!target) {
+      return;
+    }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    let goods:Good[];
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    switch (target.dataset.cy) {
+      case 'all-button':
+        goods = await getAll();
+        break;
+      case 'first-five-button':
+        goods = await get5First();
+        break;
+      case 'red-button':
+        goods = await getRedGoods();
+        break;
+      default:
+        goods = [];
+    }
+
+    setGoodsToRender(goods);
+  }
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={clickHandler}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={clickHandler}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={clickHandler}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goodsToRender} />
+    </div>
+  );
+};
