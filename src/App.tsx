6bf8ec1a01adc18072c21f308a221b7200af6899
maasiimka <mrozisalovec@gmail.com>
2023-02-17 16:13:03 +0200

    Carried handlers to separate functions, added handing of errors

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,24 @@ import { getAll, get5First, getRedGoods } from './api/goods';
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
+  const getAllGoods = () => {
+    getAll()
+      .then(setGoods)
+      .catch(() => alert('Can\'t find goods'));
+  };
+
+  const get5FirstGoods = () => {
+    get5First()
+      .then(setGoods)
+      .catch(() => alert('Can\'t find first five goods'));
+  };
+
+  const getRed = () => {
+    getRedGoods()
+      .then(setGoods)
+      .catch(() => alert('Can\'t find red goods'));
+  };
+
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
@@ -15,7 +33,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="all-button"
-        onClick={() => getAll().then(setGoods)}
+        onClick={getAllGoods}
       >
         Load all goods
       </button>
@@ -23,7 +41,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={() => get5First().then(setGoods)}
+        onClick={get5FirstGoods}
       >
         Load 5 first goods
       </button>
@@ -31,7 +49,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="red-button"
-        onClick={() => getRedGoods().then(setGoods)}
+        onClick={getRed}
       >
         Load red goods
       </button>
