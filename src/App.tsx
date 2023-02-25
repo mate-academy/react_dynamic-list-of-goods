03eceff5fb81b8bb11511e26f80ffed48147705d
Yevhen Veselov <veselovyevhen@gmail.com>
2023-02-25 18:48:02 +0200

    react_dynamic-list-of-goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [hasLoadingError, setHasLoadingError] = useState(false);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleClickGetAll = async () => {
+    try {
+      const visibleGoods = await getAll();
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      setGoods(visibleGoods);
+      setHasLoadingError(false);
+    } catch {
+      setHasLoadingError(true);
+    }
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleClickGet5First = async () => {
+    try {
+      const visibleGoods = await get5First();
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      setGoods(visibleGoods);
+      setHasLoadingError(false);
+    } catch {
+      setHasLoadingError(true);
+    }
+  };
+
+  const handleClickGetRedGoods = async () => {
+    try {
+      const visibleGoods = await getRedGoods();
+
+      setGoods(visibleGoods);
+      setHasLoadingError(false);
+    } catch {
+      setHasLoadingError(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={handleClickGetAll}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleClickGet5First}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={handleClickGetRedGoods}
+      >
+        Load red goods
+      </button>
+
+      {hasLoadingError
+        ? <p>No goods yet</p>
+        : <GoodsList goods={goods} />}
+    </div>
+  );
+};
