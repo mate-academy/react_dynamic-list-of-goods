df568acdbe632b642410f7aee23f95218d855631
ShkarupaDenys <shkarupadenys@gmail.com>
2023-03-02 16:13:14 +0200

    Solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,17 +3,14 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
-}
+export const getAll = (): Promise<Good[]> => fetch(API_URL)
+  .then(response => response.json());
 
-export const get5First = () => {
-  return getAll()
-    .then(goods => goods); // sort and get the first 5
-};
+export const get5First = () => getAll()
+  .then(goods => goods
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .slice(0, 5));
 
-export const getRedGoods = () => {
-  return getAll()
-    .then(goods => goods); // get only red
-};
+export const getRedGoods = () => getAll()
+  .then(goods => goods
+    .filter(({ color }) => color === 'red'));
