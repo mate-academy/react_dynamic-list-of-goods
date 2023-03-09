03a1eb9212bfbabe2b22ef9e599fa42583235373
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-03-09 18:26:09 +0200

    did the task

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,17 +3,41 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export function getAll(): Promise<Good[] | void> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(response.status);
+      }
+
+      return response.json();
+    })
+    .then(result => result)
+    .catch(error => error);
 }
 
-export const get5First = () => {
+export const get5First = (): Promise<Good[] | void> => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => {
+      if (!goods) {
+        return Promise.reject(goods);
+      }
+
+      return goods.sort().slice(0, 4);
+    })
+    .then(result => result)
+    .catch(error => error);
 };
 
-export const getRedGoods = () => {
+export const getRed = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => {
+      if (!goods) {
+        return Promise.reject(goods);
+      }
+
+      return goods.filter(good => good.color === 'red');
+    })
+    .then(result => result)
+    .catch(error => error);
 };
