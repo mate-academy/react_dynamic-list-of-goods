89096b5e4fd47d749904c4201e97a65f9c23ddc3
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-03-11 18:58:32 +0200

    dynamic list of goods ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,78 @@
-import React from 'react';
+import { FC, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { get5First, getAll, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+enum SortType {
+  ALL,
+  FIRST5,
+  RED,
+}
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const getGoods = (param: SortType): void => {
+    switch (param) {
+      case SortType.ALL:
+        getAll()
+          .then(newGoods => {
+            setGoods(newGoods);
+          });
+        break;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case SortType.FIRST5:
+        get5First()
+          .then(newGoods => {
+            setGoods(newGoods);
+          });
+        break;
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      case SortType.RED:
+        getRedGoods()
+          .then(newGoods => {
+            setGoods(newGoods);
+          });
+        break;
+
+      default:
+        throw new Error('unknown sort type');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => getGoods(SortType.ALL)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => getGoods(SortType.FIRST5)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => getGoods(SortType.RED)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
