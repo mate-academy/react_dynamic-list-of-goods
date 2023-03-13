adb043fe064268fbca5c69203990c74bf34de220
Vasyl Pavlenko <genlus@ukr.net>
2023-03-13 18:03:08 +0200

    Catches for api were added

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
@@ -5,16 +6,33 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch goods data');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
+      throw error;
+    });
 }
 
 export const get5First = () => {
   return getAll()
     .then(goods => goods.sort((a, b) => a.name.localeCompare(b.name))
-      .slice(0, 5));
+      .slice(0, 5))
+    .catch(error => {
+      console.error(error);
+      throw error;
+    });
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods.filter(good => good.color === 'red'));
+    .then(goods => goods.filter(good => good.color === 'red'))
+    .catch(error => {
+      console.error(error);
+      throw error;
+    });
 };
