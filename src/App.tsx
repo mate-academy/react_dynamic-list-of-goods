6b3a4dd827879e4bffb92ac47901671c7ce2d218
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-03-17 15:58:30 +0200

    enum map added + universal handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,72 @@
 import { FC, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
 import { get5First, getAll, getRedGoods } from './api/goods';
-import { Good } from './types/Good';
-// or
-// import * as goodsAPI from './api/goods';
 
 enum SortType {
-  ALL,
-  FIRST5,
-  RED,
+  NONE = '',
+  ALL = 'all-button',
+  FIRST5 = 'first-five-button',
+  RED = 'red-button',
 }
+
+const buttonNames = {
+  [SortType.NONE]: '',
+  [SortType.ALL]: 'Load all goods',
+  [SortType.FIRST5]: 'Load 5 first goods',
+  [SortType.RED]: 'Load red goods',
+};
 
 export const App: FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
-  const [sortType, setSortType] = useState<SortType>();
+  const [sortType, setSortType] = useState<SortType>(SortType.NONE);
 
   const getGoods = async (promise: Promise<Good[]>): Promise<void> => {
     setGoods(await promise);
+  };
+
+  const handleButtonClick = (newSortType: SortType) => {
+    if (sortType === newSortType) {
+      return;
+    }
+
+    setSortType(newSortType);
+
+    switch (newSortType) {
+      case SortType.ALL:
+        getGoods(getAll());
+        break;
+
+      case SortType.FIRST5:
+        getGoods(get5First());
+        break;
+
+      case SortType.RED:
+        getGoods(getRedGoods());
+        break;
+
+      default:
+        setSortType(SortType.NONE);
+    }
   };
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={() => {
-          if (sortType !== SortType.ALL) {
-            setSortType(SortType.ALL);
-            getGoods(getAll());
-          }
-        }}
-      >
-        Load all goods
-      </button>
-
-      <button
-        type="button"
-        data-cy="first-five-button"
-        onClick={() => {
-          if (sortType !== SortType.FIRST5) {
-            setSortType(SortType.FIRST5);
-            getGoods(get5First());
-          }
-        }}
-      >
-        Load 5 first goods
-      </button>
-
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={() => {
-          if (sortType !== SortType.RED) {
-            setSortType(SortType.RED);
-            getGoods(getRedGoods());
-          }
-        }}
-      >
-        Load red goods
-      </button>
+      {Object.values(SortType).filter(Boolean).map((currentSortType) => (
+        <button
+          type="button"
+          data-cy={currentSortType}
+          onClick={() => {
+            handleButtonClick(currentSortType);
+          }}
+        >
+          {buttonNames[currentSortType]}
+        </button>
+      ))}
 
       <GoodsList goods={goods} />
     </div>
