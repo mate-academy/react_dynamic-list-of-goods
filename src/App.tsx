7d66c30c1294b0e41c5f0aab5a2e535e0868ff19
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-03-21 09:41:30 +0200

    commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  useEffect(() => {
+    getAll()
+      .then(setGoods);
+  }, []);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const loadAll = async () => {
+    const goodsFromServer = await getAll();
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    setGoods(goodsFromServer);
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const load5First = async () => {
+    const goodsFromServer = await get5First();
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    setGoods(goodsFromServer);
+  };
+
+  const loadRedGoods = async () => {
+    const goodsFromServer = await getRedGoods();
+
+    setGoods(goodsFromServer);
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={loadAll}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={load5First}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={loadRedGoods}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
