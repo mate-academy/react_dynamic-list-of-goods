13d3e269bb21f248d8b311e88e151191e1136d50
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-03-24 00:00:22 +0200

    Added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-
+import classnames from 'classnames';
 import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
-
-import './App.scss';
 
 enum GoodsType {
   Default,
@@ -38,45 +36,83 @@ export const App: React.FC = () => {
   };
 
   return (
-    <div className="App">
-      <h1>Dynamic list of Goods</h1>
+    <div className="App ">
+      <div className="columns m-3 is-flex is-justify-content-center">
+        <div className="column is-half">
+          <article className="panel is-link">
+            <h1 className="panel-heading has-text-centered">
+              Dynamic list of Goods
+            </h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={getLoadHandler(getAll, GoodsType.All)}
-      >
-        Load all goods
-      </button>
+            <div
+              className="
+              panel-block
+              buttons
+              buttons-are-small
+              m-0
+              is-flex
+              is-justify-content-center
+              px-6
+              pt-5
+              pb-2"
+            >
+              <button
+                className={classnames('button', 'is-link', {
+                  'is-loading': isLoading && currentGoodsType === GoodsType.All,
+                })}
+                type="button"
+                data-cy="all-button"
+                onClick={getLoadHandler(getAll, GoodsType.All)}
+              >
+                Load all goods
+              </button>
 
-      <button
-        type="button"
-        data-cy="first-five-button"
-        onClick={getLoadHandler(get5First, GoodsType.FirstFive)}
-      >
-        Load 5 first goods
-      </button>
+              <button
+                className={classnames('button', 'is-link', 'mx-4', {
+                  'is-loading':
+                    isLoading && currentGoodsType === GoodsType.FirstFive,
+                })}
+                type="button"
+                data-cy="first-five-button"
+                onClick={getLoadHandler(get5First, GoodsType.FirstFive)}
+              >
+                Load 5 first goods
+              </button>
 
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={getLoadHandler(getRedGoods, GoodsType.OnlyRed)}
-      >
-        Load red goods
-      </button>
+              <button
+                className={classnames('button', 'is-link', {
+                  'is-loading':
+                    isLoading && currentGoodsType === GoodsType.OnlyRed,
+                })}
+                type="button"
+                data-cy="red-button"
+                onClick={getLoadHandler(getRedGoods, GoodsType.OnlyRed)}
+              >
+                Load red goods
+              </button>
+            </div>
 
-      {isLoadingError ? (
-        <p className="text">An error occurred while loading goods!</p>
-      ) : (
-        <>
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : (
-            <GoodsList goods={goods} />
-          )}
-        </>
-      )}
-
+            {isLoadingError ? (
+              <div
+                className="
+                  block
+                  has-background-danger-light
+                  is-flex
+                  is-justify-content-center
+                  is-flex-grow-1
+                  px-6
+                  py-5"
+              >
+                <p className="has-text-danger">
+                  An error occurred while loading goods!
+                </p>
+              </div>
+            ) : (
+              <>{goods.length > 0 && <GoodsList goods={goods} />}</>
+            )}
+          </article>
+        </div>
+      </div>
     </div>
   );
 };
