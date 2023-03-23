d08b6c571668880896655ced9d0a8e44a7fd7b47
Lilia Obushenko <liliaobushenko@gmail.com>
2023-03-23 21:57:43 +0200

    Some optimization was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
-import { FC, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 import './App.scss';
 import { Box } from '@mui/material';
-import { FilterButton } from './Button';
-import { GoodsList } from './GoodsList';
+
+import { FilterButton } from './components/Button/Button';
+import { GoodsList } from './components/Goodlist/GoodsList';
+import { Loader } from './components/Loader/Loader';
+
 import { getAll, get5First, getRed } from './api/goods';
 import { Good } from './types/Good';
 
 export const App: FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [isFetched, setIsFetched] = useState(false);
+  const [lastClickedButton, setLastClickedButton] = useState<string>('');
 
-  const getAllGoods = () => {
-    getAll()
-      .then(result => setGoods(result));
-  };
+  const handleClick = useCallback(
+    async (callback: () => Promise<Good[]>, buttonName: string) => {
+      if (isFetched || lastClickedButton === buttonName) {
+        return;
+      }
 
-  const getFirstFiveGoods = () => {
-    get5First()
-      .then(result => setGoods(result));
-  };
+      setIsLoading(true);
+      setIsFetched(true);
 
-  const getRedGoods = () => {
-    getRed()
-      .then(result => setGoods(result));
-  };
+      try {
+        setGoods(await callback());
+        setLastClickedButton(buttonName);
+      } catch {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+        setIsFetched(false);
+      }
+    }, [lastClickedButton, isFetched],
+  );
 
   return (
     <Box
@@ -33,32 +46,44 @@ export const App: FC = () => {
       }}
     >
       <div className="App">
-        <h1 style={{ color: '#ff5722' }}>
+        <h1 style={{ color: '#42a5f5' }}>
           Dynamic list of Goods
         </h1>
 
         <FilterButton
           dataCy="all-button"
-          onClick={getAllGoods}
+          onClick={() => handleClick(getAll, 'all-button')}
         >
           Load all goods
         </FilterButton>
 
         <FilterButton
           dataCy="first-five-button"
-          onClick={getFirstFiveGoods}
+          onClick={() => handleClick(get5First, 'first-five-button')}
         >
           Load 5 first goods
         </FilterButton>
 
         <FilterButton
           dataCy="red-button"
-          onClick={getRedGoods}
+          onClick={() => handleClick(getRed, 'red-button')}
         >
           Load red goods
         </FilterButton>
 
-        <GoodsList goods={goods} />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            {error && (
+              <p style={{ color: 'red' }}>
+                Error. Something went wrong
+              </p>
+            )}
+
+            <GoodsList goods={goods} />
+          </>
+        )}
       </div>
     </Box>
   );
---
+++ src/components/Goodlist/GoodsList.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material';
+import { Good } from '../../types/Good';
+
+type Props = {
+  goods: Good[]
+};
+
+export const GoodsList: React.FC<Props> = React.memo(
+  ({ goods }) => (
+    <List
+      sx={{
+        width: '100%',
+        position: 'relative',
+        overflow: 'auto',
+        maxHeight: 250,
+        '& ul': { padding: 0 },
+      }}
+      subheader={<li />}
+    >
+      <ul>
+        {goods.map(good => {
+          const { id, name, color } = good;
+
+          return (
+            <ListItem
+              key={id}
+              style={{ color }}
+              data-cy="good"
+            >
+              <ListItemText primary={name} />
+            </ListItem>
+          );
+        })}
+      </ul>
+    </List>
+  ),
+);
