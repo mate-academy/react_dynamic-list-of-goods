3627903f4febd0eb910155c765b46297970a8606
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-03-31 16:14:31 +0300

    Refinements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React, {
-  useCallback, useEffect, useRef, useState,
-} from 'react';
-import { getFiveFirst, getAll, getRed } from './api/goods';
-import './App.scss';
-import { GoodsList } from './GoodsList';
-import { Good } from './types/Good';
+import React, { useEffect, useState } from "react";
+import { getFiveFirst, getAll, getRed } from "./api/goods";
+import "./App.scss";
+import { GoodsList } from "./GoodsList";
+import { Good } from "./types/Good";
 
 // import { getAll, get5First, getRed } from './api/goods';
 // or
@@ -12,52 +10,22 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
-  const mountedRef = useRef(false);
 
   useEffect(() => {
-    let mounted = true;
-
-    mountedRef.current = true;
-
-    const load = async (): Promise<void> => {
-      const newGoods = await getAll();
-
-      if (mounted) {
-        setGoods(newGoods);
-      }
-    };
-
-    load();
-
-    return () => {
-      mounted = false;
-      mountedRef.current = false;
-    };
+    getAll().then((result) => setGoods(result));
   }, []);
 
-  const handleButtonAllClicked = useCallback(async (): Promise<void> => {
-    const newGoods = await getAll();
+  const handleButtonAllClicked = () => {
+    getAll().then((result) => setGoods(result));
+  };
 
-    if (mountedRef.current) {
-      setGoods(newGoods);
-    }
-  }, []);
+  const handleButtonFiveClicked = () => {
+    getFiveFirst().then((result) => setGoods(result));
+  };
 
-  const handleButtonFiveClicked = useCallback(async (): Promise<void> => {
-    const newGoods = await getFiveFirst();
-
-    if (mountedRef.current) {
-      setGoods(newGoods);
-    }
-  }, []);
-
-  const handleButtonRedClicked = useCallback(async (): Promise<void> => {
-    const newGoods = await getRed();
-
-    if (mountedRef.current) {
-      setGoods(newGoods);
-    }
-  }, []);
+  const handleButtonRedClicked = () => {
+    getRed().then((result) => setGoods(result));
+  };
 
   return (
     <div className="App">
