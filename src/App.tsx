872a02b1c5846a130db57a5be75e870192502e4c
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-04-01 19:50:43 +0300

    app optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 
 import './App.scss';
 import { GoodsList } from './GoodsList';
@@ -10,52 +11,70 @@ import { getAll, get5First, getRedGoods } from './api/goods';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [lastButtonClicked, setLastButtonClicked] = useState('');
 
-  const handleGoodsLoading = useMemo(
-    () => async (getGoods: () => Promise<Good[]>) => {
-      const goodsFromServer = await getGoods();
+  const handleGoodsLoading = async (
+    getGoods: () => Promise<Good[]>,
+    buttonName: string,
+  ) => {
+    if (buttonName === lastButtonClicked) {
+      return;
+    }
 
-      setGoods(goodsFromServer);
-    },
-    [],
-  );
+    setLastButtonClicked(buttonName);
+    setIsLoading(true);
+
+    const goodsFromServer = await getGoods()
+      .catch(() => {
+        throw new Error('Something went wrong:(');
+      })
+      .finally(() => setIsLoading(false));
+
+    setGoods(goodsFromServer);
+  };
 
   return (
     <div className="App">
       <div className="header">
         <h1 className="page-header">Dynamic list of Goods</h1>
+
         <div className="buttons-container">
           <Button
             type="button"
             data-cy="all-button"
             variant="info"
             className="load-button"
-            onClick={() => handleGoodsLoading(getAll)}
+            onClick={() => handleGoodsLoading(getAll, 'all-button')}
           >
             Load all goods
           </Button>
+
           <Button
             type="button"
             data-cy="first-five-button"
             variant="success"
             className="load-button"
-            onClick={() => handleGoodsLoading(get5First)}
+            onClick={() => handleGoodsLoading(get5First, 'first-five-button')}
           >
             Load 5 first goods
           </Button>
+
           <Button
             type="button"
             data-cy="red-button"
             variant="danger"
             className="load-button"
-            onClick={() => handleGoodsLoading(getRedGoods)}
+            onClick={() => handleGoodsLoading(getRedGoods, 'red-button')}
           >
             Load red goods
           </Button>
         </div>
       </div>
 
-      <GoodsList goods={goods} />
+      {isLoading
+        ? <Spinner animation="border" role="status" />
+        : <GoodsList goods={goods} />}
     </div>
   );
 };
