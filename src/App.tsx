ffb74ebf7eb551de661bd2f0fa0cec00432d2ede
Valeriia Yarzhemska <lera.strikalo@icloud.com>
2023-04-04 16:34:56 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,93 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import 'bulma/css/bulma.min.css';
+import { Vortex } from 'react-loader-spinner';
 import './App.scss';
-import { GoodsList } from './GoodsList';
+import { GoodsList } from './components/GoodList';
+import { Button } from './components/Button';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [clickedButton, setClickedButton] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleClick = useCallback(
+    async (sortedGoods: () => Promise<Good[]>, buttonName: string) => {
+      if (clickedButton === buttonName) {
+        return;
+      }
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      setIsLoading(true);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      try {
+        setGoods(await sortedGoods());
+        setClickedButton(buttonName);
+        setIsLoading(false);
+      } catch {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }, [clickedButton],
+  );
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App page page__container">
+      <h1 className="page page__title">
+        Dynamic list of Goods
+      </h1>
+
+      <div className="buttons">
+        <Button
+          dataCy="all-button"
+          onClick={() => handleClick(getAll, 'all-button')}
+          clickedButton={clickedButton}
+        >
+          Load all goods
+        </Button>
+
+        <Button
+          dataCy="first-five-button"
+          onClick={() => handleClick(get5First, 'first-five-button')}
+          clickedButton={clickedButton}
+        >
+          Load 5 first goods
+        </Button>
+
+        <Button
+          dataCy="red-button"
+          onClick={() => handleClick(getRedGoods, 'red-button')}
+          clickedButton={clickedButton}
+        >
+          Load red goods
+        </Button>
+      </div>
+
+      {isLoading
+        ? (
+          <div>
+            <Vortex
+              height="80"
+              width="80"
+              ariaLabel="vortex-loading"
+              wrapperStyle={{}}
+              wrapperClass="vortex-wrapper"
+              colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
+            />
+          </div>
+        )
+        : (
+          <>
+            {hasError && (
+              <p>Sorry, something went wrong</p>
+            )}
+
+            <GoodsList goods={goods} />
+          </>
+        )}
+    </div>
+  );
+};
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  dataCy: string,
+  onClick: () => Promise<void>,
+  clickedButton: string,
+  children: React.ReactNode,
+};
+
+export const Button: React.FC<Props> = ({
+  dataCy,
+  onClick,
+  clickedButton,
+  children,
+}) => {
+  const isButtonActive = clickedButton === dataCy;
+
+  return (
+    <button
+      type="button"
+      data-cy={dataCy}
+      onClick={onClick}
+      className={classNames(
+        'button is-light', {
+          'is-warning': isButtonActive,
+        },
+      )}
+    >
+      {children}
+    </button>
+  );
+};
