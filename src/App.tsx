f691c4c1b6bacdc9364161c163110b1e29fa146a
Sasha Kononenko <nurchiksasha@gmail.com>
2023-04-04 21:45:09 +0300

    dynamic list-of-goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import classNames from 'classnames';
+import 'bulma';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+enum GoodsType {
+  Default,
+  All,
+  FirstFive,
+  OnlyRed,
+}
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [hasLoadingError, setHasLoadingError] = useState(false);
+  const [currentGoodsType, setcurrentGoodsType] = useState(GoodsType.Default);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const loadGoods = (
+    getGoods: () => Promise<Good[]>,
+    goodsType = GoodsType.Default,
+  ) => {
+    if (goodsType !== currentGoodsType) {
+      setLoading(true);
+      setHasLoadingError(false);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      getGoods()
+        .then((loadedGoods) => setGoods(loadedGoods))
+        .catch(() => setHasLoadingError(true))
+        .finally(() => setLoading(false));
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      setcurrentGoodsType(goodsType);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1 className="title is-2">Dynamic list of Goods</h1>
+
+      <button
+        disabled={loading}
+        className={classNames(
+          'button',
+          'is-link',
+          { 'is-loading': loading },
+        )}
+        type="button"
+        data-cy="all-button"
+        onClick={() => loadGoods(getAll, GoodsType.All)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        disabled={loading}
+        className={classNames(
+          'button',
+          'is-link',
+          { 'is-loading': loading },
+        )}
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => loadGoods(get5First, GoodsType.FirstFive)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        disabled={loading}
+        className={classNames(
+          'button',
+          'is-link',
+          { 'is-loading': loading },
+        )}
+        type="button"
+        data-cy="red-button"
+        onClick={() => loadGoods(getRedGoods, GoodsType.OnlyRed)}
+      >
+        Load red goods
+      </button>
+      <GoodsList goods={goods} />
+
+      {hasLoadingError && (
+        <div className="notification is-danger">
+          An error occured when loading data!
+        </div>
+      )}
+    </div>
+  );
+};
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,17 +3,26 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
+function wait(delay: number) {
+  return new Promise(resolve => setTimeout(resolve, delay));
+}
+
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
+  return wait(500)
+    .then(() => fetch(API_URL))
     .then(response => response.json());
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods.sort((prevGood, nextGood) => (
+      prevGood.name.localeCompare(nextGood.name)
+    )))
+    .then(goods => goods.slice(0, 5)); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods
+      .filter(good => good.color === 'red')); // get only red
 };
