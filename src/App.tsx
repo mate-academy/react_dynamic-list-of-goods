cca281159b87e1f400b223ce428e5fd450244221
Denys Dubas <den.dubas.44@gmail.com>
2023-04-04 21:21:02 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,98 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import 'bulma/css/bulma.css';
+import classNames from 'classnames';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [isChoosedType, setIsChoosedType] = useState({
+    all: false,
+    fiveFirst: false,
+    onlyRed: false,
+  });
+  const [getError, setGetError] = useState(false);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleGetGoods = useCallback(async (sortType: string) => {
+    let goodsFromServer: React.SetStateAction<Good[]> = [];
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    // setChoosedType(prev => ({
+    //   ...prev,
+    //   [sortType]: true,
+    // }));
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    try {
+      switch (sortType) {
+        case 'all':
+          goodsFromServer = await getAll();
+          break;
+        case 'fiveFirst':
+          goodsFromServer = await get5First();
+          break;
+        case 'onlyRed':
+          goodsFromServer = await getRed();
+          break;
+        default:
+          goodsFromServer = [];
+          break;
+      }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      setGoods(goodsFromServer);
+    } catch {
+      setGetError(true);
+    } finally {
+      setIsChoosedType(prev => ({
+        ...prev,
+        [sortType]: false,
+      }));
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 className="h1">Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        className={classNames('button',
+          { 'is-loading': isChoosedType.all })}
+        onClick={() => handleGetGoods('all')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        className={classNames('button',
+          { 'is-loading': isChoosedType.fiveFirst })}
+        onClick={() => handleGetGoods('fiveFirst')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        className={classNames('button',
+          { 'is-loading': isChoosedType.onlyRed })}
+        onClick={() => handleGetGoods('onlyRed')}
+      >
+        Load red goods
+      </button>
+
+      {getError
+        ? (
+          <h1 className="notification is-danger is-light">
+            Oops, something went wrong!
+          </h1>
+        ) : (
+          <GoodsList goods={goods} />
+        )}
+    </div>
+  );
+};
