f45d5b9d0e4888398e4d75e78b3616dab08f822c
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-04-05 21:30:35 +0200

    done task

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+enum TypeOfGoods {
+  None = '',
+  AllGoods = 'all-button',
+  FirstFiveGoods = 'first-five-button',
+  RedGoods = 'red-button',
+}
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [currentType, setCurrentType] = useState(TypeOfGoods.None);
+  const [isLoading, setLoading] = useState(false);
+  const [hasLoadingError, setLoadingError] = useState(false);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleClickButton = (
+    getGoods: () => Promise<Good[]>,
+    typeOfGoods = TypeOfGoods.None,
+  ) => {
+    return () => {
+      if (currentType !== typeOfGoods) {
+        setLoading(true);
+        getGoods()
+          .then(setGoods)
+          .catch(() => (setLoadingError(true)))
+          .finally(() => {
+            setLoading(false);
+            setLoading(false);
+          });
+        setCurrentType(typeOfGoods);
+      }
+    };
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={handleClickButton(getAll, TypeOfGoods.AllGoods)}
+        className="buttonload"
+      >
+        {isLoading && currentType === TypeOfGoods.AllGoods
+          ? (
+            <>
+              <i className="fa fa-spinner fa-spin" />
+              <span>Loading</span>
+            </>
+          )
+          : 'Load all goods'}
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleClickButton(get5First, TypeOfGoods.FirstFiveGoods)}
+      >
+        {isLoading && currentType === TypeOfGoods.FirstFiveGoods
+          ? (
+            <>
+              <i className="fa fa-spinner fa-spin" />
+              <span>Loading</span>
+            </>
+          )
+          : 'Load 5 first goods'}
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={handleClickButton(getRedGoods, TypeOfGoods.RedGoods)}
+      >
+        {isLoading && currentType === TypeOfGoods.RedGoods
+          ? (
+            <>
+              <i className="fa fa-spinner fa-spin" />
+              <span>Loading</span>
+            </>
+          )
+          : 'Load red goods'}
+      </button>
+
+      {hasLoadingError
+        ? (
+          <div>
+            An error occored when loadin goods!
+          </div>
+        )
+        : (
+          <GoodsList goods={goods} />
+        )}
+
+    </div>
+  );
+};
