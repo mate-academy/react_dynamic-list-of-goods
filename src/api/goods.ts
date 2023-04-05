2585d7d35a16f558f600f95ab29a57dddb8bb421
NikitaStambul <stambul.n@gmail.com>
2023-04-05 14:57:20 +0300

    error handling added

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,11 +1,30 @@
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goodss.json`;
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(
+          new Error(`${response.status} - ${response.text}`),
+        );
+      }
+
+      if (!response.headers.get('content-type')?.includes('application/json')) {
+        return Promise.reject(
+          new Error('Content-type is not json'),
+        );
+      }
+
+      return response.json();
+    })
+    .catch(error => {
+      window.console.warn(error);
+
+      return [];
+    });
 }
 
 export const get5First = () => {
