a509d3959f04618bc4250eb31c05146cf55f1362
Ruslan Liapin <joy.esenin@gmail.com>
2023-04-08 01:09:42 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  type StateSetter<T> = React.Dispatch<React.SetStateAction<T>>;
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  function usePromiseState<T>(
+    promise: Promise<T>,
+    initialState: T,
+  ): [T, StateSetter<T>] {
+    const [state, setState] = useState<T>(initialState);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    useEffect(() => {
+      promise.then(result => setState(result));
+    }, []);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    return [state, setState];
+  }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  const [first5] = usePromiseState(get5First(), []);
+  const [allGoods] = usePromiseState(getAll(), []);
+  const [redGoods] = usePromiseState(getRedGoods(), []);
+  const [listTypes, setList] = useState('');
+  let goodsToShow: Good[] = [];
+
+  const handleListType = (listType: string) => {
+    setList(listType);
+
+    switch (listTypes) {
+      case 'all':
+        goodsToShow = allGoods;
+        break;
+      case '5First':
+        goodsToShow = first5;
+        break;
+      case 'red':
+        goodsToShow = redGoods;
+        break;
+      default:
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handleListType('all')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handleListType('5First')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handleListType('red')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goodsToShow} />
+    </div>
+  );
+};
