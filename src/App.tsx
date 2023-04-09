dcf53a3772db6537a9980b433beb2341ca58a2f7
vadym4che <vadym4che@gmail.com>
2023-04-09 05:28:40 +0300

    add error handling, some nice styling with bulma and fantawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,94 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import { GoodsList } from './GoodsList';
+import classNames from 'classnames';
+import 'bulma';
+import '@fortawesome/fontawesome';
+import { GoodsList } from './components/GoodsList';
+import { Loader } from './components/Loader';
 
-import { getAll, get5First, getRedGoods } from './api/goods';
+import {
+  getAll,
+  get5First,
+  getRedGoods,
+} from './api/goods';
 import { Good } from './types/Good';
-// or
-// import * as goodsAPI from './api/goods';
 
 export const App: React.FC = () => {
   const [goodsToShow, setGoodsToShow] = useState<Good[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(false);
 
-  useEffect(() => {
+  type Loaded = 'all' | 'five' | 'red' | 'none';
 
-  }, []);
+  const [buttonClicked, setButtonClicked] = useState('none');
+  const [loadedState, setLoadedState] = useState<Loaded>('none');
 
-  const loadAllGoods = () => getAll()
-    .then((goods) => setGoodsToShow([...goods]));
+  const reset = () => {
+    setLoading(false);
+    setButtonClicked('none');
+  };
 
-  const loadFiveFirstGoods = () => get5First()
-    .then((goods) => setGoodsToShow([...goods]));
+  const loadAllGoods = () => {
+    setLoading(true);
+    setButtonClicked('all');
 
-  const loadRedGoods = () => getRedGoods()
-    .then((goods) => setGoodsToShow([...goods]));
+    getAll()
+      .then((goods) => {
+        setGoodsToShow([...goods]);
+        setLoadedState('all');
+        reset();
+      })
+      .catch(() => {
+        setLoadingError(true);
+        reset();
+      });
+  };
 
-  // eslint-disable-next-line no-console
-  console.log(goodsToShow, typeof goodsToShow);
+  const loadFiveFirstGoods = () => {
+    setLoading(true);
+    setButtonClicked('five');
+
+    return get5First()
+      .then((goods) => {
+        setGoodsToShow([...goods]);
+        setLoadedState('five');
+        reset();
+      })
+      .catch(() => {
+        setLoadingError(true);
+        reset();
+      });
+  };
+
+  const loadRedGoods = () => {
+    setLoading(true);
+    setButtonClicked('red');
+
+    return getRedGoods()
+      .then((goods) => {
+        setGoodsToShow([...goods]);
+        setLoadedState('red');
+        reset();
+      })
+      .catch(() => {
+        setLoadingError(true);
+        reset();
+      });
+  };
 
   return (
-    <div className="App">
-      <h1>Dynamic list of Goods</h1>
+    <div className="App is-family-monospace">
+      <h1 className="title m-6">Dynamic list of Goods</h1>
 
       <button
         type="button"
         data-cy="all-button"
         onClick={loadAllGoods}
+        disabled={loadedState === 'all' || loadingError}
+        className={classNames(
+          'button is-warning m-1',
+          { 'is-loading': buttonClicked === 'all' },
+        )}
       >
         Load all goods
       </button>
@@ -42,6 +97,11 @@ export const App: React.FC = () => {
         type="button"
         data-cy="first-five-button"
         onClick={loadFiveFirstGoods}
+        disabled={loadedState === 'five' || loadingError}
+        className={classNames(
+          'button is-success m-1',
+          { 'is-loading': buttonClicked === 'five' },
+        )}
       >
         Load 5 first goods
       </button>
@@ -50,11 +110,24 @@ export const App: React.FC = () => {
         type="button"
         data-cy="red-button"
         onClick={loadRedGoods}
+        disabled={loadedState === 'red' || loadingError}
+        className={classNames(
+          'button is-danger m-1',
+          { 'is-loading': buttonClicked === 'red' },
+        )}
       >
         Load red goods
       </button>
 
-      <GoodsList goods={goodsToShow} />
+      {loading
+        ? <Loader />
+        : (
+          <GoodsList
+            goods={goodsToShow}
+            loadingError={loadingError}
+            setLoadingError={setLoadingError}
+          />
+        )}
     </div>
   );
 };
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,21 +1,23 @@
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+export const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      return response.json();
+    });
 }
 
 export const get5First = () => {
   return getAll()
     .then(goods => goods
       .sort(({ name: A }, { name: B }) => A.localeCompare(B))
-      .filter((good) => goods.indexOf(good) < 5)); // sort and get the first 5
+      .slice(0, 5));
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods.filter(({ color }) => color === 'red')); // get only red
+    .then(goods => goods.filter(({ color }) => color === 'red'));
 };
