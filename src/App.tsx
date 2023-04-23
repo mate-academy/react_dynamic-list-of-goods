2e18067100e3dca1ceb89141e936dfe29981d116
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-04-23 20:51:14 +0300

    dynamic-list-of-goods was done

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[] | null>(null);
+  const [isDisabled, setIsDisabled] = useState('');
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const loadAllGoods = async () => {
+    const items = await getAll();
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    setIsDisabled('all');
+    setGoods(items);
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  const load5FirstGoods = async () => {
+    const items = await get5First();
+
+    setIsDisabled('5First');
+    setGoods(items);
+  };
+
+  const loadRedGoods = async () => {
+    const items = await getRedGoods();
+
+    setIsDisabled('red');
+    setGoods(items);
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={loadAllGoods}
+        disabled={isDisabled === 'all'}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={load5FirstGoods}
+        disabled={isDisabled === '5First'}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={loadRedGoods}
+        disabled={isDisabled === 'red'}
+      >
+        Load red goods
+      </button>
+
+      {goods && <GoodsList goods={goods} />}
+    </div>
+  );
+};
