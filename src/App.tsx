b858b729fefaab80e466574e521325b809f40f7f
Vlad <bojko.vladislav16@gmail.com>
2023-04-29 23:44:47 +0300

    All goods have been successfully loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+const allGoods = getAll();
+const firstFiveGoods = get5First();
+const redGoods = getRedGoods();
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [isError, setIsError] = useState(false);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const loadAvailableGoods = async (goodsFromServer: Promise<Good[]>) => {
+    setIsError(false);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    try {
+      const availableGoods = await goodsFromServer;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      setGoods(availableGoods);
+    } catch {
+      setIsError(true);
+    }
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => loadAvailableGoods(allGoods)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => loadAvailableGoods(firstFiveGoods)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => loadAvailableGoods(redGoods)}
+      >
+        Load red goods
+      </button>
+
+      {goods.length > 0 ? (
+        <GoodsList goods={goods} />
+      ) : (
+        <h1>None of the available Goods were loaded yet</h1>
+      )}
+      {isError && <h1>Server is not responding</h1>}
+    </div>
+  );
+};
