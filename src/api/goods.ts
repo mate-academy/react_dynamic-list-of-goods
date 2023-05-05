c91acaa085f365d2a09736022dd3e7b5364c15ea
Mark Boiko <boikomark555@gmail.com>
2023-05-05 21:20:49 +0300

    add checking for function getAll

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,6 +6,10 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 export async function getAll(): Promise<Good[]> {
   const response = await fetch(API_URL);
 
+  if (!response.ok) {
+    throw new Error('Request failed');
+  }
+
   return response.json();
 }
 
