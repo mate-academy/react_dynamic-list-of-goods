65fd8e03f3a8f0cc23114f3c34f7307b175e5c95
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-05-08 13:47:58 +0100

    added sorting and filtering

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -9,11 +9,17 @@ export function getAll(): Promise<Good[]> {
 }
 
 export const get5First = () => {
-  return getAll()
-    .then(goods => goods); // sort and get the first 5
+  return getAll().then(goods => {
+    return goods
+      .sort((firstGood, secondGood) => {
+        return firstGood.name.localeCompare(secondGood.name);
+      })
+      .slice(0, 5);
+  });
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods
+      .filter(good => good.color === 'red'));
 };
