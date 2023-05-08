f4762803efab7c5111a1dd499aad8110576b465c
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-05-08 11:43:47 +0300

    added error handler

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,7 +4,9 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then((response) => response.json());
+  return fetch(API_URL).then((response) => (response.ok
+    ? response.json()
+    : Promise.reject(new Error(`${response.status}: ${response.statusText}`))));
 }
 
 export const get5First = () => {
