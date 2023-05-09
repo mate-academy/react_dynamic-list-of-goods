e3f1ce7a54cd845c7654f9c5cf00e97dfaecae8e
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-05-09 14:50:51 +0300

    func works

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import _ from 'lodash';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+import { Good } from './types/Good';
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+type GetAllFunc = () => Promise<Good[]>;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+export const App: React.FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const loadGoods = useCallback(async (getAllFunc: GetAllFunc) => {
+    try {
+      const goods = await getAllFunc();
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      setVisibleGoods(prevVisibleGoods => {
+        return _.isEqual(prevVisibleGoods, goods)
+          ? prevVisibleGoods
+          : goods;
+      });
+    } catch {
+      throw new Error('Error while loading goods');
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => loadGoods(getAll)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => loadGoods(get5First)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => loadGoods(getRedGoods)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={visibleGoods} />
+    </div>
+  );
+};
