9f4146799268feb0ca04d762dc31fea0c0f7b11e
Myla Voronevska <code.crystal.008@gmail.com>
2023-05-10 19:22:43 +0300

    Solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import { FC } from 'react';
 import { Good } from './types/Good';
 
 type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
+export const GoodsList: FC<Props> = ({ goods }) => (
   <ul>
-    {goods.map(good => (
-      <li key={good.id} data-cy="good">
-        {good.name}
+    {goods.map(({ id, name, color }) => (
+      <li
+        key={id}
+        data-cy="good"
+        style={{ color }}
+      >
+        {name}
       </li>
     ))}
   </ul>
