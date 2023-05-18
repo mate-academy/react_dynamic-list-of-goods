4ec706cf4fb488a4bbc7e99140d61e955c9cca38
Oleksandr <98667890az@gmail.com>
2023-05-18 18:37:42 +0300

    added error handlers

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,21 @@ import { Good } from './types/Good';
 export const App: React.FC = () => {
   const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
 
+  const fetchAll = () => (
+    getAll()
+      .then((goods => setVisibleGoods(goods)))
+      .catch(() => 'There no goods'));
+
+  const fetch5First = () => (
+    get5First()
+      .then((goods => setVisibleGoods(goods)))
+      .catch(() => 'There no goods'));
+
+  const fetchRed = () => (
+    getRedGoods()
+      .then(goods => setVisibleGoods(goods))
+      .catch(() => 'There no red goods'));
+
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
@@ -14,7 +29,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="all-button"
-        onClick={() => getAll().then((goods => setVisibleGoods(goods)))}
+        onClick={fetchAll}
       >
         Load all goods
       </button>
@@ -22,7 +37,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={() => get5First().then(goods => setVisibleGoods(goods))}
+        onClick={fetch5First}
       >
         Load 5 first goods
       </button>
@@ -30,7 +45,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="red-button"
-        onClick={() => getRedGoods().then(goods => setVisibleGoods(goods))}
+        onClick={fetchRed}
       >
         Load red goods
       </button>
