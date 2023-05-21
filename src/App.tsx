15d23cb7e5f2faeff90f009d8b04491cbf7996ee
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-05-21 23:34:12 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
-
+import { Good } from './types/Good';
 // import { getAll, get5First, getRed } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+const getGoods = () => {
+  return fetch(
+    'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json',
+  ).then(response => response.json()).catch((error) => {
+    throw new Error(error);
+  });
+};
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState([]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const renderAllGoods = () => {
+    getGoods().then(json => {
+      setGoods(json);
+    });
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const renderFiveGoods = () => {
+    getGoods().then(json => {
+      const sortGoodsByName = json.sort((a: Good, b: Good) => (
+        a.name.localeCompare(b.name)
+      ));
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      setGoods(sortGoodsByName.slice(0, 5));
+    });
+  };
+
+  const renderRedGoods = () => {
+    getGoods().then(json => {
+      const redGoods = json.filter((good: Good) => good.color === 'red');
+
+      setGoods(redGoods);
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={renderAllGoods}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={renderFiveGoods}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={renderRedGoods}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
