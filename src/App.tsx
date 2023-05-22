37cae72cf0d67bfb4c0b5d36f38ca196a7c90686
Paweł Smoliński <ski1993@o2.pl>
2023-05-22 13:02:20 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[] | []>([]);
+  const [error, setError] = useState<null | string>(null);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const renderAll = () => {
+    getAll()
+      .then(setGoods)
+      .catch(dataError => setError(dataError.message));
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const render5First = () => {
+    get5First()
+      .then(setGoods)
+      .catch(dataError => setError(dataError.message));
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const renderRedGoods = () => {
+    getRedGoods()
+      .then(setGoods)
+      .catch(dataError => setError(dataError.message));
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={renderAll}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={render5First}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={renderRedGoods}
+      >
+        Load red goods
+      </button>
+
+      {error
+        ? <p>{error}</p>
+        : <GoodsList goods={goods} />}
+    </div>
+  );
+};
