37aa6b94248863acb51d8d935e28f97551905678
Ilia <koleshnik160799@gmail.com>
2023-05-22 23:03:46 +0300

    Fix code

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
 import { getAll, get5First, getRedGoods } from './api/goods';
 
-enum Sort {
-  ALL = 'all',
-  FIVEFIRST = 'fivefirst',
-  REDGOODS = 'redgoods',
-}
-
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
-  const [modGood, setModGood] = useState<Sort | undefined>(undefined);
-
-  useEffect(() => {
-    const funcGoodsInfo = async () => {
-      let result;
-
-      switch (modGood) {
-        case Sort.ALL:
-          result = await getAll().then(allGoods => setGoods(allGoods));
-          break;
-
-        case Sort.FIVEFIRST:
-          result = await get5First().then(items => setGoods(items
-            .sort((item1, item2) => item1.name.localeCompare(item2.name))
-            .slice(0, 5)));
-          break;
-
-        case Sort.REDGOODS:
-          result = await getRedGoods().then(goodsInfo => setGoods(
-            goodsInfo.filter(good => good.color === 'red'),
-          ));
-          break;
-
-        default:
-          result = 0;
-      }
-
-      return result;
-    };
-
-    funcGoodsInfo();
-  }, [modGood]);
 
   return (
     <div className="App">
@@ -52,7 +14,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="all-button"
-        onClick={() => setModGood(Sort.ALL)}
+        onClick={() => getAll().then(allGoods => setGoods(allGoods))}
       >
         Load all goods
       </button>
@@ -60,7 +22,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={() => setModGood(Sort.FIVEFIRST)}
+        onClick={() => get5First().then(items => setGoods(items))}
       >
         Load 5 first goods
       </button>
@@ -68,7 +30,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="red-button"
-        onClick={() => setModGood(Sort.REDGOODS)}
+        onClick={() => getRedGoods().then(goodsInfo => setGoods(goodsInfo))}
       >
         Load red goods
       </button>
