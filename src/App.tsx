b3308adf2f99e540fb603bb82e7d69ed5b47a89b
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-05-23 11:37:54 +0200

    requested changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,13 @@ import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
-
-const getGoods = () => {
-  return fetch(
-    'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json',
-  ).then(response => response.json()).catch((error) => {
-    throw new Error(error);
-  });
-};
+import { getAll, get5First, getRedGoods } from './api/goods';
 
 export const App: React.FC = () => {
-  const [goods, setGoods] = useState([]);
+  const [goods, setGoods] = useState<Good[]>([]);
 
-  const renderAllGoods = () => {
-    getGoods().then(json => {
-      setGoods(json);
-    });
-  };
-
-  const renderFiveGoods = () => {
-    getGoods().then(json => {
-      const sortGoodsByName = json.sort((a: Good, b: Good) => (
-        a.name.localeCompare(b.name)
-      ));
-
-      setGoods(sortGoodsByName.slice(0, 5));
-    });
-  };
-
-  const renderRedGoods = () => {
-    getGoods().then(json => {
-      const redGoods = json.filter((good: Good) => good.color === 'red');
-
-      setGoods(redGoods);
-    });
+  const renderGoods = async (render: () => Promise<Good[]>) => {
+    setGoods(await render());
   };
 
   return (
@@ -48,7 +18,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="all-button"
-        onClick={renderAllGoods}
+        onClick={() => renderGoods(getAll)}
       >
         Load all goods
       </button>
@@ -56,7 +26,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={renderFiveGoods}
+        onClick={() => renderGoods(get5First)}
       >
         Load 5 first goods
       </button>
@@ -64,7 +34,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="red-button"
-        onClick={renderRedGoods}
+        onClick={() => renderGoods(getRedGoods)}
       >
         Load red goods
       </button>
