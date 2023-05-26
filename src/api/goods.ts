e021fb7a30cdf810fcd00111874a255f38571cd7
sylcym <prusak.syl@gmail.com>
2023-05-26 13:45:40 +0200

    task solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,19 +1,25 @@
 import { Good } from '../types/Good';
 
-// eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => response.json())
+    .then(json => json)
+    .catch((error) => {
+      throw new Error(error);
+    });
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods
+      .sort((goodA, goodB) => goodA.name.localeCompare(goodB.name))
+      .slice(0, 5));
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods
+      .filter(good => good.color === 'red'));
 };
