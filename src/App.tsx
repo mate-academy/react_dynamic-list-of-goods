26679da3088cfb6724b394120d6f79e2b325827a
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-06-02 12:56:10 +0200

    error handling

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
+  const [error, setError] = useState('');
 
   const loadAll = async () => {
     const allGoods = await goodsAPI.getAll();
@@ -26,6 +27,18 @@ export const App: React.FC = () => {
     setGoods(getRedGoods);
   };
 
+  const handleLoadAll = () => {
+    loadAll().then(() => setError('')).catch(err => setError(`The following error occured: ${err}`));
+  };
+
+  const handleLoadFirst5Goods = () => {
+    loadFirst5Goods().then(() => setError('')).catch(err => setError(`The following error occured: ${err}`));
+  };
+
+  const handleLoadRedGoods = () => {
+    loadRedGoods().then(() => setError('')).catch(err => setError(`The following error occured: ${err}`));
+  };
+
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
@@ -33,7 +46,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="all-button"
-        onClick={loadAll}
+        onClick={handleLoadAll}
       >
         Load all goods
       </button>
@@ -41,7 +54,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={loadFirst5Goods}
+        onClick={handleLoadFirst5Goods}
       >
         Load 5 first goods
       </button>
@@ -49,12 +62,14 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="red-button"
-        onClick={loadRedGoods}
+        onClick={handleLoadRedGoods}
       >
         Load red goods
       </button>
 
       <GoodsList goods={goods} />
+
+      {error && <p>{error}</p>}
     </div>
   );
 };
