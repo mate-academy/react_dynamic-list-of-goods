d4d2fa552891a0fed862a6247d0de119e08168cc
Bogdan Parubchenko <bogdanparubchenko@gmail.com>
2023-06-23 17:20:09 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [sortGods, setSortGods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleClick = (valueSortGods: () => Promise<Good[]>) => {
+    valueSortGods().then(result => {
+      setSortGods(result);
+    });
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const buttons = [
+    {
+      title: 'Load all goods',
+      methodSort: getAll,
+      dataCY: 'all-button',
+    },
+    {
+      title: 'Load 5 first goods',
+      methodSort: get5First,
+      dataCY: 'first-five-button',
+    },
+    {
+      title: 'Load red goods',
+      methodSort: getRedGoods,
+      dataCY: 'red-button',
+    },
+  ];
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+      {buttons.map(button => (
+        <button
+          type="button"
+          data-cy={button.dataCY}
+          key={button.dataCY}
+          onClick={() => handleClick(button.methodSort)}
+        >
+          {button.title}
+        </button>
+      ))}
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <GoodsList goods={sortGods} />
+    </div>
+  );
+};
