37ec789f4ad10ac7077c7ae73ac43601196da110
Sonya Liubchenko <conilub@gmail.com>
2023-06-30 00:37:42 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [sortedGoods, setSortedGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleAllGoods = () => {
+    getAll().then(goods => setSortedGoods(goods));
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handle5Goods = () => {
+    get5First().then(goods => setSortedGoods(goods));
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleRedGoods = () => {
+    getRedGoods().then(goods => setSortedGoods(goods));
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <Typography
+        variant="h1"
+        align="center"
+        style={{ fontSize: '32px', marginBottom: '10px' }}
+      >
+        Dynamic list of Goods
+      </Typography>
+      <Button
+        type="button"
+        data-cy="all-button"
+        variant="outlined"
+        style={{ marginRight: '10px' }}
+        onClick={() => handleAllGoods()}
+      >
+        Load all goods
+      </Button>
+
+      <Button
+        type="button"
+        data-cy="first-five-button"
+        variant="outlined"
+        style={{ marginRight: '10px' }}
+        onClick={() => handle5Goods()}
+      >
+        Load 5 first goods
+      </Button>
+
+      <Button
+        type="button"
+        data-cy="red-button"
+        variant="outlined"
+        onClick={() => handleRedGoods()}
+      >
+        Load red goods
+      </Button>
+
+      <GoodsList goods={sortedGoods} />
+    </div>
+  );
+};
