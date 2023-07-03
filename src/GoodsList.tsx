dbaf141f114c00212864084fd867d84bb62e7e81
holytimy4 <ez.lvl.faceit@gmail.com>
2023-07-04 02:46:52 +0300

    added task solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
+import classNames from 'classnames';
 import { Good } from './types/Good';
 
 type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
+export const GoodsList: React.FC<Props> = React.memo(({ goods }) => (
+  <ul className="my-3 is-size-5">
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li
+        key={good.id}
+        data-cy="good"
+        className={classNames({
+          blue: good.color === 'blue',
+          red: good.color === 'red',
+          green: good.color === 'green',
+        })}
+      >
         {good.name}
       </li>
     ))}
   </ul>
-);
+));
