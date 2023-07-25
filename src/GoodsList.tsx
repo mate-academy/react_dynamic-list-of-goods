47a0204c2bd5ec6a2a6e2fb300ad46a4dc7db4f1
outofs <rtranscendental@gmail.com>
2023-07-25 15:37:18 +0300

    Adding some optimization

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,10 +1,11 @@
+import { memo } from 'react';
 import { Good } from './types/Good';
 
 type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
+export const GoodsList: React.FC<Props> = memo(({ goods }) => (
   <ul>
     {goods.map(good => (
       <li
@@ -16,4 +17,5 @@ export const GoodsList: React.FC<Props> = ({ goods }) => (
       </li>
     ))}
   </ul>
-);
+)
+)
