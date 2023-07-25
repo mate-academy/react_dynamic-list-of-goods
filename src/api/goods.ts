a5ca0c6a030f105ed179f26c9817586e55e37ca5
maxyma-sanx <maxymasan.dev@gmail.com>
2023-07-25 17:51:02 +0300

    add solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,17 +3,21 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export const getAll = (): Promise<Good[]> => {
   return fetch(API_URL)
     .then(response => response.json());
-}
+};
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods)
+    .then(goods => goods
+      .sort((goodA, goodB) => goodA.name.localeCompare(goodB.name)))
+    .then(goods => goods.slice(0, 5));
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods)
+    .then(goods => goods.filter(good => good.color === 'red'));
 };
