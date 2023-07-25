1a96734d690f93b5db3ec5751806b3abb5b80bf5
Vladyslav Roznatovskyi <morphey138@gmail.com>
2023-07-25 18:29:26 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+const enum Actions {
+  AllGoods = 'All goods',
+  FirstFive = 'first-five',
+  OnlyRed = 'red',
+}
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
+  const [handleAction, setHandleAction] = useState(Actions.AllGoods);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    // eslint-disable-next-line default-case
+    switch (handleAction) {
+      case Actions.AllGoods:
+        getAll()
+          .then(goodsFromServer => {
+            setVisibleGoods(goodsFromServer);
+          });
+        break;
+      case Actions.FirstFive:
+        get5First()
+          .then(goodsFromServer => {
+            setVisibleGoods(goodsFromServer);
+          });
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+        break;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case Actions.OnlyRed:
+        getRedGoods()
+          .then(goodsFromServer => {
+            setVisibleGoods(goodsFromServer);
+          });
 
-    <GoodsList goods={[]} />
-  </div>
-);
+        break;
+    }
+  }, [handleAction]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setHandleAction(Actions.AllGoods)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setHandleAction(Actions.FirstFive)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setHandleAction(Actions.OnlyRed)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={visibleGoods} />
+    </div>
+  );
+};
