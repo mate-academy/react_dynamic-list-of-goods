ff0541068ba2ed69b8243c8fde95cab032145f6d
Kateryna Kylyvnyk <kkylyvnyk@gmail.com>
2023-07-26 17:26:26 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { getAll, get5First, getRedGoods } from './api/goods';
 import { Good } from './types/Good';
 
-enum Filter {
-  All = 'all',
-  Five = 'five',
-  Red = 'red',
-  None = '',
-}
-
-function preparedGoods(filter: Filter): Promise<Good[]> | null {
-  switch (filter) {
-    case Filter.All:
-      return getAll();
-    case Filter.Five:
-      return get5First();
-    case Filter.Red:
-      return getRedGoods();
-    default:
-      return null;
-  }
-}
-
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
-  const [filter, setFilter] = useState<Filter>(Filter.None);
-
-  useEffect(() => {
-    preparedGoods(filter)?.then(response => setGoods(response));
-  }, [filter]);
-
-  function getAllGoods() {
-    setFilter(Filter.All);
-  }
-
-  function get5FirstGoods() {
-    setFilter(Filter.Five);
-  }
-
-  function getRed() {
-    setFilter(Filter.Red);
-  }
 
   return (
     <div className="App">
@@ -51,7 +14,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="all-button"
-        onClick={getAllGoods}
+        onClick={() => getAll().then(setGoods)}
       >
         Load All goods
       </button>
@@ -59,7 +22,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={get5FirstGoods}
+        onClick={() => get5First().then(setGoods)}
       >
         Load 5 first goods
       </button>
@@ -67,7 +30,7 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="red-button"
-        onClick={getRed}
+        onClick={() => getRedGoods().then(setGoods)}
       >
         Load red goods
       </button>
