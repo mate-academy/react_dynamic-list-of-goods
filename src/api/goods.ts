9138339d74f32dd1744078e5ed3990167854a8bd
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-07-26 15:37:34 +0300

    After fix

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,19 +4,21 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then((response) => response.json());
+  return fetch(API_URL).then((resp) => {
+    if (!resp.ok) {
+      throw new Error('Error');
+    }
+
+    return resp.json();
+  });
 }
 
 export const get5First = () => {
-  return getAll()
-    .then((goods) => goods)
-    .then((answ) => {
-      return answ.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
-    });
+  return getAll().then((answ) => {
+    return answ.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
+  });
 };
 
 export const getRedGoods = () => {
-  return getAll()
-    .then((goods) => goods)
-    .then((answ) => answ.filter((good) => good.color === 'red'));
+  return getAll().then((answ) => answ.filter((good) => good.color === 'red'));
 };
