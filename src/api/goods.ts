01f092df5b5b1159e41c4e651c9540c85c91f09e
Daniil Postoiuk <daniil.postoyuk@gmail.com>
2023-07-26 11:35:49 +0300

    add error handler in goods.ts

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,10 +1,14 @@
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json1`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then((response) => response.json());
+  return fetch(API_URL)
+    .then((response) => response.json())
+    .catch(error => {
+      throw new Error(`Помилка в отриманих даних: ${error.message}`);
+    });
 }
 
 export const get5First = () => {
