a16abab7903bd70303c0aa7bae81c54b8c5085a1
Maksym Ptashnyk <super.zet65@gmail.com>
2023-07-26 16:11:28 +0300

    fixed

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,11 +6,11 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
     .then(response => {
-      if(!response.ok) {
-        throw new Error(response.status + response.statusText)
+      if (!response.ok) {
+        throw new Error(response.status + response.statusText);
       }
 
-      return response.json()
+      return response.json();
     });
 }
 
