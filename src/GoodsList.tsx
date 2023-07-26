cd6061954d88a5c65d22b29d8738129c129740f9
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-07-26 11:51:47 +0300

    Remove unnecessary code

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { isEqual } from 'lodash';
 import { Good } from './types/Good';
-import { GoodItem } from './GoodItem';
 
 type Props = {
   goods: Good[]
 };
 
-const RegularGoodsList: React.FC<Props> = ({ goods }) => (
+export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <GoodItem good={good} key={good.id} />
+      <li
+        key={good.id}
+        data-cy="good"
+        style={{ color: good.color }}
+      >
+        {good.name}
+      </li>
     ))}
   </ul>
 );
-
-const arePropsEqual = (prevProps: Props, newProps: Props) => {
-  return isEqual(prevProps.goods, newProps.goods);
-};
-
-export const GoodsList = React.memo(RegularGoodsList, arePropsEqual);
