dba7d6a850700f972c77ab29e1d1e36c51c44616
tetianakostiuk <ipz213_kto@student.ztu.edu.ua>
2023-07-27 13:40:10 +0300

    fixed

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,7 +1,7 @@
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods1.json`;
+const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
