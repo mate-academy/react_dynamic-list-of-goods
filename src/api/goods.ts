5ff230714456aad07d5b5d9abddba57acea40145
Evgeniy <gekachumak@gmail.com>
2023-07-28 05:35:51 +0300

    checklist changes

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,7 +4,13 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Cannot fetch goods');
+      }
+
+      return response.json();
+    });
 }
 
 export const get5First = () => {
