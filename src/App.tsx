fad6ecb60f3665d8b293e43691691529bea144bc
Nadiia Kovalchuk <nadiaaaa1256@gmail.com>
2023-07-31 08:54:40 +0300

    add rendering optimisation and handle possible errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
@@ -8,26 +9,34 @@ export const App: React.FC = () => {
   const [arrayOfGoods, setArrayOfGoods] = useState<Good[]>([]);
 
   const handleGetAll = () => {
-    getAll().then(setArrayOfGoods);
+    getAll()
+      .then(setArrayOfGoods)
+      .catch((error) => {
+        console.error('Error while fetching all goods:', error);
+      });
   };
 
   const handleGet5First = () => {
-    get5First().then(setArrayOfGoods);
+    get5First()
+      .then(setArrayOfGoods)
+      .catch((error) => {
+        console.error('Error while fetching 5 first goods:', error);
+      });
   };
 
   const handleGetRedGoods = () => {
-    getRedGoods().then(setArrayOfGoods);
+    getRedGoods()
+      .then(setArrayOfGoods)
+      .catch((error) => {
+        console.error('Error while fetching red goods:', error);
+      });
   };
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={handleGetAll}
-      >
+      <button type="button" data-cy="all-button" onClick={handleGetAll}>
         Load all goods
       </button>
 
@@ -39,11 +48,7 @@ export const App: React.FC = () => {
         Load 5 first goods
       </button>
 
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={handleGetRedGoods}
-      >
+      <button type="button" data-cy="red-button" onClick={handleGetRedGoods}>
         Load red goods
       </button>
 
