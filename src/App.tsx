4dd5761c39b9b21128534934e6b7a6da2f57aba8
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-08-01 17:56:39 +0300

    added errors handling

--- src/App.tsx
+++ src/App.tsx
@@ -7,54 +7,70 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [allGoods, setAllGoods] = React.useState<Good[]>([]);
+  const [error, setError] = React.useState('');
+
+  const successfulLoading = React.useCallback((goods: Good[]) => {
+    setAllGoods(goods);
+    setError('');
+  }, []);
 
   const loadAll = React.useCallback(() => {
-    getAll().then((goods) => {
-      setAllGoods(goods);
-    });
+    getAll()
+      .then((goods) => {
+        successfulLoading(goods);
+      })
+      .catch(() => {
+        setError('Error loading all goods');
+      });
   }, []);
 
   const loadFiveGoods = React.useCallback(() => {
-    get5First().then((goods) => {
-      setAllGoods(goods);
-    });
+    get5First()
+      .then((goods) => {
+        successfulLoading(goods);
+      })
+      .catch(() => {
+        setError('Error loading first 5 goods');
+      });
   }, []);
 
   const loadRedGoods = React.useCallback(() => {
-    getRedGoods().then((goods) => {
-      setAllGoods(goods);
-    });
+    getRedGoods()
+      .then((goods) => {
+        successfulLoading(goods);
+      })
+      .catch(() => {
+        setError('Error loading red goods');
+      });
   }, []);
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={loadAll}
-      >
+      <button type="button" data-cy="all-button" onClick={loadAll}>
         Load all goods
       </button>
 
-      <button
-        type="button"
-        data-cy="first-five-button"
-        onClick={loadFiveGoods}
-      >
+      <button type="button" data-cy="first-five-button" onClick={loadFiveGoods}>
         Load 5 first goods
       </button>
 
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={loadRedGoods}
-      >
+      <button type="button" data-cy="red-button" onClick={loadRedGoods}>
         Load red goods
       </button>
 
-      <GoodsList goods={allGoods} />
+      {
+        error
+          ? (
+            <p>
+              {error}
+            </p>
+
+          ) : (
+            <GoodsList goods={allGoods} />
+          )
+      }
     </div>
   );
 };
