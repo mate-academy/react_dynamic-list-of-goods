b2d732343c203d31d90f5b31c2425fb31af3355e
Dima Astapenko <bonum_taurus@ukr.net>
2023-08-15 16:09:43 +0300

    improved code

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,30 @@ export const App: React.FC = () => {
   const [filteredGoods, setFilteredGoods] = useState<Good[]>([]);
   const [errorMessage, setErrorMessage] = useState('');
 
+  const handleClickByAllGoods = () => {
+    getAll()
+      .then(setFilteredGoods)
+      .catch(error => setErrorMessage(error.message));
+  };
+
+  const handleClick5Goods = () => {
+    get5First()
+      .then(setFilteredGoods)
+      .catch(error => setErrorMessage(error.message));
+  };
+
+  const handleClickByRedGoods = () => {
+    getRedGoods()
+      .then(setFilteredGoods)
+      .catch(error => setErrorMessage(error.message));
+  };
+
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
       <button
-        onClick={() => getAll()
-          .then(setFilteredGoods)
-          .catch(error => setErrorMessage(error.message))}
+        onClick={handleClickByAllGoods}
         type="button"
         data-cy="all-button"
       >
@@ -26,9 +42,7 @@ export const App: React.FC = () => {
       </button>
 
       <button
-        onClick={() => get5First()
-          .then(setFilteredGoods)
-          .catch(error => setErrorMessage(error.message))}
+        onClick={handleClick5Goods}
         type="button"
         data-cy="first-five-button"
       >
@@ -36,9 +50,7 @@ export const App: React.FC = () => {
       </button>
 
       <button
-        onClick={() => getRedGoods()
-          .then(setFilteredGoods)
-          .catch(error => setErrorMessage(error.message))}
+        onClick={handleClickByRedGoods}
         type="button"
         data-cy="red-button"
       >
