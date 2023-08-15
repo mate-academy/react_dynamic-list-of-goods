be27d0da20796a71717b47392711a43c9009566b
Natalia Tsiutsiura <89_22@ukr.net>
2023-08-15 13:14:36 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { get5First, getAll, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [visibleGoods, getVisibleGoods] = useState<Good[]>([]);
+  const [error, setError] = useState('');
+  const errorMessage = 'Помилка при завантаженні';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const onClick = (callback: () => Promise<Good[]>) => {
+    callback()
+      .then(getVisibleGoods)
+      .catch(() => setError(errorMessage));
+  };
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => onClick(getAll)}
+      >
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => onClick(get5First)}
+      >
+        Load 5 first goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => onClick(getRedGoods)}
+      >
+        Load red goods
+      </button>
+
+      {!error ? (
+        <GoodsList goods={visibleGoods} />
+      ) : (
+        <p style={{ color: 'red' }}>
+          {errorMessage}
+        </p>
+      )}
+    </div>
+  );
+};
