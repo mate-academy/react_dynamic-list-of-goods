35f4b8efc952abef22c7fe47e1457fe9f89ff1b4
Rodionsav <rodionsav@mate.academy>
2023-08-20 17:39:51 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([])
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const fetchAllData = () => {
+    getAll().then(setGoods);
+  }
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const fetchRedData = () => {
+    getRedGoods().then(setGoods);
+  }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const fetchFiveData = () => {
+    get5First().then(setGoods);
+  }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  useEffect(() => {
+    fetchAllData();
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+       type="button"
+       data-cy="all-button"
+       onClick={fetchAllData}
+       >
+        Load all goods
+      </button>
+
+      <button
+       type="button"
+       data-cy="first-five-button"
+       onClick={fetchFiveData}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+       type="button"
+       data-cy="red-button"
+       onClick={fetchRedData}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  )
+};
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,19 +1,30 @@
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+const API_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      return response.json();
+    });
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => {const sortedGoods = goods.sort((a, b) =>
+       a.name.localeCompare(b.name)
+      )
+
+      return sortedGoods.slice(0, 5); // sort and get the first 5
+    });
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods.filter(good => good.color === 'red')); // get only red
 };
