046f6d60f50e903debedfd99e1f6a280c90b8003
Denis Kononenko <deniskononenko88@gmail.com>
2023-08-22 18:25:07 +0300

    add solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,17 +3,31 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export async function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      return response.json();
+    });
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods
+      .sort((good1, good2) => good1.name.localeCompare(good2.name))
+      .slice(0, 5)) // sort and get the first 5
+    .catch(error => {
+      throw error;
+    });
 };
 
-export const getRedGoods = () => {
+export const getRed = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods.filter(good => good.color === 'red')) // get only red
+    .catch(error => {
+      throw error;
+    });
 };
