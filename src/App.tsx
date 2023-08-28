bd87b3fd74d40d4f8630f1c5fae777c7b922879a
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-08-28 09:59:37 +0200

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+enum FUNCS {
+  GET_ALL = 'getAll',
+  GET_FIRST_5 = 'getFirstFive',
+  GET_RED = 'getRed',
+}
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [functionGetData, setFunctionGetData] = useState(FUNCS.GET_ALL);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  useEffect(() => {
+    if (functionGetData === FUNCS.GET_RED) {
+      getRedGoods()
+        .then(goodsFromServer => {
+          setGoods(goodsFromServer);
+        });
+    } else if (functionGetData === FUNCS.GET_FIRST_5) {
+      get5First()
+        .then(goodsFromServer => {
+          setGoods(goodsFromServer);
+        });
+    } else {
+      getAll()
+        .then(goodsFromServer => {
+          setGoods(goodsFromServer);
+        });
+    }
+  }, [functionGetData]);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setFunctionGetData(FUNCS.GET_ALL)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setFunctionGetData(FUNCS.GET_FIRST_5)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setFunctionGetData(FUNCS.GET_RED)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,19 +1,23 @@
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+const API_URL = `https://mate.academy/students-api/goods`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
+export function getAll(url = ''): Promise<Good[]> {
+  return fetch(API_URL + url)
     .then(response => response.json());
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods
+      .sort((good1, good2) => {
+        return good1.name.localeCompare(good2.name);
+      })
+      .slice(0, 5)); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
-  return getAll()
+  return getAll('?color=red')
     .then(goods => goods); // get only red
 };
