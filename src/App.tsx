ead7a30b7e3812dbd8849d86b08e140acac9c0df
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-09-09 17:33:11 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [selectedGoods, setSelectedGoods] = useState<Good[]>([]);
+  const [currentButton, setCurrentButton] = useState('');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    if (!currentButton) {
+      setSelectedGoods([]);
+    } else {
+      
+    }
+  }, [currentButton]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.dataset.cy);
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  // const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   const buttonText = event.currentTarget.textContent;
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  //   switch (buttonText) {
+  //     case 'Load all goods':
+  //       setCurrentButton(getAll());
+  //       break;
+
+  //     case 'Load 5 first goods':
+  //       setCurrentButton(get5First);
+  //       break;
+
+  //     case 'Load red goods':
+  //       setCurrentButton(getRedGoods);
+  //       break;
+
+  //     default:
+  //       setCurrentButton([]);
+  //   }
+  // };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        onClick={handleClick}
+        // onClick={(event) => {
+        //   setCurrentButton(event.currentTarget.textContent || '')
+        // }}
+        type="button"
+        data-cy="all-button"
+      >
+        Load all goods
+      </button>
+
+      <button
+        onClick={handleClick}
+        // onClick={(event) => setCurrentButton(event.currentTarget.textContent || '')}
+        type="button"
+        data-cy="first-five-button"
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        onClick={handleClick}
+        // onClick={(event) => setCurrentButton(event.currentTarget.textContent || '')}
+        type="button"
+        data-cy="red-button"
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={selectedGoods} />
+    </div>
+  );
+};
