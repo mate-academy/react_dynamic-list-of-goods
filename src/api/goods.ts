7c86a630b3d432b59807ac85b20178c8c3badcac
Yevhenii Voronin <voronine@ukr.net>
2023-09-13 18:25:42 +0300

    resolve sort abc and first 5

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -12,7 +12,7 @@ export function getAll(): Promise<Good[]> {
 export const get5First = () => {
   return getAll()
     .then(goods => goods
-      .slice(0, 5).sort((a, b) => a.name.localeCompare(b.name))); // sort and get the first 5
+      .sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5)); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
