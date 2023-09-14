36ad2ccb8e8e91e14431f1f6491a0b779f4ed830
Ivanna Romanovych <ivankaromanovych12@gmail.com>
2023-09-14 09:57:10 +0300

    fixed

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,23 +1,20 @@
-import { Good } from '../types/Good';
+import React from 'react';
+import { Good } from './types/Good';
 
-// eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
-
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
-}
-
-export const get5First = () => {
-  return getAll()
-    .then(goods => (
-      goods
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .slice(0, 5)
-    ));
+type Props = {
+  goods: Good[]
 };
 
-export const getRedGoods = () => {
-  return getAll()
-    .then(goods => goods.filter(good => good.color === 'red'));
-};
+export const GoodsList: React.FC<Props> = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li
+        key={good.id}
+        data-cy="good"
+        style={{ color: good.color }}
+      >
+        {good.name}
+      </li>
+    ))}
+  </ul>
+);
