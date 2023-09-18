0be25c18f253008e64602f657d5f4363f07312d6
Capoverde <piotr_as@yahoo.com>
2023-09-18 15:05:52 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,82 @@
-import React from 'react';
-import './App.scss';
+import React, { useEffect, useState } from 'react';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import { getAll, get5First, getRed } from './api/goods';
+import './App.scss';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [goodsData, setGoodsData] = useState<{
+    all: Good[] | null;
+    first5: Good[] | null;
+    red: Good[] | null;
+  }>({
+    all: null,
+    first5: null,
+    red: null,
+  });
+  const [error, setError] = useState<null | string>(null);
+  const [showGoods, setShowGoods] = useState<Good[] | null>(null);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  useEffect(() => {
+    const loadGoods = async () => {
+      setIsLoading(true);
+      setError(null);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+      try {
+        const [usersAll, first5users, getRedUsers] = await Promise.all([
+          getAll(),
+          get5First(),
+          getRed(),
+        ]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+        setGoodsData({
+          all: usersAll,
+          first5: first5users,
+          red: getRedUsers,
+        });
+        setShowGoods(usersAll);
+        // eslint-disable-next-line
+      } catch (e: any) {
+        setError(e.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    loadGoods();
+  }, []);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setShowGoods(goodsData.all)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setShowGoods(goodsData.first5)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setShowGoods(goodsData.red)}
+      >
+        Load red goods
+      </button>
+      {isLoading && <p>Loading</p>}
+      {error && <p>{error}</p>}
+      <GoodsList goods={showGoods} />
+    </div>
+  );
+};
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,19 +1,64 @@
+// import { Good } from '../types/Good';
+// // eslint-disable-next-line
+// const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+
+// export async function getAll(): Promise<Good[]> {
+//   const response = await fetch(API_URL);
+
+//   return response.json();
+// }
+
+// export const get5First = () => {
+//   return getAll()
+//     .then(goods => goods
+//       .sort((a, b) => a.name.localeCompare(b.name))
+//       .filter((_, index) => index >= 0 && index < 5));
+// };
+
+// export const getRed = () => {
+//   return getAll()
+//     .then(goods => goods.filter(good => good.color === 'red'));
+// };
+
 import { Good } from '../types/Good';
 
-// eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+const API_URL
+= 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => response.json());
+export async function getAll(): Promise<Good[]> {
+  try {
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch goods data');
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error('Error fetching data');
+  }
 }
 
-export const get5First = () => {
-  return getAll()
-    .then(goods => goods); // sort and get the first 5
+export const get5First = async (): Promise<Good[]> => {
+  try {
+    const goods = await getAll();
+
+    return goods
+      .slice(0, 5)
+      .sort((a, b) => a.name.localeCompare(b.name));
+  } catch (error) {
+    throw new Error('Error getting first 5 goods');
+  }
 };
 
-export const getRedGoods = () => {
-  return getAll()
-    .then(goods => goods); // get only red
+export const getRed = async (): Promise<Good[]> => {
+  try {
+    const goods = await getAll();
+
+    return goods.filter(good => good.color === 'red');
+  } catch (error) {
+    throw new Error('Error getting red goods');
+  }
 };
