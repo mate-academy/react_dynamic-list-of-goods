e752011120e2de55adb5f5fc1dd59b489cf0d806
Nataliia Henda <nataliehenda1296@gmail.com>
2023-10-04 12:47:21 +0300

    Solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -7,10 +7,20 @@ type Props = {
 
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
-    {goods.map(good => (
-      <li key={good.id} data-cy="good">
-        {good.name}
-      </li>
-    ))}
+    {goods.map(good => {
+      const liStyle = {
+        color: good.color,
+      };
+
+      return (
+        <li
+          key={good.id}
+          data-cy="good"
+          style={liStyle}
+        >
+          {good.name}
+        </li>
+      );
+    })}
   </ul>
 );
