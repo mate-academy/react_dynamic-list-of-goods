eb9cbb59bfe71cc981f521abeb1833e504774e6e
Leonid Krainik <rialleons@gmail.com>
2023-10-09 13:10:59 +0300

    Implement dynamic list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { get5First, getAll, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
+import { LoadParameter } from './types/LoadParameter';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const handleClick = (loadParameter: LoadParameter) => {
+    let promise: Promise<Good[]>;
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+    switch (loadParameter) {
+      case LoadParameter.All:
+        promise = getAll();
+        break;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      case LoadParameter.FiveFirst:
+        promise = get5First();
+        break;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case LoadParameter.Red:
+        promise = getRedGoods();
+        break;
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      default:
+        throw new Error('Wrong load parameter!');
+    }
+
+    promise.then(setVisibleGoods);
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handleClick(LoadParameter.All)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handleClick(LoadParameter.FiveFirst)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handleClick(LoadParameter.Red)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={visibleGoods} />
+    </div>
+  );
+};
---
+++ src/types/LoadParameter.ts
@@ -0,0 +1,5 @@
+export enum LoadParameter {
+  All,
+  FiveFirst,
+  Red,
+}
