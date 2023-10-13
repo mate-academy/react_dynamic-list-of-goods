13fd367fae004eb9e321cf4ae36fb840a01a7bf7
Ilona Romaniuk <ilona09041983@gmail.com>
2023-10-13 12:26:01 +0300

    Changed typescriptversion

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,9 +10,8 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(
-      goods => goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
-    );
+    .then(goods => goods.sort((a, b) => a.name.localeCompare(b.name))
+      .slice(0, 5));
 };
 
 export const getRedGoods = () => {
