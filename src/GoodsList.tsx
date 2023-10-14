1a482e1d9d7b229409e666fb70c93db2dc0435ec
donizer <bogdan.ua45@gmail.com>
2023-10-14 17:15:14 +0300

    fixes

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { Good } from './types/Good';
 
 type Props = {
-  goods: Good[]
+  goods: Good[];
 };
 
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
-    {goods.map(good => {
-      const style: React.CSSProperties = {
-        color: good.color,
-      };
-
+    {goods.map((good) => {
       return (
-        <li key={good.id} data-cy="good" style={style}>
+        <li
+          key={good.id}
+          data-cy="good"
+          style={{
+            color: good.color,
+          }}
+        >
           {good.name}
         </li>
       );
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-React.createElement('div');
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
