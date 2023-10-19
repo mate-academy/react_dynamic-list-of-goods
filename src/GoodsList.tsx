36818774e2cdbb5edcf1f3b290f35be1700451cd
Yuliia Dub <yuliia.kasaraba@gmail.com>
2023-10-19 09:21:53 +0300

    add colours

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -8,7 +8,11 @@ export const GoodsList: React.FC<Props> = ({ goods }) => {
   return (
     <ul>
       {goods.map(good => (
-        <li key={good.id} data-cy="good">
+        <li
+          key={good.id}
+          data-cy="good"
+          style={{ color: good.color }}
+        >
           {good.name}
         </li>
       ))}
