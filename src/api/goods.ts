1ff32b25b2399b098a7f5c15ef1eaf14c2ee2ee8
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2023-10-20 12:29:31 +0300

    add fix

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,7 +10,8 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods.sort((a, b) => a.name.localeCompare(b.name))
+    .then(goods => goods.sort((good1, good2) => good1.name
+      .localeCompare(good2.name))
       .slice(0, 5)); // sort and get the first 5
 };
 
