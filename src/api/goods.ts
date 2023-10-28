cafcc5bf7b11f3c989b670747072f2f004131378
Oleh Obiukh <oleg.obiukh@gmail.com>
2023-10-28 20:54:21 +0100

    Goods solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -8,7 +8,8 @@ export function getAll(): Promise<Good[]> {
 }
 
 export const get5First = () => {
-  return getAll().then((goods) => goods.slice(0, 5));
+  return getAll().then((goods) => goods.slice(0, 5)
+    .sort((a, b) => a.name.localeCompare(b.name)));
 };
 
 export const getRed = () => {
