fe298892c009874922679db8051c7284ac464dec
Helen Tsyhypalo <lena.tsigipalo@gmail.com>
2023-10-30 17:11:13 +0100

    add solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,10 +10,15 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => {
+      const sorted = [...goods]
+        .sort((good1, good2) => good1.name.localeCompare(good2.name));
+
+      return sorted.slice(0, 5);
+    });
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods.filter(good => good.color === 'red'));
 };
