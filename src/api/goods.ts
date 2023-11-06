c6b16991906c7acd5b494c11ab0b045168fb80f1
Artem Trofymovych <navrotskymetra@outlook.com>
2023-11-06 14:31:40 +0100

    develop

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,7 +10,9 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods.slice(0, 5)); // sort and get the first 5
+    .then(goods => goods
+      .sort((goodA, goodB) => goodA.name.localeCompare(goodB.name))
+      .slice(0, 5));
 };
 
 export const getRedGoods = () => {
