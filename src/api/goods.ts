4b0ed80670cb9107687e5e6f961ef34a50498d17
Hanna Anishyna <ann.986@gmail.com>
2023-11-06 20:15:14 -0800

    foxes

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,7 +10,8 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods.sort((good1, good2) => good1.name.localeCompare(good2.name))
+    .then(goods => goods.sort((good1, good2) => (
+      good1.name.localeCompare(good2.name)))
       .slice(0, 5));
 };
 
