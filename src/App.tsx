6a078e4ff686b1d9a76b4fe6ba20ce2502c24a2b
Hanna Anishyna <ann.986@gmail.com>
2023-11-06 16:34:39 -0800

    foxes

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { getAll, get5First, getRedGoods } from './api/goods';
-import {Good} from "./types/Good";
+import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
-    const handleOnClickAll = () => {
-      getAll().then(setGoods);
-    };
+  const handleOnClickAll = () => {
+    getAll().then(setGoods);
+  };
 
-    const handleOnClick5Goods = () => {
-      get5First().then(setGoods);
-    };
+  const handleOnClick5Goods = () => {
+    get5First().then(setGoods);
+  };
 
-    const handleOnClickRedGoods = () => {
-      getRedGoods().then(setGoods);
-    };
+  const handleOnClickRedGoods = () => {
+    getRedGoods().then(setGoods);
+  };
 
   return (
     <div className="App">
@@ -47,7 +47,7 @@ export const App: React.FC = () => {
         Load red goods
       </button>
 
-      <GoodsList goods={goods}/>
+      <GoodsList goods={goods} />
     </div>
-  )
+  );
 };
