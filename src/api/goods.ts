fb44e8eedba437e5bc731e67536b08b943f73712
OlegRak86 <rakolegit@gmail.com>
2023-11-12 19:43:42 +0200

    react_dynamic-list-of-goods

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -5,15 +5,17 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => response.json())
+    .then(good => good);
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(good => good.sort((a:Good, b:Good) => a.name.localeCompare(b.name))
+      .slice(0, 5)); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(good => good.filter(item => item.color === 'red')); // get only red
 };
