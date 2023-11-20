aab7463e8668784d81a7db87ad47ec796344474a
Bahtijar <no4kar@gmail.com>
2023-11-20 18:25:50 +0100

    stage-1

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -8,7 +8,13 @@ type Props = {
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li
+        key={good.id}
+        className={`${good.color === 'red' ? 'is-ripe ' : ''}`
+          + `${good.color === 'green' ? 'is-raw ' : ''}`
+          + `${good.color === 'blue' ? 'is-cool ' : ''}`}
+        data-cy="good"
+      >
         {good.name}
       </li>
     ))}
