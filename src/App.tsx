3580b6f6f0cfae9197dad46593742fce4670e268
Mykhailo <eglet777@ukr.net>
2023-11-21 12:19:11 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { get5First, getAll, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [option, setOption] = useState('all');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    let promise;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    switch (option) {
+      case 'all':
+        promise = getAll();
+        break;
+      case 'first5':
+        promise = get5First();
+        break;
+      case 'red':
+        promise = getRedGoods();
+        break;
+      default:
+        promise = getAll();
+        break;
+    }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    promise
+      .then(allGoods => setGoods(allGoods));
+  }, [option]);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => {
+          setOption('all');
+        }}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => {
+          setOption('first5');
+        }}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => {
+          setOption('red');
+        }}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
