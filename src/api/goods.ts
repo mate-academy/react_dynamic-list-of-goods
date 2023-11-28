686a3a157adbfa41a0ede829f8a935e4c016f5a5
4xmplme <andreykav2002@gmail.com>
2023-11-28 20:33:47 +0200

    add task solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,17 +3,19 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export async function getAll(): Promise<Good[]> {
   return fetch(API_URL)
     .then(response => response.json());
 }
 
-export const get5First = () => {
+export async function getFirst5() {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
-};
+    .then(goods => goods
+      .sort((goodA, goodB) => goodA.name.localeCompare(goodB.name))
+      .slice(0, 5));
+}
 
-export const getRedGoods = () => {
+export async function getRed() {
   return getAll()
-    .then(goods => goods); // get only red
-};
+    .then(goods => goods.filter(good => good.color === 'red')); // get only red
+}
