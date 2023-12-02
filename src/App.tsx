c6b0faee0aea2bc773f471bcc32fcae84423a2a4
Oleksandr <dedikad@gmail.com>
2023-12-02 10:33:00 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [user, setUser] = useState<Good[]>([]);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const handleAllUsers = async () => {
+    const goodsArray = await goodsAPI.getAll();
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+    setUser(goodsArray);
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleFiveUsers = async () => {
+    const goodsArray = await goodsAPI.get5First();
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    setUser(goodsArray);
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  const handleRedUsers = async () => {
+    const goodsArray = await goodsAPI.getRedGoods();
+
+    setUser(goodsArray);
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={handleAllUsers}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleFiveUsers}
+
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={handleRedUsers}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={user} />
+    </div>
+  );
+};
