d5dc50974f43261f4996eea7e30ccfe7aef5d009
Yevhen Kolosov <kolosov214a@gmail.com>
2023-12-11 16:49:38 +0200

    a small change

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,4 +1,4 @@
-import { Good } from '../types/Good';
+import { Good, GoodColor } from '../types/Good';
 
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
@@ -21,5 +21,5 @@ export const get5First = () => {
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods.filter(good => good.color === 'red'));
+    .then(goods => goods.filter(good => good.color === GoodColor.Red));
 };
