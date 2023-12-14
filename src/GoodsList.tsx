408347f799eda8906b815f2f729b38b1654aa30a
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-12-14 18:14:54 +0200

    'done'

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -8,10 +8,10 @@ type Props = {
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li 
+      <li
         key={good.id}
         data-cy="good"
-        style={{color: good.color}}
+        style={{ color: good.color }}
       >
         {good.name}
       </li>
