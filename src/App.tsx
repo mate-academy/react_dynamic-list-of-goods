ddec1769004c5df1a2555fef35a4bdc96e8fa9cc
Roma <roma150269@gmail.com>
2023-12-16 17:39:03 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const allGoods = () => {
+    getAll().then(setGoods);
+  }
+
+  const firstGoods = () => {
+    get5First().then((good) => setGoods(good.sort((a, b) =>
+      a.name.localeCompare(b.name)).splice(0,5))
+    )
+  }
+
+  const findColor = () => {
+    getRedGoods().then(setGoods);
+  }
+
+  return (
   <div className="App">
     <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button">
+    <button
+      type="button"
+      data-cy="all-button"
+      onClick={allGoods}
+    >
       Load all goods
     </button>
 
-    <button type="button" data-cy="first-five-button">
+    <button
+      type="button"
+      data-cy="first-five-button"
+      onClick={firstGoods}
+    >
       Load 5 first goods
     </button>
 
-    <button type="button" data-cy="red-button">
+    <button
+      type="button"
+      data-cy="red-button"
+      onClick={findColor}
+    >
       Load red goods
     </button>
 
-    <GoodsList goods={[]} />
+    <GoodsList goods={goods} />
   </div>
-);
+)};
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -5,7 +5,8 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => response.json()
+    );
 }
 
 export const get5First = () => {
@@ -15,5 +16,5 @@ export const get5First = () => {
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods.filter(good => good.color === 'red')); // get only red
 };
