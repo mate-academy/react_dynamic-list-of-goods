4d2c1817e722099c34802e5dc3c084924d169318
Marcin Szymanski <mszymanski1709@outlook.com>
2023-12-19 01:07:01 +0100

    fixed get5first function

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,7 +10,8 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods.sort((a, b) => a.name.localeCompare(b.name)));
+    .then(goods => goods.sort((a, b) => a.name.localeCompare(b.name))
+      .slice(0, 5));
 };
 
 export const getRed = () => {
