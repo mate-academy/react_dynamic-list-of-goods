b92307ddd53ffc411b1f598949f89f7ffcfcc4c0
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2024-01-15 16:27:50 +0200

    done

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -5,15 +5,25 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
-    .then(response => response.json());
+    .then(response => response.json())
+    .then(goods => goods);
 }
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => {
+      const sortedGoods = goods
+        .sort((good1, good2) => good1.name.localeCompare(good2.name));
+
+      return sortedGoods.slice(0, 5);
+    }); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => {
+      const redGoods = goods.filter(g => g.color === 'red');
+
+      return redGoods;
+    }); // get only red
 };
