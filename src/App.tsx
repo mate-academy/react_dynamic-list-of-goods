24b532711f8fd573997abcb613de0fc60164d152
Mykhailo Loniak <loniakmykhail@gmail.com>
2024-01-17 17:48:43 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import {
+  getAll,
+  get5First,
+  getRedGoods,
+} from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goodsArr, setGoodsArr] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handliClick = (click: string): void => {
+    switch (click) {
+      case 'first-five':
+        get5First().then((data: Good[]) => setGoodsArr(data));
+        break;
+      case 'red':
+        getRedGoods().then((data: Good[]) => setGoodsArr(data));
+        break;
+      default:
+        getAll().then((data: Good[]) => setGoodsArr(data));
+        break;
+    }
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handliClick('all')}
+      >
+        Load all goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handliClick('first-five')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handliClick('red')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goodsArr} />
+    </div>
+  );
+};
