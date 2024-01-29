35a2bce538e94d45a0162466002343f70eb37bee
Vasilliy Yarosh <vasilliy.yarosh@gmail.com>
2024-01-29 19:42:28 +0200

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    getAll()
+      .then((data) => setGoods(data));
+  }, []);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  useEffect(() => {
+    get5First()
+      .then((data) => setGoods(data));
+  }, []);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  useEffect(() => {
+    getRedGoods()
+      .then((data) => setGoods(data));
+  }, []);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={async () => {
+          const data = await getAll();
+
+          setGoods(data);
+        }}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={async () => {
+          const data = await get5First();
+
+          setGoods(data);
+        }}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={async () => {
+          const data = await getRedGoods();
+
+          setGoods(data);
+        }}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// import './App.scss';
+// import cn from 'classnames';
 import { Good } from './types/Good';
 
 type Props = {
@@ -8,7 +10,13 @@ type Props = {
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li
+        key={good.id}
+        data-cy="good"
+        style={
+          { color: good.color }
+        }
+      >
         {good.name}
       </li>
     ))}
