a3e82621a2b00209314dd8d20860e011a6418dae
Yaroslav Samoilenko <yasik2255@live.com>
2024-01-29 13:10:29 +0200

    add solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,10 +10,17 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => {
+      const goodsCopy = [...goods];
+
+      goodsCopy.sort((g1, g2) => g1.name.localeCompare(g2.name));
+      goodsCopy.length = 5;
+
+      return goodsCopy;
+    }); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods.filter(g => g.color === 'red')); // get only red
 };
