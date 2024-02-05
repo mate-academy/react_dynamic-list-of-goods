1cd3ddda7f96fac1b53a64eeb96972744afdccf4
Bohdan Liutyi <liutyibohdan13@gmail.com>
2024-02-05 19:45:35 +0200

    goods

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -10,10 +10,12 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll()
-    .then(goods => goods); // sort and get the first 5
+    .then(goods => goods
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .slice(0, 5)); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
   return getAll()
-    .then(goods => goods); // get only red
+    .then(goods => goods.filter(good => good.color === 'red')); // get only red
 };
