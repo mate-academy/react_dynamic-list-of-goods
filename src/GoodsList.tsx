d9dfc465a078e1015113eddf24577db9a6b8155f
Olena Shapkun <shapkunkh@gmail.com>
2024-02-08 18:24:38 +0200

    solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -5,12 +5,31 @@ type Props = {
   goods: Good[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
-    {goods.map(good => (
-      <li key={good.id} data-cy="good">
-        {good.name}
-      </li>
-    ))}
-  </ul>
-);
+export const GoodsList: React.FC<Props> = ({ goods }) => {
+  const getColorClass = (color: string) => {
+    switch (color) {
+      case 'red':
+        return 'color-red';
+      case 'green':
+        return 'color-green';
+      case 'blue':
+        return 'color-blue';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <ul>
+      {goods.map(good => (
+        <li
+          key={good.id}
+          data-cy="good"
+          className={getColorClass(good.color)}
+        >
+          {good.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
