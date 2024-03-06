ffe2067c4dc50eaa9043a8ab2fe2015aa0b8c02c
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-03-07 01:52:54 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [shownGoods, setShownGoods] = useState<Good[]>([]);
+  let receivedGoods = [];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const handleFull = async () => {
+    receivedGoods = await goodsAPI.getAll();
+    setShownGoods(receivedGoods);
+  };
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleFive = async () => {
+    receivedGoods = await goodsAPI.get5First();
+    setShownGoods(receivedGoods);
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleRed = async () => {
+    receivedGoods = await goodsAPI.getRedGoods();
+    setShownGoods(receivedGoods);
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  useEffect(() => {
+    handleFull();
+  }, []);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" data-cy="all-button" onClick={handleFull}>
+        Load all goods
+      </button>
+
+      <button type="button" data-cy="first-five-button" onClick={handleFive}>
+        Load 5 first goods
+      </button>
+
+      <button type="button" data-cy="red-button" onClick={handleRed}>
+        Load red goods
+      </button>
+
+      <GoodsList goods={shownGoods} />
+    </div>
+  );
+};
