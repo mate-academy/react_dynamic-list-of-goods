2737bed2141b1f38bdbcd47f67150cefb7124845
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-03-27 17:40:34 +0200

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
 
 import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
