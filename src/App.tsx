b78ae3fda3c0e6c1841756eeaf5cac12831a3eb9
Davwidos <d.jurkowski015@gmail.com>
2024-04-08 11:41:52 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods as getRed } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+type GoodsFilter = 'all' | 'five' | 'red';
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[] | null>(null);
+  const [goodsFilter, setGoodsFilter] = useState<GoodsFilter>('all');
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  useEffect(() => {
+    let p: Promise<Good[]>;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    switch (goodsFilter) {
+      case 'five':
+        p = get5First();
+        break;
+      case 'red':
+        p = getRed();
+        break;
+      case 'all':
+      default:
+        p = getAll();
+    }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    p.then(setGoods);
+  }, [goodsFilter]);
+
+  const handleGoodsFilterChange = (filter: GoodsFilter) => () =>
+    setGoodsFilter(filter);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        onClick={handleGoodsFilterChange('all')}
+        type="button"
+        data-cy="all-button"
+      >
+        Load all goods
+      </button>
+
+      <button
+        onClick={handleGoodsFilterChange('five')}
+        type="button"
+        data-cy="first-five-button"
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        onClick={handleGoodsFilterChange('red')}
+        type="button"
+        data-cy="red-button"
+      >
+        Load red goods
+      </button>
+
+      {goods && <GoodsList goods={goods} />}
+    </div>
+  );
+};
