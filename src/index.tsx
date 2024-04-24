86fab5f86ad54cc8ebd1d8ff51883fc2a0a2c908
kubsen <jzagrobelny24@gmail.com>
2024-04-24 17:06:20 +0200

    npm test v2

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import React from 'react';
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
