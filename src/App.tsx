19e747dcc6a4d8cc3648586649b786c0c1d77ee2
kubsen <jzagrobelny24@gmail.com>
2024-04-24 16:50:27 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
-
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[] | null>(null);
+  const handleAll: React.MouseEventHandler<HTMLButtonElement> = () =>
+    getAll().then(setGoods);
+  const handleFirstFive: React.MouseEventHandler<HTMLButtonElement> = () =>
+    get5First().then(setGoods);
+  const handleReds: React.MouseEventHandler<HTMLButtonElement> = () =>
+    getRed().then(setGoods);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      <button type="button" data-cy="all-button" onClick={handleAll}>
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleFirstFive}
+      >
+        Load 5 first goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button type="button" data-cy="red-button" onClick={handleReds}>
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
