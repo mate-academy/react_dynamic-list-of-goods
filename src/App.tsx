97ec7ff7068afd5a371e6a09832af9ec51e5d39a
Viacheslav Khavalko <slawik.maggot@gmail.com>
2024-05-15 11:23:58 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,42 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
 
 import { getAll, get5First, getRedGoods } from './api/goods';
+// or
+// import * as goodsAPI from './api/goods';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
+
+  const onClickAll = useCallback(() => {
+    getAll().then(res => {
+      setGoods(res);
+    });
+  }, []);
+
+  const onClickFive = useCallback(() => {
+    get5First(setGoods);
+  }, []);
+
+  const onClickRed = useCallback(() => {
+    getRedGoods(setGoods);
+  }, []);
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={() => getAll().then(setGoods)}
-      >
+      <button type="button" data-cy="all-button" onClick={onClickAll}>
         Load all goods
       </button>
 
-      <button
-        type="button"
-        data-cy="first-five-button"
-        onClick={() => get5First().then(setGoods)}
-      >
+      <button onClick={onClickFive} type="button" data-cy="first-five-button">
         Load 5 first goods
       </button>
 
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={() => getRedGoods().then(setGoods)}
-      >
+      <button onClick={onClickRed} type="button" data-cy="red-button">
         Load red goods
       </button>
 
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,18 +1,27 @@
+import { Dispatch } from 'react';
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL)
+    .then(response => response.json())
+    .catch();
 }
 
-export const get5First = () => {
+export const get5First = (setGoods: Dispatch<Good[]>) => {
   return getAll().then(goods =>
-    goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
+    setGoods(
+      goods
+        .sort((a: Good, b: Good) => a.name.localeCompare(b.name))
+        .slice(0, 5),
+    ),
   );
 };
 
-export const getRedGoods = () => {
-  return getAll().then(goods => goods.filter(good => good.color === 'red')); // get only red
+export const getRedGoods = (setGoods: Dispatch<Good[]>) => {
+  return getAll().then(goods =>
+    setGoods(goods.filter(good => good.color === 'red')),
+  );
 };
