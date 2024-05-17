0d61e8ee011de06181ee6dc19b35eeb0abad9d44
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-05-17 15:26:08 +0200

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
+import { Selector } from './types/Selector';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [selector, setSelector] = useState<Selector>('all');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    const fetchGoods = () => {
+      let fetchFunction: () => Promise<Good[]>;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      switch (selector) {
+        case 'all':
+          fetchFunction = getAll;
+          break;
+        case 'first 5':
+          fetchFunction = get5First;
+          break;
+        case 'red only':
+          fetchFunction = getRedGoods;
+          break;
+        default:
+          fetchFunction = getAll;
+      }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      fetchFunction().then(data => setGoods(data));
+    };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    fetchGoods();
+  }, [selector]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setSelector('all')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setSelector('first 5')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setSelector('red only')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
