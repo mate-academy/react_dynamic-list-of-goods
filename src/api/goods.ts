45bce6e18606bc66d1c0138c92473d62cb4b2b3a
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-05-23 10:31:35 +0200

    task solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,16 +1,27 @@
+import { Dispatch } from 'react';
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL)
+    .then(response => response.json())
+    .catch();
 }
 
-export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+export const get5First = (setGoods: Dispatch<Good[]>) => {
+  return getAll().then(goods =>
+    setGoods(
+      goods
+        .sort((a: Good, b: Good) => a.name.localeCompare(b.name))
+        .slice(0, 5),
+    ),
+  );
 };
 
-export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+export const getRedGoods = (setGoods: Dispatch<Good[]>) => {
+  return getAll().then(goods =>
+    setGoods(goods.filter(good => good.color === 'red')),
+  );
 };
