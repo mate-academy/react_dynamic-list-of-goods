eb3e20466c1fce9e63afc8d03d34c8fc32306a6e
Katya <evil.666.buny@gmail.com>
2024-06-11 12:01:44 +0300

    -

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,7 +4,13 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+  });
 }
 
 export const get5First = () => {
