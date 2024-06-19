b7b322b2aee71af3112ca539267b37710d965bb8
Makhmud <bmm0065@gmail.com>
2024-06-19 21:22:05 +0300

    add task solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,3 +1,4 @@
+
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
@@ -8,9 +9,11 @@ export function getAll(): Promise<Good[]> {
 }
 
 export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+  return getAll().then(goods =>
+    goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
+  );
 };
 
-export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+export const getRed = () => {
+  return getAll().then(goods => goods.filter(good => good.color === 'red'));
 };
