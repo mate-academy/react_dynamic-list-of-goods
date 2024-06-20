7c872dc49add43a0f57b856279aa1d6e1bdcd0a1
Andrii-Mysheniuk <amysheniuk@webfx.co.uk>
2024-06-20 15:24:05 +0100

    add missing spaces and semicolons

--- src/App.tsx
+++ src/App.tsx
@@ -7,40 +7,43 @@ import { Good } from './types/Good';
 // import * as goodsAPI from './api/goods';
 
 export const App: React.FC = () => {
-  const [goods, setGoods] = useState<Good[]>([])
+  const [goods, setGoods] = useState<Good[]>([]);
 
   const showAllGoods = async () => {
     const allGoods = await getAll();
+
     setGoods(allGoods);
   }
 
   const showFiveGoods = async () => {
     const fiveGoods = await get5First();
+
     setGoods(fiveGoods);
   }
 
   const showRedGoods = async () => {
     const redGoods = await getRedGoods();
+
     setGoods(redGoods);
   }
 
   return (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button" onClick={showAllGoods}>
-      Load all goods
-    </button>
+      <button type="button" data-cy="all-button" onClick={showAllGoods}>
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="first-five-button" onClick={showFiveGoods}>
-      Load 5 first goods
-    </button>
+      <button type="button" data-cy="first-five-button" onClick={showFiveGoods}>
+        Load 5 first goods
+      </button>
 
-    <button type="button" data-cy="red-button" onClick={showRedGoods}>
-      Load red goods
-    </button>
+      <button type="button" data-cy="red-button" onClick={showRedGoods}>
+        Load red goods
+      </button>
 
-    <GoodsList goods={goods} />
-  </div>
-);
+      <GoodsList goods={goods} />
+    </div>
+  );
 }
