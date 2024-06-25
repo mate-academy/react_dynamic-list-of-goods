8ffb4dfbd294d86e5ae3f51fd2750445666b3abf
Hanna Svirina <26anna07@ukr.net>
2024-06-25 22:13:39 +0200

    solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -5,12 +5,17 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL).then(response => response.json());
+  // console.log(Response);
 }
 
 export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
-};
+  return getAll().then(goods =>
+    goods
+      .sort((good1, good2) => good1.name.localeCompare(good2.name))
+      .slice(0, 5),
+  );
+}; // sort and get the first 5
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+  return getAll().then(goods => goods.filter(good => good.color === 'red')); // get only red
 };
