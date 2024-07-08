999f60298b1d7b57cebb5748c4a66b977defd6a6
DSB-18 <boboshko.dmitriy@gmail.com>
2024-07-08 08:58:21 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+enum TypesOfGoods {
+  All = 'all',
+  Five = 'five',
+  Red = 'red',
+}
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goodsFromServer, setGoodsFromServer] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  function handleShowlist(type: string) {
+    switch (type) {
+      case TypesOfGoods.All:
+        getAll().then(setGoodsFromServer);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+        return;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case TypesOfGoods.Five:
+        get5First().then(setGoodsFromServer);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+        return;
+
+      case TypesOfGoods.Red:
+        getRedGoods().then(setGoodsFromServer);
+
+        return;
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handleShowlist(TypesOfGoods.All)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handleShowlist(TypesOfGoods.Five)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handleShowlist(TypesOfGoods.Red)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goodsFromServer} />
+    </div>
+  );
+};
