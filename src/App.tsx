d84ad61e9bd6fb45d9bfcd4da38e35125819936f
Hryniuk_Taras <gritaras484@gmail.com>
2024-07-23 16:30:06 +0300

    react dynamic list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [sortedGoods, setSortedGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleButtonClick = (sortType: string) => {
+    if (sortType === 'getAll') {
+      getAll().then(goods => setSortedGoods(goods));
+    }
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    if (sortType === 'get5First') {
+      get5First().then(goods => setSortedGoods(goods));
+    }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    if (sortType === 'getRedGoods') {
+      getRedGoods().then(goods => setSortedGoods(goods));
+    }
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handleButtonClick('getAll')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handleButtonClick('get5First')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handleButtonClick('getRedGoods')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={sortedGoods} />
+    </div>
+  );
+};
