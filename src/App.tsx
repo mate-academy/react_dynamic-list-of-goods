138cba1ac5db104d2678652b264259b5af5277c6
ZakZh <38009936+ZakZh@users.noreply.github.com>
2024-08-02 14:44:51 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [loading, setLoading] = useState(false);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleGetGoodsButtonClick = async (
+    e: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    e.preventDefault();
+    setLoading(true);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    const buttonId = e.currentTarget.id;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    try {
+      switch (buttonId) {
+        case 'all-goods':
+          const allGoods = await getAll();
 
-    <GoodsList goods={[]} />
-  </div>
-);
+          setGoods(allGoods);
+          break;
+        case 'first-five':
+          const firstFiveGoods = await get5First();
+
+          setGoods(firstFiveGoods);
+          break;
+        case 'red-only':
+          const redGoods = await getRedGoods();
+
+          setGoods(redGoods);
+          break;
+        default:
+          break;
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Failed to load goods:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        id="all-goods"
+        type="button"
+        data-cy="all-button"
+        onClick={handleGetGoodsButtonClick}
+      >
+        Load all goods
+      </button>
+
+      <button
+        id="first-five"
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleGetGoodsButtonClick}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        id="red-only"
+        type="button"
+        data-cy="red-button"
+        onClick={handleGetGoodsButtonClick}
+      >
+        Load red goods
+      </button>
+
+      {loading ? <div>Loading...</div> : <GoodsList goods={goods} />}
+    </div>
+  );
+};
