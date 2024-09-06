a6b44a7e407ed08af89983856e90b4341f73f37f
Artem Vasylenko <irbiskronos@gmail.com>
2024-09-06 17:02:22 +0300

    sort fix

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -8,7 +8,9 @@ export function getAll(): Promise<Good[]> {
 }
 
 export const get5First = () => {
-  return getAll().then(goods => goods.slice(0, 5)); // sort and get the first 5
+  return getAll().then(goods =>
+    goods.sort((a: Good, b: Good) => a.name.localeCompare(b.name)).slice(0, 5),
+  ); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
