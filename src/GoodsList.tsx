6bf043a0072f61d485e2bb750c95a5d45b3b1914
Seva Podolskiy <s_podolskiy@live.com>
2024-09-16 08:54:21 -0400

    solutioin

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li key={good.id} data-cy="good" style={{ color: good.color }}>
         {good.name}
       </li>
     ))}
