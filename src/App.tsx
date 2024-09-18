7dd7aaf0cf0c9e384bf35b377aeed5925e3aa55a
Oleksandr Sushko <alexandrsushko25@gmail.com>
2024-09-18 19:07:09 +0200

    commit message

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
-import { getAll, get5First, getRedGoods } from '../public/api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
 export const App: React.FC = () => {
   const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
---
+++ src/api/goods.ts
@@ -0,0 +1,24 @@
+import { Good } from '../types/Good';
+
+// eslint-disable-next-line
+const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+
+export function getAll(): Promise<Good[]> {
+  return fetch(API_URL).then(response => response.json());
+}
+
+export const get5First = () => {
+  return getAll().then(goods => {
+    const sortedGoods = goods.sort((a, b) => a.name.localeCompare(b.name));
+
+    return sortedGoods.slice(0, 5);
+  });
+};
+
+export const getRedGoods = () => {
+  return getAll().then(goods => {
+    const filteredRedGoods = goods.filter(good => good.color === 'red');
+
+    return filteredRedGoods;
+  });
+};
