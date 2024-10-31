61dd0c92dcb21c71ac8933785d84e475d26da930
Serhii Bizin <seregabizin@icloud.com>
2024-10-31 10:15:08 +0200

    add task solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+mport React from 'react';
 import { Good } from './types/Good';
 
 type Props = {
@@ -8,7 +8,7 @@ type Props = {
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li key={good.id} data-cy="good" style={{ color: good.color }}> {/* Добавляем цвет для текста */}
         {good.name}
       </li>
     ))}
