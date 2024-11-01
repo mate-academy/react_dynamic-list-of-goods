49efe09c7552536e7b96fb51b23b4fe90c3586f9
Victor Komara <vitioklss@gmail.com>
2024-11-01 18:16:28 +0200

    add task solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -7,10 +7,20 @@ export function getAll(): Promise<Good[]> {
   return fetch(API_URL).then(response => response.json());
 }
 
-export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+export function getAllGoods(): Promise<Good[]> {
+  return getAll().then(response => response);
+}
+
+export const get5FirstGoods = () => {
+  return getAll().then(goods =>
+    goods
+      .sort((good1, good2) => {
+        return good1.name.localeCompare(good2.name);
+      })
+      .slice(0, 5),
+  ); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+  return getAll().then(goods => goods.filter(good => good.color === 'red')); // get only red
 };
