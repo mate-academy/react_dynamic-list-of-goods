19d77b02d65eb759fd0fe30d34cb1cc555c663b7
Kateryna <moskkat40@gmail.com>
2024-11-20 14:48:27 +0100

    add react dynamic list of goods task

---
+++ src/utils/httpClients.ts
@@ -0,0 +1,11 @@
+import { Good } from '../types/Good';
+
+const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+
+export function getAll(): Promise<Good[]> {
+  return fetch(API_URL).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+  });
+}
