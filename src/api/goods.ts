fad754b92f5397f899744996800e6e3d7c96c0c5
Dmytro Moshkivskyi <dmosh@ukr.net>
2024-12-11 13:29:45 +0200

    react_dynamic-list-of-goods by Dimoshua

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -5,12 +5,14 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL).then(response => response.json());
-}
+} // get all goods
 
-export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+export const get5First = (): Promise<Good[]> => {
+  return getAll().then(goods =>
+    goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
+  ); // sort and get 5 first goods
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+  return getAll().then(goods => goods.filter(good => good.color === 'red')); // get red goods
 };
