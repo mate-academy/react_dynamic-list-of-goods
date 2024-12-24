5f35dee82d2ee5daf8256c865609c1cfe101bfc2
Yevhen Rudnytskyi <yevhen.doit@gmail.com>
2024-12-24 09:34:31 +0200

    solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,13 +4,36 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch goods');
+      }
+
+      return response.json();
+    })
+    .catch(error => {
+      // console.error(error);
+      throw error;
+    });
 }
 
-export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+export const get5First = (): Promise<Good[]> => {
+  return getAll()
+    .then(goods =>
+      goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
+    )
+    .catch(error => {
+      // console.error('Failed to get the first 5 goods:', error);
+      throw error;
+    });
 };
 
-export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+export const getRedGoods = (): Promise<Good[]> => {
+  return getAll()
+    .then(goods => goods.filter(good => good.color === 'red'))
+    .catch(error => {
+      // console.error('Failed to get red goods:', error);
+      throw error;
+    });
 };
