596f8f37291e535f051263413d00ea31cf8b6eaf
Andrii Tkachuk <kovklad@gmail.com>
2024-12-25 21:45:33 +0100

    Solution the task

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -12,7 +12,7 @@ export const get5First = () => {
     goods
       .sort((good1, good2) => good1.name.localeCompare(good2.name))
       .slice(0, 5),
-  ); // sort and get the first 5
+  );
 };
 
 export const getRedGoods = () => {
