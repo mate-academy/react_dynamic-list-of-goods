d521a208d69eb27e0586d122c8c0a1a24674685f
dima <shymdima74@gmail.com>
2025-01-06 09:48:20 +0200

    add task solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -8,9 +8,13 @@ export function getAll(): Promise<Good[]> {
 }
 
 export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+  return getAll().then(goods => goods.sort((goodFirst, goodSecond) => {
+    return goodFirst.name.localeCompare(goodSecond.name);
+  })
+  .slice(0, 5)
+);
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+  return getAll().then(goods => goods.filter(good => good.color === 'red'));
 };
