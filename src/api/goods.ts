e05682d7cee9f4b28aae68314b492b95f32917b5
Yurii Mosin <m.yuriy@icloud.com>
2025-01-21 18:31:16 +0100

    done

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,14 +3,18 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
+export function getAll<T>(): Promise<T> {
   return fetch(API_URL).then(response => response.json());
 }
 
 export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+  return getAll<Good[]>().then(goods =>
+    goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
+  );
 };
 
-export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+export const getRed = () => {
+  return getAll<Good[]>().then(goods =>
+    goods.filter(good => good.color === 'red'),
+  );
 };
