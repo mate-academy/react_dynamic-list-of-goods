46e1b228cdf489245f9fe69bc5d015f66d3842a9
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-01-27 11:59:54 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  // useEffect(() => {
+  //   getAll().then(data => setGoods(data));
+  // }, []);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  // const loafingGetAll = () => {
+  //   getAll().then(data => setGoods(data));
+  // };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  // const loadingGet5First = () => {
+  //   get5First().then(data => setGoods(data));
+  // };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  const loadGoods = (fetchFunction: () => Promise<Good[]>) => {
+    fetchFunction().then(data => setGoods(data));
+  };
+
+  useEffect(() => {
+    loadGoods(getAll);
+    loadGoods(get5First);
+    loadGoods(getRedGoods);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => loadGoods(getAll)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => loadGoods(get5First)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => loadGoods(getRedGoods)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
