b1a8734e7e2ef417515704a9f07f0e4ddc562952
artur.manuilenko <artur.manuilenko@yalantis.net>
2025-02-09 12:16:14 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
 // import { getAll, get5First, getRed } from './api/goods';
 // or
-// import * as goodsAPI from './api/goods';
+import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [fetchTrigger, setFetchTrigger] = useState('');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    switch (fetchTrigger) {
+      case 'all':
+        goodsAPI.getAll().then(setGoods);
+        break;
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      case '5first':
+        goodsAPI.get5First().then(setGoods);
+        break;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case 'red':
+        goodsAPI.getRedGoods().then(setGoods);
+        break;
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      default:
+        // eslint-disable-next-line no-console
+        console.warn('unknown fetch trigger');
+        break;
+    }
+  }, [fetchTrigger]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setFetchTrigger('all')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setFetchTrigger('5first')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setFetchTrigger('red')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
