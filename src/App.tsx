ab8c30a3b28c05019584cdbe6f8e9bee1d0c1ecd
Anna Tuhusova <bilachayka@mail.com>
2025-02-20 23:18:50 +0100

    some commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { getAll, get5First, getRedGoods } from './api/goods';
 import { Good } from './types/Good';
 
 export const App: React.FC = () => {
-
   const [goods, setGoods] = useState<Good[]>([]);
 
   const handleLoadAll = () => {
@@ -20,27 +19,27 @@ export const App: React.FC = () => {
     getRedGoods().then(newGoods => setGoods(newGoods));
   };
 
-
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button type="button" data-cy="all-button"
-      onClick={handleLoadAll}>
+      <button type="button" data-cy="all-button" onClick={handleLoadAll}>
         Load all goods
       </button>
 
-      <button type="button" data-cy="first-five-button"
-      onClick={handleLoad5First}>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleLoad5First}
+      >
         Load 5 first goods
       </button>
 
-      <button type="button" data-cy="red-button"
-      onClick={handleLoadRedGoods}>
+      <button type="button" data-cy="red-button" onClick={handleLoadRedGoods}>
         Load red goods
       </button>
 
-      {goods.length > 0 && <GoodsList goods={goods}/>}
+      {goods.length > 0 && <GoodsList goods={goods} />}
     </div>
-  )
+  );
 };
