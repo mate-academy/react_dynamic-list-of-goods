14b2467683e4b94bdde968bc77f6be6b3a656974
Anna <ania.somko@gmail.com>
2025-02-20 12:17:03 +0200

    bugs fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,47 @@ import { getAll, get5First, getRedGoods } from './api/goods';
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
+  const handleGetAll = () => {
+    getAll()
+      .then(setGoods)
+      .catch(error => {
+        return error;
+      });
+  };
+
+  const handleGet5First = () => {
+    get5First()
+      .then(setGoods)
+      .catch(error => {
+        return error;
+      });
+  };
+
+  const handleGetRedGoods = () => {
+    getRedGoods()
+      .then(setGoods)
+      .catch(error => {
+        return error;
+      });
+  };
+
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={() => getAll().then(setGoods)}
-      >
+      <button type="button" data-cy="all-button" onClick={handleGetAll}>
         Load all goods
       </button>
 
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={() => get5First().then(setGoods)}
+        onClick={handleGet5First}
       >
         Load 5 first goods
       </button>
 
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={() => getRedGoods().then(setGoods)}
-      >
+      <button type="button" data-cy="red-button" onClick={handleGetRedGoods}>
         Load red goods
       </button>
 
