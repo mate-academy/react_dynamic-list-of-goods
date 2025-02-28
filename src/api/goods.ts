3bd6bc13f4610f104edea21e7934dbdd3dfca7c5
Olena7202 <olenahorodn7202@gmail.com>
2025-02-28 12:44:38 +0200

    develop correct

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,17 +1,18 @@
 import { Good } from '../types/Good';
 
+// eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL).then(response => response.json());
 }
 
-export const get5First = (): Promise<Good[]> => {
+export const get5First = () => {
   return getAll().then(goods =>
     goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
   );
 };
 
-export const getRedGoods = (): Promise<Good[]> => {
-  return getAll().then(goods => goods.filter(good => good.color === 'red'));
+export const getRedGoods = () => {
+  return getAll().then(goods => goods.filter(good => good.color === 'red')); // get only red
 };
