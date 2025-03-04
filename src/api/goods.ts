9e3eb3b69ec5fa70886668874e01018d19c6e731
Oleksii Petrychenko <apetr@MacBook-Air.local>
2025-03-04 14:47:53 +0200

    add task solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,16 +1,27 @@
 import { Good } from '../types/Good';
 
-// eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+export const getAll = async (): Promise<Good[]> => {
+  const response = await fetch(
+    'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json',
+  );
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
-}
+  if (!response.ok) {
+    throw new Error('Failed to fetch goods');
+  }
 
-export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+  return response.json();
 };
 
-export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+export const get5First = async (): Promise<Good[]> => {
+  const goods = await getAll();
+
+  return goods
+    .sort((a: Good, b: Good) => a.name.localeCompare(b.name))
+    .slice(0, 5);
+};
+
+export const getRed = async (): Promise<Good[]> => {
+  const goods = await getAll();
+
+  return goods.filter((good: Good) => good.color === 'red');
 };
