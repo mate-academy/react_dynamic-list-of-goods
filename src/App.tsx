c9457e0a72ce2d979a3b1e251694a3de8e513f38
Valentyn <valentyn93beltsyk@gmail.com>
2025-03-06 14:59:13 +0200

    fix_solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import './App.scss';
 import { GoodsList } from './GoodsList';
 import { getAll, get5First, getRedGoods } from './api/goods';
 import { Good } from './types/Good';
-
-// import { getAll, get5First, getRed } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
 
