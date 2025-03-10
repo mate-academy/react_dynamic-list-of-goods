d6a4d79f13e51cbcc88be9acf95891ee041e23b3
Nadiia <zoryvchaknv@gmail.com>
2025-03-10 17:18:37 +0200

    solution for http

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+type LoadGoods = 'all' | 'five' | 'red';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[] | []>([]);
+  const [visible, setVisible] = useState(false);
+  const [load, setLoad] = useState<LoadGoods | null>(null);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    if (load === 'all') {
+      goodsAPI.getAll().then(setGoods);
+    }
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    if (load === 'five') {
+      goodsAPI.get5First().then(setGoods);
+    }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    if (load === 'red') {
+      goodsAPI.getRedGoods().then(setGoods);
+    }
+  }, [load]);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => {
+          setVisible(true);
+          setLoad('all');
+        }}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => {
+          setVisible(true);
+          setLoad('five');
+        }}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => {
+          setVisible(true);
+          setLoad('red');
+        }}
+      >
+        Load red goods
+      </button>
+
+      {visible && <GoodsList goods={goods} />}
+    </div>
+  );
+};
