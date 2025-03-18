a0d092c690c14c9838721b347d38b399adbf2988
Vlad Popenko <krokodil94dynamo@gmail.com>
2025-03-18 17:32:48 +0200

    fix solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -5,7 +5,8 @@ interface Props {
   goods: Good[];
 }
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
+// eslint-disable-next-line react/display-name
+export const GoodsList: React.FC<Props> = React.memo(({ goods }) => (
   <ul>
     {goods.map(good => (
       <li key={good.id} data-cy="good" style={{ color: good.color }}>
@@ -13,4 +14,4 @@ export const GoodsList: React.FC<Props> = ({ goods }) => (
       </li>
     ))}
   </ul>
-);
+));
