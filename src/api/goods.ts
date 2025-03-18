4748f7dddfeb58849d4dac8614af85ea1f431042
Vlad Popenko <krokodil94dynamo@gmail.com>
2025-03-18 18:00:53 +0200

    add fix

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,19 +4,35 @@ const BASE_URL =
   'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
 
 export const getAllGoods = async (): Promise<Good[]> => {
-  const response = await fetch(BASE_URL);
+  try {
+    const response = await fetch(BASE_URL);
 
-  return response.json();
+    if (!response.ok) {
+      throw new Error(`Failed to fetch goods: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    throw new Error(`Error fetching goods: ${error}`);
+  }
 };
 
 export const getFiveFirstGoods = async (): Promise<Good[]> => {
-  const goods = await getAllGoods();
+  try {
+    const goods = await getAllGoods();
 
-  return goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
+    return goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
+  } catch (error) {
+    throw new Error(`Error fetching first five goods: ${error}`);
+  }
 };
 
 export const getRedGoods = async (): Promise<Good[]> => {
-  const goods = await getAllGoods();
+  try {
+    const goods = await getAllGoods();
 
-  return goods.filter(good => good.color === 'red');
+    return goods.filter(good => good.color === 'red');
+  } catch (error) {
+    throw new Error(`Error fetching red goods: ${error}`);
+  }
 };
