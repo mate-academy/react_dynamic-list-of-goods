041d23c3958ac432d73171c9b7961eaecda18520
Volodymyr Makhonin <zavozi@gmail.com>
2025-03-20 16:33:43 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+const goodsConditions = {
+  all: getAll,
+  first5: get5First,
+  allRed: getRedGoods,
+};
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+type GoodCondition = 'all' | 'first5' | 'allRed';
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[] | []>([]);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const filterGoods = useCallback((condition: GoodCondition) => {
+    goodsConditions[condition]().then(setGoods);
+  }, []);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => filterGoods('all')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => filterGoods('first5')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => filterGoods('allRed')}
+      >
+        Load red goods
+      </button>
+
+      {goods.length !== 0 && <GoodsList goods={goods} />}
+    </div>
+  );
+};
