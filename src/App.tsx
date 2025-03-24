78ca3b98404c7f99dcc25f8aa49eec79416665d8
Daria Radtkina <dasharadtkina@gmail.com>
2025-03-24 17:03:59 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
+  const [error, setError] = useState('');
 
   return (
     <div className="App">
@@ -15,7 +16,11 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="all-button"
-        onClick={() => getAll().then(setGoods)}
+        onClick={() => {
+          getAll()
+            .then(setGoods)
+            .catch(() => setError('Error'));
+        }}
       >
         Load all goods
       </button>
@@ -23,7 +28,11 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={() => get5First().then(setGoods)}
+        onClick={() => {
+          get5First()
+            .then(setGoods)
+            .catch(() => setError('Error'));
+        }}
       >
         Load 5 first goods
       </button>
@@ -31,12 +40,15 @@ export const App: React.FC = () => {
       <button
         type="button"
         data-cy="red-button"
-        onClick={() => getRedGoods().then(setGoods)}
+        onClick={() => {
+          getRedGoods()
+            .then(setGoods)
+            .catch(() => setError('Error'));
+        }}
       >
         Load red goods
       </button>
-
-      <GoodsList goods={goods} />
+      {!error && <GoodsList goods={goods} />}
     </div>
   );
 };
