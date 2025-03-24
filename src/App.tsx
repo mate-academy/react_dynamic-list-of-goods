eaf18f85275753fc9809210660821d5c7e2f88dc
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-03-24 16:55:19 +0200

    fix task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
@@ -9,49 +9,41 @@ export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
+  const handleLoadAll = useCallback(() => {
+    getAll()
+      .then(setGoods)
+      .catch(error => setErrorMessage(error));
+  }, []);
+
+  const handleLoadFirstFive = useCallback(() => {
+    get5First()
+      .then(setGoods)
+      .catch(error => setErrorMessage(error));
+  }, []);
+
+  const handleLoadRed = useCallback(() => {
+    getRedGoods()
+      .then(setGoods)
+      .catch(error => setErrorMessage(error));
+  }, []);
+
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={() =>
-          getAll()
-            .then(setGoods)
-            .catch(error => {
-              setErrorMessage(error);
-            })
-        }
-      >
+      <button type="button" data-cy="all-button" onClick={handleLoadAll}>
         Load all goods
       </button>
 
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={() =>
-          get5First()
-            .then(setGoods)
-            .catch(error => {
-              setErrorMessage(error);
-            })
-        }
+        onClick={handleLoadFirstFive}
       >
         Load 5 first goods
       </button>
 
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={() =>
-          getRedGoods()
-            .then(setGoods)
-            .catch(error => {
-              setErrorMessage(error);
-            })
-        }
-      >
+      <button type="button" data-cy="red-button" onClick={handleLoadRed}>
         Load red goods
       </button>
 
