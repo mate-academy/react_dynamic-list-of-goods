37d7867d6e027beca46af9d41c24b032893f91fb
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-03-25 15:50:08 +0200

    task solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,11 +1,11 @@
-import React, { memo } from 'react';
+import React from 'react';
 import { Good } from './types/Good';
 
 type Props = {
   goods: Good[];
 };
 
-export const GoodsList: React.FC<Props> = memo(({ goods }) => (
+export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
       <li key={good.id} data-cy="good" style={{ color: good.color }}>
@@ -13,6 +13,4 @@ export const GoodsList: React.FC<Props> = memo(({ goods }) => (
       </li>
     ))}
   </ul>
-));
-
-GoodsList.displayName = 'GoodsList';
+);
