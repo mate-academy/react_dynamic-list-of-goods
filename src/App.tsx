69aa848b0b1beb95e293e1bb3c73290aba58df1a
Lirik <melnicukkiril2002@gmail.com>
2025-03-25 23:41:38 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
@@ -9,36 +9,48 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleGetAll = useCallback(() => {
+    getAll()
+      .then(setGoods)
+      .catch(error => setErrorMessage(error.message));
+  }, []);
+
+  const handleGet5First = useCallback(() => {
+    get5First()
+      .then(setGoods)
+      .catch(error => setErrorMessage(error.message));
+  }, []);
+
+  const handleGetRedGoods = useCallback(() => {
+    getRedGoods()
+      .then(setGoods)
+      .catch(error => setErrorMessage(error.message));
+  }, []);
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={() => getAll().then(setGoods)}
-      >
+      <button type="button" data-cy="all-button" onClick={handleGetAll}>
         Load all goods
       </button>
 
       <button
         type="button"
         data-cy="first-five-button"
-        onClick={() => get5First().then(setGoods)}
+        onClick={handleGet5First}
       >
         Load 5 first goods
       </button>
 
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={() => getRedGoods().then(setGoods)}
-      >
+      <button type="button" data-cy="red-button" onClick={handleGetRedGoods}>
         Load red goods
       </button>
 
       <GoodsList goods={goods} />
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
     </div>
   );
 };
