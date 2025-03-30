1dc5882ae84fdf28522d99d56a372917adccf21d
lublubuterbrodi <lublubuterbrodi@gmail.com>
2025-03-30 14:21:54 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  // Explicitly typing the state as Good[] to fix the type issue
+  const [goods, setGoods] = useState<Good[]>([]);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const loadAllGoods = () => {
+    getAll().then(setGoods);
+  };
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const loadFirst5Goods = () => {
+    get5First().then(setGoods);
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const loadRedGoods = () => {
+    getRedGoods().then(setGoods);
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
-
-    <GoodsList goods={[]} />
-  </div>
-);
+  return React.createElement(
+    'div',
+    { className: 'App' },
+    React.createElement('h1', null, 'Dynamic list of Goods'),
+    React.createElement(
+      'button',
+      { type: 'button', 'data-cy': 'all-button', onClick: loadAllGoods },
+      'Load all goods',
+    ),
+    React.createElement(
+      'button',
+      {
+        type: 'button',
+        'data-cy': 'first-five-button',
+        onClick: loadFirst5Goods,
+      },
+      'Load 5 first goods',
+    ),
+    React.createElement(
+      'button',
+      { type: 'button', 'data-cy': 'red-button', onClick: loadRedGoods },
+      'Load red goods',
+    ),
+    React.createElement(GoodsList, { goods }), // Passing the goods state to the GoodsList component
+  );
+};
