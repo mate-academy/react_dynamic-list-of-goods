af24736d883d4ea06386b7312efb72642d4a6ed7
Nadiia Bulmak <nadgaval@gmail.com>
2025-04-08 17:56:14 +0300

    add react_dynamic-list-of-goods solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,20 +1,33 @@
+// import { error } from 'console';
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL)
+    .then(response => response.json())
+    .catch(error => {
+      throw new Error(error.message);
+    });
 }
 
 export const get5First = () => {
-  return getAll().then(goods =>
-    goods
-      .sort((good1, good2) => good1.name.localeCompare(good2.name))
-      .slice(0, 5),
-  ); // sort and get the first 5
+  return getAll()
+    .then(goods =>
+      goods
+        .sort((good1, good2) => good1.name.localeCompare(good2.name))
+        .slice(0, 5),
+    )
+    .catch(error => {
+      throw new Error(error.message);
+    });
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods.filter(good => good.color === 'red')); // get only red
+  return getAll()
+    .then(goods => goods.filter(good => good.color === 'red'))
+    .catch(error => {
+      throw new Error(error.message);
+    });
 };
