3c8c80a6f67eba15fc05479fb9a26634ccf6c87e
Anton Polishchuk <hardixxxxx@gmail.com>
2025-04-10 11:58:39 +0200

    Add buttons to load and filter goods using GoodsList

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+enum GoodQuery {
+  All = 'all',
+  First5 = 'first5',
+  Red = 'red',
+}
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [query, setQuery] = useState('');
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  useEffect(() => {
+    switch (query) {
+      case GoodQuery.All:
+        getAll().then(goodsFromServer => setGoods(goodsFromServer));
+        break;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case GoodQuery.First5:
+        get5First().then(goodsFromServer => setGoods(goodsFromServer));
+        break;
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      case GoodQuery.Red:
+        getRedGoods().then(goodsFromServer => setGoods(goodsFromServer));
+        break;
+    }
+  }, [query]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        onClick={() => setQuery(GoodQuery.All)}
+        type="button"
+        data-cy="all-button"
+      >
+        Load all goods
+      </button>
+
+      <button
+        onClick={() => setQuery(GoodQuery.First5)}
+        type="button"
+        data-cy="first-five-button"
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        onClick={() => setQuery(GoodQuery.Red)}
+        type="button"
+        data-cy="red-button"
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
