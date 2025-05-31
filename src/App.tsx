1a4580a60bb5e658dc4f9b03b576353733ed9a6e
Liudmyla Hnydyn <gnydynluda@gmail.com>
2025-05-31 00:03:57 -0400

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -27,27 +27,15 @@ export const App: React.FC = () => {
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        type="button"
-        data-cy="all-button"
-        onClick={handleAllGoods}
-      >
+      <button type="button" data-cy="all-button" onClick={handleAllGoods}>
         Load all goods
       </button>
 
-      <button
-        type="button"
-        data-cy="first-five-button"
-        onClick={handle5First}
-      >
+      <button type="button" data-cy="first-five-button" onClick={handle5First}>
         Load 5 first goods
       </button>
 
-      <button
-        type="button"
-        data-cy="red-button"
-        onClick={handleRedGoods}
-      >
+      <button type="button" data-cy="red-button" onClick={handleRedGoods}>
         Load red goods
       </button>
 
