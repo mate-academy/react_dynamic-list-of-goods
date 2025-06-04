6b057ac29b228d3c88f890a25f8323c630e73f6d
Nadiia Bryk <25nadin47@gmail.com>
2025-06-04 20:23:10 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [filteredGoods, setFilteredGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const hadnleLoadAll = () => {
+    getAll()
+      .then(goods => {
+        setFilteredGoods(goods);
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load all goods:', error);
+      });
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const hadnleLoadFive = () => {
+    get5First()
+      .then(goods => {
+        setFilteredGoods(goods);
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load first 5 goods:', error);
+      });
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const hadnleLoadRed = () => {
+    getRedGoods()
+      .then(goods => {
+        setFilteredGoods(goods);
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load first red goods:', error);
+      });
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" data-cy="all-button" onClick={hadnleLoadAll}>
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={hadnleLoadFive}
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" data-cy="red-button" onClick={hadnleLoadRed}>
+        Load red goods
+      </button>
+
+      <GoodsList goods={filteredGoods} />
+    </div>
+  );
+};
