d539d9d5d30d4c7b9a92c6c16111ae0f4a844ea0
Andrew Pavlenko <andrew.pawllenko@gmail.com>
2025-06-05 11:02:39 +0300

    did the task react_dynamic-list-of-goods

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -8,9 +8,14 @@ export function getAll(): Promise<Good[]> {
 }
 
 export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+  return getAll().then(goods => {
+    return goods
+      .slice()
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .slice(0, 5);
+  });
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+  return getAll().then(goods => goods.filter(good => good.color === 'red'));
 };
