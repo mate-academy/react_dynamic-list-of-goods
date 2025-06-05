7bcf5ade2071ef93914f543e6d1844667e1bd3a7
lidiia-tsymborovych <lidacimbirovic@gmail.com>
2025-06-05 12:20:22 +0300

    make getSortedGoods immutable

--- src/utils/getSortedGoods.ts
+++ src/utils/getSortedGoods.ts
@@ -1,5 +1,7 @@
 import { Good } from '../types/Good';
 
 export function getSortedGoods(goods: Good[]): Good[] {
-  return goods.sort((good1, good2) => good1.name.localeCompare(good2.name));
+  return [...goods].sort((good1, good2) =>
+    good1.name.localeCompare(good2.name),
+  );
 }
