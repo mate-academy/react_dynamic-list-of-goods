086d3fdfc3f9379a002c36e8189038d33c24657f
Denis Pitsul <denia.pitsul@gmail.com>
2025-06-05 14:38:05 +0300

    add react dynamic list of goods solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -7,10 +7,12 @@ export function getAll(): Promise<Good[]> {
   return fetch(API_URL).then(response => response.json());
 }
 
-export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+export const get5First = (): Promise<Good[]> => {
+  return getAll().then(goods =>
+    [...goods].sort((g1, g2) => g1.name.localeCompare(g2.name)).slice(0, 5),
+  );
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+  return getAll().then(goods => goods.filter(good => good.color === 'red'));
 };
