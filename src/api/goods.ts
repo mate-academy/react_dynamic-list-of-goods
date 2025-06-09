5c03d34b8dde50cdb30da295e86f0aeb7d5177ad
Veronika Okhten <veronika.oxt12@gmail.com>
2025-06-10 02:06:48 +0300

    add task solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,13 +4,23 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL).then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return response.json();
+  });
 }
 
 export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+  return getAll().then(goods => {
+    const sorted = [...goods].sort((a, b) => a.name.localeCompare(b.name));
+
+    return sorted.slice(0, 5);
+  });
 };
 
-export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+export const getRedGoods = (color: string = 'red') => {
+  return getAll().then(goods => goods.filter(good => good.color === color));
 };
