ec912a9dfe01163cd897f7053810fd8dfdc948da
Anori <anorifrell@gmail.com>
2025-06-10 22:14:30 +0300

    fix

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,9 +3,20 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
-}
+export const getAll = async (): Promise<Good[]> => {
+  // return fetch(API_URL).then(response => response.json());
+  try {
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch');
+    }
+
+    return await response.json();
+  } catch (error) {
+    return [];
+  }
+};
 
 export const get5First = () => {
   return getAll().then(goods =>
